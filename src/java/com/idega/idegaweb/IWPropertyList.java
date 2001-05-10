c5f7a53c018f0f003d3a25bda1e01e11f23cfa45
Pall Helgason <palli@idega.com>
2001-05-10 10:45:28 +0000

    no message

--- src/java/com/idega/idegaweb/IWPropertyList.java
+++ src/java/com/idega/idegaweb/IWPropertyList.java
@@ -1,10 +1,13 @@
-//idega 2001 - Tryggvi Larusson
 /*
-*Copyright 2001 idega.is All Rights Reserved.
-*/
-
+ * $Id: IWPropertyList.java,v 1.2 2001/05/10 10:45:28 palli Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
 package com.idega.idegaweb;
-
 
 import java.util.List;
 import java.util.Iterator;
@@ -21,14 +24,10 @@ import org.jdom.input.SAXBuilder;
 import org.jdom.output.XMLOutputter;
 
 /**
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-*@version 0.8 - Under development
-*/
-public class IWPropertyList{
-
-
-
-
+ *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ *@version 0.8 - Under development
+ */
+public class IWPropertyList {
   private Document xmlDocument;
   private File xmlFile;
   private Element rootElement;
@@ -39,128 +38,126 @@ public class IWPropertyList{
   private static String typeString = "type";
   private static String stringString = "java.lang.String";
 
-  public IWPropertyList(){
+  public IWPropertyList() {
   }
 
-   public IWPropertyList(String path){
-     load(path);
-   }
-
-   public void setProperty(String key, Object value){
-      setProperty(key,value.toString(),value.getClass().getName());
-   }
-
-   public void setProperty(String key, String value){
-      setProperty(key,value,stringString);
-   }
-
-   public void setProperty(String key, int value){
-      setProperty(key, new Integer(value));
-   }
-
-   public void setProperty(String key, String value,String type){
-      Element property = findPropertyElement(key);
-      if(property==null){
-        addProperty(key,value,type);
-      }
-      else{
-        property.removeChild(valueString);
-        property.removeChild(typeString);
-        setProperty(property,value,type);
-      }
-   }
-
-   public void addProperty(String key, String value){
-      addProperty(key,value,stringString);
-   }
-
-   public void addProperty(String key, Object value){
-      addProperty(key,value.toString(),value.getClass().getName());
-   }
-
-  public void addProperty(String key, int value){
-      addProperty(key,new Integer(value));
+  public IWPropertyList(String path) {
+    load(path);
   }
 
-   public void addProperty(String key, String value,String type){
-      Element property = new Element(propertyString);
-      Element keyElement = new Element(keyString);
-      keyElement.addContent(key);
-      property.addContent(keyElement);
+  public void setProperty(String key, Object value) {
+    setProperty(key,value.toString(),value.getClass().getName());
+  }
+
+  public void setProperty(String key, String value) {
+    setProperty(key,value,stringString);
+  }
+
+  public void setProperty(String key, int value) {
+    setProperty(key, new Integer(value));
+  }
+
+  public void setProperty(String key, String value,String type) {
+    Element property = findPropertyElement(key);
+    if (property == null) {
+      addProperty(key,value,type);
+    }
+    else {
+      property.removeChild(valueString);
+      property.removeChild(typeString);
       setProperty(property,value,type);
-      rootElement.addContent(property);
-   }
+    }
+  }
 
-   private void setProperty(Element property, String value,String type){
-      Element valueElement = new Element(valueString);
-      valueElement.addContent(value);
-      Element typeElement = new Element(typeString);
-      typeElement.addContent(type);
-      property.addContent(valueElement);
-      property.addContent(typeElement);
-   }
+  public void addProperty(String key, String value) {
+    addProperty(key,value,stringString);
+  }
 
-   public String getPropertyType(String key){
-      return findPropertyElement(key).getChild(typeString).getText();
-   }
+  public void addProperty(String key, Object value) {
+    addProperty(key,value.toString(),value.getClass().getName());
+  }
 
-   public String getProperty(String key){
-      try{
-        return findPropertyElement(key).getChild(valueString).getText();
+  public void addProperty(String key, int value) {
+    addProperty(key,new Integer(value));
+  }
+
+  public void addProperty(String key, String value,String type) {
+    Element property = new Element(propertyString);
+    Element keyElement = new Element(keyString);
+    keyElement.addContent(key);
+    property.addContent(keyElement);
+    setProperty(property,value,type);
+    rootElement.addContent(property);
+  }
+
+  private void setProperty(Element property, String value,String type) {
+    Element valueElement = new Element(valueString);
+    valueElement.addContent(value);
+    Element typeElement = new Element(typeString);
+    typeElement.addContent(type);
+    property.addContent(valueElement);
+    property.addContent(typeElement);
+  }
+
+  public String getPropertyType(String key) {
+    return findPropertyElement(key).getChild(typeString).getText();
+  }
+
+  public String getProperty(String key) {
+    try {
+      return findPropertyElement(key).getChild(valueString).getText();
+    }
+    catch(NullPointerException ex) {
+      return null;
+    }
+  }
+
+  /**
+   * @return null if no match
+   */
+  private Element findPropertyElement(String key) {
+    List list = rootElement.getChildren();
+    Iterator iter = list.iterator();
+
+    while(iter.hasNext()) {
+      Element property = (Element)iter.next();
+      Element keyElement = property.getChild(keyString);
+      if (keyElement.getText().equalsIgnoreCase(key)) {
+        return property;
       }
-      catch(NullPointerException ex){
-        return null;
-      }
-   }
+    }
 
-   /**
-    * Returns null if no match
-    */
-   private Element findPropertyElement(String key){
-        List list = rootElement.getChildren();
-        Iterator iter = list.iterator();
+    return null;
+  }
 
-        while(iter.hasNext()){
-          Element property = (Element)iter.next();
-          Element keyElement = property.getChild(keyString);
-          if(keyElement.getText().equalsIgnoreCase(key)){
-              return property;
-          }
-        }
-        return null;
-   }
-
-   public void load(String path){
-      SAXBuilder builder = new SAXBuilder(false);
-      xmlFile = new File(path);
-      try{
+  public void load(String path) {
+    SAXBuilder builder = new SAXBuilder(false);
+    xmlFile = new File(path);
+    try {
       xmlDocument = builder.build(xmlFile);
-      }
-      catch(JDOMException e){
-        e.printStackTrace();
-      }
-      rootElement = xmlDocument.getRootElement();
-   }
+    }
+    catch(JDOMException e) {
+      e.printStackTrace();
+    }
+    rootElement = xmlDocument.getRootElement();
+  }
 
-    public void store(){
-      try{
+  public void store() {
+    try {
       store(new FileOutputStream(xmlFile));
-      }
-      catch(FileNotFoundException e){
-        e.printStackTrace();
-      }
     }
-
-    public void store(OutputStream stream){
-
-      try{
-        XMLOutputter outputter = new XMLOutputter();
-        outputter.output(xmlDocument,stream);
-      }
-      catch(IOException e){
-        e.printStackTrace();
-      }
-
+    catch(FileNotFoundException e) {
+      e.printStackTrace();
     }
+  }
 
+  public void store(OutputStream stream) {
+    try {
+      XMLOutputter outputter = new XMLOutputter();
+      outputter.output(xmlDocument,stream);
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
