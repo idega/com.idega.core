1365f9665358bd5ab1404e9c14bd8e33d7ddfc7e
Pall Helgason <palli@idega.com>
2001-07-24 16:06:53 +0000

    no message

--- src/java/com/idega/util/CypherText.java
+++ src/java/com/idega/util/CypherText.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CypherText.java,v 1.1 2001/07/13 09:31:59 palli Exp $
+ * $Id: CypherText.java,v 1.2 2001/07/24 16:06:53 palli Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -9,46 +9,166 @@
  */
 package com.idega.util;
 
-import java.util.Vector;
-import java.util.Collections;
-
 /**
  *
  * @author <a href="mailto:palli@idega.is">Pall Helgason</a>
  * @version 1.0
  */
 public class CypherText {
-  private StringBuffer cypherKey = null;
+  private final String alphabet_ = "nk2cmqYgidw9spTuyDXFZt4HA8Ma3xEr75QClBV0IUPo6LezWNbOv1SjRfJGKh";
+  private String cypherKey_ = null;
 
   public CypherText() {
   }
 
-  public static void main(String[] args) {
-    CypherText cypherText1 = new CypherText();
-    cypherText1.doCypher("Palli var einn í heiminum");
+  /**
+   *
+   */
+  public String doCyper(String text, String cypherKey) throws NullPointerException {
+    setKey(cypherKey);
+
+    return(cypher(text));
   }
 
-  public void doCypher(String text) {
-    if (cypherKey == null)
-      createKey();
-    System.out.println("Before : " + text);
-    System.out.println("CypherKey = " + cypherKey);
+  /**
+   *
+   */
+  public String doDeCypher(String text, String cypherKey) throws NullPointerException {
+    setKey(cypherKey);
 
+    return(deCypher(text));
   }
 
-  private void createKey() {
-    cypherKey = new StringBuffer();
-    for (int i = 0; i < 4000; i++) {
-      char rnd = (char)(Math.random() * 256);
-      cypherKey.append(rnd);
+  /**
+   * Returns the current cypher key for this object. If the cypher key is null
+   * a new cypher key is created and returned.
+   *
+   * @param
+   *
+   * @return
+   */
+  public String getKey(int length) {
+    if (cypherKey_ == null)
+      createKey(length);
+
+    if (cypherKey_.length() != length)
+      createKey(length);
+
+    return(cypherKey_);
+  }
+
+  /**
+   *
+   */
+  public void setKey(String key) throws NullPointerException {
+    if (key == null)
+      throw(new NullPointerException("Cypher key is null"));
+
+    cypherKey_ = key;
+  }
+
+  /*
+   *
+   */
+  private void createKey(int length) {
+    StringBuffer key = new StringBuffer();
+    for (int i = 0; i < length; i++) {
+      char rnd = (char)(Math.random() * alphabet_.length());
+      key.append(alphabet_.charAt(rnd));
     }
+
+    cypherKey_ = key.toString();
   }
 
-  private String cypher(String text) {
-    return(null);
+  /*
+   *
+   */
+  private String cypher(String text) throws NullPointerException, IllegalArgumentException {
+    if (text == null)
+      throw(new NullPointerException("No text to cypher"));
+
+    boolean isAlphabet = checkAlphabet(text);
+    if (!isAlphabet)
+      throw(new IllegalArgumentException("Text contains characters which are not cypherable. Please use only [a..z][A..Z][0..9]"));
+
+    StringBuffer cyphered = new StringBuffer();
+
+    while (cypherKey_.length() < text.length())
+      cypherKey_ = cypherKey_.concat(cypherKey_);
+
+    for (int i = 0; i < text.length(); i++) {
+      int a = getIntValue(text.charAt(i));
+      int b = getIntValue(cypherKey_.charAt(i));
+      int c = (a + b) % alphabet_.length();
+
+      cyphered.append(alphabet_.charAt(c));
+    }
+
+    String reversed = cyphered.toString();
+    reversed = reverse(reversed);
+
+    return(reversed);
   }
 
-  private String deCypher(String cypherText) {
-    return(null);
+  /*
+   *
+   */
+  public String reverse(String text) {
+    String ret = new String();
+    for (int i = text.length() - 1; i >= 0; i--)
+      ret = ret.concat(String.valueOf(text.charAt(i)));
+
+    return(ret);
+  }
+
+  /*
+   *
+   */
+  private boolean checkAlphabet(String text) {
+    for (int i = 0; i < alphabet_.length(); i++)
+      text = text.replace(alphabet_.charAt(i),' ');
+
+    text = text.trim();
+    if ((text == null) || (text.equalsIgnoreCase("")))
+      return(true);
+    else
+      return(false);
+  }
+
+  /*
+   *
+   */
+  private String deCypher(String cypherText) throws NullPointerException {
+    if (cypherText == null)
+      throw(new NullPointerException("No text to decypher"));
+
+    StringBuffer decyphered = new StringBuffer();
+    cypherText = reverse(cypherText);
+
+    while (cypherKey_.length() < cypherText.length())
+      cypherKey_ = cypherKey_.concat(cypherKey_);
+
+    for (int i = 0; i < cypherText.length(); i++) {
+      int a = getIntValue(cypherText.charAt(i));
+      int b = getIntValue(cypherKey_.charAt(i));
+      int c = (a - b) % alphabet_.length();
+
+      while (c < 0) {
+        c += alphabet_.length();
+      }
+
+      decyphered.append(alphabet_.charAt(c));
+    }
+
+    return(decyphered.toString());
+  }
+
+  /*
+   *
+   */
+  private int getIntValue(char a) {
+    int i = alphabet_.indexOf(a);
+
+    return(i);
   }
 }
