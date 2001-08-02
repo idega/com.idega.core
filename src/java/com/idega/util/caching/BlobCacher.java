751c5ce5c73ca186bdfd83ea6077e9fd98c26989
Eirikur Hrafnsson <eiki@idega.com>
2001-08-02 10:47:16 +0000

    no message

--- src/java/com/idega/util/caching/BlobCacher.java
+++ src/java/com/idega/util/caching/BlobCacher.java
@@ -24,31 +24,29 @@ public class BlobCacher  {
 
   private static String rootCachingPath = "blobcache";
   private static String applicationObject = "blobcacher";
-  private static String seperator = FileUtil.getFileSeparator();
+  private static String separator = FileUtil.getFileSeparator();
 
-  public static String getCachedUrl( GenericEntity entity, ModuleInfo modinfo, String blobColumnName){
-    //check if the cachingtable exists
-    HashtableDoubleKeyed cache = ( HashtableDoubleKeyed )modinfo.getApplicationAttribute(applicationObject);
-    if ( cache == null ) {
-     cache = new HashtableDoubleKeyed();
-     modinfo.setApplicationAttribute(applicationObject,cache);
+  public static String getCachedUrl( String entityClassString, int id, ModuleInfo modinfo, String blobColumnName){
+     String url = null;
+    //check for cachetable in memory
+    if( cacheTableExists( entityClassString, id, modinfo) ){
+      //check if this blob has already been cached
+      HashtableDoubleKeyed cache = getCacheTable(modinfo);
+      url = (String) cache.get(GenericEntity.getStaticInstance(entityClassString).getEntityName(), Integer.toString(id) );
+      if( url == null ) {//if null cache it for next time
+       cacheToFile(entityClassString,id,modinfo.getApplication().getApplicationRealPath(), cache, blobColumnName);
+      }
     }
 
-    //check if this blob has already been cached
-    String url = (String) cache.get(entity.getEntityName(), Integer.toString(entity.getID()) );
-    if( url == null ) {//if null cache it for next time
-     cacheToFile(entity,modinfo.getApplication().getApplicationRealPath(), cache, blobColumnName);
-     return null;
-    }
-    else return url;
-
+    return url;
   }
 
 
-
-  private static void cacheToFile(GenericEntity entity, String applicationURL, HashtableDoubleKeyed cache, String blobColumnName ){
+  private static void cacheToFile(String entityClassString, int id, String applicationURL, HashtableDoubleKeyed cache, String blobColumnName ){
     InputStream input;
+    GenericEntity entity;
     try{
+      entity = GenericEntity.getEntityInstance(Class.forName(entityClassString),id);
       input = entity.getInputStreamColumnValue(blobColumnName);
     }
     catch( Exception e ){
@@ -57,7 +55,7 @@ public class BlobCacher  {
      return;
     }
 
-    String realPath = applicationURL+seperator+rootCachingPath;
+    String realPath = applicationURL+separator+rootCachingPath;
     String virtualPath = "/"+rootCachingPath+"/";
     String fileName = entity.getName();
 
@@ -72,6 +70,34 @@ public class BlobCacher  {
 
     cache.put(entity.getEntityName(), Integer.toString(entity.getID()),virtualPath+java.net.URLEncoder.encode(entity.getID()+"_"+fileName));
 
+  }
+
+ public static boolean isCached(String entityClassString, int id, ModuleInfo modinfo){
+    if(!cacheTableExists( entityClassString, id, modinfo)) return false;
+
+    //check if this blob has already been cached
+    String url = (String) getCacheTable(modinfo).get(GenericEntity.getStaticInstance(entityClassString).getEntityName(), Integer.toString(id) );
+    if( url == null ) {//if null cache it for next time
+     return false;
+    }
+    else return true;
+
+  }
+
+  private static boolean cacheTableExists(String entityClassString, int id, ModuleInfo modinfo){
+    //check if the cachingtable exists if not create it
+    HashtableDoubleKeyed cache = ( HashtableDoubleKeyed )modinfo.getApplicationAttribute(applicationObject);
+    if ( cache == null ) {
+     cache = new HashtableDoubleKeyed();
+     modinfo.setApplicationAttribute(applicationObject,cache);
+     return false;
+    }
+    else return true;
+  }
+
+  private static HashtableDoubleKeyed getCacheTable(ModuleInfo modinfo){
+    HashtableDoubleKeyed cache = ( HashtableDoubleKeyed )modinfo.getApplicationAttribute(applicationObject);
+    return cache;
   }
 
   public static void deleteCache(IWMainApplication app){
