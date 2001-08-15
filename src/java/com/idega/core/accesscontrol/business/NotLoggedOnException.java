757df734a602d28f6229e0826097511eefc5beb9
Gudmundur Agust Saemundsson <gummi@idega.com>
2001-08-15 18:32:38 +0000

    no message

---
+++ src/java/com/idega/core/accesscontrol/business/NotLoggedOnException.java
@@ -0,0 +1,22 @@
+package com.idega.core.accesscontrol.business;
+
+/**
+ * Title:        IW Accesscontrol
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:      idega.is
+ * @author 2000 - idega team - <a href="mailto:gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
+ * @version 1.0
+ */
+
+public class NotLoggedOnException extends RuntimeException {
+
+
+  public NotLoggedOnException(){
+    super("");
+  }
+
+  public NotLoggedOnException(String s){
+    super("NotLoggedOnException: "+ s);
+  }
+}
