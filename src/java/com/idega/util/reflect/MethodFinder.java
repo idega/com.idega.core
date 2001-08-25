49800e7c1a131d512e333ffde2682dd4a2b95463
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2001-08-25 12:20:37 +0000

    *** empty log message ***

--- src/java/com/idega/util/reflect/MethodFinder.java
+++ src/java/com/idega/util/reflect/MethodFinder.java
@@ -28,22 +28,34 @@ public class MethodFinder {
     return instance;
   }
 
-
   public Method getMethod(String methodIdentifier){
     try{
       String[] idArray = getIdentifierAsArray(methodIdentifier);
       Class c = getDeclaringClass(idArray);
+      System.out.println("Declaring class: "+c.getName());
       Method[] methods = getMethods(c);
       for (int i = 0; i < methods.length; i++) {
         if(methods[i].getName().equals(getMethodName(idArray))){
+          System.out.println("MethodName: "+methods[i].getName()+" returnType = "+methods[i].getReturnType().getName());
           if(methods[i].getReturnType().equals(getReturnTypeClass(idArray))){
             Class[] classes = getArgumentClasses(idArray);
             Class[] methodClasses = methods[i].getParameterTypes();
             boolean check=true;
-            for (int j = 0;(j < classes.length && check); j++) {
-              if(!methodClasses[j].equals(classes[j])){
-                check=false;
+            if(methodClasses.length>0){
+              for (int j = 0;(j < methodClasses.length && check); j++) {
+                if(!methodClasses[j].equals(classes[j])){
+                  System.out.println("MethodClasses["+j+"]"+methodClasses[j].getName());
+                  check=false;
+                }
               }
+            }
+            else{
+                System.out.println("length "+classes.length);
+                if(classes.length>0){
+                  if(!classes[0].equals(java.lang.Void.class)){
+                    check=false;
+                  }
+                }
             }
             if(check){
               return methods[i];
@@ -72,9 +84,15 @@ public class MethodFinder {
     buf.append(method.getName());
     buf.append(separator);
     Class[] parameterTypes = method.getParameterTypes();
-    for (int i = 0; i < parameterTypes.length; i++) {
-        buf.append(parameterTypes[i].getName());
-        buf.append(separator);
+    if(parameterTypes.length>0){
+      for (int i = 0; i < parameterTypes.length; i++) {
+          buf.append(parameterTypes[i].getName());
+          buf.append(separator);
+      }
+    }
+    else{
+      buf.append("void");
+      buf.append(separator);
     }
     return buf.toString();
   }
@@ -82,15 +100,39 @@ public class MethodFinder {
   private String[] getIdentifierAsArray(String identifier){
     StringTokenizer token = new StringTokenizer(identifier,separator);
     int size = token.countTokens();
-    String array[] = new String[size-1];
+    String array[] = new String[size];
     for (int i = 0; i < array.length; i++) {
       array[i]=token.nextToken();
     }
     return array;
   }
 
+  private static Class getClass(String className)throws ClassNotFoundException{
+    if(className.equals("void")){
+      return java.lang.Void.TYPE;
+    }
+    else if(className.equals("int")){
+      return java.lang.Integer.TYPE;
+    }
+    else if(className.equals("boolean")){
+      return java.lang.Boolean.TYPE;
+    }
+    else if(className.equals("float")){
+      return java.lang.Float.TYPE;
+    }
+    else if(className.equals("double")){
+      return java.lang.Double.TYPE;
+    }
+    else if(className.equals("long")){
+      return java.lang.Long.TYPE;
+    }
+    else{
+      return Class.forName(className);
+    }
+  }
+
   public String getDeclaringClassName(String[] identifierArray){
-    return identifierArray[1];
+    return identifierArray[2];
   }
 
   public Class getDeclaringClass(String[] identifierArray)throws ClassNotFoundException{
@@ -98,20 +140,23 @@ public class MethodFinder {
   }
 
   public String getReturnTypeClassName(String[] identifierArray){
-    return identifierArray[2];
-  }
-
-  public Class getReturnTypeClass(String[] identifierArray)throws ClassNotFoundException{
-    return Class.forName(getReturnTypeClassName(identifierArray));
-  }
-
-  public String getMethodName(String[] identifierArray){
     return identifierArray[3];
   }
 
+  public Class getReturnTypeClass(String[] identifierArray)throws ClassNotFoundException{
+    return getClass(getReturnTypeClassName(identifierArray));
+  }
+
+  public String getMethodName(String[] identifierArray){
+    return identifierArray[4];
+  }
+
   public String[] getArgumentClassNames(String[] identifierArray){
-    String[] theReturn = new String[identifierArray.length-4];
-    System.arraycopy(identifierArray,4,theReturn,0,theReturn.length);
+    int length = identifierArray.length-5;
+    String[] theReturn = new String[length];
+    if(length!=0){
+      System.arraycopy(identifierArray,5,theReturn,0,theReturn.length);
+    }
     return theReturn;
   }
 
@@ -119,7 +164,7 @@ public class MethodFinder {
     String[] strings =  getArgumentClassNames(identifierArray);
     Class[] theReturn = new Class[strings.length];
     for (int i = 0; i < theReturn.length; i++) {
-        theReturn[i]=Class.forName(strings[i]);
+        theReturn[i]=getClass(strings[i]);
     }
     return theReturn;
   }
