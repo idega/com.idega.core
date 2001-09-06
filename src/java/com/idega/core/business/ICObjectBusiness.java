84cd0690e4cca57d71a7d8dc15a025027f2940b4
Gudmundur Agust Saemundsson <gummi@idega.com>
2001-09-06 16:21:58 +0000

    no message

---
+++ src/java/com/idega/core/business/ICObjectBusiness.java
@@ -0,0 +1,36 @@
+package com.idega.core.business;
+
+import com.idega.core.data.*;
+import java.sql.SQLException;
+
+/**
+ * Title:        IW Core
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:      idega.is
+ * @author 2000 - idega team - <a href="mailto:gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
+ * @version 1.0
+ */
+
+public class ICObjectBusiness {
+
+
+  public static Class getICObjectClass(int ICObjectInstanceId) throws SQLException{
+    ICObjectInstance instance = new ICObjectInstance(ICObjectInstanceId);
+    ICObject obj = instance.getObject();
+    if(obj != null){
+      try {
+        return obj.getObjectClass();
+      }
+      catch (ClassNotFoundException ex) {
+        ex.printStackTrace();
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+
+
+} // Class
