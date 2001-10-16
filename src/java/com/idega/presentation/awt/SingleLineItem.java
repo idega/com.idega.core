331159b02addd499c469ef9c08d3be2e376d768b
Eirikur Hrafnsson <eiki@idega.com>
2001-10-16 12:16:26 +0000

    no message

--- src/java/com/idega/presentation/awt/SingleLineItem.java
+++ src/java/com/idega/presentation/awt/SingleLineItem.java
@@ -33,7 +33,7 @@ public class SingleLineItem extends Panel {
 
   public SingleLineItem(Container parent) {
     addMouseListener(new ClickAdapter());
-    //setSize(getPreferredSize());
+    setSize(getPreferredSize());
     GridBagLayout grid = new GridBagLayout();
     setLayout(grid);
     setBackground(bgColor);
