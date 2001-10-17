780b637ca8cc786efac9f15dbe45101e5da3b630
Gudmundur Agust Saemundsson <gummi@idega.com>
2001-10-17 20:25:07 +0000

    no message

---
+++ src/java/com/idega/core/accesscontrol/business/LoggedOnInfo.java
@@ -0,0 +1,68 @@
+package com.idega.core.accesscontrol.business;
+
+import javax.servlet.http.HttpSession;
+import com.idega.core.user.data.User;
+import com.idega.util.idegaTimestamp;
+
+/**
+ * Title:        idegaWeb
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:      idega
+ * @author <a href="gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
+ * @version 1.0
+ */
+
+public class LoggedOnInfo {
+
+  private User _user = null;
+  private HttpSession _session = null;
+  private idegaTimestamp _timeOfLoggon = null;
+
+  public LoggedOnInfo() {
+
+  }
+  //getters
+  public void setUser(User user){
+    _user = user;
+  }
+
+  public void setSession(HttpSession session){
+    _session = session;
+  }
+
+  public void setTimeOfLoggon(idegaTimestamp timeOfLoggon){
+    _timeOfLoggon = timeOfLoggon;
+  }
+
+  //setters
+  public User getUser(){
+    return _user;
+  }
+
+  public HttpSession getSession(){
+    return _session;
+  }
+
+  public idegaTimestamp getTimeOfLoggon(){
+    return _timeOfLoggon;
+  }
+
+  public boolean equals(Object obj){
+    if(obj instanceof LoggedOnInfo ){
+      return this.equals((LoggedOnInfo)obj);
+    }else if(obj instanceof HttpSession){
+      return this.equals((HttpSession)obj);
+    }else {
+      return super.equals(obj);
+    }
+  }
+
+  public boolean equals(LoggedOnInfo obj){
+    return this.getSession().equals((HttpSession)((LoggedOnInfo)obj).getSession());
+  }
+
+  public boolean equals(HttpSession obj){
+    return this.getSession().equals((HttpSession)obj);
+  }
+}
