412465daab638f18dc3b76f0c0e8f69f1cb55e42
Eirikur Hrafnsson <eiki@idega.com>
2001-10-20 00:49:03 +0000

    no message

--- src/java/com/idega/presentation/Image.java
+++ src/java/com/idega/presentation/Image.java
@@ -518,6 +518,7 @@ public void limitImageWidth( boolean limitImageWidth ){
   }
 
   public void main(IWContext iwc) {
+    System.out.println("inni í image main");
     if( this.overImageUrl != null ){
       Script rollOverScript = getParentPage().getAssociatedScript();
       rollOverScript.addFunction("swapImgRestore()","function swapImgRestore() {var i,x,a=document.sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;}");
