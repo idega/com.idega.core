91015f00935f91a89869a1299be24b2a7d55242b
Aron Birkir Gudmundsson <aron@idega.com>
2001-11-17 15:07:09 +0000

    no message

---
+++ src/java/com/idega/util/text/Edit.java
@@ -0,0 +1,76 @@
+package com.idega.util.text;
+
+
+
+import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.InterfaceObject;
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+public class Edit {
+
+  public static String colorLight = "#D7DADF";
+  public static String colorMiddle = "#9fA9B3";
+  public static String colorDark  = "#27334B";
+  public static String colorWhite =  "#FFFFFF";
+  public static String colorTextFont = "#000000";
+  public static String colorTitleFont = "#FFFFFF";
+  public static String colorHeaderFont = "#27334B";
+  public static String colorIndexFont = "#000000";
+  public static String colorRed = "#942829";
+  public static String colorBlue = "#27324B";
+  public static String colorLightBlue ="#ECEEF0";
+
+  public static String bottomBarThickness = "8";
+
+  public static int textFontSize = 1;
+  public static int titleFontSize = 1;
+  public static int headerFontSize = 1;
+  public static String styleAttribute = "font-size: 8pt";
+
+  public static Text formatText(String text){
+    return getText(text,false,colorTextFont,textFontSize);
+  }
+  public static Text formatText(String text,int size){
+    return getText(text,true,colorTextFont,size);
+  }
+  public static Text formatText(int i){
+    return formatText(String.valueOf(i));
+  }
+  public static Text titleText(String text){
+    return getText(text,true,colorTitleFont,titleFontSize);
+  }
+  public static Text titleText(String text,int size){
+    return getText(text,true,colorTitleFont,size);
+  }
+  public static Text titleText(int i){
+    return titleText(String.valueOf(i));
+  }
+  public static Text headerText(String text){
+    return getText(text,true,colorHeaderFont,headerFontSize);
+  }
+   public static Text headerText(String text,int size){
+    return getText(text,true,colorHeaderFont,size);
+  }
+  public static Text headerText(int i){
+    return headerText(String.valueOf(i));
+  }
+  private static Text getText(String text,boolean bold,String color,int size){
+    Text T = new Text(text,bold,false,false);
+    T.setFontColor(color);
+    T.setFontSize(size);
+    return T;
+  }
+
+
+  public static void setStyle(InterfaceObject O){
+    O.setAttribute("style",styleAttribute);
+  }
+
+}
