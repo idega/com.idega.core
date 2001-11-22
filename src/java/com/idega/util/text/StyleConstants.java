64626b61d1f3f71e098f8f76af3bc3d6ba9e3e37
Thorhallur Helgason <laddi@idega.com>
2001-11-22 18:39:56 +0000

    no message

---
+++ src/java/com/idega/util/text/StyleConstants.java
@@ -0,0 +1,94 @@
+package com.idega.util.text;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+public class StyleConstants {
+
+public static final String DELIMITER_COLON = ":";
+public static final String DELIMITER_SEMICOLON = ";";
+
+public static final String ATTRIBUTE_COLOR = "color";
+public static final String ATTRIBUTE_FONT_FAMILY = "font-family";
+public static final String ATTRIBUTE_FONT_SIZE = "font-size";
+public static final String ATTRIBUTE_FONT_STRETCH = "font-stretch";
+public static final String ATTRIBUTE_FONT_STYLE = "font-style";
+public static final String ATTRIBUTE_FONT_VARIANT = "font-variant";
+public static final String ATTRIBUTE_FONT_WEIGHT = "font-weight";
+public static final String ATTRIBUTE_LETTER_SPACING = "letter-spacing";
+public static final String ATTRIBUTE_LINE_HEIGHT = "line-height";
+public static final String ATTRIBUTE_TEXT_ALIGN = "text-align";
+public static final String ATTRIBUTE_TEXT_DECORATION = "text-decoration";
+public static final String ATTRIBUTE_TEXT_SHADOW = "text-shadow";
+public static final String ATTRIBUTE_TEXT_TRANSFORM = "text-transform";
+public static final String ATTRIBUTE_WORD_SPACING = "word-spacing";
+
+public static final String[] ALL_STYLES = {ATTRIBUTE_FONT_FAMILY,ATTRIBUTE_FONT_SIZE,ATTRIBUTE_FONT_STRETCH,ATTRIBUTE_FONT_VARIANT,ATTRIBUTE_FONT_WEIGHT,ATTRIBUTE_COLOR,ATTRIBUTE_LETTER_SPACING,ATTRIBUTE_LINE_HEIGHT,ATTRIBUTE_FONT_STYLE,ATTRIBUTE_TEXT_ALIGN,ATTRIBUTE_TEXT_DECORATION,ATTRIBUTE_TEXT_SHADOW,ATTRIBUTE_TEXT_TRANSFORM,ATTRIBUTE_WORD_SPACING};
+
+public static final String FONT_FAMILY_ARIAL = "Arial,Helvetica,sans-serif";
+public static final String FONT_FAMILY_COURIER = "Courier New,Courier,mono";
+public static final String FONT_FAMILY_GENEVA = "Geneva,Arial,Helvetica,sans-serif";
+public static final String FONT_FAMILY_GEORGIA = "Georgia,Times New Roman,Times,serif";
+public static final String FONT_FAMILY_TIMES = "Times New Roman,Times,serif";
+public static final String FONT_FAMILY_VERDANA = "Verdana,Arial,Helvetica,sans-serif";
+
+public static final String FONT_STRETCH_NORMAL = "normal";
+public static final String FONT_STRETCH_WIDER = "wider";
+public static final String FONT_STRETCH_NARROWER = "narrower";
+public static final String FONT_STRETCH_ULTRA_CONDENSED = "ultra-condensed";
+public static final String FONT_STRETCH_EXTRA_CONDENSED = "extra-condensed";
+public static final String FONT_STRETCH_CONDENSED = "condensed";
+public static final String FONT_STRETCH_SEMI_DONDENSED = "semi-condensed";
+public static final String FONT_STRETCH_SEMI_EXPANDED = "semi-expanded";
+public static final String FONT_STRETCH_EXPANDED = "expanded";
+public static final String FONT_STRETCH_EXTRA_EXPANDED = "extra-expanded";
+public static final String FONT_STRETCH_ULTRA_EXPANDED = "ultra-expanded";
+
+public static final String FONT_STYLE_NORMAL = "normal";
+public static final String FONT_STYLE_ITALIC = "italic";
+public static final String FONT_STYLE_OBLIQUE = "oblique";
+
+public static final String TEXT_DECORATION_UNDERLINE = "underline";
+public static final String TEXT_DECORATION_OVERLINE = "overline";
+public static final String TEXT_DECORATION_LINETHROUGH = "line-through";
+public static final String TEXT_DECORATION_BLINK = "blink";
+public static final String TEXT_DECORATION_NONE = "none";
+
+public static final String FONT_WEIGHT_NORMAL = "normal";
+public static final String FONT_WEIGHT_BOLD = "bold";
+public static final String FONT_WEIGHT_BOLDER = "bolder";
+public static final String FONT_WEIGHT_LIGHT = "light";
+public static final String FONT_WEIGHT_100 = "100";
+public static final String FONT_WEIGHT_200 = "200";
+public static final String FONT_WEIGHT_300 = "300";
+public static final String FONT_WEIGHT_400 = "400";
+public static final String FONT_WEIGHT_500 = "500";
+public static final String FONT_WEIGHT_600 = "600";
+public static final String FONT_WEIGHT_700 = "700";
+public static final String FONT_WEIGHT_800 = "800";
+public static final String FONT_WEIGHT_900 = "900";
+
+public static final String FONT_VARIANT_NORMAL = "normal";
+public static final String FONT_VARIANT_SMALLCAPS = "small-caps";
+
+public static final String TEXT_TRANSFORM_CAPITALIZE = "capitalize";
+public static final String TEXT_TRANSFORM_UPPERCASE = "uppercase";
+public static final String TEXT_TRANSFORM_LOWERCASE = "lowercase";
+public static final String TEXT_TRANSFORM_NONE = "none";
+
+public static final String LETTER_SPACING_NORMAL = "normal";
+
+public static final String WORD_SPACING_NORMAL = "normal";
+
+public static final String TEXT_ALIGN_LEFT = "left";
+public static final String TEXT_ALIGN_CENTER = "center";
+public static final String TEXT_ALIGN_RIGHT = "right";
+public static final String TEXT_ALIGN_JUSTIFY = "justify";
+
+}
