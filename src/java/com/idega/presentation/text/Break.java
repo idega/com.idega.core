e5232e2310431108a493709917ae351ab9b8a48f
Thorhallur Helgason <laddi@idega.com>
2002-03-08 19:44:39 +0000

    no message

--- src/java/com/idega/presentation/text/Break.java
+++ src/java/com/idega/presentation/text/Break.java
@@ -3,33 +3,37 @@ package com.idega.presentation.text;
 import com.idega.presentation.Block;
 import com.idega.presentation.IWContext;
 
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
-
 public class Break extends Block {
 
-private int _numberOfBreaks = 1;
+  private int _numberOfBreaks = 1;
 
-  public Break() {
-  }
+  public Break() { }
 
   public Break(int numberOfBreaks) {
     _numberOfBreaks = numberOfBreaks;
   }
 
   public void main(IWContext iwc) {
-    for ( int a = 1; a <= _numberOfBreaks; a++ ) {
+    for (int a = 1; a <= _numberOfBreaks; a++) {
       super.add(Text.getBreak());
     }
   }
 
+  /**
+   *  Sets how many breaks should be displayed
+   *
+   *@param  numberOfBreaks  The new numberOfBreaks value
+   */
   public void setNumberOfBreaks(int numberOfBreaks) {
     _numberOfBreaks = numberOfBreaks;
+  }
+
+  /**
+   *  Sets how many breaks should be displayed
+   *
+   *@param  numberOfBreaks  The new numberOfBreaks value
+   */
+  public void setNumberOfBreaks(String numberOfBreaks) {
+    _numberOfBreaks = Integer.parseInt(numberOfBreaks);
   }
 }
