f07d28e4b61f94bdf5a6d4f129d37c242d1f3efd
Aron Birkir Gudmundsson <aron@idega.com>
2002-04-03 17:49:38 +0000

    no message

--- src/java/com/idega/util/ErrorChecker.java
+++ src/java/com/idega/util/ErrorChecker.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ErrorChecker.java,v 1.4 2001/07/30 11:46:38 palli Exp $
+ * $Id: ErrorChecker.java,v 1.5 2002/04/03 17:49:38 aron Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -36,50 +36,7 @@ public class ErrorChecker {
    *
    */
   public static boolean isValidIcelandicSocialSecurityNumber(String socialSecurityNumber) {
-    if (socialSecurityNumber.length() != 10) {
-      return(false);
-    }
-
-    try {
-      int var1 = Integer.parseInt(socialSecurityNumber.substring(0,1));
-      int var2 = Integer.parseInt(socialSecurityNumber.substring(1,2));
-      int var3 = Integer.parseInt(socialSecurityNumber.substring(2,3));
-      int var4 = Integer.parseInt(socialSecurityNumber.substring(3,4));
-      int var5 = Integer.parseInt(socialSecurityNumber.substring(4,5));
-      int var6 = Integer.parseInt(socialSecurityNumber.substring(5,6));
-      int var7 = Integer.parseInt(socialSecurityNumber.substring(6,7));
-      int var8 = Integer.parseInt(socialSecurityNumber.substring(7,8));
-      int var9 = Integer.parseInt(socialSecurityNumber.substring(8,9));
-
-      int sum = (3 * var1) +
-                (2 * var2) +
-                (7 * var3) +
-                (6 * var4) +
-                (5 * var5) +
-                (4 * var6) +
-                (3 * var7) +
-                (2 * var8);
-
-      int res = sum % 11;
-      int vartala = 11 - res;
-
-      if (vartala == 10) {
-        vartala = 1;
-      }
-      else if (vartala == 11) {
-        vartala = 0;
-      }
-
-      if(var9 == vartala) {
-        return(true);
-      }
-      else {
-        return(false);
-      }
-    }
-    catch(Exception e) {
-      return(false);
-    }
+    return com.idega.util.text.SocialSecurityNumber.isValidIcelandicSocialSecurityNumber(socialSecurityNumber);
   }
 
   /**
--- src/java/com/idega/util/text/SocialSecurityNumber.java
+++ src/java/com/idega/util/text/SocialSecurityNumber.java
@@ -49,4 +49,55 @@ import java.util.*;
 
   }
 
+  /**
+   *  Checks for validity of an icelandic ssn
+   */
+  public static boolean isValidIcelandicSocialSecurityNumber(String socialSecurityNumber) {
+    if (socialSecurityNumber.length() != 10) {
+      return(false);
+    }
+
+    try {
+      int var1 = Integer.parseInt(socialSecurityNumber.substring(0,1));
+      int var2 = Integer.parseInt(socialSecurityNumber.substring(1,2));
+      int var3 = Integer.parseInt(socialSecurityNumber.substring(2,3));
+      int var4 = Integer.parseInt(socialSecurityNumber.substring(3,4));
+      int var5 = Integer.parseInt(socialSecurityNumber.substring(4,5));
+      int var6 = Integer.parseInt(socialSecurityNumber.substring(5,6));
+      int var7 = Integer.parseInt(socialSecurityNumber.substring(6,7));
+      int var8 = Integer.parseInt(socialSecurityNumber.substring(7,8));
+      int var9 = Integer.parseInt(socialSecurityNumber.substring(8,9));
+
+      int sum = (3 * var1) +
+                (2 * var2) +
+                (7 * var3) +
+                (6 * var4) +
+                (5 * var5) +
+                (4 * var6) +
+                (3 * var7) +
+                (2 * var8);
+
+      int res = sum % 11;
+      int vartala = 11 - res;
+
+      if (vartala == 10) {
+        vartala = 1;
+      }
+      else if (vartala == 11) {
+        vartala = 0;
+      }
+
+      if(var9 == vartala) {
+        return(true);
+      }
+      else {
+        return(false);
+      }
+    }
+    catch(Exception e) {
+      return(false);
+    }
+  }
+
+
 }
