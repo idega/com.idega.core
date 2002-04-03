62ca9acc7568c7df841db61a5a123d7f175751d6
Eirikur Hrafnsson <eiki@idega.com>
2002-04-03 20:57:34 +0000

    no message

--- src/java/com/idega/presentation/ui/PageIncluder.java
+++ src/java/com/idega/presentation/ui/PageIncluder.java
@@ -1,8 +1,8 @@
 package com.idega.presentation.ui;
 
-import java.net.MalformedURLException;
+import java.net.*;
+import java.util.Enumeration;
 import java.util.Vector;
-import java.net.URL;
 import java.io.IOException;
 import com.idega.presentation.PresentationObject;
 import com.idega.presentation.IWContext;
@@ -32,37 +32,56 @@ public class PageIncluder extends PresentationObject{
       initVariables(iwc);
       if( doPrint(iwc) ){
         StringBuffer location = new StringBuffer();
-        String query = iwc.getQueryString();
-        if( query==null ) query = "";
+        StringBuffer queryBuf = new StringBuffer();
+        String query = null;
 
-        if( iwc.isParameterSet(PAGE_INCLUDER_PARAMETER_NAME) ){
-          query = TextSoap.findAndCut(decodeQueryString(query),PAGE_INCLUDER_PARAMETER_NAME+"=");
-          URL = query;
-        }
+        if( iwc.isParameterSet(PAGE_INCLUDER_PARAMETER_NAME) ){//after clicking a link og submitting a form
+          //get all parameters even from post actions
+          Enumeration enum = iwc.getParameterNames();
+          while (enum.hasMoreElements()) {
+            String param = (String) enum.nextElement();
+            debug(param+" : "+iwc.getParameter(param));
+            /**@todo use a post method to get the page or stringbuffer this URL  encode?**/
+            if( param.equals(PAGE_INCLUDER_PARAMETER_NAME) ){
+             URL = decodeQueryString(iwc.getParameter(param));
+             location.append(URL);
+            }
+            else{
+              queryBuf.append(param);
+              queryBuf.append("=");
+              queryBuf.append(URLEncoder.encode(iwc.getParameter(param)));
+              queryBuf.append("&");
+            }
+          }//while ends
 
-        location.append(URL);
+          query = queryBuf.toString();
 
-        if( !query.equals("") ){
-          if(URL.endsWith("/")){//check if the url ends with a slash
-            location.append("?");
-          }
-          else{//no slash at end
-            if( URL.indexOf("?")==-1 ){//check if the url contains a ?
-              if(URL.indexOf("/",8)!=-1){//check if the url contains a slash
-                location.append("?");
+          if( !query.equals("") ){
+            if(URL.endsWith("/")){//check if the url ends with a slash
+              location.append("?");
+            }
+            else{//no slash at end
+              if( URL.indexOf("?")==-1 ){//check if the url contains a ?
+                if(URL.indexOf("/",8)!=-1){//check if the url contains a slash
+                  location.append("?");
+                }
+                else{
+                  location.append("/?");
+                }
               }
-              else{
-                location.append("/?");
+              else{//just add to the parameters
+                location.append("&");
               }
             }
-            else{//just add to the parameters
-              location.append("&");
-            }
+            //add the extra parameters
+            location.append(query);
           }
-
-          location.append(query);
+        }//if iw_uri check ends
+        else {//using starting state url
+          location.append(URL);
         }
-        //debug(location.toString());
+
+        debug("Location = "+location.toString());
         String html = FileUtil.getStringFromURL(location.toString());
 
         /**@todo fix if e.g. some src= do include the URI so that the URI isn't added twice
@@ -81,26 +100,51 @@ public class PageIncluder extends PresentationObject{
   }
 
   protected String changeAHrefAttributes(String html,IWContext iwc){
-    return TextSoap.findAndInsertAfter(html,"href=\"",iwc.getRequestURI()+"?"+PAGE_INCLUDER_PARAMETER_NAME+"=");
+    html = TextSoap.findAndInsertAfter(html,"href=\"",iwc.getRequestURI()+"?"+PAGE_INCLUDER_PARAMETER_NAME+"=");
+    return TextSoap.findAndInsertAfter(html,"HREF=\"",iwc.getRequestURI()+"?"+PAGE_INCLUDER_PARAMETER_NAME+"=");
   }
 
   protected String changeFormActionAttributes(String html,IWContext iwc){
-    return TextSoap.findAndInsertAfter(html,"action=\"",iwc.getRequestURI()+"?"+PAGE_INCLUDER_PARAMETER_NAME+"=");
+    html = TextSoap.findAndInsertAfter(html,"action=\"",iwc.getRequestURI()+"?"+PAGE_INCLUDER_PARAMETER_NAME+"=");
+    return TextSoap.findAndInsertAfter(html,"ACTION=\"",iwc.getRequestURI()+"?"+PAGE_INCLUDER_PARAMETER_NAME+"=");
   }
 
   protected String changeSrcAttributes(String html, String location) throws MalformedURLException{
     URL url = new URL(location);
-    return TextSoap.findAndInsertAfter(html,"src=\"",url.getProtocol()+"://"+url.getHost()+"/");
+    html = TextSoap.findAndInsertAfter(html,"background\"",url.getProtocol()+"://"+url.getHost()+"/");
+    html = TextSoap.findAndInsertAfter(html,"BACKGROUND\"",url.getProtocol()+"://"+url.getHost()+"/");
+    html = TextSoap.findAndInsertAfter(html,"src=\"",url.getProtocol()+"://"+url.getHost()+"/");
+    return TextSoap.findAndInsertAfter(html,"SRC=\"",url.getProtocol()+"://"+url.getHost()+"/");
   }
 
   protected String encodeQueryStrings(String html){
     html = TextSoap.findAndReplace(html,"&","#");
+    //fixing this should be done with a HTMLEditor object
     html = TextSoap.findAndReplace(html,"#eth;","&eth;");
-    html = TextSoap.findAndReplace(html,"#thorn;","&eth;");
-    html = TextSoap.findAndReplace(html,"#iacute;","&iacute;");
-    html = TextSoap.findAndReplace(html,"#uacute;","&uacute;");
+    html = TextSoap.findAndReplace(html,"#ETH;","&ETH;");
+    html = TextSoap.findAndReplace(html,"#thorn;","&thorn;");
+    html = TextSoap.findAndReplace(html,"#THORN;","&THORN;");
+    html = TextSoap.findAndReplace(html,"#aelig;","&aelig;");
+    html = TextSoap.findAndReplace(html,"#AELIG;","&AELIG;");
     html = TextSoap.findAndReplace(html,"#ouml;","&ouml;");
+    html = TextSoap.findAndReplace(html,"#OUML;","&OUML;");
     html = TextSoap.findAndReplace(html,"#nbsp;","&nbsp;");
+    html = TextSoap.findAndReplace(html,"#amp;","&amp;");
+
+//islenskir broddstafir
+    html = TextSoap.findAndReplace(html,"#aacute;","&aacute;");
+    html = TextSoap.findAndReplace(html,"#Aacute;","&Aacute;");
+    html = TextSoap.findAndReplace(html,"#eacute;","&eacute;");
+    html = TextSoap.findAndReplace(html,"#Eacute;","&Eacute;");
+    html = TextSoap.findAndReplace(html,"#iacute;","&iacute;");
+    html = TextSoap.findAndReplace(html,"#Iacute;","&Iacute;");
+    html = TextSoap.findAndReplace(html,"#uacute;","&uacute;");
+    html = TextSoap.findAndReplace(html,"#Uacute;","&Uacute;");
+    html = TextSoap.findAndReplace(html,"#oacute;","&oacute;");
+    html = TextSoap.findAndReplace(html,"#Oacute;","&Oacute;");
+    html = TextSoap.findAndReplace(html,"#yacute;","&yacute;");
+    html = TextSoap.findAndReplace(html,"#Yacute;","&Yacute;");
+
   //  html = TextSoap.findAndReplace(html,"#iacute;","&iacute;");
   return html;
   }
