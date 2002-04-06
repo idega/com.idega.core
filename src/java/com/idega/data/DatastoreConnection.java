56e3fd9f468f0e9753078a2bdfa36210bbc401b9
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-04-06 19:28:21 +0000

    no message

--- src/java/com/idega/data/DatastoreConnection.java
+++ src/java/com/idega/data/DatastoreConnection.java
@@ -1023,7 +1023,7 @@ public class DatastoreConnection implements Connection{
 
  *  Comment in for JDK 1.4 compile compatability!!!
 
-*/
+
     public java.sql.Statement createStatement(int iOne, int iTwo, int iThree){return null;}
 
 
@@ -1048,7 +1048,7 @@ public class DatastoreConnection implements Connection{
     public java.sql.PreparedStatement prepareStatement(String sql, int[] one){return null;}
 
     public java.sql.PreparedStatement prepareStatement(String sql, String[] one){return null;}
-
+*/
 
 }
 
