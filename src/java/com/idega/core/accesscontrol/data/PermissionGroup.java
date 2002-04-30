fa25238a0b65a60049941c6f0349fdbf8fa25ab4
Gudmundur Agust Saemundsson <gummi@idega.com>
2002-04-30 14:56:22 +0000

    no message

--- src/java/com/idega/core/accesscontrol/data/PermissionGroup.java
+++ src/java/com/idega/core/accesscontrol/data/PermissionGroup.java
@@ -2,7 +2,7 @@ package com.idega.core.accesscontrol.data;
 
 import javax.ejb.*;
 
-public interface PermissionGroup extends com.idega.core.data.GenericGroup
+public interface PermissionGroup extends com.idega.user.data.Group
 {
  public java.lang.String getGroupTypeValue();
 }
--- src/java/com/idega/core/accesscontrol/data/PermissionGroupBMPBean.java
+++ src/java/com/idega/core/accesscontrol/data/PermissionGroupBMPBean.java
@@ -28,7 +28,7 @@ import java.sql.*;
 
 
 
-public class PermissionGroupBMPBean extends com.idega.core.data.GenericGroupBMPBean implements com.idega.core.accesscontrol.data.PermissionGroup {
+public class PermissionGroupBMPBean extends com.idega.user.data.GroupBMPBean implements com.idega.core.accesscontrol.data.PermissionGroup {
 
 
 
