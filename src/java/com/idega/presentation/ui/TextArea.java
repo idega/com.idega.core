dbcc0431d3c831d6b69a9e5f4543d0874d30cb42
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-07-22 17:22:38 +0000

    Added setWidth(String) and setHeight(String) that uses style attributes deprecated old setWidth(int) and setHeight(int)

--- src/java/com/idega/presentation/ui/TextArea.java
+++ src/java/com/idega/presentation/ui/TextArea.java
@@ -1,27 +1,13 @@
 //idega 2000 - Tryggvi Larusson
-
 /*
 
 *Copyright 2000 idega.is All Rights Reserved.
 
 */
-
-
-
 package com.idega.presentation.ui;
-
-
-
 import java.io.*;
-
 import java.util.*;
-
 import com.idega.presentation.*;
-
-
-
-
-
 /**
 
 *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
@@ -29,261 +15,193 @@ import com.idega.presentation.*;
 *@version 1.2
 
 */
-
-public class TextArea extends InterfaceObject{
-
-
-
-
-
-protected String content;
-
-protected boolean editable;
-
-protected boolean keepContent;
-
-protected boolean wrap = true;
-
-
-
-
-
-public TextArea(){
-
-	this("untitled");
-
-}
-
-
-
-public TextArea(String name){
-
-	this(name,"");
-
-}
-
-
-
-public TextArea(String name,String content){
-
-	super();
-
-	setName(name);
-
-	this.content=content;
-
-	editable=true;
-
-	keepContent = false;
-
-}
-
-
-
-public TextArea(String name,int width,int height){
-
-	this(name,"");
-
-	setWidth(width);
-
-	setHeight(height);
-
-}
-
-
-
-public TextArea(String name,String content,int width,int height){
-
-	this(name,content);
-
-	setWidth(width);
-
-	setHeight(height);
-
-}
-
-
-
-
-
-public void setWidth(int width){
-
-	setAttribute("cols",Integer.toString(width));
-
-}
-
-
-
-public void setHeight(int height){
-
-	setAttribute("rows",Integer.toString(height));
-
-}
-
-
-
-
-
-public void setAsEditable(){
-
-	editable=true;
-
-}
-
-
-
-public void setAsNotEditable(){
-
-	editable=false;
-
-}
-
-
-
-public void setValue(String value){
-
-  setContent(value);
-
-}
-
-
-
-public void setValue(int value){
-
-  setValue(Integer.toString(value));
-
-}
-
-
-
-public void setContent(String s){
-
-	content=s;
-
-}
-
-
-
-public String getValue(){
-
-  return getContent();
-
-}
-
-
-
-public void setWrap(boolean wrapping){
-
-  this.wrap=wrapping;
-
-}
-
-
-
-//Enables the possibility of maintaining the content of the area between requests.
-
-public void keepContent(){
-
-	keepContent = true;
-
-}
-
-
-
-public void keepStatusOnAction(){
-
-	keepContent();
-
-}
-
-
-
-public String getContent(){
-
-	if (getRequest().getParameter(getName()) == null){
-
-		return content;
-
+public class TextArea extends InterfaceObject
+{
+	protected String content;
+	protected boolean editable;
+	protected boolean keepContent;
+	protected boolean wrap = true;
+	private static String ROWS_ATTRIBUTE = "rows";
+	private static String COLS_ATTRIBUTE = "cols";
+	private static String UNTITLED_STRING = "untitled";
+	private static String EMPTY_STRING = "";
+	public TextArea()
+	{
+		this(UNTITLED_STRING);
 	}
-
-	else{
-
-		if (keepContent == true ){
-
-			return getRequest().getParameter(getName());
-
-		}
-
-		else{
-
+	public TextArea(String name)
+	{
+		this(name, EMPTY_STRING);
+	}
+	public TextArea(String name, String content)
+	{
+		super();
+		setName(name);
+		this.content = content;
+		editable = true;
+		keepContent = false;
+	}
+	public TextArea(String name, int width, int height)
+	{
+		this(name, "");
+		setWidth(width);
+		setHeight(height);
+	}
+	public TextArea(String name, String content, int width, int height)
+	{
+		this(name, content);
+		setWidth(width);
+		setHeight(height);
+	}
+	/**
+	 * Sets the width in columns
+	 * @deprecated Replaced with setWidth(String) or setColumnn(int)
+	 */
+	public void setWidth(int columns)
+	{
+		setColumns(columns);
+	}
+	/**
+	 * Sets the height in rows
+	 * @deprecated Replaced with setHeight(String) or setRows(int)
+	 */
+	public void setHeight(int rows)
+	{
+		setRows(rows);
+	}
+	public void setAsEditable()
+	{
+		editable = true;
+	}
+	public void setAsNotEditable()
+	{
+		editable = false;
+	}
+	public void setValue(String value)
+	{
+		setContent(value);
+	}
+	public void setValue(int value)
+	{
+		setValue(Integer.toString(value));
+	}
+	public void setContent(String s)
+	{
+		content = s;
+	}
+	public String getValue()
+	{
+		return getContent();
+	}
+	public void setWrap(boolean wrapping)
+	{
+		this.wrap = wrapping;
+	}
+	//Enables the possibility of maintaining the content of the area between requests.
+	public void keepContent()
+	{
+		keepContent = true;
+	}
+	public void keepStatusOnAction()
+	{
+		keepContent();
+	}
+	public String getContent()
+	{
+		if (getRequest().getParameter(getName()) == null)
+		{
 			return content;
-
 		}
-
-	}
-
-}
-
-
-
-public void print(IWContext iwc)throws IOException{
-
-
-	//if ( doPrint(iwc) ){
-
-		if (getLanguage().equals("HTML")){
-
-			String EditableString = "";
-
-			if (!editable){EditableString = "READONLY";}
-
-                       //eiki,idega iceland
-
-                        if ( !wrap ) { EditableString += " wrap=\"OFF\"";}
-
-
-
-			//if (getInterfaceStyle().equals("default"))
-
-				print("<textarea name=\""+getName()+"\""+getAttributeString()+EditableString+" >");
-
-				print(getContent() );
-
-				print("</textarea>");
-
-			// }
-
-		}
-
-		else if (getLanguage().equals("WML")){
-
-			if ( content != null){
-
-				setValue(content);
-
+		else
+		{
+			if (keepContent == true)
+			{
+				return getRequest().getParameter(getName());
 			}
-
-			print("<input type=\"text\" name=\""+getName()+"\" value=\""+getContent()+"\" >");
-
-			print("</input>");
-
+			else
+			{
+				return content;
+			}
 		}
-
-	//}
-
+	}
+	public void print(IWContext iwc) throws IOException
+	{
+		//if ( doPrint(iwc) ){
+		if (getLanguage().equals("HTML"))
+		{
+			String EditableString = "";
+			if (!editable)
+			{
+				EditableString = "READONLY";
+			}
+			//eiki,idega iceland
+			if (!wrap)
+			{
+				EditableString += " wrap=\"OFF\"";
+			}
+			//if (getInterfaceStyle().equals("default"))
+			print("<textarea name=\"" + getName() + "\"" + getAttributeString() + EditableString + " >");
+			print(getContent());
+			print("</textarea>");
+			// }
+		}
+		else if (getLanguage().equals("WML"))
+		{
+			if (content != null)
+			{
+				setValue(content);
+			}
+			print("<input type=\"text\" name=\"" + getName() + "\" value=\"" + getContent() + "\" >");
+			print("</input>");
+		}
+		//}
+	}
+	public void setStyle(String style)
+	{
+		setAttribute("class", style);
+	}
+	/**
+	 * Sets the width in pixels or percents
+	 */
+	public void setWidth(String width)
+	{
+		try
+		{
+			int w = Integer.parseInt(width);
+			this.setStyleAttribute("width:" + width + "px");
+		}
+		catch (NumberFormatException e)
+		{
+			this.setStyleAttribute("width:" + width);
+		}
+	}
+	/**
+	 * Sets the height in pixels or percents
+	 */
+	public void setHeight(String height)
+	{
+		try
+		{
+			int h = Integer.parseInt(height);
+			this.setStyleAttribute("height:" + height + "px");
+		}
+		catch (NumberFormatException e)
+		{
+			this.setStyleAttribute("height:" + height);
+		}
+	}
+	/**
+	 * Sets the number of character columns in this text area
+	 */
+	public void setColumns(int cols)
+	{
+		setAttribute(COLS_ATTRIBUTE, Integer.toString(cols));
+	}
+	/**
+	 * Sets the number of character rows in this text area
+	 */
+	public void setRows(int rows)
+	{
+		setAttribute(ROWS_ATTRIBUTE, Integer.toString(rows));
+	}
 }
-
-
-
-  public void setStyle(String style) {
-
-    setAttribute("class",style);
-
-  }
-
-
-
-}
-
-
-
