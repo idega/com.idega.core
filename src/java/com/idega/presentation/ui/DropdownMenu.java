675c4c50ac0f7caa3c2c1e3302c698e3793511ae
Pall Helgason <palli@idega.com>
2002-07-22 10:10:41 +0000

    Added new handler for DropdownMenu.

--- src/java/com/idega/presentation/ui/DropdownMenu.java
+++ src/java/com/idega/presentation/ui/DropdownMenu.java
@@ -1,611 +1,343 @@
-//idega 2000 - Tryggvi Larusson
-
 /*
-
-*Copyright 2000 idega.is All Rights Reserved.
-
-*/
-
-
-
+ * $Id: DropdownMenu.java,v 1.7 2002/07/22 10:10:41 palli Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
 package com.idega.presentation.ui;
 
-
-
-import java.io.IOException;
-
 import java.util.Collection;
-
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
 import java.util.Vector;
-
 import java.util.Iterator;
-
 import com.idega.presentation.IWContext;
-
 import com.idega.presentation.Script;
-
-import com.idega.presentation.PresentationObject;
-
 import com.idega.presentation.PresentationObjectContainer;
-
 import com.idega.data.IDOLegacyEntity;
 
-
-
 /**
-
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-
-*@version 1.2
-
-*/
-
-public class DropdownMenu extends InterfaceObject{
-
-
-
-private Vector theElements;
-
-private boolean keepStatus;
-
-private String selectedElementValue;
-
-private Script script;
-
-private final static String untitled = "untitled";
-
-
-
-public DropdownMenu(){
-
-	this(untitled);
-
-}
-
-
-
-public DropdownMenu(String Name){
-
-	//super();
-
-	setName(Name);
-
-	initialize();
-
-	//setAttribute("CLASS","select");
-
-}
-
-
-
-/**
-
- * Populates the dropdownMenu from a List of IDOLegacyEntity objects
-
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.2
  */
+public class DropdownMenu extends InterfaceObject {
+	private Vector theElements;
+	private boolean keepStatus;
+	private String selectedElementValue;
+	private Script script;
+	private final static String untitled = "untitled";
 
-public DropdownMenu(Collection entityList){
-
-  this(untitled);
-
-  addMenuElements(entityList);
-
-}
-
-
-
-public DropdownMenu(IDOLegacyEntity[] entity){
-
-	//super();
-
-	//setName(untitled);
-
-	initialize();
-
-	//setAttribute("CLASS","select");
-
-
-
-	if(entity != null){
-
-		if(entity.length > 0){
-
-			setName(entity[0].getEntityName());
-
-      for (int i=0;i<entity.length;i++){
-
-	      //if(entity[i].getID() != -1 && entity[i].getName() != null){
-
-	  addMenuElement(entity[i].getID(),entity[i].getName());
-
-	      //}
-
-      }
-
-		}
-
-    else{
-
-      setName(untitled);
-
-    }
-
+	public DropdownMenu() {
+		this(untitled);
 	}
 
-  else{
-
-    setName(untitled);
-
-  }
-
-}
-
-
-
-public DropdownMenu(IDOLegacyEntity[] entity, String Name){
-
-  this(entity);
-
-  setName(Name);
-
-}
-
-
-
-public void initialize(){
-
-	this.theElements = new Vector(10);
-
-	this.keepStatus = false;
-
-}
-
-
-
-/**
-
- * Populates the dropdownMenu from a List of IDOLegacyEntity objects with a custom name
-
- */
-
-public DropdownMenu(Collection entityList, String Name){
-
-  this(entityList);
-
-  setName(Name);
-
-}
-
-
-
-public void removeElements(){
-
-  theElements.clear();
-
-}
-
-
-
-
-
-public void addMenuElementFirst(String value,String DisplayString){
-
-	theElements.add(0,new MenuElement(DisplayString,value));
-
-}
-
-
-
-//Here is a possible bug, if there are many elements with the same value
-
-public void addMenuElement(String value, String DisplayString){
-
-	theElements.addElement( new MenuElement(DisplayString,value));
-
-}
-
-
-
-public void addMenuElement(int value,String DisplayString){
-
-	addMenuElement(Integer.toString(value),DisplayString);
-
-}
-
-
-
-public void addMenuElement(String value){
-
-	theElements.addElement(new MenuElement(value,value));
-
-}
-
-
-
-public void setMenuElementDisplayString(String elementValue,String displayString){
-
-  MenuElement element = getMenuElement(elementValue);
-
-  element.setName(displayString);
-
-}
-
-
-
-/**
-
-* Add menu elements from an Collection of IDOLegacyEntity Objects
-
-*/
-
-public void addMenuElements(Collection entityList){
-
-	if(entityList != null){
-
-    int length = entityList.size();
-
-    IDOLegacyEntity entity = null;
-
-    Iterator iter = entityList.iterator();
-
-    while(iter.hasNext()){
-
-       entity = (IDOLegacyEntity) iter.next();
-
-       addMenuElement(entity.getID(),entity.getName());
-
-    }
-
-    if(getName().equals(untitled) && entity!=null)
-
-      setName(entity.getEntityName());
-
+	public DropdownMenu(String Name) {
+		setName(Name);
+		initialize();
 	}
 
-}
-
-
-
-public void addSeparator(){
-
-	theElements.addElement( new MenuElement("----------------------------",""));
-
-}
-
-
-
-public void setAttributeToElement(String ElementValue,String AttributeName, String AttributeValue){
-
-	getMenuElement(ElementValue).setAttribute(AttributeName,AttributeValue);
-
-}
-
-
-
-public void addDisabledMenuElement(String Value, String DisplayString){
-
-	addMenuElement(Value,DisplayString);
-
-	setDisabled(Value);
-
-}
-
-
-
-public void setDisabled(String ElementValue){
-
-	getMenuElement(ElementValue).setDisabled(true);
-
-}
-
-
-
-private void deselectElements(){
-
-  Iterator iter = theElements.iterator();
-
-  while(iter.hasNext()){
-
-    ((MenuElement)iter.next()).setSelected(false);
-
-  }
-
-}
-
-
-
-public void setSelectedElement(String ElementValue){
-
-	deselectElements();
-
-	getMenuElement(ElementValue).setSelected(true);
-
-	selectedElementValue=ElementValue;
-
-}
-
-
-
-public String getSelectedElementValue(){
-
-	if (selectedElementValue == null){
-
-		return "";
-
+	/**
+	 * Populates the dropdownMenu from a List of IDOLegacyEntity objects
+	 */
+	public DropdownMenu(Collection entityList) {
+		this(untitled);
+		addMenuElements(entityList);
 	}
 
-	else{
-
-		return selectedElementValue;
-
-	}
-
-}
-
-
-
-
-
-public void keepStatusOnAction(){
-
-	this.keepStatus=true;
-
-}
-
-
-
-public void setToGoToURL(){
-
-	this.setOnChange("location.href=this.form."+getName()+".options[this.form."+getName()+".selectedIndex].value");
-
-}
-
-
-
-/**
-
-
-
- * Sets the dropdown to submit automatically.
-
-
-
- * Must add to a form before this function is used!!!!
-
-
-
- */
-
-public void setToSubmit(){
-
-	this.setOnChange("this.form.submit()");
-
-}
-
-
-
-public void setOnSelect(String onValue,DropdownMenu menuToChange,String valueToChangeToInMenu){
-
-  Script script = this.getAssociatedScript();
-
-  String functionName="connectDropdown";
-
-  if(script.doesFunctionExist(functionName)){
-
-    script.addFunction(functionName,"function "+functionName+"(){\r \r}");
-
-    this.setOnChange(functionName+"()");
-
-  }
-
-  script.addToFunction(functionName,"this.form."+menuToChange.getName()+".value=this.form."+getName()+".options[this.form."+getName()+".selectedIndex].value;\r");
-
-}
-
-
-
-//Returns the first menuelement in the menu if there is no match
-
-private MenuElement getMenuElement(String ElementValue){
-
-
-
-	MenuElement theReturn = new MenuElement();
-
-
-
-  Iterator iter = theElements.iterator();
-
-  while(iter.hasNext()){
-
-    MenuElement tempobj = (MenuElement)  iter.next();
-
-		if (tempobj.getValue().equals(ElementValue)){
-
-			theReturn=tempobj;
-
-		}
-
-  }
-
-	return theReturn;
-
-}
-
-
-
-public void setStyle(String style) {
-
-  setAttribute("class",style);
-
-}
-
-
-
-public void print(IWContext iwc)throws Exception{
-
-
-
-	theElements.trimToSize();
-
-
-	//if ( doPrint(iwc) ){
-
-    if(script!=null){
-
-      ((PresentationObjectContainer)getParentObject()).add(script);
-
-    }
-
-		if (getLanguage().equals("HTML")){
-
-
-
-			if (this.keepStatus==true){
-
-				if(iwc.getParameter(getName()) != null){
-
-					setSelectedElement(iwc.getParameter(getName()));
-
+	public DropdownMenu(IDOLegacyEntity[] entity) {
+		initialize();
+		if (entity != null) {
+			if (entity.length > 0) {
+				setName(entity[0].getEntityName());
+				for (int i = 0; i < entity.length; i++) {
+					addMenuElement(entity[i].getID(), entity[i].getName());
 				}
-
 			}
-
-
-
-			if (getInterfaceStyle().equals("default")){
-
-				println("<select name=\""+getName()+"\" "+getAttributeString()+" >");
-
-
-
-	Iterator iter = theElements.iterator();
-
-	while(iter.hasNext()){
-
-	  MenuElement tempobj = (MenuElement)  iter.next();
-
-					tempobj._print(iwc);
-
+			else {
+				setName(untitled);
+			}
+		}
+		else {
+			setName(untitled);
+		}
 	}
 
-				println("</select>");
+	public DropdownMenu(IDOLegacyEntity[] entity, String Name) {
+		this(entity);
+		setName(Name);
+	}
 
+	public void initialize() {
+		this.theElements = new Vector(10);
+		this.keepStatus = false;
+	}
+
+	/**	
+	 * Populates the dropdownMenu from a List of IDOLegacyEntity objects with a custom name	
+	 */
+	public DropdownMenu(Collection entityList, String Name) {
+		this(entityList);
+		setName(Name);
+	}
+
+	public void removeElements() {
+		theElements.clear();
+	}
+
+	public void addMenuElementFirst(String value, String DisplayString) {
+		theElements.add(0, new MenuElement(DisplayString, value));
+	}
+
+	//Here is a possible bug, if there are many elements with the same value
+	public void addMenuElement(String value, String DisplayString) {
+		theElements.addElement(new MenuElement(DisplayString, value));
+	}
+
+	public void addMenuElement(int value, String DisplayString) {
+		addMenuElement(Integer.toString(value), DisplayString);
+	}
+
+	public void addMenuElement(String value) {
+		theElements.addElement(new MenuElement(value, value));
+	}
+
+	public void setMenuElementDisplayString(String elementValue, String displayString) {
+		MenuElement element = getMenuElement(elementValue);
+		element.setName(displayString);
+	}
+
+	/**	
+	* Add menu elements from an Collection of IDOLegacyEntity Objects
+	*/
+	public void addMenuElements(Collection entityList) {
+		if (entityList != null) {
+			int length = entityList.size();
+			IDOLegacyEntity entity = null;
+			Iterator iter = entityList.iterator();
+			while (iter.hasNext()) {
+				entity = (IDOLegacyEntity) iter.next();
+				addMenuElement(entity.getID(), entity.getName());
 			}
-
+			if (getName().equals(untitled) && entity != null)
+				setName(entity.getEntityName());
 		}
+	}
 
+	public void addSeparator() {
+		theElements.addElement(new MenuElement("----------------------------", ""));
+	}
 
+	public void setAttributeToElement(String ElementValue, String AttributeName, String AttributeValue) {
+		getMenuElement(ElementValue).setAttribute(AttributeName, AttributeValue);
+	}
 
-		else if (getLanguage().equals("WML")){
+	public void addDisabledMenuElement(String Value, String DisplayString) {
+		addMenuElement(Value, DisplayString);
+		setDisabled(Value);
+	}
 
+	public void setDisabled(String ElementValue) {
+		getMenuElement(ElementValue).setDisabled(true);
+	}
 
+	private void deselectElements() {
+		Iterator iter = theElements.iterator();
+		while (iter.hasNext()) {
+			((MenuElement) iter.next()).setSelected(false);
+		}
+	}
 
-			if (this.keepStatus==true){
+	public void setSelectedElement(String ElementValue) {
+		deselectElements();
+		getMenuElement(ElementValue).setSelected(true);
+		selectedElementValue = ElementValue;
+	}
 
-				if(iwc.getParameter(getName()) != null){
+	public String getSelectedElementValue() {
+		if (selectedElementValue == null) {
+			return "";
+		}
+		else {
+			return selectedElementValue;
+		}
+	}
 
+	public void keepStatusOnAction() {
+		this.keepStatus = true;
+	}
+
+	public void setToGoToURL() {
+		this.setOnChange("location.href=this.form." + getName() + ".options[this.form." + getName() + ".selectedIndex].value");
+	}
+
+	/**
+	 * Sets the dropdown to submit automatically.
+	 * Must add to a form before this function is used!!!!
+	 */
+	public void setToSubmit() {
+		this.setOnChange("this.form.submit()");
+	}
+
+	public void setOnSelect(String onValue, DropdownMenu menuToChange, String valueToChangeToInMenu) {
+		Script script = this.getAssociatedScript();
+		String functionName = "connectDropdown";
+		if (script.doesFunctionExist(functionName)) {
+			script.addFunction(functionName, "function " + functionName + "(){\r \r}");
+			this.setOnChange(functionName + "()");
+		}
+		script.addToFunction(
+			functionName,
+			"this.form." + menuToChange.getName() + ".value=this.form." + getName() + ".options[this.form." + getName() + ".selectedIndex].value;\r");
+	}
+
+	//Returns the first menuelement in the menu if there is no match
+	private MenuElement getMenuElement(String ElementValue) {
+		MenuElement theReturn = new MenuElement();
+		Iterator iter = theElements.iterator();
+		while (iter.hasNext()) {
+			MenuElement tempobj = (MenuElement) iter.next();
+			if (tempobj.getValue().equals(ElementValue)) {
+				theReturn = tempobj;
+			}
+		}
+		return theReturn;
+	}
+
+	public void setStyle(String style) {
+		setAttribute("class", style);
+	}
+
+	public void print(IWContext iwc) throws Exception {
+		theElements.trimToSize();
+		if (script != null) {
+			((PresentationObjectContainer) getParentObject()).add(script);
+		}
+		if (getLanguage().equals("HTML")) {
+			if (this.keepStatus == true) {
+				if (iwc.getParameter(getName()) != null) {
 					setSelectedElement(iwc.getParameter(getName()));
-
 				}
-
 			}
-
-
-
-			if (getInterfaceStyle().equals("default")){
-
-				println("<select name=\""+getName()+"\" "+getAttributeString()+" >");
-
-
-
-
-
-	Iterator iter = theElements.iterator();
-
-	while(iter.hasNext()){
-
-	  MenuElement tempobj = (MenuElement)  iter.next();
-
+			if (getInterfaceStyle().equals("default")) {
+				println("<select name=\"" + getName() + "\" " + getAttributeString() + " >");
+				Iterator iter = theElements.iterator();
+				while (iter.hasNext()) {
+					MenuElement tempobj = (MenuElement) iter.next();
 					tempobj._print(iwc);
-
+				}
+				println("</select>");
+			}
+		}
+		else if (getLanguage().equals("WML")) {
+			if (this.keepStatus == true) {
+				if (iwc.getParameter(getName()) != null) {
+					setSelectedElement(iwc.getParameter(getName()));
+				}
+			}
+			if (getInterfaceStyle().equals("default")) {
+				println("<select name=\"" + getName() + "\" " + getAttributeString() + " >");
+				Iterator iter = theElements.iterator();
+				while (iter.hasNext()) {
+					MenuElement tempobj = (MenuElement) iter.next();
+					tempobj._print(iwc);
+				}
+				println("</select>");
+			}
+		}
 	}
 
-				println("</select>");
-
+	public Object clone() {
+		DropdownMenu obj = null;
+		try {
+			obj = (DropdownMenu) super.clone();
+			if (this.theElements != null) {
+				obj.theElements = (Vector) this.theElements.clone();
+				java.util.ListIterator iter = obj.theElements.listIterator();
+				while (iter.hasNext()) {
+					int index = iter.nextIndex();
+					Object item = iter.next();
+					if (item instanceof MenuElement) {
+						obj.theElements.set(index, ((MenuElement) item).clone());
+					}
+				}
 			}
+			obj.keepStatus = this.keepStatus;
+			obj.selectedElementValue = this.selectedElementValue;
+			if (this.script != null) {
+				obj.script = (Script) this.script.clone();
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace(System.err);
+		}
+		return obj;
+	}
 
+	public void setMenuElements(List elements) {
+		removeElements();
+
+		if (elements != null) {
+			Iterator it = elements.iterator();
+			while (it.hasNext()) {
+				String item = (String) it.next();
+				StringTokenizer tok = new StringTokenizer(item, ",");
+				String name = null;
+				String value = null;
+				String selected = null;
+				try {
+					name = tok.nextToken();
+					value = tok.nextToken();
+					while (name.startsWith(" "))
+						name = name.substring(1);
+//					debug("name = " + name);
+//					debug("value = " + value);
+					addMenuElement(value, name);
+					if (tok.hasMoreTokens())
+						selected = tok.nextToken();
+						
+					if (selected != null && selected.equals("selected"))
+						setSelectedElement(value);					
+				}
+				catch (NoSuchElementException e) {
+//					debug("Err: name = " + name);
+//					debug("Err: value = " + value);
+					if (value == null) {
+						if (name != null) { 
+							while (name.startsWith(" "))
+								name = name.substring(1);
+							if (name.equals("delim"))
+								addSeparator();
+							else if (!name.equals(""))
+								addMenuElement("",name);
+						}
+					}
+				}
+			}
+		}
+	}
+  
+	/**
+	 * Overrides the dropdown menu with the list of name/value pairs contained in the String.
+	 * 
+	 * @param elements A comma seperated list of name/value pairs, each pair seperated by a ;.
+	 */
+	public void setMenuElements(String elements) {
+		Vector vec = new Vector();
+
+		if (elements != null && !elements.equals("")) {
+			elements = elements.replace('\n', ' ');
+			StringTokenizer tok = new StringTokenizer(elements, ";");
+			while (tok.hasMoreTokens()) {
+				String element = (String) tok.nextToken();
+				if (element != null)
+					vec.add(element);
+			}
 		}
 
-
-
-	//}
-
-}
-
-
-
-
-
-  public Object clone() {
-
-    DropdownMenu obj = null;
-
-    try {
-
-      obj = (DropdownMenu)super.clone();
-
-
-
-      if(this.theElements != null){
-
-	obj.theElements = (Vector)this.theElements.clone();
-
-	java.util.ListIterator iter = obj.theElements.listIterator();
-
-	while (iter.hasNext()) {
-
-	  int index = iter.nextIndex();
-
-	  Object item = iter.next();
-
-	  if (item instanceof MenuElement) {
-
-	    obj.theElements.set(index,((MenuElement)item).clone());
-
-	  }
-
+		setMenuElements(vec);
 	}
-
-      }
-
-
-
-      obj.keepStatus = this.keepStatus;
-
-      obj.selectedElementValue = this.selectedElementValue;
-
-      if(this.script != null){
-
-	obj.script = (Script)this.script.clone();
-
-      }
-
-    }
-
-    catch(Exception ex) {
-
-      ex.printStackTrace(System.err);
-
-    }
-
-
-
-    return obj;
-
-  }
-
 }
-
-
-
--- src/java/com/idega/presentation/ui/MenuElement.java
+++ src/java/com/idega/presentation/ui/MenuElement.java
@@ -1,213 +1,102 @@
-//idega 2000 - Tryggvi Larusson
-
 /*
-
-*Copyright 2000 idega.is All Rights Reserved.
-
-*/
-
-
-
+ * $Id: MenuElement.java,v 1.5 2002/07/22 10:10:41 palli Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
 package com.idega.presentation.ui;
 
-
-
-import java.io.*;
-
-import java.util.*;
-
-import com.idega.presentation.*;
-
-
+import com.idega.presentation.IWContext;
+import java.io.IOException;
 
 /**
-
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-
-*@version 1.2
-
-*/
-
-public class MenuElement extends InterfaceObject{
-
-
-
-  private boolean isDisabled;
-
-  private boolean isSelected;
-
-
-
-  private static String emptyString = "";
-
-
-
-  public MenuElement(){
-
-          this("untitled");
-
-  }
-
-
-
-  public MenuElement(String ElementName){
-
-          this(ElementName,ElementName);
-
-  }
-
-
-
-  public MenuElement(String ElementName,String ElementValue){
-
-          super();
-
-          setName(ElementName);
-
-          setValue(ElementValue);
-
-          setSelected(false);
-
-          setDisabled(false);
-
-  }
-
-
-
-  public void setElementName(String ElementName){
-
-          setName(ElementName);
-
-  }
-
-
-
-  public void setElementValue(String ElementValue){
-
-          setValue(ElementValue);
-
-  }
-
-
-
-  public boolean isSelected(){
-
-          return isSelected;
-
-  }
-
-
-
-  public boolean isDisabled(){
-
-          return isDisabled;
-
-  }
-
-
-
-  public void setSelected(boolean ifSelected){
-
-          isSelected=ifSelected;
-
-  }
-
-
-
-  public void setDisabled(boolean ifDisabled){
-
-          isDisabled = ifDisabled;
-
-  }
-
-
-
-  public String getElementValue(){
-
-          return getValue();
-
-  }
-
-
-
-  public String getElementName(){
-
-          return getName();
-
-  }
-
-
-
-  public void print(IWContext iwc)throws IOException{
-
-
-          //if ( doPrint(iwc) ){
-
-            if (getLanguage().equals("HTML")){
-
-                    String disabledString = emptyString;
-
-                    String selectedString = emptyString;
-
-                    if (isSelected()){
-
-                            selectedString = "selected";
-
-                    }
-
-                    if (isDisabled()){
-
-                            disabledString = "disabled";
-
-                    }
-
-                            //if (getInterfaceStyle().equals("default"))
-
-                                    print("<option name=\""+getName()+"\" "+getAttributeString()+" "+disabledString+" "+selectedString+" >");
-
-                                    print(getName());
-
-                                    print("</option>");
-
-                            // }
-
-                    }
-
-
-
-            else if (getLanguage().equals("WML")){
-
-                    print("<option name=\""+getName()+"\" value=\""+getValue()+"\" >");
-
-                    print(getName());
-
-                    print("</option>");
-
-            }
-
-          //}
-
-  }
-
-
-
-  public synchronized Object clone() {
-
-    MenuElement obj = null;
-
-      obj = (MenuElement)super.clone();
-
-      obj.isDisabled = isDisabled;
-
-      obj.isSelected = isSelected;
-
-
-
-    return obj;
-
-  }
-
-
-
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.2
+ */
+public class MenuElement extends InterfaceObject {
+	private boolean isDisabled;
+	private boolean isSelected;
+
+	private static String emptyString = "";
+
+	public MenuElement() {
+		this("untitled");
+	}
+
+	public MenuElement(String ElementName) {
+		this(ElementName, ElementName);
+	}
+
+	public MenuElement(String ElementName, String ElementValue) {
+		super();
+		setName(ElementName);
+		setValue(ElementValue);
+		setSelected(false);
+		setDisabled(false);
+	}
+
+	public void setElementName(String ElementName) {
+		setName(ElementName);
+	}
+
+	public void setElementValue(String ElementValue) {
+		setValue(ElementValue);
+	}
+
+	public boolean isSelected() {
+		return isSelected;
+	}
+
+	public boolean isDisabled() {
+		return isDisabled;
+	}
+
+	public void setSelected(boolean ifSelected) {
+		isSelected = ifSelected;
+	}
+
+	public void setDisabled(boolean ifDisabled) {
+		isDisabled = ifDisabled;
+	}
+
+	public String getElementValue() {
+		return getValue();
+	}
+
+	public String getElementName() {
+		return getName();
+	}
+
+	public void print(IWContext iwc) throws IOException {
+		if (getLanguage().equals("HTML")) {
+			String disabledString = emptyString;
+			String selectedString = emptyString;
+			if (isSelected()) {
+				selectedString = "selected";
+			}
+			if (isDisabled()) {
+				disabledString = "disabled";
+			}
+			print("<option name=\"" + getName() + "\" " + getAttributeString() + " " + disabledString + " " + selectedString + " >");
+			print(getName());
+			print("</option>");
+		}
+		else if (getLanguage().equals("WML")) {
+			print("<option name=\"" + getName() + "\" value=\"" + getValue() + "\" >");
+			print(getName());
+			print("</option>");
+		}
+	}
+
+	public synchronized Object clone() {
+		MenuElement obj = null;
+		obj = (MenuElement) super.clone();
+		obj.isDisabled = isDisabled;
+		obj.isSelected = isSelected;
+
+		return obj;
+	}
 }
-
