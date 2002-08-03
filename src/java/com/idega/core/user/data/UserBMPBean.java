10e3ad8d122da0d0932c8ad1484cf8d3cfe4f549
Thorhallur Helgason <laddi@idega.com>
2002-08-03 12:20:04 +0000

    *** empty log message ***

--- src/java/com/idega/core/user/data/UserBMPBean.java
+++ src/java/com/idega/core/user/data/UserBMPBean.java
@@ -1,6 +1,7 @@
 package com.idega.core.user.data;
 
 import com.idega.data.*;
+import com.idega.user.data.User;
 import com.idega.core.data.*;
 import com.idega.core.accesscontrol.data.PermissionGroup;
 import java.util.List;
@@ -19,7 +20,7 @@ import java.sql.Date;
 /**
  * @deprecated Replaced with com.idega.user.data.UserBMPBean
  */
-public class UserBMPBean extends com.idega.user.data.UserBMPBean implements com.idega.core.user.data.User {
+public class UserBMPBean extends com.idega.user.data.UserBMPBean implements com.idega.core.user.data.User,User {
 
 }
 
