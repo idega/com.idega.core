129f59ee3cdf23613badf9cdca05f5febb8f71bc
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-08-13 19:16:33 +0000

    initial implementation for Nacka project

---
+++ src/java/com/idega/util/CustomDateFormat.java
@@ -0,0 +1,64 @@
+package com.idega.util;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+/**
+ * Title:        A class for formatting presentation of PersonalIDs
+ * Description:
+ * Copyright:    Copyright (c) 2002
+ * Company:      idega
+ * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
+ */
+public class CustomDateFormat {
+	private static String DASH = "-";
+	private static DateFormat swedishDateFormat;
+	private CustomDateFormat() {
+	}
+	private static DateFormat getSwedishDateTimeFormat(){
+		if(swedishDateFormat==null){
+			swedishDateFormat= new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		}
+		return swedishDateFormat;
+	}
+	/**
+	 * Accepts a java.util.Date object
+	 * @return A formatted version of the date with time
+	 **/
+	public static String formatDateTime(Date date, Locale locale) {
+		return getDateTimeFormatIntance(locale).format(date);
+	}
+	/**
+	 * Accepts an input Locale
+	 * @return A default DateFormat instance for the locale
+	 **/
+	public static DateFormat getDateTimeFormatIntance(Locale locale){
+		if (locale.equals(LocaleUtil.getSwedishLocale())) {
+			return getSwedishDateTimeFormat();
+		}
+		else{
+			return DateFormat.getDateTimeInstance(2, 2, locale);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		test(args);
+	}
+	
+	public static void test(String[] args) {
+		String localeString = null;
+		try {
+			localeString = args[0];
+		} catch (RuntimeException rme) {
+		}
+		if (localeString == null) {
+			localeString = "sv_SE";
+		}
+		System.out.println(
+			"Output: "
+				+ formatDateTime(new Date(), LocaleUtil.getLocale(localeString)));
+	}
+}
