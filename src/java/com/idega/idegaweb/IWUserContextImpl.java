b9a32a68cc319e9a71f9f1d41b3f9a777ea6a5fb
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-08-15 16:07:48 +0000

    initial implementation

---
+++ src/java/com/idega/idegaweb/IWUserContextImpl.java
@@ -0,0 +1,32 @@
+//idega 2001 - Tryggvi Larusson
+/*
+*Copyright 2002 idega.is All Rights Reserved.
+*/
+
+package com.idega.idegaweb;
+
+import javax.servlet.http.HttpSession;
+
+import com.idega.presentation.IWContext;
+
+
+
+/**
+ * Title:        A default implementation of IWUserContext
+ * Description:
+ * Copyright:    Copyright (c) 2002
+ * Company:      idega
+ * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
+ */
+public class IWUserContextImpl extends IWContext implements IWUserContext{
+	private HttpSession session;
+
+	public IWUserContextImpl(HttpSession session){
+		this.session=session;	
+	}
+	
+	public HttpSession getSession(){
+		return session;	
+	}
+}
