edac3c83fca3737eb92e1d0aaa38afb7eb355d75
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-08-29 14:20:43 +0000

    Fix of insert() for legacy compatability with new User system

--- src/java/com/idega/core/user/data/UserBMPBean.java
+++ src/java/com/idega/core/user/data/UserBMPBean.java
@@ -22,5 +22,43 @@ import java.sql.Date;
  */
 public class UserBMPBean extends com.idega.user.data.UserBMPBean implements com.idega.core.user.data.User,User {
 
+	/**
+	 * Overriding insert to invoke store() for backwards compatability
+	 **/
+	public void insert()throws SQLException{
+		try{
+			store();	
+		}
+		catch(Exception e){
+			throw new SQLException("Error inserting for old UserBMPBean. Message was: "+e.getMessage());
+		}
+	}
+	
+	
+	
+	/**
+	 * Overriding store to avoid circular invokation of store() and insert()
+	 **/
+	public void store() throws IDOStoreException
+	{
+		try
+		{
+			if ((getEntityState() == STATE_NEW) || (getEntityState() == STATE_NEW_AND_NOT_IN_SYNCH_WITH_DATASTORE))
+			{
+				super.insert();
+			}
+			else if (this.getEntityState() == STATE_NOT_IN_SYNCH_WITH_DATASTORE)
+			{
+				update();
+			}
+		}
+		catch (Exception e)
+		{
+			//e.printStackTrace();
+			throw new IDOStoreException(e.getMessage());
+		}
+	}
+	
+	
 }
 
