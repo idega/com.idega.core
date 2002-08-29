4199097765e12c906debd9892278bd692d686029
Thorhallur Helgason <laddi@idega.com>
2002-08-29 18:29:25 +0000

    Minor changes, added default constructor and removed all static methods

--- src/java/com/idega/idegaweb/IWStyleManager.java
+++ src/java/com/idega/idegaweb/IWStyleManager.java
@@ -10,6 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
@@ -24,87 +25,105 @@ import java.util.Vector;
 
 public class IWStyleManager {
 
+	public IWStyleManager(IWMainApplication application) {
+		this.application = application;
+	}
+	
+	public IWStyleManager() {
+		this(null);
+	}
+	
+	public IWMainApplication application;
 	public static HashMap map;
 	public static File file;
 	public static final String[] defaultStyles = { "A", "A:hover", "body", "table", "form" };
 
-	public static void getStyleSheet(IWMainApplication app) {
-		boolean createNew = false;
-		String URL = app.getApplicationRealPath() + "style" + FileUtil.getFileSeparator() + "style.css";
-		Vector vector = null;
-
-		try {
-			file = FileUtil.getFileAndCreateRecursiveIfNotExists(URL);
-		}
-		catch (IOException e) {
-			e.printStackTrace(System.err);
-		}
-
-		if (file != null) {
+	public void getStyleSheet() {
+		if ( application != null ) {
+			boolean createNew = false;
+			String URL = application.getApplicationRealPath() + "style" + FileUtil.getFileSeparator() + "style.css";
+			Vector vector = null;
+	
 			try {
-				vector = FileUtil.getLinesFromFile(file);
+				file = FileUtil.getFileAndCreateRecursiveIfNotExists(URL);
 			}
 			catch (IOException e) {
-				vector = null;
+				e.printStackTrace(System.err);
 			}
-
-			if (vector != null) {
-				if (vector.size() == 0) {
-					addDefaultValues();
+	
+			if (file != null) {
+				try {
+					vector = FileUtil.getLinesFromFile(file);
 				}
-				else {
-					getStylesFromFile(vector);
+				catch (IOException e) {
+					vector = null;
+				}
+	
+				if (vector != null) {
+					if (vector.size() == 0) {
+						addDefaultValues();
+					}
+					else {
+						getStylesFromFile(vector);
+					}
 				}
 			}
 		}
 	}
 
-	public static void addDefaultValues() {
-		addStyleParameter(defaultStyles[0], IWConstants.LINK_STYLE);
-		addStyleParameter(defaultStyles[1], IWConstants.LINK_HOVER_STYLE);
-		addStyleParameter(defaultStyles[2], IWConstants.BODY_STYLE);
-		addStyleParameter(defaultStyles[3], IWConstants.BODY_STYLE);
-		addStyleParameter(defaultStyles[4], IWConstants.FORM_STYLE);
+	public void addDefaultValues() {
+		setStyle(defaultStyles[0], IWConstants.LINK_STYLE);
+		setStyle(defaultStyles[1], IWConstants.LINK_HOVER_STYLE);
+		setStyle(defaultStyles[2], IWConstants.BODY_STYLE);
+		setStyle(defaultStyles[3], IWConstants.BODY_STYLE);
+		setStyle(defaultStyles[4], IWConstants.FORM_STYLE);
 		writeStyleSheet();
 	}
 
-	public static void addStyleParameter(String name, String style) {
-		if (map == null)
-			map = new HashMap();
-
-		map.put(name, style);
-		writeStyleSheet();
+	public void setStyle(String name, String style) {
+		if ( name.length() > 0 ) {
+			getStyleMap().put(name, style);
+			writeStyleSheet();
+		}
+	}
+	
+	public String getStyle(String name) {
+		return (String) getStyleMap().get(name);
+	}
+	
+	public void removeStyle(String name) {
+		getStyleMap().remove(name);
+		writeStyleSheet();	
 	}
 
-	public static void getStylesFromFile(Vector vector) {
+	public void getStylesFromFile(Vector vector) {
 		Iterator iter = vector.iterator();
 		StringTokenizer tokenizer;
 		while (iter.hasNext()) {
 			tokenizer = new StringTokenizer((String) iter.next(), "{");
 			while (tokenizer.hasMoreTokens()) {
-				/** @todo  not working in jdk 1.3.1 : */
-				//addStyleParameter(tokenizer.nextToken().replaceAll(" ",""),tokenizer.nextToken().replaceAll("}","").replaceAll(" ",""));
-
-				/** Fix for jdk 1.3.1 */
 				String styleName = TextSoap.findAndReplace(tokenizer.nextToken(), " ", "");
+				styleName = TextSoap.findAndReplace(styleName, ".", "");
 				String styleParameter = TextSoap.findAndReplace(tokenizer.nextToken(), "}", "");
 				styleParameter = TextSoap.findAndReplace(styleParameter, " ", "");
-				addStyleParameter(styleName, styleParameter);
+				setStyle(styleName, styleParameter);
 			}
 		}
 	}
 
-	public static void writeStyleSheet() {
+	public void writeStyleSheet() {
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			Iterator iter = map.keySet().iterator();
+			Iterator iter = getStyleMap().keySet().iterator();
 			while (iter.hasNext()) {
 				String name = (String) iter.next();
-				String style = (String) map.get(name);
+				String style = getStyle(name);
 				int length = name.length() + style.length() + 5;
 				String writeString = name + " { " + style + " }";
-				if ( !isDefaultStyle(name) )
+				if ( !isDefaultStyle(name) && name.indexOf(".") == -1 ) {
 					writeString = "." + writeString; 
+					length++;
+				}
 
 				writer.write(writeString, 0, length);
 				writer.newLine();
@@ -117,37 +136,32 @@ public class IWStyleManager {
 		}
 	}
 
-	public static HashMap getStyleMap() {
+	private HashMap getStyleMap() {
+		if ( map == null )
+			map = new HashMap();	
 		return map;
 	}
 
-	public static DropdownMenu getStyleDropdown(String parameterName) {
-		DropdownMenu menu = new DropdownMenu(parameterName);
-
+	public List getStyleList() {
+		List list = new Vector();
 		if (map != null) {
-			Iterator iter = map.keySet().iterator();
+			Iterator iter = getStyleMap().keySet().iterator();
 			while (iter.hasNext()) {
 				String name = (String) iter.next();
-				String value = name;
-
 				if (name.length() > 0 && name.indexOf(":") == -1) {
 					if (name.indexOf(".") != -1) {
 						name = name.substring(name.indexOf(".") + 1);
 					}
-					if (value.indexOf(".") != -1 && value.indexOf(".") == 0) {
-						value = value.substring(value.indexOf(".") + 1);
-					}
 
 					if (!isDefaultStyle(name))
-						menu.addMenuElement(name, value);
+						list.add(name);
 				}
 			}
 		}
-
-		return menu;
+		return list;
 	}
 
-	public static boolean isDefaultStyle(String styleName) {
+	private boolean isDefaultStyle(String styleName) {
 		for (int a = 0; a < defaultStyles.length; a++) {
 			if (defaultStyles[a].equalsIgnoreCase(styleName))
 				return true;
