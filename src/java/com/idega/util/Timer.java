af85f387e83ac3a718237aa928f1a960f250acbe
Eirikur Hrafnsson <eiki@idega.com>
2002-09-12 23:18:49 +0000

    measures time in milliseconds...how handy

---
+++ src/java/com/idega/util/Timer.java
@@ -0,0 +1,19 @@
+package com.idega.util;
+
+/**
+ * <p>Title: Timer</p>
+ * <p>Description: A simple "stopwatch" class with millisecond accuracy</p>
+ * <p>Copyright (c) 2002</p>
+ * <p>Company: Idega Software</p>
+ * @author <a href="mailto:eiki@idega.is"> Eirikur Sveinn Hrafnsson</a>
+ * @version 1.0
+ */
+
+public class Timer {
+
+  private long startTime, endTime;
+
+  public void start()   {  startTime = System.currentTimeMillis();       }
+  public void stop()    {  endTime   = System.currentTimeMillis();       }
+  public long getTime() {  return endTime - startTime;                   }
+}
