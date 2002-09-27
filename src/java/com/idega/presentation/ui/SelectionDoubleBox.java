967e12c5ad16d3577495ec28862648abbe179aab
Thorhallur Helgason <laddi@idega.com>
2002-09-27 13:16:37 +0000

    Added a clone method

--- src/java/com/idega/presentation/ui/SelectionDoubleBox.java
+++ src/java/com/idega/presentation/ui/SelectionDoubleBox.java
@@ -5,109 +5,130 @@
 
 package com.idega.presentation.ui;
 
-
 import com.idega.presentation.*;
-
 
 /**
 *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
 *@version 1.0
 */
-public class SelectionDoubleBox extends InterfaceObjectContainer{
+public class SelectionDoubleBox extends InterfaceObjectContainer {
 
+	private SelectionBox leftBox;
+	private SelectionBox rightBox;
+	private GenericButton toTheRight;
+	private GenericButton toTheLeft;
 
-private SelectionBox leftBox;
-private SelectionBox rightBox;
-private GenericButton toTheRight;
-private GenericButton toTheLeft;
+	public SelectionDoubleBox() {
+		this("untitled");
+	}
 
-public SelectionDoubleBox(){
-	this("untitled");
-}
+	public SelectionDoubleBox(String nameOfRightBox) {
+		this(nameOfRightBox + "_left", nameOfRightBox);
+	}
 
-public SelectionDoubleBox(String nameOfRightBox){
-  this(nameOfRightBox+"_left",nameOfRightBox);
-}
+	public SelectionDoubleBox(String nameOfRightBox, String headerOfLeftBox, String headerOfRightBox) {
+		this(nameOfRightBox + "_left", nameOfRightBox);
+		this.getLeftBox().setTextHeading(headerOfLeftBox);
+		this.getRightBox().setTextHeading(headerOfRightBox);
+	}
 
-public SelectionDoubleBox(String nameOfRightBox,String headerOfLeftBox,String headerOfRightBox){
-  this(nameOfRightBox+"_left",nameOfRightBox);
-  this.getLeftBox().setTextHeading(headerOfLeftBox);
-  this.getRightBox().setTextHeading(headerOfRightBox);
-}
+	public SelectionDoubleBox(String nameOfLeftBox, String nameOfRightBox) {
+		leftBox = new SelectionBox(nameOfLeftBox);
+		rightBox = new SelectionBox(nameOfRightBox);
+		toTheRight = new GenericButton("sdb_right", ">>");
+		toTheLeft = new GenericButton("sdb_left", "<<");
+	}
 
+	public void main(IWContext iwc) throws Exception {
+		//this.getParentPage().initializeAssociatedScript();
+		if (getStyleAttribute() != null) {
+			leftBox.setStyleAttribute(getStyleAttribute());
+			rightBox.setStyleAttribute(getStyleAttribute());
+			if (getWidth() != null) {
+				leftBox.setWidth(getWidth());
+				rightBox.setWidth(getWidth());
+			}
+			toTheLeft.setStyleAttribute(getStyleAttribute());
+			toTheRight.setStyleAttribute(getStyleAttribute());
+		}
 
+		Table table = new Table(3, 1);
+		add(table);
 
-public SelectionDoubleBox(String nameOfLeftBox,String nameOfRightBox){
-    leftBox = new SelectionBox(nameOfLeftBox);
-    rightBox = new SelectionBox(nameOfRightBox);
-    toTheRight = new GenericButton("sdb_right",">>");
-    toTheLeft = new GenericButton("sdb_left","<<");
-}
+		table.add(leftBox, 1, 1);
 
-public SelectionBox getLeftBox(){
-  return leftBox;
-}
+		toTheRight.setOnClick("move( this.form." + leftBox.getName() + ", this.form." + rightBox.getName() + " )");
+		table.add(toTheRight, 2, 1);
 
-public SelectionBox getRightBox(){
-  return rightBox;
-}
+		table.addBreak(2, 1);
 
-public GenericButton getLeftButton() {
-  return toTheLeft;
-}
+		toTheLeft.setOnClick("move( this.form." + rightBox.getName() + ", this.form." + leftBox.getName() + " )");
+		table.add(toTheLeft, 2, 1);
 
-public GenericButton getRightButton() {
-  return toTheRight;
-}
+		table.add(rightBox, 3, 1);
 
-public void addToScripts(Script script) {
-    script.addFunction("addOpt","function addOpt( list, val, text, idx, selected ) {  if( selected == null ) selected = false;  if( idx != null ) {          list.options[idx] = new Option( text, val, false, selected );  } else {          list.options[list.length] = new Option( text, val, false, selected );  }}");
-    script.addFunction("move","function move( from, to ) {\n  var here =  from ;\n  var there =  to ;\n\n  if( here.selectedIndex != -1 && here.length > 0) {\n\n    for( h=0; h<here.length;h++) {\n      if( here.options[h].selected ) {\n        addOpt( there, here.options[h].value, here.options[h].text );\n      }\n    }\n    for( h1=here.length-1;h1>-1; h1-- ) {\n      if( here.options[h1].selected ) {\n        here.options[h1] = null;\n      }\n    }\n          \n  }\n}");
-}
-public void main(IWContext iwc)throws Exception{
-    //this.getParentPage().initializeAssociatedScript();
-    if ( getStyleAttribute() != null ) {
-    	leftBox.setStyleAttribute(getStyleAttribute());
-    	rightBox.setStyleAttribute(getStyleAttribute());
-	    toTheLeft.setStyleAttribute(getStyleAttribute());
-	    toTheRight.setStyleAttribute(getStyleAttribute());
-    }
+		Script script = this.getParentPage().getAssociatedScript();
+		addToScripts(script);
+	}
 
-    Table table = new Table(3,1);
-    add(table);
+	public SelectionBox getLeftBox() {
+		return leftBox;
+	}
 
-    table.add(leftBox,1,1);
+	public SelectionBox getRightBox() {
+		return rightBox;
+	}
 
-    toTheRight.setOnClick("move( this.form."+leftBox.getName()+", this.form."+rightBox.getName()+" )");
-    table.add(toTheRight,2,1);
+	public GenericButton getLeftButton() {
+		return toTheLeft;
+	}
 
-    table.addBreak(2,1);
+	public GenericButton getRightButton() {
+		return toTheRight;
+	}
 
-    toTheLeft.setOnClick("move( this.form."+rightBox.getName()+", this.form."+leftBox.getName()+" )");
-    table.add(toTheLeft,2,1);
+	public void addToScripts(Script script) {
+		script.addFunction("addOpt", "function addOpt( list, val, text, idx, selected ) {  if( selected == null ) selected = false;  if( idx != null ) {          list.options[idx] = new Option( text, val, false, selected );  } else {          list.options[list.length] = new Option( text, val, false, selected );  }}");
+		script.addFunction("move", "function move( from, to ) {\n  var here =  from ;\n  var there =  to ;\n\n  if( here.selectedIndex != -1 && here.length > 0) {\n\n    for( h=0; h<here.length;h++) {\n      if( here.options[h].selected ) {\n        addOpt( there, here.options[h].value, here.options[h].text );\n      }\n    }\n    for( h1=here.length-1;h1>-1; h1-- ) {\n      if( here.options[h1].selected ) {\n        here.options[h1] = null;\n      }\n    }\n          \n  }\n}");
+	}
+	
+	public void addToSelectedBox(String value, String displayString) {
+		getRightBox().addElement(value, displayString);
+	}
 
-    table.add(rightBox,3,1);
+	public void addToAvailableBox(String value, String displayString) {
+		getLeftBox().addElement(value, displayString);
+	}
 
-    Script script = this.getParentPage().getAssociatedScript();
-    addToScripts(script);
-}
+	public void addToLeftBox(String value, String displayString) {
+		addToAvailableBox(value, displayString);
+	}
 
-public void addToSelectedBox(String value, String displayString){
-	getRightBox().addElement(value,displayString);	
-}
+	public void addToRightBox(String value, String displayString) {
+		addToSelectedBox(value, displayString);
+	}
 
+	public Object clone() {
+		SelectionDoubleBox obj = null;
+		try {
+			obj = (SelectionDoubleBox) super.clone();
 
-public void addToAvailableBox(String value, String displayString){
-	getLeftBox().addElement(value,displayString);	
-}
-
-public void addToLeftBox(String value, String displayString){
-	addToAvailableBox(value,displayString);	
-}
-
-public void addToRightBox(String value, String displayString){
-	addToSelectedBox(value,displayString);	
-}
-
-
+			if (this.leftBox != null) {
+				obj.leftBox = (SelectionBox) this.leftBox.clone();
+			}
+			if (this.rightBox != null) {
+				obj.rightBox = (SelectionBox) this.rightBox.clone();
+			}
+			if (this.toTheRight != null) {
+				obj.toTheRight = (GenericButton) this.toTheRight.clone();
+			}
+			if (this.toTheLeft != null) {
+				obj.toTheLeft = (GenericButton) this.toTheLeft.clone();
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace(System.err);
+		}
+		return obj;
+	}
 }
