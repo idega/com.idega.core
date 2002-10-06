babf8cd590ca796ef0ca9422e0fc657b1b7f17da
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-10-06 02:20:04 +0000

    added constructor

--- src/java/com/idega/data/IDOException.java
+++ src/java/com/idega/data/IDOException.java
@@ -1,6 +1,6 @@
 package com.idega.data;
-
 /**
+ * A cheched Exception thrown when an error occurs on runtime in IDO.
  * Title:        idega Data Objects
  * Description:  Idega Data Objects is a Framework for Object/Relational mapping and seamless integration between datastores
  * Copyright:    Copyright (c) 2002
@@ -8,20 +8,28 @@ package com.idega.data;
  * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
  * @version 1.0
  */
-
-public class IDOException extends Exception {
-
-  public IDOException() {
-    super();
-  }
-
-
-  public IDOException(String message) {
-    super(message);
-  }
-
-
-  public IDOException(Exception exeption,IDOEntity thrower) {
-    this(exeption.getMessage()+" : from : "+thrower.getClass().getName());
-  }
+public class IDOException extends Exception
+{
+	public IDOException()
+	{
+		super();
+	}
+	public IDOException(String message)
+	{
+		super(message);
+	}
+	public IDOException(Exception exeption)
+	{
+		this("IDOException - Originally: " + exeption.getClass().getName() + " : " + exeption.getMessage());
+	}
+	public IDOException(Exception exeption, IDOEntity thrower)
+	{
+		this(
+			"IDOException - Originally: "
+				+ exeption.getClass().getName()
+				+ " : "
+				+ exeption.getMessage()
+				+ " : from : "
+				+ thrower.getClass().getName());
+	}
 }
