9390168e9cf938fb6dfd009772e2cc0d224b740a
Thorhallur Helgason <laddi@idega.com>
2002-10-12 17:26:22 +0000

    Minor cleanup and added some javadoc.

--- src/java/com/idega/presentation/ui/CheckBox.java
+++ src/java/com/idega/presentation/ui/CheckBox.java
@@ -1,25 +1,13 @@
 //idega 2000 - Tryggvi Larusson
-
 /*
 
 *Copyright 2000 idega.is All Rights Reserved.
 
 */
-
-
-
 package com.idega.presentation.ui;
-
-
-
 import java.io.*;
-
 import java.util.*;
-
 import com.idega.presentation.*;
-
-
-
 /**
 
 *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
@@ -27,159 +15,64 @@ import com.idega.presentation.*;
 *@version 1.2
 
 */
-
-public class CheckBox extends InterfaceObject{
-
-
-
-private String checkedString;
-
-
-
-public CheckBox(){
-
-	this("untitled");
-
-}
-
-
-
-public CheckBox(String name){
-
-	this(name,"unspecified");
-
-}
-
-
-
-public CheckBox(String name,String value){
-
-	super();
-
-	setName(name);
-
-	setContent(value);
-
-	setChecked(false);
-
-}
-
-
-
-public void setChecked(boolean ifChecked){
-
-	if (ifChecked){
-
-		checkedString="checked";
-
+public class CheckBox extends InterfaceObject {
+	
+	private String checkedString;
+	
+	public CheckBox() {
+		this("untitled");
 	}
-
-	else{
-
-		checkedString="";
-
+	
+	public CheckBox(String name) {
+		this(name, "unspecified");
 	}
-
-}
-
-
-
-public String getCheckedString(){
-
-	return this.checkedString;
-
-}
-
-
-
-public void handleKeepStatus(IWContext iwc){
-
-	if (statusKeptOnAction()){
-
-		if(iwc.getParameter(this.getName()) != null){
-
-			if(iwc.getParameter(this.getName()).equals(this.getValue())){
-
-				setChecked(true);
-
+	
+	public CheckBox(String name, String value) {
+		super();
+		setName(name);
+		setContent(value);
+		setChecked(false);
+	}
+	
+	/**
+	 * Sets whether the checkbox is checked or not.
+	 * @param ifChecked
+	 */
+	public void setChecked(boolean ifChecked) {
+		if (ifChecked) {
+			setAttribute("checked");
+		}
+		else {
+			removeAttribute("checked");
+		}
+	}
+	
+	public void handleKeepStatus(IWContext iwc) {
+		if (statusKeptOnAction()) {
+			if (iwc.getParameter(this.getName()) != null) {
+				if (iwc.getParameter(this.getName()).equals(this.getValue())) {
+					setChecked(true);
+				}
 			}
-
 		}
-
 	}
-
-}
-
-
-
-
-
-public void print(IWContext iwc)throws IOException{
-
-
-	handleKeepStatus(iwc);
-
-	//if ( doPrint(iwc) ){
-
-		if (getLanguage().equals("HTML")){
-
-			//if (getInterfaceStyle().equals("something")){
-
-			//
-
-			//}
-
-			//else{
-
-				println("<input type=\"checkbox\" name=\""+getName()+"\" "+getCheckedString()+" "+getAttributeString()+" >");
-
-				//println("</input>");
-
-			//}
-
+	
+	public void print(IWContext iwc) throws IOException {
+		handleKeepStatus(iwc);
+		if (getLanguage().equals("HTML")) {
+			println("<input type=\"checkbox\" name=\"" + getName() + "\" " + getAttributeString() + " >");
 		}
-
-	//}
-
-
-
-}
-
-
-
-  public synchronized Object clone() {
-
-    CheckBox obj = null;
-
-    try {
-
-      obj = (CheckBox)super.clone();
-
-      obj.checkedString = this.checkedString;
-
-    }
-
-    catch(Exception ex) {
-
-      ex.printStackTrace(System.err);
-
-    }
-
-    return obj;
-
-  }
-
-
-
-  public void setStyle(String style) {
-
-    setAttribute("class",style);
-
-  }
-
-
-
-}
-
-
-
+	}
+	
+	public synchronized Object clone() {
+		CheckBox obj = null;
+		try {
+			obj = (CheckBox) super.clone();
+			obj.checkedString = this.checkedString;
+		}
+		catch (Exception ex) {
+			ex.printStackTrace(System.err);
+		}
+		return obj;
+	}
+	}
