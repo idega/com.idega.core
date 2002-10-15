76816be83e1bafcc8447697dcecea2de03cd879f
Thorhallur Helgason <laddi@idega.com>
2002-10-15 11:03:51 +0000

    A class that extends InterfaceObject and all interfaces that use the input tag.

---
+++ src/java/com/idega/presentation/ui/GenericInput.java
@@ -0,0 +1,53 @@
+package com.idega.presentation.ui;
+
+import com.idega.presentation.IWContext;
+
+/**
+ * @author Laddi
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public abstract class GenericInput extends InterfaceObject {
+
+	public static final String INPUT_TYPE_TEXT = "text";
+	public static final String INPUT_TYPE_PASSWORD = "password";
+	public static final String INPUT_TYPE_CHECKBOX = "checkbox";
+	public static final String INPUT_TYPE_RADIO = "radio";
+	public static final String INPUT_TYPE_SUBMIT = "submit";
+	public static final String INPUT_TYPE_RESET = "reset";
+	public static final String INPUT_TYPE_FILE = "file";
+	public static final String INPUT_TYPE_HIDDEN = "hidden";
+	public static final String INPUT_TYPE_IMAGE = "image";
+	public static final String INPUT_TYPE_BUTTON = "button";
+	
+	private String inputType = INPUT_TYPE_TEXT;
+
+	public GenericInput() {
+		super();
+	}
+
+	public String getInputType() {
+		return inputType;
+	}
+	
+	public void setInputType(String inputType) {
+		this.inputType = inputType;
+	}
+	
+	public void print(IWContext main) throws Exception {
+		if (getLanguage().equals("HTML")) {
+			println("<input type=\"" + getInputType() + "\" name=\"" + getName() + "\" " + getAttributeString() + " >");
+		}
+		else if (getLanguage().equals("WML")) {
+			println("<input type=\"" + getInputType() + "\" name=\"" + getName() + "\" " + getAttributeString() + " />");
+		}
+	}
+	
+	/**
+	 * @see com.idega.presentation.ui.InterfaceObject#handleKeepStatus(IWContext)
+	 */
+	public abstract void handleKeepStatus(IWContext iwc);
+}
