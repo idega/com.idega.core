b71600122a8adaa85ff78dc76052890d3de5f54c
Thorhallur Helgason <laddi@idega.com>
2002-10-23 14:51:46 +0000

    Added javadoc comments and changed superclass.

--- src/java/com/idega/presentation/text/Break.java
+++ src/java/com/idega/presentation/text/Break.java
@@ -1,78 +1,55 @@
 package com.idega.presentation.text;
 
-
-
-import com.idega.presentation.Block;
-
 import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
 
-
-
-public class Break extends Block {
-
-
+/**
+ * @author laddi
+ *
+ * A <code>PresentationObject</code> that displays a break on the page it is added on.
+ */
+public class Break extends PresentationObject {
 
   private int _numberOfBreaks = 1;
 
+	/**
+	 * Constructs a new <code>Break</code> object with a single break.
+	 */
+  public Break() {
+  }
 
-
-  public Break() { }
-
-
-
+	/**
+	 * Constructs a new <code>Break</code> object with the number of breaks specified.
+	 * @param numberOfBreaks
+	 */
   public Break(int numberOfBreaks) {
-
     _numberOfBreaks = numberOfBreaks;
-
   }
 
-
-
-  public void main(IWContext iwc) {
-
+  public void print(IWContext iwc) {
     for (int a = 1; a <= _numberOfBreaks; a++) {
-
-      super.add(Text.getBreak());
-
+	    if (getLanguage().equals("HTML")) {
+	      print("<br />");
+	    }
+	    else if (getLanguage().equals("WML")) {
+	      print("<br />");
+	    }
     }
-
   }
 
-
-
   /**
-
-   *  Sets how many breaks should be displayed
-
-   *
-
-   *@param  numberOfBreaks  The new numberOfBreaks value
-
+   * Sets how many breaks should be displayed
+   * @param  numberOfBreaks  The number of breaks to add
    */
-
   public void setNumberOfBreaks(int numberOfBreaks) {
-
     _numberOfBreaks = numberOfBreaks;
-
   }
-
-
 
   /**
-
-   *  Sets how many breaks should be displayed
-
-   *
-
-   *@param  numberOfBreaks  The new numberOfBreaks value
-
+   * Sets how many breaks should be displayed
+   * @param  numberOfBreaks  The number of breaks to add
    */
-
   public void setNumberOfBreaks(String numberOfBreaks) {
-
-    _numberOfBreaks = Integer.parseInt(numberOfBreaks);
-
+    setNumberOfBreaks(Integer.parseInt(numberOfBreaks));
   }
-
 }
-
