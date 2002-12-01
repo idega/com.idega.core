348c1158abf1dafd0051e3bf0958183e01588b6e
Aron Birkir Gudmundsson <aron@idega.com>
2002-12-01 11:22:20 +0000

    *** empty log message ***

--- src/java/com/idega/data/TextEntity.java
+++ src/java/com/idega/data/TextEntity.java
@@ -6,4 +6,8 @@ public interface TextEntity extends com.idega.data.IDOLegacyEntity
 {
  public int getTextId();
  public void setTextId(int p0);
+ public String getName();
+ public String getInfo();
+ public void setInfo(String info);
+ public void setName(String name);
 }
