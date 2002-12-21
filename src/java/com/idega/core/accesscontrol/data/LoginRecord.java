32cf02c83411768126cdcc40b8d98ebdd3833354
Thomas Hilbig <thomas@idega.com>
2002-12-21 00:36:34 +0000

    change password function was added

--- src/java/com/idega/core/accesscontrol/data/LoginRecord.java
+++ src/java/com/idega/core/accesscontrol/data/LoginRecord.java
@@ -4,12 +4,13 @@ import javax.ejb.*;
 
 public interface LoginRecord extends com.idega.data.IDOEntity
 {
- public void setLoginId(int p0);
- public int getLoginId();
- public void setLogOutStamp(java.sql.Timestamp p0);
- public void setIPAdress(java.lang.String p0);
- public java.sql.Timestamp getLogInStamp();
  public java.lang.String getIPAdress();
+ public java.sql.Timestamp getLogInStamp();
  public java.sql.Timestamp getLogOutStamp();
+ public int getLoginId();
+ public void initializeAttributes();
+ public void setIPAdress(java.lang.String p0);
  public void setLogInStamp(java.sql.Timestamp p0);
+ public void setLogOutStamp(java.sql.Timestamp p0);
+ public void setLoginId(int p0);
 }
