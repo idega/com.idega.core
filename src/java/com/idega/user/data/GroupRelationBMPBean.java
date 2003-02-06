c6e12ecb5906066cfd2d4f246c53ab6fd922bf32
Eirikur Hrafnsson <eiki@idega.com>
2003-02-06 21:56:53 +0000

    fixed sql for bidirectional find methods

--- src/java/com/idega/user/data/GroupRelationBMPBean.java
+++ src/java/com/idega/user/data/GroupRelationBMPBean.java
@@ -236,14 +236,14 @@ public class GroupRelationBMPBean extends GenericEntity implements GroupRelation
    * Finds all active relationships specified bidirectionally (in both directions) with groupID and relatedGroupID as specified
    */
   public Collection ejbFindGroupsRelationshipsContainingBiDirectional(int groupID,int relatedGroupID)throws FinderException,RemoteException{
-    return this.idoFindPKsBySQL("select * from "+this.getTableName()+" where ("+this.GROUP_ID_COLUMN+"="+groupID+" and "+this.RELATED_GROUP_ID_COLUMN+"="+relatedGroupID+") or ("+this.RELATED_GROUP_ID_COLUMN+"="+groupID+" and "+this.GROUP_ID_COLUMN+"="+relatedGroupID+") and "+this.STATUS_COLUMN+"='"+STATUS_ACTIVE+"'");
+    return this.idoFindPKsBySQL("select * from "+this.getTableName()+" where ( ("+this.GROUP_ID_COLUMN+"="+groupID+" and "+this.RELATED_GROUP_ID_COLUMN+"="+relatedGroupID+") or ("+this.RELATED_GROUP_ID_COLUMN+"="+groupID+" and "+this.GROUP_ID_COLUMN+"="+relatedGroupID+") ) and "+this.STATUS_COLUMN+"='"+STATUS_ACTIVE+"'");
   }
 
   /**
    * Finds all active relationships specified bidirectionally (in both directions) with groupID and relatedGroupID and relationshipType as specified
    */
   public Collection ejbFindGroupsRelationshipsContainingBiDirectional(int groupID,int relatedGroupID,String relationshipType)throws FinderException,RemoteException{
-    return this.idoFindPKsBySQL("select * from "+this.getTableName()+" where ("+this.GROUP_ID_COLUMN+"="+groupID+" and "+this.RELATED_GROUP_ID_COLUMN+"="+relatedGroupID+") or ("+this.RELATED_GROUP_ID_COLUMN+"="+groupID+" and "+this.GROUP_ID_COLUMN+"="+relatedGroupID+") and "+this.RELATIONSHIP_TYPE_COLUMN+"='"+relationshipType+"' and "+this.STATUS_COLUMN+"='"+STATUS_ACTIVE+"'");
+    return this.idoFindPKsBySQL("select * from "+this.getTableName()+" where ( ("+this.GROUP_ID_COLUMN+"="+groupID+" and "+this.RELATED_GROUP_ID_COLUMN+"="+relatedGroupID+") or ("+this.RELATED_GROUP_ID_COLUMN+"="+groupID+" and "+this.GROUP_ID_COLUMN+"="+relatedGroupID+") ) and "+this.RELATIONSHIP_TYPE_COLUMN+"='"+relationshipType+"' and "+this.STATUS_COLUMN+"='"+STATUS_ACTIVE+"'");
   }
 
   /**
