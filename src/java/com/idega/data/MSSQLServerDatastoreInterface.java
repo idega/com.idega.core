15b182086ae60e9f120aae10bbdc40585daa8a99
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2003-03-02 13:49:56 +0000

    Fixed handling of IDENTITY columns

--- src/java/com/idega/data/MSSQLServerDatastoreInterface.java
+++ src/java/com/idega/data/MSSQLServerDatastoreInterface.java
@@ -5,10 +5,10 @@
  * Window>Preferences>Java>Code Generation>Code Template
  */
 package com.idega.data;
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
-
 /**
  * Title:        MSSQLServerDatastoreInterface
  * Description:  A class to handle Microsoft SQL Server specific jdbc implementations.
@@ -93,23 +93,36 @@ public class MSSQLServerDatastoreInterface extends DatastoreInterface
 	{
 		// TODO Auto-generated method stub
 	}
-	
-	
 	/**
 	 * @param entity
 	 * @param conn
 	 */
 	protected void updateNumberGeneratedValue(IDOLegacyEntity entity, Connection conn)
 	{
-		try{
-			if(((IDOEntityBean)entity).getPrimaryKeyClass().equals(Integer.class)){
-				if (entity.isNull(entity.getIDColumnName())) {
-					Object value = this.executeQuery(entity,"select @@IDENTITY");
-					entity.setID(Integer.parseInt(value.toString()));
-				}	
+		try
+		{
+			if (((IDOEntityBean) entity).getPrimaryKeyClass().equals(Integer.class))
+			{
+				boolean pkIsNull = entity.isNull(entity.getIDColumnName()) && entity.getPrimaryKey() == null;
+				if (pkIsNull)
+				{
+					//Object value = this.executeQuery(entity, "select @@IDENTITY");
+					Statement stmt = conn.createStatement();
+					ResultSet rs = stmt.executeQuery("select @@IDENTITY");
+					rs.next();
+					int id = rs.getInt(1);
+					entity.setID(id);
+					rs.close();
+					stmt.close();
+					//String tableName = entity.getTableName();
+					//Statement stmt2 = conn.createStatement();
+					//stmt2.executeUpdate("set IDENTITY_INSERT " + tableName + " off");
+					//stmt2.close();
+				}
 			}
 		}
-		catch(Exception e){
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
@@ -121,44 +134,142 @@ public class MSSQLServerDatastoreInterface extends DatastoreInterface
 		// TODO Auto-generated method stub
 		return true;
 	}
-	
-	public String getIDColumnType() {
+	public String getIDColumnType()
+	{
 		return "INTEGER IDENTITY";
 	}
-	/* (non-Javadoc)
-	 * @see com.idega.data.DatastoreInterface#executeAfterInsert(com.idega.data.IDOLegacyEntity)
-	 */
-	protected void executeBeforeInsert(IDOLegacyEntity entity) throws Exception
+	/*protected void executeBeforeInsert(IDOLegacyEntity entity) throws Exception
 	{
-		try{
-			if(((IDOEntityBean)entity).getPrimaryKeyClass().equals(Integer.class)){
-				if (!entity.isNull(entity.getIDColumnName())) {
+		try
+		{
+			if (((IDOEntityBean) entity).getPrimaryKeyClass().equals(Integer.class))
+			{
+				boolean pkIsNull = entity.isNull(entity.getIDColumnName()) && entity.getPrimaryKey() == null;
+				if (!pkIsNull)
+				{
 					String tableName = entity.getTableName();
-					executeUpdate(entity,"set IDENTITY_INSERT "+tableName+" on");
-				}	
+					executeUpdate(entity, "set IDENTITY_INSERT " + tableName + " on");
+				}
 			}
 		}
-		catch(Exception e){
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 		super.executeBeforeInsert(entity);
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.data.DatastoreInterface#executeBeforeInsert(com.idega.data.IDOLegacyEntity)
-	 */
+	
 	protected void executeAfterInsert(IDOLegacyEntity entity) throws Exception
 	{
-		try{
-			if(((IDOEntityBean)entity).getPrimaryKeyClass().equals(Integer.class)){
+		try
+		{
+			if (((IDOEntityBean) entity).getPrimaryKeyClass().equals(Integer.class))
+			{
 				String tableName = entity.getTableName();
-				executeUpdate(entity,"set IDENTITY_INSERT "+tableName+" off");
+				executeUpdate(entity, "set IDENTITY_INSERT " + tableName + " off");
 			}
-			}
-		catch(Exception e){
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 		super.executeAfterInsert(entity);
+	}*/
+	/**
+	 * 
+	 * Hacked version of the insert method.
+	 * @todo: Implement in a better way.
+	 */
+	public void insert(IDOLegacyEntity entity, Connection conn) throws Exception
+	{
+		executeBeforeInsert(entity);
+		PreparedStatement Stmt = null;
+		ResultSet RS = null;
+		try
+		{
+			boolean entityInsertModeIsOn = false;
+			entityInsertModeIsOn = turnOnIdentityInsertFlag(entity, conn, entityInsertModeIsOn);
+			StringBuffer statement = new StringBuffer("");
+			statement.append("insert into ");
+			statement.append(entity.getTableName());
+			statement.append("(");
+			statement.append(getCommaDelimitedColumnNamesForInsert(entity));
+			statement.append(") values (");
+			statement.append(getQuestionmarksForColumns(entity));
+			statement.append(")");
+			if (isDebugActive())
+				debug(statement.toString());
+			Stmt = conn.prepareStatement(statement.toString());
+			setForPreparedStatement(STATEMENT_INSERT, Stmt, entity);
+			Stmt.execute();
+			Stmt.close();
+			if (updateNumberGeneratedValueAfterInsert())
+			{
+				updateNumberGeneratedValue(entity, conn);
+			}
+			turnOffIdentityInsertFlag(entity, conn, entityInsertModeIsOn);
+		}
+		finally
+		{
+			if (RS != null)
+			{
+				RS.close();
+			}
+			if (Stmt != null)
+			{
+				Stmt.close();
+			}
+		}
+		executeAfterInsert(entity);
+		entity.setEntityState(entity.STATE_IN_SYNCH_WITH_DATASTORE);
 	}
-
+	private boolean turnOnIdentityInsertFlag(IDOLegacyEntity entity, Connection conn, boolean entityInsertModeIsOn)
+	{
+		try
+		{
+			if (((IDOEntityBean) entity).getPrimaryKeyClass().equals(Integer.class))
+			{
+				boolean pkIsNull = entity.isNull(entity.getIDColumnName()) && entity.getPrimaryKey() == null;
+				if (!pkIsNull)
+				{
+					String tableName = entity.getTableName();
+					Statement stmt2 = conn.createStatement();
+					String sql = "set IDENTITY_INSERT " + tableName + " on";
+					stmt2.executeUpdate(sql);
+					debug(sql);
+					stmt2.close();
+					return true;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+	private boolean turnOffIdentityInsertFlag(IDOLegacyEntity entity, Connection conn, boolean entityInsertModeIsOn)
+	{
+		if (entityInsertModeIsOn)
+		{
+			try
+			{
+				if (((IDOEntityBean) entity).getPrimaryKeyClass().equals(Integer.class))
+				{
+					String tableName = entity.getTableName();
+					Statement stmt2 = conn.createStatement();
+					String sql = "set IDENTITY_INSERT " + tableName + " off";
+					stmt2.executeUpdate(sql);
+					debug(sql);
+					return false;
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				return true;
+			}
+		}
+		return false;
+	}
 }
