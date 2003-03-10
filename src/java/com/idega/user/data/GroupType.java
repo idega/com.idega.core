30238ebe037aaee54f72bbd87335a94315ebe27c
Eirikur Hrafnsson <eiki@idega.com>
2003-03-10 22:39:43 +0000

    *** empty log message ***

--- src/java/com/idega/user/data/GroupType.java
+++ src/java/com/idega/user/data/GroupType.java
@@ -2,13 +2,13 @@ package com.idega.user.data;
 
 import javax.ejb.*;
 
-public interface GroupType extends com.idega.data.IDOEntity
+public interface GroupType extends com.idega.data.IDOEntity,com.idega.data.TreeableEntity
 {
- public java.lang.String getIDColumnName() throws java.rmi.RemoteException;
+ public java.lang.String getIDColumnName();
  public boolean getVisibility() throws java.rmi.RemoteException;
  public java.lang.Class getPrimaryKeyClass() throws java.rmi.RemoteException;
  public void setGroupTypeAsPermissionGroup() throws java.rmi.RemoteException;
- public void initializeAttributes() throws java.rmi.RemoteException;
+ public void initializeAttributes();
  public void setGroupTypeAsGeneralGroup() throws java.rmi.RemoteException;
  public void setGroupTypeAsAliasGroup() throws java.rmi.RemoteException;
  public void setDescription(java.lang.String p0) throws java.rmi.RemoteException;
@@ -16,7 +16,7 @@ public interface GroupType extends com.idega.data.IDOEntity
  public void setType(java.lang.String p0) throws java.rmi.RemoteException;
  public void setVisibility(boolean p0) throws java.rmi.RemoteException;
  public java.lang.String getGeneralGroupTypeString() throws java.rmi.RemoteException;
- public java.lang.String getType() throws java.rmi.RemoteException;
+ public java.lang.String getType();
  public java.lang.String getPermissionGroupTypeString() throws java.rmi.RemoteException;
  public java.lang.String getAliasGroupTypeString() throws java.rmi.RemoteException;
  public void setDefaultGroupName(String name);
