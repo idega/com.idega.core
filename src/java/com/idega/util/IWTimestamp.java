91635787552cd3db372ff9d13321d9c7b23ca42e
Pall Helgason <palli@idega.com>
2003-03-31 17:43:27 +0000

    Resubmitted oracle version

--- src/java/com/idega/util/IWTimestamp.java
+++ src/java/com/idega/util/IWTimestamp.java
@@ -979,7 +979,7 @@ public class IWTimestamp implements Comparable {
 	 */
 	//To methods
 	public String toString() {
-		return this.getTimestamp().toString();//.substring(0,19);
+		return this.getTimestamp().toString().substring(0,19);
 	}
 
 	/**
@@ -993,7 +993,7 @@ public class IWTimestamp implements Comparable {
 		else if (isTime())
 			return toSQLTimeString();
 		else
-			return getTimestamp().toString();//.substring(0,19);
+			return getTimestamp().toString().substring(0,19);
 	}
 
 	/**
