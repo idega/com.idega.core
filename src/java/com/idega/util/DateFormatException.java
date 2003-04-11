00e4c8d6027d4d8347b610c016f1f2c1151db973
Joakim Johnsson <joakim@idega.com>
2003-04-11 09:30:17 +0000

    Created

---
+++ src/java/com/idega/util/DateFormatException.java
@@ -0,0 +1,17 @@
+package com.idega.util;
+
+/**
+ * This exception is thrown when a date couldn´t be parsed
+ * @author <a href="mailto:joakim@idega.is">Joakim Johnson</a>
+ * @version 1.0
+ */
+public class DateFormatException extends Exception {
+
+  public DateFormatException() {
+	super();
+  }
+
+  public DateFormatException(String s) {
+	super(s);
+  }
+}	
