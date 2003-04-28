41975e8e7e09c68b4b9768f267e61464a5796781
Thorhallur Helgason <laddi@idega.com>
2003-04-28 13:07:18 +0000

    "Fixed" rendering of javascript.

--- src/java/com/idega/presentation/NavigationJSMenu.java
+++ src/java/com/idega/presentation/NavigationJSMenu.java
@@ -40,6 +40,8 @@ public class NavigationJSMenu extends Block {
 	private Map menuXPosition;
 	private Map menuYPosition;
 	
+	private int row = 0;
+	
 	public NavigationJSMenu() {
 	}
 
@@ -65,12 +67,12 @@ public class NavigationJSMenu extends Block {
 			
 			PageTreeNode node = new PageTreeNode(rootNode, iwc);
 			Iterator iterator = node.getChildren();
-			int row = 0;
+			row = 0;
 			int parentRow = -1;
 			while (iterator.hasNext()) {
 				parentRow = -1;
 				PageTreeNode child = (PageTreeNode) iterator.next();
-				addRow(iwc, row, parentRow, child, buffer);
+				addRow(iwc, parentRow, 0, child, buffer);
 			}
 			buffer.append("\n").append("createMenuTree();").append("\n\n");
 			
@@ -82,51 +84,48 @@ public class NavigationJSMenu extends Block {
 		}
 	}
 	
-	public void addRow(IWContext iwc, int row, int parentRow, PageTreeNode parent, StringBuffer buffer) {
+	public void addRow(IWContext iwc, int parentRow, int subRow, PageTreeNode parent, StringBuffer buffer) {
+		int subSubRow = 0;
 		if (parent.getChildCount() > 0) {
-			Iterator iter = parent.getChildren();
-			int subSubRow = 0;
-			while (iter.hasNext()) {
-				PageTreeNode child = (PageTreeNode) iter.next();
-				if (child.getChildCount() > 0) {
-					buffer.append("menuContent [").append(row++).append("] = new Array(");
-					buffer.append(parentRow).append(",");
-					if (parentRow == -1)
-					buffer.append(0).append(",");
-					else
-						buffer.append(subSubRow).append(",");
-					
-					if (menuWidth != null && menuWidth.get(new Integer(row - 1)) != null)
-						buffer.append(menuWidth.get(new Integer(row - 1))).append(",");
-					else
-						buffer.append(itemWidth).append(",");
-
-					if (menuXPosition != null && menuXPosition.get(new Integer(row - 1)) != null)
-						buffer.append(menuXPosition.get(new Integer(row - 1))).append(",");
-					else
-						buffer.append(-1).append(",");
-
-					if (menuYPosition != null && menuYPosition.get(new Integer(row - 1)) != null)
-						buffer.append(menuYPosition.get(new Integer(row - 1))).append(",");
-					else
-						buffer.append(-1).append(",");
-
-					buffer.append("new Array(");
-					
-					Iterator iterator = child.getChildren();
-					while (iterator.hasNext()) {
-						PageTreeNode grandChild = (PageTreeNode) iterator.next();
-						buffer.append("'").append(grandChild.getLocalizedNodeName(iwc)).append("',");
-						buffer.append("'").append(BuilderLogic.getInstance().getIBPageURL(iwc, grandChild.getNodeID())).append("'");
-						if (iterator.hasNext())
-							buffer.append(",");
-						else
-							buffer.append(")");
-					}
-					buffer.append(");").append("\n");
-
-					addRow(iwc, row, (row - 1), child, buffer);
-				}
+			buffer.append("menuContent [").append(row++).append("] = new Array(");
+			buffer.append(parentRow).append(",");
+			if (parentRow == -1)
+				buffer.append(0).append(",");
+			else
+				buffer.append(subRow).append(",");
+			
+			if (menuWidth != null && menuWidth.get(new Integer(row - 1)) != null)
+				buffer.append(menuWidth.get(new Integer(row - 1))).append(",");
+			else
+				buffer.append(itemWidth).append(",");
+	
+			if (menuXPosition != null && menuXPosition.get(new Integer(row - 1)) != null)
+				buffer.append(menuXPosition.get(new Integer(row - 1))).append(",");
+			else
+				buffer.append(-1).append(",");
+	
+			if (menuYPosition != null && menuYPosition.get(new Integer(row - 1)) != null)
+				buffer.append(menuYPosition.get(new Integer(row - 1))).append(",");
+			else
+				buffer.append(-1).append(",");
+	
+			buffer.append("new Array(");
+			
+			Iterator iterator = parent.getChildren();
+			while (iterator.hasNext()) {
+				PageTreeNode grandChild = (PageTreeNode) iterator.next();
+				buffer.append("'").append(grandChild.getLocalizedNodeName(iwc)).append("',");
+				buffer.append("'").append(BuilderLogic.getInstance().getIBPageURL(iwc, grandChild.getNodeID())).append("'");
+				if (iterator.hasNext())
+					buffer.append(",");
+				else
+					buffer.append(")");
+			}
+			buffer.append(");").append("\n");
+	
+			Iterator iterator2 = parent.getChildren();
+			while (iterator2.hasNext()) {
+				addRow(iwc, (row - 1), subSubRow, (PageTreeNode) iterator2.next(), buffer);
 				subSubRow++;
 			}
 		}
@@ -143,8 +142,8 @@ public class NavigationJSMenu extends Block {
 			menuYPosition = new HashMap();
 		}
 		menuWidth.put(new Integer(menu), String.valueOf(width));
-		menuXPosition.put(new Integer(menu), String.valueOf(xOffset));
-		menuYPosition.put(new Integer(menu), String.valueOf(yOffset));
+		menuXPosition.put(new Integer(menu), String.valueOf(xPosition));
+		menuYPosition.put(new Integer(menu), String.valueOf(yPosition));
 	}
 	
 	public void setRootNode(IBPage page) {
