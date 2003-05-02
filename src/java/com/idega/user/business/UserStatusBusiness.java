074533fcf364f0749cb40c1a00d65860d7ca6e80
Pall Helgason <palli@idega.com>
2003-05-02 06:28:55 +0000

    Added methods to upload status to users

--- src/java/com/idega/user/business/UserStatusBusiness.java
+++ src/java/com/idega/user/business/UserStatusBusiness.java
@@ -3,5 +3,6 @@ package com.idega.user.business;
 
 public interface UserStatusBusiness extends com.idega.business.IBOService
 {
+ public boolean removeUserFromGroup(int p0,int p1) throws java.rmi.RemoteException;
  public boolean setUserGroupStatus(int p0,int p1,int p2) throws java.rmi.RemoteException;
 }
--- src/java/com/idega/user/business/UserStatusBusinessBean.java
+++ src/java/com/idega/user/business/UserStatusBusinessBean.java
@@ -8,7 +8,13 @@
  */
 package com.idega.user.business;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import com.idega.business.IBOServiceBean;
+import com.idega.user.data.UserStatus;
+import com.idega.user.data.UserStatusHome;
+import com.idega.util.IWTimestamp;
 
 /**
  * @author palli
@@ -17,7 +23,39 @@ import com.idega.business.IBOServiceBean;
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 public class UserStatusBusinessBean extends IBOServiceBean implements UserStatusBusiness {
+	public boolean removeUserFromGroup(int user_id, int group_id) {
+		return setUserGroupStatus(user_id,group_id,-1);
+	}
+	
 	public boolean setUserGroupStatus(int user_id, int group_id, int status_id) {
+		try {
+			Collection obj = ((UserStatusHome) com.idega.data.IDOLookup.getHome(UserStatus.class)).findAllByUserIdAndGroupId(user_id,group_id);
+			
+			IWTimestamp now = new IWTimestamp();
+			if (obj != null) {
+				Iterator it = obj.iterator();
+				while (it.hasNext()) {
+					UserStatus uStatus = (UserStatus)it.next();
+					if (uStatus.getDateTo() == null) {
+						uStatus.setDateTo(now.getTimestamp());
+						uStatus.store();
+					}
+				}
+			}
+			
+			if (status_id > 0) {
+				UserStatus uStatus = ((UserStatusHome) com.idega.data.IDOLookup.getHome(UserStatus.class)).create();
+				uStatus.setUserId(user_id);
+				uStatus.setGroupId(group_id);
+				uStatus.setDateFrom(now.getTimestamp());
+				uStatus.store();
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			
+			return false;
+		}
 		
 		
 		return true;
