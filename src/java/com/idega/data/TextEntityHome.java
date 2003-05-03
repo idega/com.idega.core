c715d0cea2a539e30558f7fc8ea94accc99020fe
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2003-05-03 01:35:12 +0000

    Refactoring of com.idega.data.TextEntity to com.idega.block.text.data.TextEntity

--- src/java/com/idega/data/TextEntityHome.java
+++
@@ -1,9 +0,0 @@
-package com.idega.data;
-
-
-public interface TextEntityHome extends com.idega.data.IDOHome
-{
- public TextEntity create() throws javax.ejb.CreateException;
- public TextEntity findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
-
-}
