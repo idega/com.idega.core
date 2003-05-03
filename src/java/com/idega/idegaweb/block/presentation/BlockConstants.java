5d43fb95d681a6acaece11eccb8b57cacca94482
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2003-05-03 00:31:21 +0000

    Refactoring and moving of com.idega.block.IWBlock and com.idega.block.BlockConstants to com.idega.idegaweb.block.presentation.Builderaware and com.idega.idegaweb.block.presentation.BlockConstants

---
+++ src/java/com/idega/idegaweb/block/presentation/BlockConstants.java
@@ -0,0 +1,19 @@
+package com.idega.idegaweb.block.presentation;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+public class BlockConstants {
+
+  public static final String HEADLINE_STYLE = "font-face: Verdana,Arial,Helvetica,sans-serif; font-size: 9pt; font-weight: bold; color: #000000; text-decoration: none;";
+  public static final String BODY_STYLE = "font-face: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; font-weight: normal; color: #000000; text-decoration: none;";
+  public static final String DATE_STYLE = "font-face: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; font-weight: bold; color: #000000; text-decoration: none;";
+  public static final String TITLE_STYLE = "font-face: Verdana,Arial,Helvetica,sans-serif; font-size: 10pt; font-weight: bold; color: #000000; text-decoration: none;";
+
+}
---
+++ src/java/com/idega/idegaweb/block/presentation/Builderaware.java
@@ -0,0 +1,23 @@
+package com.idega.idegaweb.block.presentation;
+
+/**
+ * An interface to declare certainn methods that are used by the Builder when manipulating Blocks.
+ * This interface is optionally implemented by blocks.
+ * Copyright:    Copyright (c) 2000-2001 idega.is All Rights Reserved
+ * Company:      idega
+  *@author <a href="mailto:aron@idega.is">Aron Birkir</a>
+ * @version 1.1
+ * @see com.idega.presentation.Block
+ */
+
+public interface Builderaware {
+
+public final static String IW_CORE_BUNDLE_IDENTIFIER="com.idega.core";
+
+  public boolean deleteBlock(int ICObjectInstanceId);
+  public boolean copyBlock(int newInstanceID);
+  public String getBundleIdentifier();
+  public String getLocalizedNameKey();
+  public String getLocalizedNameValue();
+
+}
