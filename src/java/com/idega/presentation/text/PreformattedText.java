764470ef7e5ba5c0d5e2c624d3982849675495c1
Thorhallur Helgason <laddi@idega.com>
2003-05-06 16:31:29 +0000

    An element to show preformatted text.

---
+++ src/java/com/idega/presentation/text/PreformattedText.java
@@ -0,0 +1,43 @@
+/*
+ * Created on 2.5.2003
+ */
+package com.idega.presentation.text;
+
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+
+/**
+ * @author laddi
+ */
+public class PreformattedText extends PresentationObject {
+
+	String _text;
+	
+	public PreformattedText(){
+		this("");
+	}
+
+	public PreformattedText(String text){
+		super();
+		setText(text);
+	}
+
+	public void print(IWContext iwc)throws Exception{
+		if (getLanguage().equals("HTML")){
+			print("<pre "+getAttributeString()+" >");
+			print(getText());
+			print("</pre>");
+		}
+		else if (getLanguage().equals("WML")){
+			print(getText());
+		}
+	}
+
+	public String getText(){
+		return _text;
+	}
+
+	public void setText(String text){
+		_text = text;
+	}
+}
