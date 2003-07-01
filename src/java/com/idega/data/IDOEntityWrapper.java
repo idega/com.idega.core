b84ddc7d5f0d573c9c7b884a5d2e4f91f2bd4695
Gudmundur Agust Saemundsson <gummi@idega.com>
2003-07-01 14:01:55 +0000

    IDOEntityWrapper to handle versions

---
+++ src/java/com/idega/data/IDOEntityWrapper.java
@@ -0,0 +1,401 @@
+/*
+ * Created on 18.6.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.idega.data;
+
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.Hashtable;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBException;
+import javax.ejb.EJBLocalHome;
+import javax.ejb.EJBLocalObject;
+import javax.ejb.EntityContext;
+import javax.ejb.FinderException;
+import javax.ejb.RemoveException;
+
+import com.idega.core.data.ICLocale;
+import com.idega.core.version.data.ICItem;
+import com.idega.core.version.data.ICItemHome;
+import com.idega.core.version.data.ICVersion;
+//import com.idega.core.version.data.ICVersionHome;
+import com.idega.core.version.data.ICVersionableEntity;
+import com.idega.core.version.data.ICVersionableHome;
+
+/**
+ * Title:		IDOEntityWrappers
+ * Description:
+ * Copyright:	Copyright (c) 2003
+ * Company:		idega Software
+ * @author		2003 - idega team - <br><a href="mailto:gummi@idega.is">Gudmundur Agust Saemundsson</a><br>
+ * @version		1.0
+ */
+public abstract class IDOEntityWrapper implements IDOEntityBean {
+
+	private ICLocale _locale = null;
+	private Object _mainPrimaryKey = null;
+
+	private Class _mainClass = null;
+	private Class _translationClass = null;
+	private Class _versionClass = null;
+
+	private IDOEntity _mainEntity = null;
+	private IDOEntity _translationEntity = null;
+	private IDOEntity _currentOpenVersionEntity = null;
+	private IDOEntity _versionInProgress = null;
+
+	private IDOHome _mainEntityHome = null;
+	private IDOHome _translationEntityHome = null;
+	private IDOHome _versionEntityHome = null;
+
+	private EntityContext _entityContext;
+	private EJBLocalHome _ejbHome;
+
+	private ICItem _mainEntityItem = null;
+	
+	private ICVersion _selectedVersion = null;
+
+	public IDOEntityWrapper(Object primaryKey) throws IDOLookupException, FinderException {
+		construct(primaryKey, null, null, null);
+	}
+
+	public IDOEntityWrapper(Object primaryKey, ICLocale locale) throws IDOLookupException, FinderException {
+		construct(primaryKey, locale, null, null);
+	}
+
+	private void construct(Object primaryKey, ICLocale locale, ICVersion version, String versionName) throws IDOLookupException, FinderException {
+		_mainPrimaryKey = primaryKey;
+		_locale = locale;
+		
+		if(version != null){
+			_selectedVersion = version;
+		} else if(versionName != null){
+//			ICVersionHome versionHome = (ICVersionHome)IDOLookup.getHome(ICVersion.class);
+//			_selectedVersion = versionHome.
+		}
+		
+		initialize();
+	}
+
+	protected abstract Class getMainClass();
+
+	protected abstract boolean useVersions();
+	protected abstract boolean useTranslations();
+	protected Class getTranslationClass() {
+		return null;
+	}
+	protected Class getVersionClass() {
+		return null;
+	}
+
+	protected void initialize() throws IDOLookupException, FinderException {
+		_mainClass = getMainClass();
+		_mainEntityHome = IDOLookup.getHome(_mainClass);
+		if (_mainPrimaryKey != null) {
+			_mainEntity = _mainEntityHome.findByPrimaryKeyIDO(_mainPrimaryKey);
+		}
+
+		if (useTranslations()) {
+			_translationClass = getTranslationClass();
+			_translationEntityHome = IDOLookup.getHome(_translationClass);
+		}
+
+		if (useVersions()) {
+			if (!(_mainEntityHome instanceof ICVersionableHome)) {
+				throw new UnsupportedOperationException("if useVersions() is true, then the Home-Interface must extend " + ICVersionableHome.class);
+			}
+			if (!(_mainEntity instanceof ICVersionableEntity)) {
+				throw new UnsupportedOperationException("if useVersions() is true, then the Bean-Interface must extend " + ICVersionableEntity.class);
+			}
+
+			_versionClass = getVersionClass();
+			_versionEntityHome = IDOLookup.getHome(_versionClass);
+
+			if (!(_versionEntityHome instanceof ICVersionableHome)) {
+				throw new UnsupportedOperationException("if useVersions() is true, then the Home-Interface must extend " + ICVersionableHome.class);
+			}
+		}
+
+		updateWrapper();
+	}
+
+	public void updateWrapper() throws IDOLookupException, FinderException {
+		if (useTranslations()) {
+			// not yet implemented
+		}
+
+		if (useVersions()) {
+			Object itemPK = ((ICVersionableEntity)_mainEntity).getItemPrimaryKey();
+			if (itemPK != null) {
+				_mainEntityItem = ((ICItemHome)IDOLookup.getHome(ICItemHome.class)).findByPrimaryKey(itemPK);
+				try {
+					_currentOpenVersionEntity = ((ICVersionableHome)_versionEntityHome).findEntityOfSpecificVersion(_mainEntityItem.getCurrentOpenVersion());
+					//TODO find currentVersionInProgress
+					_versionInProgress = _currentOpenVersionEntity;
+				} catch (FinderException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (_currentOpenVersionEntity == null) {
+				System.out.println("IDOEntityWrapper: No version is available for this item. ");
+				System.out.println("IDOEntityWrapper: MainClass: " + this.getMainClass() + " PK: " + _mainEntity.getPrimaryKey());
+				_currentOpenVersionEntity = _mainEntity;
+				_versionInProgress = _currentOpenVersionEntity;
+			}
+
+			if (!(_currentOpenVersionEntity instanceof ICVersionableEntity)) {
+				throw new UnsupportedOperationException("if useVersions() is true, then the Bean-Interface must extend " + ICVersionableEntity.class);
+			}
+		}
+
+	}
+
+	public ICLocale getICLocale() {
+		return _locale;
+	}
+
+	public IDOHome getMainEntityHome() {
+		return _mainEntityHome;
+	}
+
+	public IDOHome getTranslationEntityHome() {
+		return _translationEntityHome;
+	}
+
+	protected IDOEntity getMainEntity() {
+		return _mainEntity;
+	}
+
+	protected IDOEntity getTranslationEntity() {
+		return _translationEntity;
+	}
+
+	protected IDOEntity getCurrentOpenVersionEntity() {
+		return _currentOpenVersionEntity;
+	}
+
+	protected IDOEntity getVersionInProgress() {
+		return _versionInProgress;
+	}
+
+	public Object getPrimaryKey() throws EJBException {
+		return _mainPrimaryKey;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.IDOEntity#store()
+	 */
+	public void store() throws IDOStoreException {
+		if (_mainEntity != null) {
+			_mainEntity.store();
+		}
+		if (_currentOpenVersionEntity != null) {
+			_currentOpenVersionEntity.store();
+		}
+		if (_translationEntity != null) {
+			_translationEntity.store();
+		}
+	}
+
+	//////IDOEntityBean	//////
+	public Object ejbCreate() throws CreateException {
+		if (_mainEntity != null) {
+			if (useVersions()) {
+				//TODO:Gummi create new version
+			}
+		} else {
+			_mainEntity = getMainEntityHome().createIDO();
+			_mainPrimaryKey = _mainEntity.getPrimaryKey();
+		}
+
+		return this;
+	}
+
+	public Object ejbFindByPrimaryKey(Object pk) throws FinderException {
+		_mainEntity = _mainEntityHome.findByPrimaryKeyIDO(pk);
+		_mainPrimaryKey = pk;
+		return _mainPrimaryKey;
+	}
+
+	public void setEJBLocalHome(javax.ejb.EJBLocalHome ejbHome) {
+		_ejbHome = ejbHome;
+	}
+	/**
+	 * Meant to be overrided in subclasses, returns default Integer.class
+	 */
+	public Class getPrimaryKeyClass() {
+		return Integer.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.ejb.EJBLocalObject#remove()
+	 */
+	public void remove() throws RemoveException, EJBException {
+		if (_currentOpenVersionEntity != null) {
+			_currentOpenVersionEntity.remove();
+		}
+		if (_translationEntity != null) {
+			_translationEntity.remove();
+		}
+		if (_mainEntity != null) {
+			_mainEntity.remove();
+		}
+	}
+
+	public java.util.Collection getAttributes() {
+		//	TODO - implement
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	//////IDOEntityBean ends //////
+
+	//////EntityBean //////
+	public void ejbActivate() throws EJBException, RemoteException {
+	}
+
+	public void ejbLoad() throws EJBException, RemoteException {
+		((IDOEntityBean)_mainEntity).ejbLoad();
+
+		if (_translationEntity != null) {
+			((IDOEntityBean)_translationEntity).ejbLoad();
+		}
+		if (_currentOpenVersionEntity != null) {
+			((IDOEntityBean)_currentOpenVersionEntity).ejbLoad();
+		}
+	}
+
+	public void ejbPassivate() throws EJBException, RemoteException {
+		_locale = null;
+
+		_mainClass = null;
+		_translationClass = null;
+
+		_mainPrimaryKey = null;
+
+		_mainEntity = null;
+		_currentOpenVersionEntity = null;
+		_translationEntity = null;
+
+		_mainEntityHome = null;
+		_translationEntityHome = null;
+
+	}
+
+	public void ejbRemove() throws javax.ejb.RemoveException, EJBException, RemoteException {
+		this.remove();
+	}
+
+	public void ejbStore() throws EJBException, RemoteException {
+		this.store();
+	}
+
+	public void setEntityContext(javax.ejb.EntityContext ctx) throws EJBException, RemoteException {
+		this._entityContext = ctx;
+	}
+	public void unsetEntityContext() throws EJBException, RemoteException {
+		this._entityContext = null;
+	}
+
+	//////EntityBean ends //////
+
+	/**
+	 * Default create method for IDO
+	 **/
+	public Object ejbCreateIDO() throws CreateException {
+		return ejbCreate();
+	}
+	/**
+	 * Default create method for IDO
+	 **/
+	public IDOEntity ejbHomeCreateIDO() throws CreateException {
+		throw new UnsupportedOperationException("Not implemented");
+		//return ejbCreate();
+	}
+	/**
+	 * Default postcreate method for IDO
+	 **/
+	public void ejbPostCreateIDO() {
+		//does nothing
+	}
+
+	/**
+	 * Default findByPrimaryKey method for IDO
+	 **/
+	public Object ejbFindByPrimaryKeyIDO(Object pk) throws FinderException {
+		return ejbFindByPrimaryKey(pk);
+	}
+
+	////////
+
+	public javax.ejb.EJBLocalHome getEJBLocalHome() {
+		if (_ejbHome == null) {
+			try {
+				_ejbHome = IDOLookup.getHome(this.getClass());
+			} catch (Exception e) {
+				throw new EJBException("Lookup for home for: " + this.getClass().getName() + " failed. Errormessage was: " + e.getMessage());
+			}
+		}
+		return _ejbHome;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.IDOEntity#getEntityDefinition()
+	 */
+	public IDOEntityDefinition getEntityDefinition() {
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.ejb.EJBLocalObject#isIdentical(javax.ejb.EJBLocalObject)
+	 */
+	public boolean isIdentical(EJBLocalObject arg0) throws EJBException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+		 * @see com.idega.data.MetadataRelation#getMetaData(java.lang.String)
+		 */
+	public String getMetaData(String metaDataKey) {
+		return ((MetaDataCapable)this.getCurrentOpenVersionEntity()).getMetaData(metaDataKey);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.MetadataRelation#setMetaDataAttributes(java.util.Hashtable)
+	 */
+	public void setMetaDataAttributes(Hashtable metaDataAttribs) {
+		((MetaDataCapable)this.getCurrentOpenVersionEntity()).setMetaDataAttributes(metaDataAttribs);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.MetadataRelation#setMetaData(java.lang.String, java.lang.String)
+	 */
+	public void setMetaData(String metaDataKey, String metaDataValue) {
+		((MetaDataCapable)this.getCurrentOpenVersionEntity()).setMetaData(metaDataKey, metaDataValue);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.MetadataRelation#removeMetaData(java.lang.String)
+	 */
+	public boolean removeMetaData(String metaDataKey) {
+		return ((MetaDataCapable)this.getCurrentOpenVersionEntity()).removeMetaData(metaDataKey);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.MetadataRelation#getMetaDataAttributes()
+	 */
+	public Hashtable getMetaDataAttributes() {
+		return ((MetaDataCapable)this.getCurrentOpenVersionEntity()).getMetaDataAttributes();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.MetadataRelation#updateMetaData()
+	 */
+	public void updateMetaData() throws SQLException {
+		((MetaDataCapable)this.getCurrentOpenVersionEntity()).updateMetaData();
+	}
+}
---
+++ src/java/com/idega/data/TreeableEntityWrapper.java
@@ -0,0 +1,168 @@
+/*
+ * Created on 24.6.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.idega.data;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import javax.ejb.FinderException;
+
+import com.idega.core.ICTreeNode;
+import com.idega.core.data.ICLocale;
+
+/**
+ * Title:		TreeableEntityWrapper
+ * Description:
+ * Copyright:	Copyright (c) 2003
+ * Company:		idega Software
+ * @author		2003 - idega team - <br><a href="mailto:gummi@idega.is">Gudmundur Agust Saemundsson</a><br>
+ * @version		1.0
+ */
+public abstract class TreeableEntityWrapper extends IDOEntityWrapper implements TreeableEntity, ICTreeNode {
+
+	/**
+	 * @param primaryKey
+	 * @throws IDOLookupException
+	 * @throws FinderException
+	 */
+	public TreeableEntityWrapper(Object primaryKey) throws IDOLookupException, FinderException {
+		super(primaryKey);
+	}
+
+	/**
+	 * @param primaryKey
+	 * @param locale
+	 * @throws IDOLookupException
+	 * @throws FinderException
+	 */
+	public TreeableEntityWrapper(Object primaryKey, ICLocale locale) throws IDOLookupException, FinderException {
+		super(primaryKey, locale);
+	}
+
+	
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#addChild(com.idega.data.TreeableEntity)
+	 */
+	public void addChild(TreeableEntity p0) throws SQLException {
+		((TreeableEntity)this.getMainEntity()).addChild(p0);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getAllowsChildren()
+	 */
+	public boolean getAllowsChildren() {
+		return ((TreeableEntity)this.getMainEntity()).getAllowsChildren();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getChildAtIndex(int)
+	 */
+	public ICTreeNode getChildAtIndex(int p0) {
+		return ((TreeableEntity)this.getMainEntity()).getChildAtIndex(p0);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getChildCount()
+	 */
+	public int getChildCount() {
+		return ((TreeableEntity)this.getMainEntity()).getChildCount();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#getChildren(java.lang.String)
+	 */
+	public Iterator getChildren(String p0) {
+		return ((TreeableEntity)this.getMainEntity()).getChildren(p0);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getChildren()
+	 */
+	public Iterator getChildren() {
+		return ((TreeableEntity)this.getMainEntity()).getChildren();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getIndex(com.idega.core.ICTreeNode)
+	 */
+	public int getIndex(ICTreeNode p0) {
+		return ((TreeableEntity)this.getMainEntity()).getIndex(p0);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getNodeID()
+	 */
+	public int getNodeID() {
+		return ((TreeableEntity)this.getMainEntity()).getNodeID();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getNodeName()
+	 */
+	public String getNodeName() {
+		return ((TreeableEntity)this.getMainEntity()).getNodeName();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#getParentEntity()
+	 */
+	public TreeableEntity getParentEntity() {
+		return ((TreeableEntity)this.getMainEntity()).getParentEntity();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getParentNode()
+	 */
+	public ICTreeNode getParentNode() {
+		return ((TreeableEntity)this.getMainEntity()).getParentNode();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#getSiblingCount()
+	 */
+	public int getSiblingCount() {
+		return ((TreeableEntity)this.getMainEntity()).getSiblingCount();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#getTreeRelationshipChildColumnName(com.idega.data.TreeableEntity)
+	 */
+	public String getTreeRelationshipChildColumnName(TreeableEntity p0) {
+		return ((TreeableEntity)this.getMainEntity()).getTreeRelationshipChildColumnName(p0);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#getTreeRelationshipTableName(com.idega.data.TreeableEntity)
+	 */
+	public String getTreeRelationshipTableName(TreeableEntity p0) {
+		return ((TreeableEntity)this.getMainEntity()).getTreeRelationshipTableName(p0);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.ICTreeNode#isLeaf()
+	 */
+	public boolean isLeaf() {
+		return ((TreeableEntity)this.getMainEntity()).isLeaf();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#moveChildrenFrom(com.idega.data.TreeableEntity)
+	 */
+	public void moveChildrenFrom(TreeableEntity p0) throws SQLException {
+		((TreeableEntity)this.getMainEntity()).moveChildrenFrom(p0);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.TreeableEntity#removeChild(com.idega.data.TreeableEntity)
+	 */
+	public void removeChild(TreeableEntity p0) throws SQLException {
+		((TreeableEntity)this.getMainEntity()).removeChild(p0);
+	}
+
+}
