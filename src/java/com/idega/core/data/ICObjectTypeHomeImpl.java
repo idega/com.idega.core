9f2474ee9c6bb508883edd6baf472698a368ea13
Grimur Jonsson <gimmi@idega.com>
2003-07-28 10:40:55 +0000

    *** empty log message ***

---
+++ src/java/com/idega/core/data/ICObjectTypeHomeImpl.java
@@ -0,0 +1,29 @@
+package com.idega.core.data;
+
+
+public class ICObjectTypeHomeImpl extends com.idega.data.IDOFactory implements ICObjectTypeHome
+{
+ protected Class getEntityInterfaceClass(){
+  return ICObjectType.class;
+ }
+
+
+ public ICObjectType create() throws javax.ejb.CreateException{
+  return (ICObjectType) super.createIDO();
+ }
+
+
+ public ICObjectType findByPrimaryKey(Object pk) throws javax.ejb.FinderException{
+  return (ICObjectType) super.findByPrimaryKeyIDO(pk);
+ }
+
+
+public java.util.Collection findAll()throws javax.ejb.FinderException{
+	com.idega.data.IDOEntity entity = this.idoCheckOutPooledEntity();
+	java.util.Collection theReturn = ((ICObjectTypeBMPBean)entity).ejbHomeFindAll();
+	this.idoCheckInPooledEntity(entity);
+	return theReturn;
+}
+
+
+}
