d71b90b1325ebabf103e41c721c6f7d8d5a12bbb
Thorhallur Helgason <laddi@idega.com>
2003-09-26 07:23:44 +0000

    Implemented getTableColumnNames(String, String

--- src/java/com/idega/data/OracleDatastoreInterface.java
+++ src/java/com/idega/data/OracleDatastoreInterface.java
@@ -11,116 +11,89 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.Vector;
 
 import oracle.jdbc.OracleResultSet;
 import oracle.sql.CLOB;
 
+import com.idega.util.database.ConnectionBroker;
 
 /**
- * @author eiki
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * A class for database abstraction for the Oracle Database.
+ * This is an implemention that overrides implementations from com.idega.data.DatastoreInterface 
+ * and performs specific functionality to the Oracle JDBC driver and database.
+ * Copyright 2000-2002 idega software All Rights Reserved.
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
  */
-/**
-*A class for database abstraction for the Oracle Database.
-* This is an implemention that ovverrides implementations from com.idega.data.DatastoreInterface 
-* and performs specific functionality to the Oracle JDBC driver and database.
-* Copyright 2000-2002 idega software All Rights Reserved.
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-*@version 1.0
-*/
 public class OracleDatastoreInterface extends DatastoreInterface {
-	
-	protected OracleDatastoreInterface(){
-		super();	
+
+	protected OracleDatastoreInterface() {
+		super();
 		EntityControl.limitTableNameToThirtyCharacters = true;
 	}
 	
-	/*public String getSQLType(String javaClassName){
-	  String theReturn;
-	  if (javaClassName.equals("java.lang.Integer")){
-	    theReturn = "NUMBER";
-	  }
-	  else if (javaClassName.equals("java.lang.String")){
-	    theReturn = "VARCHAR2";
-	  }
-	  else if (javaClassName.equals("java.lang.Boolean")){
-	    theReturn = "CHAR";
-	  }
-	  else if (javaClassName.equals("java.lang.Float")){
-	    theReturn = "DOUBLE";
-	  }
-	  else if (javaClassName.equals("java.lang.Double")){
-	    theReturn = "DOUBLE";
-	  }
-	  else if (javaClassName.equals("java.sql.Timestamp")){
-	    theReturn = "DATE";
-	  }
-	  else if (javaClassName.equals("java.sql.Date")){
-	    theReturn = "DATE";
-	  }
-	  else if (javaClassName.equals("java.sql.Blob")){
-	    theReturn = "BLOB";
-	  }
-	  else if (javaClassName.equals("java.sql.Time")){
-	    theReturn = "TIME";
-	  }
-	  else{
-	    theReturn = "";
-	  }
-	  return theReturn;
-	}*/
 	public String getSQLType(String javaClassName, int maxlength) {
 		String theReturn;
 		if (javaClassName.equals("java.lang.Integer")) {
 			theReturn = "NUMBER";
 		}
-		else if (javaClassName.equals("java.lang.String")) {
-			if (maxlength < 0) {
-				theReturn = "VARCHAR2(255)";
+		else
+			if (javaClassName.equals("java.lang.String")) {
+				if (maxlength < 0) {
+					theReturn = "VARCHAR2(255)";
+				}
+				else
+					if (maxlength <= 4000) {
+						theReturn = "VARCHAR2(" + maxlength + ")";
+					}
+					else {
+						theReturn = "CLOB";
+					}
 			}
-			else if (maxlength <= 4000) {
-				theReturn = "VARCHAR2(" + maxlength + ")";
-			}
-			else {
-				theReturn = "CLOB";
-			}
-		}
-		else if (javaClassName.equals("java.lang.Boolean")) {
-			theReturn = "CHAR(1)";
-		}
-		else if (javaClassName.equals("java.lang.Float")) {
-			theReturn = "FLOAT";
-		}
-		else if (javaClassName.equals("java.lang.Double")) {
-			theReturn = "FLOAT";
-		}
-		else if (javaClassName.equals("java.sql.Timestamp")) {
-			theReturn = "DATE";
-		}
-		else if (javaClassName.equals("java.sql.Date") || javaClassName.equals("java.util.Date")) {
-			theReturn = "DATE";
-		}
-		else if (javaClassName.equals("java.sql.Blob")) {
-			theReturn = "BLOB";
-		}
-		else if (javaClassName.equals("java.sql.Time")) {
-			theReturn = "TIME";
-		}
-		else if (javaClassName.equals("com.idega.util.Gender")) {
-			theReturn = "VARCHAR(1)";
-		}
-		else if (javaClassName.equals("com.idega.data.BlobWrapper")) {
-			theReturn = "BLOB";
-		}
-		else {
-			theReturn = "";
-		}
+			else
+				if (javaClassName.equals("java.lang.Boolean")) {
+					theReturn = "CHAR(1)";
+				}
+				else
+					if (javaClassName.equals("java.lang.Float")) {
+						theReturn = "FLOAT";
+					}
+					else
+						if (javaClassName.equals("java.lang.Double")) {
+							theReturn = "FLOAT";
+						}
+						else
+							if (javaClassName.equals("java.sql.Timestamp")) {
+								theReturn = "DATE";
+							}
+							else
+								if (javaClassName.equals("java.sql.Date") || javaClassName.equals("java.util.Date")) {
+									theReturn = "DATE";
+								}
+								else
+									if (javaClassName.equals("java.sql.Blob")) {
+										theReturn = "BLOB";
+									}
+									else
+										if (javaClassName.equals("java.sql.Time")) {
+											theReturn = "TIME";
+										}
+										else
+											if (javaClassName.equals("com.idega.util.Gender")) {
+												theReturn = "VARCHAR(1)";
+											}
+											else
+												if (javaClassName.equals("com.idega.data.BlobWrapper")) {
+													theReturn = "BLOB";
+												}
+												else {
+													theReturn = "";
+												}
 		return theReturn;
 	}
+	
 	public void createTrigger(GenericEntity entity) throws Exception {
 		createSequence(entity);
 		Connection conn = null;
@@ -128,18 +101,7 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 		try {
 			conn = entity.getConnection();
 			Stmt = conn.createStatement();
-			Stmt.executeUpdate(
-					"CREATE TRIGGER "
-						+ entity.getTableName()
-						+ "_trig BEFORE INSERT ON "
-						+ entity.getTableName()
-						+ " FOR EACH ROW WHEN (NEW."
-						+ entity.getIDColumnName()
-						+ " is null) DECLARE TEMP INTEGER; BEGIN SELECT "
-						+ entity.getTableName()
-						+ "_seq.NEXTVAL INTO TEMP FROM DUAL; :NEW."
-						+ entity.getIDColumnName()
-						+ ":=TEMP;END;");
+			Stmt.executeUpdate("CREATE TRIGGER " + entity.getTableName() + "_trig BEFORE INSERT ON " + entity.getTableName() + " FOR EACH ROW WHEN (NEW." + entity.getIDColumnName() + " is null) DECLARE TEMP INTEGER; BEGIN SELECT " + entity.getTableName() + "_seq.NEXTVAL INTO TEMP FROM DUAL; :NEW." + entity.getIDColumnName() + ":=TEMP;END;");
 		}
 		finally {
 			if (Stmt != null) {
@@ -150,24 +112,18 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 			}
 		}
 	}
-	
-	
-	
+
 	public void createSequence(GenericEntity entity) throws Exception {
 		createSequence(entity, 1);
 	}
+	
 	public void createSequence(GenericEntity entity, int startNumber) throws Exception {
 		Connection conn = null;
 		Statement Stmt = null;
 		try {
 			conn = entity.getConnection();
 			Stmt = conn.createStatement();
-			String seqCreate =
-				"create sequence "
-					+ entity.getTableName()
-					+ "_seq INCREMENT BY 1 START WITH "
-					+ startNumber
-					+ " MAXVALUE 1.0E28 MINVALUE 0 NOCYCLE CACHE 20 NOORDER";
+			String seqCreate = "create sequence " + entity.getTableName() + "_seq INCREMENT BY 1 START WITH " + startNumber + " MAXVALUE 1.0E28 MINVALUE 0 NOCYCLE CACHE 20 NOORDER";
 			Stmt.executeUpdate(seqCreate);
 		}
 		finally {
@@ -179,34 +135,13 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 			}
 		}
 	}
-	
-	/*
-	public void createSequence(IDOLegacyEntity entity) throws Exception {
-		Connection conn = null;
-		Statement Stmt = null;
-		try {
-			conn = entity.getConnection();
-			Stmt = conn.createStatement();
-			int i =
-				Stmt.executeUpdate(
-					"create sequence "
-						+ entity.getTableName()
-						+ "_seq INCREMENT BY 1 START WITH 1 MAXVALUE 1.0E28 MINVALUE 1 NOCYCLE CACHE 20 NOORDER");
-		}
-		finally {
-			if (Stmt != null) {
-				Stmt.close();
-			}
-			if (conn != null) {
-				entity.freeConnection(conn);
-			}
-		}
-	}*/
+
 	public void deleteEntityRecord(GenericEntity entity) throws Exception {
 		super.deleteEntityRecord(entity);
 		deleteTrigger(entity);
 		deleteSequence(entity);
 	}
+	
 	protected void deleteTrigger(GenericEntity entity) throws Exception {
 		Connection conn = null;
 		Statement Stmt = null;
@@ -224,6 +159,7 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 			}
 		}
 	}
+	
 	protected void deleteSequence(GenericEntity entity) throws Exception {
 		Connection conn = null;
 		Statement Stmt = null;
@@ -241,119 +177,27 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 			}
 		}
 	}
-	/*public void createForeignKeys(IDOLegacyEntity entity)throws Exception{
-			Connection conn= null;
-			Statement Stmt= null;
-			try{
-				conn = entity.getConnection();
-	 		        String[] names = entity.getColumnNames();
-			        for (int i = 0; i < names.length; i++){
-	                        if (!entity.getRelationShipClassName(names[i]).equals("")){
-	                          Stmt = conn.createStatement();
-	                          String statement = "ALTER TABLE "+entity.getTableName()+" ADD FOREIGN KEY ("+names[i]+") REFERENCES "+((IDOLegacyEntity)Class.forName(entity.getRelationShipClassName(names[i])).newInstance()).getTableName()+" ";
-	                          System.out.println(statement);
-				    int n = Stmt.executeUpdate(statement);
-	                  	    if(Stmt != null){
-					Stmt.close();
-				    }
-	                        }
-	                      }	
-			}
-			finally{
-				if(Stmt != null){
-					Stmt.close();
-				}
-				if (conn != null){
-					entity.freeConnection(conn);
-				}
-			}
-	}*/
+	
 	protected void executeBeforeInsert(GenericEntity entity) throws Exception {
 		if (entity.isNull(entity.getIDColumnName())) {
 			entity.setID(createUniqueID(entity));
 		}
 	}
-	
-	/*
-	protected void insertBlob(IDOLegacyEntity entity) throws Exception {
-		Connection Conn = null;
-		BLOB blob;
-		try {
-			Conn = entity.getConnection();
-			if (Conn == null)
-				return;
-			//Conn.setAutoCommit(false);
-			Statement stmt2 = Conn.createStatement();
-			String cmd =
-				"SELECT "
-					+ entity.getLobColumnName()
-					+ " FROM "
-					+ entity.getEntityName()
-					+ " WHERE "
-					+ entity.getIDColumnName()
-					+ " ='"
-					+ entity.getPrimaryKey()
-					+ "' FOR UPDATE ";
-			ResultSet RS2 = stmt2.executeQuery(cmd);
-			RS2.next();
-			//insert into empty_blob()
-			
-			blob = ((OracleResultSet) RS2).getBLOB(1);
-			// write the array of binary data to a BLOB
-			OutputStream outstream = blob.getBinaryOutputStream();
-			int size = blob.getBufferSize();
-			byte[] buffer = new byte[size];
-			int length = -1;
-			BlobWrapper wrapper = entity.getBlobColumnValue(entity.getLobColumnName());
-			if (wrapper != null) {
-				BufferedInputStream in = new BufferedInputStream(wrapper.getInputStreamForBlobWrite());
-				while ((length = in.read(buffer)) != -1)
-					outstream.write(buffer, 0, length);
-				in.close();
-			}
-			outstream.flush();
-			outstream.close();
-			stmt2.close();
-			RS2.close();
-			//Conn.commit();
-			//Conn.setAutoCommit(true);
-		}
-		catch (SQLException ex) {
-			ex.printStackTrace();
-			System.err.println("error saving to db");
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		finally {
-			if (Conn != null)
-				entity.freeConnection(Conn);
-		}
-	}
-	
-	
-	*/
+
 	protected String getCreateUniqueIDQuery(GenericEntity entity) {
 		return "SELECT " + getOracleSequenceName(entity) + ".nextval FROM dual";
 	}
+	
 	private static String getSequenceName(GenericEntity entity) {
 		return getOracleSequenceName(entity);
 	}
+	
 	private static String getOracleSequenceName(GenericEntity entity) {
 		String entityName = entity.getTableName();
 		return entityName + "_seq";
-		/*if (entityName.endsWith("_")){
-		return entityName+"seq";
-		}
-		else{
-		return entityName+"_seq";
-		}*/
 	}
 
-
 	public void setNumberGeneratorValue(GenericEntity entity, int value) {
-		//throw new RuntimeException("setSequenceValue() not implemented for "+this.getClass().getName());
-		//String statement = "update sequences set last_number="+value+" where sequence_name='"+this.getSequenceName(entity)+"'";
 		String statement = "drop sequence " + this.getSequenceName(entity);
 		try {
 			this.executeUpdate(entity, statement);
@@ -367,8 +211,8 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 	/**
 	 * Override in subclasses
 	 **/
-	public void onConnectionCreate(Connection newConn){
-		try{
+	public void onConnectionCreate(Connection newConn) {
+		try {
 			Statement stmt = newConn.createStatement();
 			stmt.execute("ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS'");
 			stmt.close();
@@ -377,21 +221,19 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 			stmt.close();
 			System.out.println("OracleDatastoreInterface: Setting date format environment variable for Oracle: ");
 		}
-		catch(SQLException sqle){		
-			System.err.println("OracleDatastoreInterface: Error setting date format environment variable for Oracle: "+sqle.getMessage());
+		catch (SQLException sqle) {
+			System.err.println("OracleDatastoreInterface: Error setting date format environment variable for Oracle: " + sqle.getMessage());
 		}
 	}
-
-
 
 	/**
 	 * Varchar is limited to 4000 chars need to use clob for larger fields. Great example http://www.experts-exchange.com/Databases/Oracle/Q_20358143.html
 	 * @see com.idega.data.DatastoreInterface#fillStringColumn(GenericEntity, String, ResultSet)
 	 */
 	protected void fillStringColumn(GenericEntity entity, String columnName, ResultSet rs) throws SQLException {
-		
+
 		int maxlength = entity.getMaxLength(columnName);
-		if (maxlength<=4000) {
+		if (maxlength <= 4000) {
 			String string = rs.getString(columnName);
 			if (string != null) {
 				entity.setColumn(columnName, string);
@@ -399,25 +241,25 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 		}
 		else {
 			try {
-				Reader chrInstream;           // Unicode clob reader
-			    char chrBuffer[];                 // Clob buffer
+				Reader chrInstream; // Unicode clob reader
+				char chrBuffer[]; // Clob buffer
 				CLOB clob = ((OracleResultSet) rs).getCLOB(columnName);
-				
-				if( clob!=null ){
+
+				if (clob != null) {
 					//set buffersize
-					chrBuffer = new char[(int)clob.length()];
-	        		
-	        		// Now get as a unicode stream.
-			        chrInstream = clob.getCharacterStream();
-			        
-			        if(chrInstream!=null ){       
-				        chrInstream.read( chrBuffer );
-				       
-				        String value = new String(chrBuffer);
-				        entity.setColumn(columnName, value); 
-			        }
+					chrBuffer = new char[(int) clob.length()];
+
+					// Now get as a unicode stream.
+					chrInstream = clob.getCharacterStream();
+
+					if (chrInstream != null) {
+						chrInstream.read(chrBuffer);
+
+						String value = new String(chrBuffer);
+						entity.setColumn(columnName, value);
+					}
 				}
-				
+
 			}
 			catch (IOException io) {
 				throw new SQLException("IOException: " + io.getMessage());
@@ -428,7 +270,7 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 		}
 	}
 
-	protected void setStringForPreparedStatement(String columnName, PreparedStatement statement, int index, GenericEntity entity) throws SQLException{
+	protected void setStringForPreparedStatement(String columnName, PreparedStatement statement, int index, GenericEntity entity) throws SQLException {
 		try {
 			int maxlength = entity.getMaxLength(columnName);
 			if (maxlength <= 4000) {
@@ -437,18 +279,44 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 			else {
 				//collect clobs
 				String stringValue = entity.getStringColumnValue(columnName);
-								
-				Reader reader = new StringReader(stringValue);
-				//InputStream stream = new StringInputStream(stringValue);				
-				//statement.setAsciiStream(index, stream, stringValue.length());
 
-				statement.setCharacterStream(index,reader, stringValue.length());
-				
+				Reader reader = new StringReader(stringValue);
+				statement.setCharacterStream(index, reader, stringValue.length());
 			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.DatastoreInterface#getTableColumnNames(java.lang.String, java.lang.String)
+	 */
+	public String[] getTableColumnNames(String dataSourceName, String tableName) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		List columns = new Vector();
+		try {
+			conn = ConnectionBroker.getConnection(dataSourceName);
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM USER_TAB_COLUMNS WHERE TABLE_NAME = '" + tableName.toUpperCase() + "'");
+			while (rs.next()) {
+				columns.add(rs.getString("COLUMN_NAME"));
+			}
+			rs.close();
+			return (String[]) columns.toArray(new String[0]);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (conn != null) {
+				ConnectionBroker.freeConnection(conn);
+			}
+		}
+		if (columns != null && !columns.isEmpty())
+			return (String[]) columns.toArray(new String[0]);
+		return null;
+	}
 }
