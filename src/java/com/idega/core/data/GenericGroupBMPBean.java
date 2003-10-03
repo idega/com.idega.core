6451d860214d30664fcd5a3ffc90f058ada30414
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2003-10-03 01:52:09 +0000

    Fixed and organised imports.

--- src/java/com/idega/core/data/GenericGroupBMPBean.java
+++ src/java/com/idega/core/data/GenericGroupBMPBean.java
@@ -1,14 +1,21 @@
 package com.idega.core.data;
-import java.sql.*;
-import com.idega.data.*;
-import com.idega.util.ListUtil;
-
-import java.util.Vector;
-import com.idega.core.user.data.User;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import com.idega.core.net.data.ICNetwork;
+import com.idega.core.net.data.ICProtocol;
+import com.idega.core.user.data.User;
+import com.idega.data.EntityFinder;
+import com.idega.data.IDOLegacyEntity;
+import com.idega.data.IDORuntimeException;
+import com.idega.data.SimpleQuerier;
+import com.idega.util.ListUtil;
 /**
  * 
  * Title:        IW Core
@@ -19,14 +26,18 @@ import java.util.Iterator;
  * @author <a href="mailto:gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
  * @version 1.0
  */
-public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements com.idega.core.data.GenericGroup {
-	public GenericGroupBMPBean() {
+public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements com.idega.core.data.GenericGroup
+{
+	public GenericGroupBMPBean()
+	{
 		super();
 	}
-	public GenericGroupBMPBean(int id) throws SQLException {
+	public GenericGroupBMPBean(int id) throws SQLException
+	{
 		super(id);
 	}
-	public void initializeAttributes() {
+	public void initializeAttributes()
+	{
 		addAttribute(getIDColumnName());
 		addAttribute(getNameColumnName(), "Hópnafn", true, true, "java.lang.String");
 		addAttribute(getGroupTypeColumnName(), "Hópgerð", true, true, "java.lang.String");
@@ -36,12 +47,12 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 		this.addManyToManyRelationShip(ICNetwork.class, "ic_group_network");
 		this.addManyToManyRelationShip(ICProtocol.class, "ic_group_protocol");
 	}
-	public String getEntityName() {
+	public String getEntityName()
+	{
 		return "ic_group";
 	}
-
-  
-	public void setDefaultValues() {
+	public void setDefaultValues()
+	{
 		setGroupType(getGroupTypeValue());
 	}
 	/**
@@ -49,60 +60,79 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 	 * overwrite in extended classes
 	
 	 */
-	public String getGroupTypeValue() {
+	public String getGroupTypeValue()
+	{
 		return "general";
 	}
 	/*  ColumNames begin   */
-	public static String getColumnNameGroupID() {
+	public static String getColumnNameGroupID()
+	{
 		return "ic_group_id";
 	}
-	public static String getNameColumnName() {
+	public static String getNameColumnName()
+	{
 		return "name";
 	}
-	public static String getGroupTypeColumnName() {
+	public static String getGroupTypeColumnName()
+	{
 		return "group_type";
 	}
-	public static String getGroupDescriptionColumnName() {
+	public static String getGroupDescriptionColumnName()
+	{
 		return "description";
 	}
-	public static String getExtraInfoColumnName() {
+	public static String getExtraInfoColumnName()
+	{
 		return "extra_info";
 	}
 	/*  ColumNames end   */
 	/*  functions begin   */
-	public String getName() {
+	public String getName()
+	{
 		return (String) getColumnValue(getNameColumnName());
 	}
-	public void setName(String name) {
+	public void setName(String name)
+	{
 		setColumn(getNameColumnName(), name);
 	}
-	public String getGroupType() {
+	public String getGroupType()
+	{
 		return (String) getColumnValue(getGroupTypeColumnName());
 	}
-	public void setGroupType(String groupType) {
+	public void setGroupType(String groupType)
+	{
 		setColumn(getGroupTypeColumnName(), groupType);
 	}
-	public String getDescription() {
+	public String getDescription()
+	{
 		return (String) getColumnValue(getGroupDescriptionColumnName());
 	}
-	public void setDescription(String description) {
+	public void setDescription(String description)
+	{
 		setColumn(getGroupDescriptionColumnName(), description);
 	}
-	public String getExtraInfo() {
+	public String getExtraInfo()
+	{
 		return (String) getColumnValue(getExtraInfoColumnName());
 	}
-	public void setExtraInfo(String extraInfo) {
+	public void setExtraInfo(String extraInfo)
+	{
 		setColumn(getExtraInfoColumnName(), extraInfo);
 	}
-	public static GenericGroup getStaticInstance() {
+	public static GenericGroup getStaticInstance()
+	{
 		return (GenericGroup) getStaticInstance(GenericGroup.class);
 	}
 	//??
-	public GenericGroup[] getAllGroupsContainingThis() throws SQLException {
+	public GenericGroup[] getAllGroupsContainingThis() throws SQLException
+	{
 		List vector = this.getParentGroups();
-		if (vector != null) {
+		if (vector != null)
+		{
 			return (GenericGroup[]) vector.toArray((Object[]) java.lang.reflect.Array.newInstance(this.getClass(), 0));
-		} else {
+		}
+		else
+		{
 			return new GenericGroup[0];
 		}
 	}
@@ -118,32 +148,34 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 	//		throw new SQLException(e.getMessage());	
 	//	}
 	//}
-	
 	/**
 	 * Gets the groups that are direct parents of this group
 	 **/
-	public List getParentGroups(){
+	public List getParentGroups()
+	{
 		//return getParentGroupsLegacy();
-		return ListUtil.convertCollectionToList(getParentGroupsLegacy());	
+		return ListUtil.convertCollectionToList(getParentGroupsLegacy());
 	}
-	
 	/**
 	 * Gets the groups that are direct parents of this group
 	 * The old implementation. Uses IC_GROUP_TREE to find parent groups.
 	 **/
-	protected Collection getParentGroupsLegacy(){
-		try{
+	protected Collection getParentGroupsLegacy()
+	{
+		try
+		{
 			return this.getListOfAllGroupsContaining(this.getID());
 		}
-		catch(SQLException e){
-			throw new IDORuntimeException(e,this);	
+		catch (SQLException e)
+		{
+			throw new IDORuntimeException(e, this);
 		}
 	}
-	
 	/**
 	 * @deprecated Old implementation. Uses IC_GROUP_TREE to find parent groups.
 	 **/
-	protected List getListOfAllGroupsContaining(int group_id) throws SQLException {
+	protected List getListOfAllGroupsContaining(int group_id) throws SQLException
+	{
 		String tableToSelectFrom = "IC_GROUP_TREE";
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("select * from ");
@@ -156,80 +188,98 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 		Connection conn = null;
 		Statement Stmt = null;
 		Vector vector = new Vector();
-		try {
+		try
+		{
 			conn = getConnection(getDatasource());
 			Stmt = conn.createStatement();
 			ResultSet RS = Stmt.executeQuery(SQLString);
-			while (RS.next()) {
+			while (RS.next())
+			{
 				IDOLegacyEntity tempobj = null;
-				try {
+				try
+				{
 					tempobj = (IDOLegacyEntity) Class.forName(this.getClass().getName()).newInstance();
 					tempobj.findByPrimaryKey(RS.getInt(this.getIDColumnName()));
-				} catch (Exception ex) {
+				}
+				catch (Exception ex)
+				{
 					System.err.println(
-						"There was an error in "
-							+ this.getClass().getName()
-							+ ".getAllGroupsContainingThis(): "
-							+ ex.getMessage());
+						"There was an error in " + this.getClass().getName() + ".getAllGroupsContainingThis(): " + ex.getMessage());
 				}
 				vector.addElement(tempobj);
 			}
 			RS.close();
-		} finally {
-			if (Stmt != null) {
+		}
+		finally
+		{
+			if (Stmt != null)
+			{
 				Stmt.close();
 			}
-			if (conn != null) {
+			if (conn != null)
+			{
 				freeConnection(getDatasource(), conn);
 			}
 		}
-		if (vector != null) {
+		if (vector != null)
+		{
 			vector.trimToSize();
 			return vector;
 			//return (GenericGroup[]) vector.toArray((Object[])java.lang.reflect.Array.newInstance(this.getClass(),0));
-		} else {
+		}
+		else
+		{
 			return null;
 		}
 	}
 	//??
-	public GenericGroup[] getAllGroupsContained() throws SQLException {
+	public GenericGroup[] getAllGroupsContained() throws SQLException
+	{
 		List vector = this.getChildGroups();
-		if (vector != null) {
+		if (vector != null)
+		{
 			return (GenericGroup[]) vector.toArray((Object[]) java.lang.reflect.Array.newInstance(this.getClass(), 0));
-		} else {
+		}
+		else
+		{
 			return new GenericGroup[0];
 		}
 	}
 	/**
 	 * @deprecated Replaced with getChildGroups
 	 */
-	public List getListOfAllGroupsContained() throws SQLException {
-		try{
-			return ListUtil.convertCollectionToList(getChildGroups());	
+	public List getListOfAllGroupsContained() throws SQLException
+	{
+		try
+		{
+			return ListUtil.convertCollectionToList(getChildGroups());
 		}
-		catch(Exception e){
-			throw new SQLException(e.getMessage());	
+		catch (Exception e)
+		{
+			throw new SQLException(e.getMessage());
 		}
-	}	
-		
+	}
 	/**
 	 * Gets the groups that are direct children of this group
 	 **/
-	public List getChildGroups(){
-		try{
+	public List getChildGroups()
+	{
+		try
+		{
 			//return getChildGroupsLegacy();
-			return ListUtil.convertCollectionToList(getChildGroupsLegacy());	
+			return ListUtil.convertCollectionToList(getChildGroupsLegacy());
 		}
-		catch(SQLException e){
-			throw new IDORuntimeException(e,this);	
+		catch (SQLException e)
+		{
+			throw new IDORuntimeException(e, this);
 		}
 	}
-	
 	/**
 	 * @deprecated The implementation belonging to the old user system.
 	 * Gets the groups that are direct children of this group. Uses the IC_GROUP_TREE table to find children.
 	 **/
-	protected Collection getChildGroupsLegacy() throws SQLException {				
+	protected Collection getChildGroupsLegacy() throws SQLException
+	{
 		String tableToSelectFrom = "IC_GROUP_TREE";
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("select CHILD_IC_GROUP_ID from ");
@@ -242,38 +292,47 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 		Connection conn = null;
 		Statement Stmt = null;
 		Vector vector = new Vector();
-		try {
+		try
+		{
 			conn = getConnection(getDatasource());
 			Stmt = conn.createStatement();
 			ResultSet RS = Stmt.executeQuery(SQLString);
-			while (RS.next()) {
+			while (RS.next())
+			{
 				IDOLegacyEntity tempobj = null;
-				try {
+				try
+				{
 					tempobj = (IDOLegacyEntity) Class.forName(this.getClass().getName()).newInstance();
 					tempobj.findByPrimaryKey(RS.getInt("CHILD_IC_GROUP_ID"));
-				} catch (Exception ex) {
+				}
+				catch (Exception ex)
+				{
 					System.err.println(
-						"There was an error in "
-							+ this.getClass().getName()
-							+ ".getAllGroupsContainingThis(): "
-							+ ex.getMessage());
+						"There was an error in " + this.getClass().getName() + ".getAllGroupsContainingThis(): " + ex.getMessage());
 				}
 				vector.addElement(tempobj);
 			}
 			RS.close();
-		} finally {
-			if (Stmt != null) {
+		}
+		finally
+		{
+			if (Stmt != null)
+			{
 				Stmt.close();
 			}
-			if (conn != null) {
+			if (conn != null)
+			{
 				freeConnection(getDatasource(), conn);
 			}
 		}
-		if (vector != null) {
+		if (vector != null)
+		{
 			vector.trimToSize();
 			return vector;
 			//return (GenericGroup[]) vector.toArray((Object[])java.lang.reflect.Array.newInstance(this.getClass(),0));
-		} else {
+		}
+		else
+		{
 			return null;
 		}
 		//return (Group[])this.findReverseRelated(this);
@@ -283,33 +342,44 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 	 * @todo change implementation: let the database handle the filtering
 	
 	 */
-	public List getChildGroups(String[] groupTypes, boolean returnSepcifiedGroupTypes) throws SQLException {
+	public List getChildGroups(String[] groupTypes, boolean returnSepcifiedGroupTypes) throws SQLException
+	{
 		List list = this.getChildGroups();
 		List specifiedGroups = new Vector();
 		List notSpecifiedGroups = new Vector();
 		int j = 0;
 		int k = 0;
 		Iterator iter2 = list.iterator();
-		if (groupTypes != null && groupTypes.length > 0) {
+		if (groupTypes != null && groupTypes.length > 0)
+		{
 			boolean specified = false;
-			while (iter2.hasNext()) {
+			while (iter2.hasNext())
+			{
 				GenericGroup tempObj = (GenericGroup) iter2.next();
-				for (int i = 0; i < groupTypes.length; i++) {
-					if (tempObj.getGroupType().equals(groupTypes[i])) {
+				for (int i = 0; i < groupTypes.length; i++)
+				{
+					if (tempObj.getGroupType().equals(groupTypes[i]))
+					{
 						specifiedGroups.add(j++, tempObj);
 						specified = true;
 					}
 				}
-				if (!specified) {
+				if (!specified)
+				{
 					notSpecifiedGroups.add(k++, tempObj);
-				} else {
+				}
+				else
+				{
 					specified = false;
 				}
 			}
 			notSpecifiedGroups.remove(this);
 			specifiedGroups.remove(this);
-		} else {
-			while (iter2.hasNext()) {
+		}
+		else
+		{
+			while (iter2.hasNext())
+			{
 				GenericGroup tempObj = (GenericGroup) iter2.next();
 				notSpecifiedGroups.add(j++, tempObj);
 			}
@@ -318,53 +388,60 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 		}
 		return (returnSepcifiedGroupTypes) ? specifiedGroups : notSpecifiedGroups;
 	}
-	public List getAllGroupsContainingUser(User user) throws SQLException {
+	public List getAllGroupsContainingUser(User user) throws SQLException
+	{
 		return this.getListOfAllGroupsContaining(user.getGroupID());
 	}
-	public void addGroup(GenericGroup groupToAdd) throws SQLException {
+	public void addGroup(GenericGroup groupToAdd) throws SQLException
+	{
 		this.addGroup(groupToAdd.getID());
 	}
-	public void addGroup(int groupId) throws SQLException {
+	public void addGroup(int groupId) throws SQLException
+	{
 		Connection conn = null;
 		Statement Stmt = null;
-		try {
+		try
+		{
 			conn = getConnection(getDatasource());
 			Stmt = conn.createStatement();
-			String sql =
-				"insert into IC_GROUP_TREE ("
-					+ getIDColumnName()
-					+ ", CHILD_IC_GROUP_ID) values("
-					+ getID()
-					+ ","
-					+ groupId
-					+ ")";
+			String sql = "insert into IC_GROUP_TREE (" + getIDColumnName() + ", CHILD_IC_GROUP_ID) values(" + getID() + "," + groupId + ")";
 			//System.err.println(sql);
 			Stmt.executeUpdate(sql);
 			//System.err.println(sql);
-		} catch (Exception ex) {
+		}
+		catch (Exception ex)
+		{
 			ex.printStackTrace(System.out);
-		} finally {
-			if (Stmt != null) {
+		}
+		finally
+		{
+			if (Stmt != null)
+			{
 				Stmt.close();
 			}
-			if (conn != null) {
+			if (conn != null)
+			{
 				freeConnection(getDatasource(), conn);
 			}
 		}
 	}
-	public void removeGroup(GenericGroup entityToRemoveFrom) throws SQLException {
+	public void removeGroup(GenericGroup entityToRemoveFrom) throws SQLException
+	{
 		if ((entityToRemoveFrom.getID() == -1) || (entityToRemoveFrom.getID() == 0)) //removing all in middle table
 			this.removeGroup(entityToRemoveFrom.getID(), true);
 		else // just removing this particular one
 			this.removeGroup(entityToRemoveFrom.getID(), false);
 	}
-	public void removeGroup() throws SQLException {
+	public void removeGroup() throws SQLException
+	{
 		this.removeGroup(-1, true);
 	}
-	public void removeGroup(int groupId, boolean AllEntries) throws SQLException {
+	public void removeGroup(int groupId, boolean AllEntries) throws SQLException
+	{
 		Connection conn = null;
 		Statement Stmt = null;
-		try {
+		try
+		{
 			conn = getConnection(getDatasource());
 			Stmt = conn.createStatement();
 			String qry;
@@ -387,47 +464,64 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 						+ groupId
 						+ "'";
 			Stmt.executeUpdate(qry);
-		} catch (Exception ex) {
+		}
+		catch (Exception ex)
+		{
 			ex.printStackTrace(System.out);
-		} finally {
-			if (Stmt != null) {
+		}
+		finally
+		{
+			if (Stmt != null)
+			{
 				Stmt.close();
 			}
-			if (conn != null) {
+			if (conn != null)
+			{
 				freeConnection(getDatasource(), conn);
 			}
 		}
 	}
-	public static void addUser(int groupId, User user) throws SQLException {
+	public static void addUser(int groupId, User user) throws SQLException
+	{
 		((com.idega.core.data.GenericGroupHome) com.idega.data.IDOLookup.getHomeLegacy(GenericGroup.class))
 			.findByPrimaryKeyLegacy(groupId)
 			.addGroup(user.getGroupID());
 	}
-	public void addUser(User user) throws SQLException {
+	public void addUser(User user) throws SQLException
+	{
 		this.addGroup(user.getGroupID());
 	}
-	public void removeUser(User user) throws SQLException {
+	public void removeUser(User user) throws SQLException
+	{
 		this.removeGroup(user.getGroupID(), false);
 	}
-	public GenericGroup findGroup(String groupName) throws SQLException {
+	public GenericGroup findGroup(String groupName) throws SQLException
+	{
 		List group =
-			EntityFinder.findAllByColumn((IDOLegacyEntity)
-				com.idega.data.GenericEntity.getStaticInstance(this.getClass().getName()),
+			EntityFinder.findAllByColumn(
+				(IDOLegacyEntity) com.idega.data.GenericEntity.getStaticInstance(this.getClass().getName()),
 				getNameColumnName(),
 				groupName,
 				getGroupTypeColumnName(),
 				this.getGroupTypeValue());
-		if (group != null) {
+		if (group != null)
+		{
 			return (GenericGroup) group.get(0);
-		} else {
+		}
+		else
+		{
 			return null;
 		}
 	}
-	public static List getAllGroups(String[] groupTypes, boolean returnSepcifiedGroupTypes) throws SQLException {
+	public static List getAllGroups(String[] groupTypes, boolean returnSepcifiedGroupTypes) throws SQLException
+	{
 		String typeList = "";
-		if (groupTypes != null && groupTypes.length > 0) {
-			for (int g = 0; g < groupTypes.length; g++) {
-				if (g > 0) {
+		if (groupTypes != null && groupTypes.length > 0)
+		{
+			for (int g = 0; g < groupTypes.length; g++)
+			{
+				if (g > 0)
+				{
 					typeList += ", ";
 				}
 				typeList += "'" + groupTypes[g] + "'";
@@ -448,7 +542,8 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 			com.idega.core.data.GenericGroupBMPBean.getStaticInstance(),
 			com.idega.core.data.GenericGroupBMPBean.getNameColumnName());
 	}
-	protected boolean identicalGroupExistsInDatabase() throws Exception {
+	protected boolean identicalGroupExistsInDatabase() throws Exception
+	{
 		return SimpleQuerier.executeStringQuery(
 			"select * from "
 				+ this.getEntityName()
@@ -464,37 +559,53 @@ public class GenericGroupBMPBean extends com.idega.data.GenericEntity implements
 			this.getDatasource()).length
 			> 0;
 	}
-	public void insert() throws SQLException {
-		try {
+	public void insert() throws SQLException
+	{
+		try
+		{
 			//            if(!this.getName().equals("")){
-			if (identicalGroupExistsInDatabase()) {
+			if (identicalGroupExistsInDatabase())
+			{
 				throw new SQLException("group with same name and type already in database");
 			}
 			//            }
 			super.insert();
-		} catch (Exception ex) {
-			if (ex instanceof SQLException) {
+		}
+		catch (Exception ex)
+		{
+			if (ex instanceof SQLException)
+			{
 				throw (SQLException) ex;
-			} else {
+			}
+			else
+			{
 				//System.err.println(ex.getMessage());
 				//ex.printStackTrace();
 				throw new SQLException(ex.getMessage());
 			}
 		}
 	}
-	public boolean equals(IDOLegacyEntity entity) {
-		if (entity != null) {
-			if (entity instanceof GenericGroup) {
+	public boolean equals(IDOLegacyEntity entity)
+	{
+		if (entity != null)
+		{
+			if (entity instanceof GenericGroup)
+			{
 				return this.equals((GenericGroup) entity);
-			} else {
+			}
+			else
+			{
 				return super.equals(entity);
 			}
 		}
 		return false;
 	}
-	public boolean equals(GenericGroup group) {
-		if (group != null) {
-			if (group.getID() == this.getID()) {
+	public boolean equals(GenericGroup group)
+	{
+		if (group != null)
+		{
+			if (group.getID() == this.getID())
+			{
 				return true;
 			}
 			return false;
