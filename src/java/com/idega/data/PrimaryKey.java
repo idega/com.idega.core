72f93a13473fc1e5dbaa25a7d8b1a44030589fcd
Thorhallur Helgason <laddi@idega.com>
2003-10-10 09:33:03 +0000

    Added support for composite primary keys.Implements IDOPrimaryKey.

--- src/java/com/idega/data/PrimaryKey.java
+++ src/java/com/idega/data/PrimaryKey.java
@@ -1,35 +1,95 @@
-//idega 2000 - Tryggvi Larusson
-
 /*
-
-*Copyright 2000 idega.is All Rights Reserved.
-
-*/
-
-
-
+ * PrimaryKey.java, 9.10.2003 11:22:58 - laddi
+ * 
+ * Copyright (C) 2002 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ * 
+ */
 package com.idega.data;
 
-
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
+ * @author laddi
+ */
+public class PrimaryKey implements IDOPrimaryKey {
 
- *
+	Map _primaryKeyMap;
+	
+	public PrimaryKey() {
+		_primaryKeyMap = new HashMap();
+	}
 
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+	/* (non-Javadoc)
+	 * @see com.idega.data.IDOPrimaryKey#getInstance()
+	 */
+	public IDOPrimaryKey getInstance() {
+		return new PrimaryKey();
+	}
+	
+	private boolean isComposite() {
+		if (_primaryKeyMap.size() > 1)
+			return true;
+		return false;
+	}
 
-*@version 0.9
+	/* (non-Javadoc)
+	 * @see com.idega.data.IDOPrimaryKey#getPrimaryKeyValue(java.lang.String)
+	 */
+	public Object getPrimaryKeyValue(String columnName) {
+		return _primaryKeyMap.get(columnName.toUpperCase());
+	}
 
-* Unimplemented Class
+	/* (non-Javadoc)
+	 * @see com.idega.data.IDOPrimaryKey#setPrimaryKeyValue(java.lang.String, java.lang.Object)
+	 */
+	public void setPrimaryKeyValue(String columnName, Object PKValue) {
+		_primaryKeyMap.put(columnName.toUpperCase(), PKValue);
+	}
 
-*/
+	public boolean equals(Object obj) {
+		if (obj instanceof IDOPrimaryKey) {
+			IDOPrimaryKey comparePK = (IDOPrimaryKey) obj;
+			Iterator iter = _primaryKeyMap.keySet().iterator();
+			while (iter.hasNext()) {
+				String columnName = (String) iter.next();
+				if (!_primaryKeyMap.get(columnName).equals(comparePK.getPrimaryKeyValue(columnName)))
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
 
-public abstract class PrimaryKey{
+	public int hashCode()  { 
+		StringBuffer buffer = new StringBuffer(); 
+		Iterator iter = _primaryKeyMap.entrySet().iterator();
+		while (iter.hasNext()) {
+			buffer.append(iter.next());
+		}
+		String keys = buffer.toString(); 
 
+		return keys.hashCode(); 
+	}
 
-
-
-
-
-
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		boolean isComposite = isComposite();
+		
+		Iterator iter = _primaryKeyMap.keySet().iterator();
+		while (iter.hasNext()) {
+			String columnName = (String) iter.next();
+			if (isComposite)
+				buffer.append(columnName).append("=");
+			buffer.append(_primaryKeyMap.get(columnName));
+			if (isComposite && iter.hasNext())
+				buffer.append("&");
+		}
+		
+		return buffer.toString();
+	}
 }
