210016af4131cc445ebe5f65938946fa1817ed1a
Thorhallur Helgason <laddi@idega.com>
2003-10-10 09:31:46 +0000

    An interface to use when creating primary keys.

---
+++ src/java/com/idega/data/IDOPrimaryKey.java
@@ -0,0 +1,22 @@
+//idega 2000 - Tryggvi Larusson
+/*
+*Copyright 2000 idega.is All Rights Reserved.
+*/
+
+package com.idega.data;
+
+/**
+ *
+*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+*@version 0.9
+* Unimplemented Class
+*/
+public interface IDOPrimaryKey{
+
+	public IDOPrimaryKey getInstance();
+	public Object getPrimaryKeyValue(String columnName);
+	public void setPrimaryKeyValue(String columnName, Object PKValue);
+	public boolean equals(Object object);
+	public int hashCode();
+
+}
