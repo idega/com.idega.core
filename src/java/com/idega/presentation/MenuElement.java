8c295ea7d90d70cd709e41e0fd5bcb8c77c63223
Eirikur Hrafnsson <eiki@idega.com>
2003-10-14 16:01:35 +0000

    Initial version. Creates a dynamic JavaScript menu using external  javaScript; coolmenus4.js

---
+++ src/java/com/idega/presentation/MenuElement.java
@@ -0,0 +1,383 @@
+package com.idega.presentation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+/**
+ * A class that constructs a <code>MenuElement</code> of the <code>DynamicJSMenu</code>
+ * @author birna
+ * 
+ */
+public class MenuElement extends PresentationObject {
+	private String menuName = "";
+	private String name = "";
+	private String parent = "";
+	private String text = "";
+	private String link = "";
+	private String target = "";
+	private String width = "";
+	private String height = "";
+	private String regImg = "";
+	private String overImg = "";
+	private String regClass = "";
+	private String overClass = "";
+	private String align = "";
+	private int rows = 0;
+	private String noLink = "";
+	private String onClick = "";
+	private String onMouseOver = "";
+	private String onMouseOut = "";
+	private boolean isRoot = false;
+	
+	private ArrayList children = new ArrayList();
+	/**
+	 * The default constructor
+	 *
+	 */
+	public MenuElement() {
+		this("untitled");
+	}
+	/**
+	 * 
+	 * @param name
+	 */
+	public MenuElement(String name){
+		setMenuName(menuName);
+		setName(name); //name is used as an id for the MenuElement
+		setParent(parent);
+		setText(text);
+		setLink(link);
+		setTarget(target);
+		setWidth(width);
+		setHeight(height);
+		setRegImg(regImg);
+		setOverImg(overImg);
+		setAlign(align);
+		setRows(rows);
+		setNoLink(noLink);
+		setOnClick(onClick);
+		setOnMouseOver(onMouseOver);
+		setOnMouseOut(onMouseOut);
+	}
+	/**
+	 * can be used to create a rootNode
+	 * the rootNode's name is set and a boolean variable <code>isRoot</code> is set to true
+	 * @param rootNode
+	 * @param rootName
+	 */
+	public void createRootNode(MenuElement rootNode, String rootName){
+		isRoot = true;
+		rootNode.setName(rootName);
+	}
+	/**
+	 * adds a child to the ArrayList containing the children of the 
+	 * specific root node
+	 * @param childNode
+	 * @param childName
+	 */
+	public void addChild(String childName, String parentName){
+		MenuElement child = new MenuElement();
+		child.setParent(parentName);
+		child.setName(childName);
+		children.add(child);
+	}
+	/**
+	 * adds a child to the ArrayList containing the children of the
+	 * specific root node
+	 * @param childName
+	 * @param parentName
+	 * @param tx
+	 * @param url
+	 */
+	public void addChild(String childName, String parentName, String tx, String url){
+		MenuElement child = new MenuElement();
+		child.setName(childName);
+		child.setParent(parentName);
+		child.setText(tx);
+		child.setLink(url);
+		children.add(child);
+	}
+	/**
+	 * 
+	 * @return Arraylist which contains the children of a node
+	 */
+	public Iterator getChildren(){
+		return this.children.iterator();
+	}
+	/**
+	 * 
+	 * @param mn
+	 */
+	public void setMenuName(String mn){
+		menuName = mn;
+	}
+	/**
+	 * @param n - sets the name of the MenuElement
+	 */
+	public void setName(String n){
+		name = n;
+	}
+	/**
+	 * 
+	 * @param par - sets the parent of a MenuElement
+	 */
+	public void setParent(String par){
+		parent = par;
+	}
+	/**
+	 * 
+	 * @param txt
+	 */
+	public void setText(String txt){
+		text = txt;
+	}
+	/**
+	 * 
+	 * @param lnk
+	 */
+	public void setLink(String lnk){
+		link = lnk;
+	}
+	/**
+	 * 
+	 * @param targ
+	 */
+	public void setTarget(String targ){
+		target = targ;		
+	}
+	/**
+	 * @param w
+	 */
+	public void setWidth(String w){
+		width = w;
+	}
+	/**
+	 * @param h
+	 */
+	public void setHeight(String h){
+		height = h;
+	}
+	/**
+	 * 
+	 * @param rImg
+	 */
+	public void setRegImg(String rImg){
+		regImg = rImg;
+	}
+	/**
+	 * 
+	 * @param oImg
+	 */
+	public void setOverImg(String oImg){
+		overImg = oImg;
+	}
+	/**
+	 * 
+	 * @param rClass
+	 */
+	public void setRegClass (String rClass){
+		regClass = rClass;
+	}
+	/**
+	 * 
+	 * @param oClass
+	 */
+	public void setOverClass(String oClass){
+		overClass = oClass;
+	}
+	/**
+	 * 
+	 * @param aln
+	 */
+	public void setAlign(String aln){
+		align = aln;
+	}
+	/**
+	 * 
+	 * @param r
+	 */
+	public void setRows(int r){
+		rows = r;
+	}
+	/**
+	 * 
+	 * @param nLink
+	 */
+	public void setNoLink(String nLink){
+		noLink = nLink;
+	}
+	/**
+	 * 
+	 * @param onCl
+	 */
+	public void setOnClick(String onCl){
+		onClick = onCl;
+	}
+	/**
+	 * 
+	 * @param onMOver
+	 */
+	public void setOnMouseOver(String onMOver){
+		onMouseOver = onMOver;
+	}
+	/**
+	 * 
+	 * @param onMOut
+	 */
+	public void setOnMouseOut(String onMOut){
+		onMouseOut = onMOut;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getMenuName(){
+		return menuName;
+	}
+	/**
+	 * 
+	 */
+	public String getName(){
+		return name;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getParent(){
+		return parent;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getText(){
+		return text;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getLink(){
+		return link;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getTarget(){
+		return target;
+	}
+	/**
+	 * @return
+	 */
+	public String getWidth(){
+		return width;
+	}
+	/**
+	 * @return
+	 */
+	public String getHeight(){
+		return height;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getRegImg(){
+		return regImg;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getOverImg(){
+		return overImg;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getRegClass(){
+		return regClass;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getOverClass(){
+		return overClass;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getAlign(){
+		return align;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getRows(){
+		return rows;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNoLink(){
+		return noLink;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getOnClick(){
+		return onClick;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getOnMouseOver(){
+		return onMouseOver;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getOnMouseOut(){
+		return onMouseOut;
+	}
+	/**
+	 * 
+	 * @return - a string of the form:
+	 * menuName.makeMenu('name', 'parent_name', 'text', 'link', 'target', 'width', 'height', 'regImage', 'overImage', 'regClass', 'overClass' , 'align', 'rows', 'nolink', 'onclick', 'onmouseover', 'onmouseout')
+	 *  
+	 */
+	public String output(){
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(this.getMenuName());
+		buffer.append(".makeMenu(");
+		buffer.append("'" + this.getName() + "',");
+		buffer.append("'" + this.getParent() + "',");
+		buffer.append("'" + this.getText() + "',");
+		buffer.append("'" + this.getLink() + "',");
+		buffer.append("'" + this.getTarget() + "',");
+		buffer.append("'" + this.getWidth() + "',");
+		buffer.append("'" + this.getHeight() + "',");
+		buffer.append("'" + this.getRegImg() + "',");
+		buffer.append("'" + this.getOverImg() + "',");
+		buffer.append("'" + this.getRegClass() + "',");
+		buffer.append("'" + this.getOverClass() + "',");
+		buffer.append("'" + this.getAlign() + "',");
+		buffer.append("'" + this.getRows() + "',");
+		buffer.append("'" + this.getNoLink() + "',");
+		buffer.append("'" + this.getOnClick() + "',");
+		buffer.append("'" + this.getOnMouseOver() + "',");
+		buffer.append("'" + this.getOnMouseOut() + "'");
+		buffer.append(")");
+		
+		return buffer.toString();
+	}	
+}
---
+++ src/java/com/idega/presentation/MenuLevelElement.java
@@ -0,0 +1,233 @@
+package com.idega.presentation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+/**
+ * 
+ * @author birna
+ * consrtucts a Level for the <code>DynamicJSMenu</code> 
+ */
+public class MenuLevelElement extends PresentationObject {
+	private DynamicJSMenu menu;
+	
+	private int width = 110;
+	private int height = 25;
+	private String regClass = "";
+	private String overClass = "";
+	private int borderX = 1;
+	private int borderY = 1;
+	private String borderClass = "";
+	private int offsetY = 0;
+	private int offsetX = 0;
+	private int rows = 0;
+	private int arrow = 0;
+	private int arrowWidth = 0;
+	private int arrowHeight = 0;
+	private String align = "bottom";
+	private String menuLevel; // = "0";
+	
+	protected ArrayList theLevels = new ArrayList();
+	/**
+	 * The default constructor
+	 *
+	 */
+	public MenuLevelElement(){
+		this("undefined");
+	}
+	/**
+	 * 
+	 * @param level
+	 */
+	public MenuLevelElement(String level){
+		setLevel(level);
+		initialLevelValues();		
+	}
+//	public MenuLevelElement(String name, int level){
+//		this(name, String.valueOf(level));
+//	}
+//	public MenuLevelElement(String name, String level){
+//		setLevel(level);
+//		initialLevelValues();
+//	}
+	/**
+	 * sets the initial values for the <code>MenuLevelElement</code>
+	 */
+	public void initialLevelValues(){
+		setWidth(width);
+		setHeight(height);
+		setRegClass(regClass);
+		setOverClass(overClass);
+		setBorderX(borderX);
+		setBorderY(borderY);
+		setBorderClass(borderClass);
+		setOffsetX(offsetX);
+		setOffsetY(offsetY);
+		setRows(rows);
+		setArrow(arrow);
+		setArrowHeight(arrowHeight);
+		setArrowWidth(arrowWidth);
+		setAlign(align);
+	}
+//	public void addLevel(String l){
+//		MenuLevelElement levelElement = new MenuLevelElement(l);
+//		menuLevel = l;
+//		levelElement.setLevel(l);
+//		levelElement.setRegClass("clLevel" + l);
+//		levelElement.setOverClass("clLevel" + l + "over");
+//		levelElement.setBorderClass("clLevel" + l + "border");
+//		theLevels.add(levelElement);
+//	}
+	/**
+	 * sets the level
+	 * @param l
+	 */
+	public void setLevel(String l){
+		menuLevel = l;
+//		setAttribute("level", l);
+	}
+	/**
+	 * 
+	 * @param l
+	 */
+	public void setLevel(int l){
+		setLevel(Integer.toString(l));
+	}
+	/**
+	 * 
+	 * @return the level of the <code> MenuLevelElement </code>
+	 */
+	public String getLevel(){
+		return menuLevel;
+//		return getAttribute("level");
+	}
+	/**
+	 * 
+	 * @return an iterator containing the levels of the <code>MenuLevelElement </code>
+	 */
+	public Iterator getLevels(){
+		return this.theLevels.iterator();
+	}
+	/**
+	 * sets the width of the level
+	 * @param w
+	 */
+	public void setWidth(int w){
+		setAttribute("width", w);
+	}
+	/**
+	 * sets the height of the level
+	 * @param h
+	 */
+	public void setHeight(int h){
+		setAttribute("height", h);
+	}
+	/**
+	 * sets the regular style class for the level
+	 * @param regCl
+	 */
+	public void setRegClass(String regCl){
+//		regCl = "clLevel" + menuLevel;
+		setAttribute("regClass", "\"" + regCl + "\"");
+	}
+	/**
+	 * sets the mouseOver style class for the level
+	 * @param overCl
+	 */
+	public void setOverClass(String overCl){
+//		overCl = "clLevel" + menuLevel + "over";
+		setAttribute("overClass", "\"" + overCl + "\"");
+	}
+	/**
+	 * 
+	 * @param bX
+	 */
+	public void setBorderX(int bX){
+		setAttribute("borderX", bX);
+	}
+	/**
+	 * 
+	 * @param bY
+	 */
+	public void setBorderY(int bY){
+		setAttribute("borderY", bY);
+	}
+	/**
+	 * sets the border class for the level
+	 * @param borderCl
+	 */
+	public void setBorderClass(String borderCl){
+//		borderCl = "clLevel" + menuLevel + "border";
+		setAttribute("borderClass", "\"" + borderCl + "\"");
+	}
+	/**
+	 * sets the x-offset of the level - if the level should overlap the previous level
+	 * @param offX
+	 */
+	public void setOffsetX(int offX){
+		setAttribute("offsetX", offX);
+	}
+	/**
+	 * sets the y-offset of the level - if the level should overlap the previous level
+	 * @param offY
+	 */
+	public void setOffsetY(int offY){
+		setAttribute("offsetY", offY);
+	}
+	/**
+	 * sets if the level should be horizontal (r=0) or vertical (r=1)
+	 * @param r
+	 */
+	public void setRows(int r){
+		setAttribute("rows", r);
+	}
+	/**
+	 * 
+	 * @param a
+	 */
+	public void setArrow(int a){
+		setAttribute("arrow", a);
+	}
+	/**
+	 * 
+	 * @param aWidth
+	 */
+	public void setArrowWidth(int aWidth){
+		setAttribute("arrowWidth", aWidth);
+	}
+	/**
+	 * 
+	 * @param aHeight
+	 */
+	public void setArrowHeight(int aHeight){
+		setAttribute("arrowHeight", aHeight);
+	}
+	/**
+	 * 
+	 * @param ali
+	 */
+	public void setAlign(String ali){
+		setAttribute("align",  "\"" + ali + "\"");
+	}
+//	public String output(){
+//		StringBuffer buffer = new StringBuffer();
+//		
+////		MenuLevelElement levelElement = new MenuLevelElement();
+//		
+//		Map levelMap = this.attributes;
+//		Map.Entry mapEntry;
+//
+////		Iterator iter = theLevels.iterator();
+////		while (iter.hasNext()) {
+////			levelElement = (MenuLevelElement) iter.next();
+//			buffer.append(menu.getName() + ".level[" + this.getLevel() + "]=new cm_makeLevel()\n");
+//			buffer.append("\n now in output!!!");
+////			Iterator levelIter = levelMap.entrySet().iterator();
+////			while (levelIter.hasNext()) {
+////				mapEntry = (Map.Entry) levelIter.next();
+////				buffer.append(menu.getName() + ".level[" + this.getLevel() + "]." + (String) mapEntry.getKey() + "=" + (String) mapEntry.getValue() + "\n");
+////			}
+//		return buffer.toString();
+////		}
+//
+//	}
+}
