1b7b860dfa14f53012631c949fcf6320d9dc436f
Jonas Kari Blandon <jonas@idega.com>
2003-10-27 11:26:42 +0000

    attaching a file now possible

--- src/java/com/idega/util/SendMail.java
+++ src/java/com/idega/util/SendMail.java
@@ -1,213 +1,131 @@
 package com.idega.util;
+import java.io.File;
 
-
-
-import javax.mail.*;
-
-import javax.mail.internet.*;
-
-
-
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 public class SendMail {
-
-
-
-
-
-
-
-        public SendMail(){
-
-
-
-        }
-
-
-
-
-
-	public static void send(String from, String to, String cc, String bcc,String host, String subject, String text) throws MessagingException {
-
-
-
-		// Start a session
-
-		java.util.Properties properties = System.getProperties();
-
-		Session session = Session.getInstance(properties, null);
-
-
-
-
-
-		// Construct a message
-
-		MimeMessage message = new MimeMessage(session);
-
-		message.setFrom(new InternetAddress(from));
-
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-		//process cc and bcc
-
-		//this Address[] ccAddressess = InternetAddress.parse(cc); or similar
-
-
-
-		if((cc!=null) && !("".equals(cc))){
-
-		message.addRecipients(Message.RecipientType.CC,InternetAddress.parse(cc));
-
-		}
-
-		if((bcc!=null) && !("".equals(bcc))){
-
-		  message.addRecipients(Message.RecipientType.BCC,InternetAddress.parse(bcc));
-
-		}
-
-
-
-    /** @todo tryggvi laga */
-    /*
-		message.setSubject(parseCharacters(subject));
-
-		message.setText(parseCharacters(text));
-
-    */
-
-    message.setSubject((subject));
-
-		message.setText((text));
-
-
-
-
-		//Connect to the transport
-
-		Transport transport = session.getTransport("smtp");
-
-		transport.connect(host, "", "");
-
-
-
-		//Send the message and close the connection
-
-		transport.sendMessage(message, message.getAllRecipients());
-
-		transport.close();
-
-
-
+	public SendMail() {
 	}
-
-
-
-    /*private static String parseCharacters(String input){
-
-      StringBuffer returnBuffer = new StringBuffer();
-
-      char[] charArray = input.toCharArray();
-
-      for (int i = 0; i < charArray.length; i++) {
-
-        char ch = charArray[i];
-
-        char newChar = ch;
-
-        switch (ch) {
-
-          case 'a':
-
-            returnBuffer.append('a');
-
-          case 'b':
-
-            returnBuffer.append('b');
-
-          default:
-
-            returnBuffer.append(ch);
-
-        }
-
-      }
-
-      return returnBuffer.toString();
-
-    }*/
-
-
-
-    private static String parseCharacters(String input){
-
-      return convertStringBody(input);
-
-    }
-
-
-
-
-
-
-
-    /**
-
-     * From 2M Business Applications DK
-
-     */
-
-    private static String convertStringBody(String tekst) {
-
-
-
-        while (tekst.indexOf("Æ") > -1) {
-
-            tekst = replace(tekst, "Æ","&AElig;");
-
-        }
-
-
-
-        while (tekst.indexOf("æ") > -1) {
-
-            tekst = replace(tekst, "æ","&aelig;");
-
-        }
-
-        while (tekst.indexOf(System.getProperty("line.separator")) > -1) {
-
-            tekst = replace(tekst,System.getProperty("line.separator"),"<br>");
-
-        }
-
-        return tekst;
-
-    }
-
-
-
-    private static String replace(String tekst, String changeFrom, String changeTo) {
-
-        String tekst1 = tekst.substring(0,tekst.indexOf(changeFrom));
-
-        String tekst2 = tekst.substring(tekst.indexOf(changeFrom)+1);
-
-        return tekst1 + changeTo + tekst2;
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+	public static void send(String from, String to, String cc, String bcc, String host, String subject, String text, File attachedFile)
+		throws MessagingException {
+		// Start a session
+		java.util.Properties properties = System.getProperties();
+		Session session = Session.getInstance(properties, null);
+		// Construct a message
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		//process cc and bcc
+		//this Address[] ccAddressess = InternetAddress.parse(cc); or similar
+		if ((cc != null) && !("".equals(cc))) {
+			message.addRecipients(Message.RecipientType.CC, InternetAddress.parse(cc));
+		}
+		if ((bcc != null) && !("".equals(bcc))) {
+			message.addRecipients(Message.RecipientType.BCC, InternetAddress.parse(bcc));
+		}
+		/** @todo tryggvi laga */
+		/*
+			message.setSubject(parseCharacters(subject));
+		
+			message.setText(parseCharacters(text));
+		
+		*/
+		message.setSubject((subject));
+		if (attachedFile == null) {
+			message.setText((text));
+		}
+		else {
+			BodyPart body = new MimeBodyPart();
+			body.setText(text);
+			BodyPart attachment = new MimeBodyPart();
+			DataSource attachmentSource = new FileDataSource(attachedFile);
+			DataHandler attachmentHandler = new DataHandler(attachmentSource);
+			attachment.setDataHandler(attachmentHandler);
+			attachment.setFileName(attachedFile.getName());
+			attachment.setDescription("Attached file");
+			MimeMultipart multipart = new MimeMultipart();
+			multipart.addBodyPart(body);
+			System.out.println("Adding attachment " + attachment);
+			multipart.addBodyPart(attachment);
+			message.setContent(multipart);
+		}
+		//Connect to the transport
+		Transport transport = session.getTransport("smtp");
+		transport.connect(host, "", "");
+		//Send the message and close the connection
+		transport.sendMessage(message, message.getAllRecipients());
+		transport.close();
+	}
+	
+	public static void send(String from, String to, String cc, String bcc, String host, String subject, String text)
+			throws MessagingException {
+		send(from, to, cc, bcc, host, subject, text, null);
+	}
+	
+	/*private static String parseCharacters(String input){
+	
+	  StringBuffer returnBuffer = new StringBuffer();
+	
+	  char[] charArray = input.toCharArray();
+	
+	  for (int i = 0; i < charArray.length; i++) {
+	
+	    char ch = charArray[i];
+	
+	    char newChar = ch;
+	
+	    switch (ch) {
+	
+	      case 'a':
+	
+	        returnBuffer.append('a');
+	
+	      case 'b':
+	
+	        returnBuffer.append('b');
+	
+	      default:
+	
+	        returnBuffer.append(ch);
+	
+	    }
+	
+	  }
+	
+	  return returnBuffer.toString();
+	
+	}*/
+	private static String parseCharacters(String input) {
+		return convertStringBody(input);
+	}
+	/**
+	 * From 2M Business Applications DK
+	 */
+	private static String convertStringBody(String tekst) {
+		while (tekst.indexOf("Æ") > -1) {
+			tekst = replace(tekst, "Æ", "&AElig;");
+		}
+		while (tekst.indexOf("æ") > -1) {
+			tekst = replace(tekst, "æ", "&aelig;");
+		}
+		while (tekst.indexOf(System.getProperty("line.separator")) > -1) {
+			tekst = replace(tekst, System.getProperty("line.separator"), "<br>");
+		}
+		return tekst;
+	}
+	private static String replace(String tekst, String changeFrom, String changeTo) {
+		String tekst1 = tekst.substring(0, tekst.indexOf(changeFrom));
+		String tekst2 = tekst.substring(tekst.indexOf(changeFrom) + 1);
+		return tekst1 + changeTo + tekst2;
+	}
 }
-
