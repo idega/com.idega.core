a0be2319a4cf00a05c617d4b61447fbf67143ecb
Thomas Hilbig <thomas@idega.com>
2003-11-04 20:31:27 +0000

    new replace method

--- src/java/com/idega/util/StringHandler.java
+++ src/java/com/idega/util/StringHandler.java
@@ -239,6 +239,20 @@ public class StringHandler {
 		}
     return true;
   }
+  
+	public static String replace(String str, String pattern, String replace) {
+		int s = 0;
+    int e = 0;
+    StringBuffer result = new StringBuffer();
+    while ((e = str.indexOf(pattern, s)) >= 0) {
+    	result.append(str.substring(s, e));
+      result.append(replace);
+      s = e+pattern.length();
+    }
+    result.append(str.substring(s));
+    return result.toString();
+	}
+  
     
   
   /**
