3f60613d92d2683f7e46a771c51bddcbb2f392ff
Eirikur Hrafnsson <eiki@idega.com>
2003-12-17 15:35:58 +0000

    deleted unused ICPermissionType bean

--- src/java/com/idega/core/accesscontrol/data/ICPermissionType.java
+++
@@ -1,8 +0,0 @@
-package com.idega.core.accesscontrol.data;
-
-
-public interface ICPermissionType extends com.idega.data.IDOEntity,com.idega.data.TreeableEntity
-{
- public java.lang.String getIDColumnName();
- public void initializeAttributes();
-}
