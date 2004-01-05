19c1ac7443daefa472e1702da33625a0bc1a1d24
Aron Birkir Gudmundsson <aron@idega.com>
2004-01-05 16:02:53 +0000

    Added legend methods and comments

--- src/java/com/idega/presentation/ui/Fieldset.java
+++ src/java/com/idega/presentation/ui/Fieldset.java
@@ -9,33 +9,76 @@ package com.idega.presentation.ui;
 import com.idega.presentation.IWContext;
 
 /**
- * Title:		Fieldset
- * Description:
+ * Title:		Fieldset 
+ * Description: The fieldset element draws a box around its containing elements.
  * Copyright:	Copyright (c) 2004
  * Company:		idega Software
  * @author		2004 - idega team - <br><a href="mailto:gummi@idega.is">Gudmundur Agust Saemundsson</a><br>
  * @version		1.0
  */
 public class Fieldset extends InterfaceObjectContainer {
+	
+	private String legendAlignment = "left";
 
 	/**
 	 * 
 	 */
 	public Fieldset() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 *  Sets the the legend text , aligned by default to the left
+	 * @param legend
+	 */
+	public void setLegend(String legend){
+		this.setName(legend);
+	}
 	
+	/**
+	 *  Sets the the legend text showing aligned with the provided alignment value ("left","center","right")
+	 * @param legend
+	 * @param alignment
+	 */
+	public void setLegend(String legend,String alignment){
+		setName(legend);
+		setLegendAlignment(alignment);
+	}
 	
+	/**
+	 * Sets the alignment of the legend ("left","center","right")
+	 * @param align
+	 */
+	public void setLegendAlignment(String align){
+		this.legendAlignment =align;
+	}
 	
+	/**
+	 * Gets the legend to be shown
+	 * @return
+	 */
+	public String getLegend(){
+		return getName();
+	}
+	
+	/**
+	 * Gets the aligment value for the legend
+	 * @return
+	 */
+	public String getLegendAlignment(){
+		return this.legendAlignment;
+	}
 	
 	public void print(IWContext iwc) throws Exception {
 		
 		//<fieldset dir="ltl"><legend> "name" </legend> "content" </fieldset>
 		
 		if (getLanguage().equals("HTML")) {
-			println("<fieldset " +getMarkupAttributesString()+ " ><legend>"+getName()+"</legend>");
+			println("<fieldset " +getMarkupAttributesString()+ " >");
+			if(getName()!=null){
+				String align = "align=\""+this.legendAlignment+"\"";
+				println("<legend "+align+" >"+getName()+"</legend>");
+			}
 			//		Catch all exceptions that are thrown in print functions of objects stored inside
 			try {
 				super.print(iwc);
