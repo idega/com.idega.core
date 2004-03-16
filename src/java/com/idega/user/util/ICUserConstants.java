76e56a233d921eb099bdc488d747fd41569a74e0
Eirikur Hrafnsson <eiki@idega.com>
2004-03-16 17:23:00 +0000

    A collection of the static variables used in the user system like group types and default metadata

---
+++ src/java/com/idega/user/util/ICUserConstants.java
@@ -0,0 +1,14 @@
+package com.idega.user.util;
+
+/**
+ * A collection of the static variables used in the user system like group types and default metadata.
+ * @author <a href="mailto:eiki@idega.is">Eirikur Hrafnsson</a>
+ *
+ */
+public class ICUserConstants {
+
+	public static final String META_DATA_GROUP_NUMBER = "GROUP_NUMBER";
+	public static final String GROUP_TYPE_ALIAS = "alias";
+	public static final String GROUP_TYPE_GENERAL = "general";
+	
+}
