50556912dab01039ff06453e3fdcbaff0968bfb4
Thorhallur Helgason <laddi@idega.com>
2004-03-29 11:51:24 +0000

    Minor fixes regarding (X)HTML validation.

--- src/java/com/idega/presentation/Image.java
+++ src/java/com/idega/presentation/Image.java
@@ -374,19 +374,19 @@ public class Image extends PresentationObject
 	}
 	public void setOnClick(String action)
 	{
-		setMarkupAttribute("onClick", action);
+		setMarkupAttribute("onclick", action);
 	}
 	public void setOnMouseOver(String action)
 	{
-		setMarkupAttribute("onMouseOver", action);
+		setMarkupAttribute("onmouseover", action);
 	}
 	public void setOnMouseOut(String action)
 	{
-		setMarkupAttribute("onMouseOut", action);
+		setMarkupAttribute("onmouseout", action);
 	}
 	public void setOnMouseDown(String action)
 	{
-		setMarkupAttribute("onMouseDown", action);
+		setMarkupAttribute("onmousedown", action);
 	}
 	public void setOnClickImageURL(String clickImageURL)
 	{
@@ -763,28 +763,6 @@ public class Image extends PresentationObject
 		{
 			this.imageId = Integer.parseInt(iwc.getParameter(PARAM_IMAGE_ID));
 		}
-		if (this.overImageUrl != null)
-		{
-			Page parent = getParentPage();
-			if (parent != null)
-			{
-				Script rollOverScript = parent.getAssociatedScript();
-				rollOverScript.addFunction(
-					"swapImgRestore()",
-					"function swapImgRestore() {var i,x,a=document.sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;}");
-				rollOverScript.addFunction(
-					"preLoadImages()",
-					"function preLoadImages(){var d=document; if(d.images){ if(!d.p) d.p=new Array(); var i,j=d.p.length,a=preLoadImages.arguments; for(i=0; i<a.length; i++)  if (a[i].indexOf(\"#\")!=0){ d.p[j]=new Image; d.p[j++].src=a[i];}}}");
-				rollOverScript.addFunction(
-					"findObj(n, d)",
-					"function findObj(n, d){var p,i,x;  if(!d) d=document; if((p=n.indexOf(\"?\"))>0&&parent.frames.length) {  d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=findObj(n,d.layers[i].document); return x;}");
-				rollOverScript.addFunction(
-					"swapImage()",
-					"function swapImage(){ var i,j=0,x,a=swapImage.arguments; document.sr=new Array; for(i=0;i<(a.length-2);i+=3) if ((x=findObj(a[i]))!=null){document.sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}}");
-				parent.setAssociatedScript(rollOverScript);
-				parent.setOnLoad(getPreloadScript(overImageUrl));
-			}
-		}
 	}
 	public void print(IWContext iwc) throws Exception
 	{
@@ -859,4 +837,11 @@ public class Image extends PresentationObject
 		setStyleAttribute("padding-bottom: "+padding+"px;");
 	}
 
+	public String getName() {
+		return super.getID();
+	}
+	
+	public void setName(String name) {
+		super.setID(name);
+	}
 }
