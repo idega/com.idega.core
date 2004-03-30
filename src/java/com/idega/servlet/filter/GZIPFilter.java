a9f63cdef76dbfc13fdc218f7105a67b428f3eb1
Thorhallur Helgason <laddi@idega.com>
2004-03-30 18:48:36 +0000

    A GZip filter to compress content.

---
+++ src/java/com/idega/servlet/filter/GZIPFilter.java
@@ -0,0 +1,60 @@
+/*
+ * Created on 30.3.2004
+ */
+package com.idega.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author laddi
+ */
+public class GZIPFilter implements Filter {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+	 *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		if (req instanceof HttpServletRequest) {
+			HttpServletRequest request = (HttpServletRequest) req;
+			HttpServletResponse response = (HttpServletResponse) res;
+			String ae = request.getHeader("accept-encoding");
+			if (ae != null && ae.indexOf("gzip") != -1) {
+				//System.out.println("GZIP supported, compressing.");
+				GZIPResponseWrapper wrappedResponse = new GZIPResponseWrapper(response);
+				chain.doFilter(req, wrappedResponse);
+				wrappedResponse.finishResponse();
+				return;
+			}
+			chain.doFilter(req, res);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+}
