1d42d53380beeee4fb8ffab073a82b62bf094fe5
Thomas Hilbig <thomas@idega.com>
2004-03-30 15:54:01 +0000

    improved version

--- src/java/com/idega/io/IBExportImportDataReader.java
+++ src/java/com/idega/io/IBExportImportDataReader.java
@@ -2,11 +2,15 @@ package com.idega.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
+import com.idega.builder.business.FileBusiness;
 import com.idega.builder.data.IBExportImportData;
+import com.idega.core.file.data.ICFile;
 import com.idega.util.xml.XMLData;
 
 /**
@@ -20,21 +24,70 @@ import com.idega.util.xml.XMLData;
  */
 public class IBExportImportDataReader {
 	
-	public IBExportImportData getData(File file) throws IOException  {
-		ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(file));
+	public IBExportImportData getData(File zipFile, FileBusiness fileBusiness) throws IOException  {
+		IBExportImportData exportImportData = readMetadata(zipFile);
+		// metadata has been read
+		// iterate again through the zip file and fill map with names as key and files as values
+		ZipInputStreamIgnoreClose zipInputStream;
 		ZipEntry entry;
-		XMLData metadata = null;
-		while ((metadata == null && (entry = zipInputStream.getNextEntry()) != null)) {
+		Map nameFile = new HashMap();
+		zipInputStream = new ZipInputStreamIgnoreClose(new FileInputStream(zipFile));
+		while ((entry = zipInputStream.getNextEntry()) != null) {
 			String name = entry.getName();
 			if (IBExportImportData.EXPORT_METADATA_FILE_NAME.equals(name)) {
-				metadata = XMLData.getInstanceForInputStream(zipInputStream);
+				// do nothing
+			}
+			else if (exportImportData.isFileEntryAPage(name)) {
+				// is a page!
+				XMLData data = XMLData.getInstanceForInputStream(zipInputStream);
+				nameFile.put(name, data);
+			}
+			else {
+				String mimeType = exportImportData.getMimeType(name);
+				ICFile file = fileBusiness.createFileFromInputStream(zipInputStream, name, mimeType);
+				nameFile.put(name, file);
+			}
+			zipInputStream.closeEntry();
+		}
+		zipInputStream.closeStream();
+		
+		// now the 
+		return exportImportData;
+	}
+
+  /**
+	 * @param zipFile
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	private IBExportImportData readMetadata(File zipFile) throws FileNotFoundException, IOException {
+		IBExportImportData exportImportData = null;
+		ZipInputStreamIgnoreClose zipInputStream = new ZipInputStreamIgnoreClose(new FileInputStream(zipFile));
+		ZipEntry entry;
+		while ((exportImportData == null && (entry = zipInputStream.getNextEntry()) != null)) {
+			String name = entry.getName();
+			// very fist check, is the entry the metadata?
+			if (IBExportImportData.EXPORT_METADATA_FILE_NAME.equals(name)) {
+				XMLData metadata = XMLData.getInstanceForInputStream(zipInputStream);
+				exportImportData = IBExportImportData.getInstance(metadata);
+			}
+			zipInputStream.closeEntry();
+		}
+		closeStream(zipInputStream);
+		return exportImportData;
+	}
+
+	protected void closeStream(ZipInputStreamIgnoreClose inputStream) {
+  	try {
+			if (inputStream != null) {
+				inputStream.closeStream();
 			}
 		}
-		String name = metadata.getName();
-		name.length();
-		return null;
-	}
-			
+		// do not hide an existing exception
+		catch (IOException io) {
+		}
+  }		
 				
 			
 			
