bf802bd18464b538c38aa69c8ebb118e9d972e48
Thomas Hilbig <thomas@idega.com>
2004-04-20 15:21:20 +0000

    first version

---
+++ src/java/com/idega/util/datastructures/MessageContainer.java
@@ -0,0 +1,48 @@
+package com.idega.util.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Apr 20, 2004
+ */
+public class MessageContainer {
+	
+	private String mainMessage;
+	private List messages;
+	
+	/**
+	 * @return Returns the mainMessage.
+	 */
+	public String getMainMessage() {
+		return mainMessage;
+	}
+	
+	/**
+	 * @param mainMessage The mainMessage to set.
+	 */
+	public void setMainMessage(String mainMessage) {
+		this.mainMessage = mainMessage;
+	}
+	
+	/**
+	 * @return Returns the messages.
+	 */
+	public List getMessages() {
+		return messages;
+	}
+	
+	public boolean addMessage(String message) {
+		if (messages == null) {
+			messages = new ArrayList();
+		}
+		return messages.add(message);
+	}
+		
+}
