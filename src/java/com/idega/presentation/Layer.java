4f37dca75fdd8c4ff19dfe93123004d8c674a55b
Thorhallur Helgason <laddi@idega.com>
2004-04-21 08:41:46 +0000

    Changed class to use styles and allow usage of markup attribute values.

--- src/java/com/idega/presentation/Layer.java
+++ src/java/com/idega/presentation/Layer.java
@@ -4,195 +4,162 @@
  */
 package com.idega.presentation;
 
-import java.util.Iterator;
+import com.idega.util.text.TextStyler;
 
 /**
- * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson </a>
  * @version 1.2
  */
-public class Layer extends PresentationObjectContainer
-{
-	public static final String RELATIVE = "relative";
+public class Layer extends PresentationObjectContainer {
+	
 	public static final String ABSOLUTE = "absolute";
+	public static final String DEFAULT_UNIT = "px";
+	public static final String NOWRAP = "nowrap";
+	public static final String RELATIVE = "relative";
+
 	public static final String DIV = "div";
 	public static final String SPAN = "span";
+
+	public static final String BACKGROUND_COLOR = "background-color";
+	public static final String BACKGROUND_IMAGE = "background-image";
 	public static final String LEFT = "left";
-	public static final String TOP = "top";
-	public static final String ZINDEX = "z-index";
+	public static final String MOUSE_OUT = "onmouseout";
+	public static final String MOUSE_OVER = "onmouseover";
+	public static final String OVERFLOW = "overflow";
 	public static final String POSITION = "position";
-	public static final String _ATTRIB_NOWRAP = "nowrap";
-	public boolean _nowrap = false;
-	private boolean noStyle = false;
-	String align;
-	String onMouseOut;
-	String absoluteOrRelative;
+	public static final String TOP = "top";
+	public static final String VISIBILITY = "visibility";
+	public static final String WHITESPACE = "white-space";
+	public static final String ZINDEX = "z-index";
+
 	String layerType;
-	public Layer()
-	{
+
+	public Layer() {
 		this(DIV);
 	}
-	public Layer(String layerType)
-	{
+
+	public Layer(String layerType) {
 		super();
 		this.layerType = layerType;
 	}
-	public String getMarkupAttributesString()
-	{
-		if (noStyle)
-		{
-			return super.getMarkupAttributesString();
-		}
-		else
-		{
-			String returnString = "";
-			if (getMarkupAttributes() != null)
-			{
-				for (Iterator iter = getMarkupAttributes().keySet().iterator(); iter.hasNext();)
-				{
-					Object Attribute = iter.next();
-					String AttributeString = (String) Attribute;
-					returnString = returnString + " " + AttributeString + ": " + (String) attributes.get(Attribute);
-					if (AttributeString.equals(LEFT) || AttributeString.equals(TOP))
-						returnString = returnString + "px";
-					returnString = returnString + ";";
-				}
+
+	private boolean isPositionSet() {
+		TextStyler styler = new TextStyler(getStyleAttribute());
+		return styler.isStyleSet(POSITION);
+	}
+
+	public void print(IWContext iwc) throws Exception {
+		if (doPrint(iwc)) {
+			if (getLanguage().equals("HTML")) {
+				print("<" + layerType + " ");
+				print(getMarkupAttributesString() + ">");
+				super.print(iwc);
+				println("</" + layerType + ">");
 			}
-			return returnString;
 		}
 	}
-	public void setLeftPosition(String xpos)
-	{
-		setMarkupAttribute(LEFT, xpos);
-		if (absoluteOrRelative == null)
-			setMarkupAttribute("position", ABSOLUTE);
+
+	public void setBackgroundColor(String backgroundColor) {
+		setStyleAttribute(BACKGROUND_COLOR, backgroundColor);
 	}
-	public void setLeftPosition(int xpos)
-	{
-		setLeftPosition(String.valueOf(xpos));
-	}
-	public void setNoWrap()
-	{
-		_nowrap = true;
-	}
-	public void setNoWrap(boolean value)
-	{
-		_nowrap = value;
-	}
-	public void setTopPosition(String ypos)
-	{
-		setMarkupAttribute(TOP, ypos);
-		if (absoluteOrRelative == null)
-			setMarkupAttribute("position", ABSOLUTE);
-	}
-	public void setTopPosition(int ypos)
-	{
-		setTopPosition(String.valueOf(ypos));
-	}
-	public void setVisibility(String visibilityType)
-	{
-		setMarkupAttribute("visibility", visibilityType);
-	}
-	public void setWidth(int width)
-	{
-		setMarkupAttribute("width", Integer.toString(width) + "px");
-	}
-	public void setHeight(int height)
-	{
-		setMarkupAttribute("height", Integer.toString(height) + "px");
-	}
-	public void setOverflow(String overflowType)
-	{
-		setMarkupAttribute("overflow", overflowType);
-	}
-	public void setZIndex(int index)
-	{
-		setZIndex(String.valueOf(index));
-	}
-	public void setZIndex(String index)
-	{
-		setMarkupAttribute(ZINDEX, index);
-	}
-	public void setBackgroundColor(String backgroundColor)
-	{
-		setMarkupAttribute("background-color", backgroundColor);
-		setMarkupAttribute("layer-background-color", backgroundColor);
-	}
-	public void setLayerType(String layerType)
-	{
-		this.layerType = layerType;
-	}
-	/*
-	 * public void setBorder(int borderWidth,String borderColor){
-	 *  }
-	 * 
-	 * public void setBorder(int borderWidth){
-	 *  }
-	 * 
-	 * 
-	 * public void setBorderColor(String color){
-	 *  }
-	 */
-	public void setPositionType(String absoluteOrRelative)
-	{
-		this.absoluteOrRelative = absoluteOrRelative;
-		setMarkupAttribute(POSITION, absoluteOrRelative);
-	}
-	public void setBackgroundImage(String url)
-	{
-		setMarkupAttribute("background-image", "url(" + url + ")");
-		setMarkupAttribute("layer-background-image", "url(" + url + ")");
-	}
-	public void setBackgroundImage(Image image)
-	{
+
+	public void setBackgroundImage(Image image) {
 		setBackgroundImage(image.getURL());
 	}
-	public void setOnMouseOut(String action)
-	{
-		onMouseOut = action;
+
+	public void setBackgroundImage(String url) {
+		setStyleAttribute(BACKGROUND_IMAGE, "url(" + url + ")");
 	}
-	public void setNoStyle(boolean value)
-	{
-		noStyle = value;
+
+	public void setHeight(int height) {
+		setHeightStyle(Integer.toString(height));
 	}
-	private String getOnMouseOut()
-	{
-		if (onMouseOut != null)
-		{
-			return "onMouseOut=\"" + onMouseOut + "\"";
+
+	public void setLayerType(String layerType) {
+		this.layerType = layerType;
+	}
+
+	public void setLeftPosition(int xpos) {
+		setLeftPosition(xpos, DEFAULT_UNIT);
+	}
+	
+	public void setLeftPosition(int xpos, String unit) {
+		setLeftPosition(String.valueOf(xpos), unit);
+	}
+	
+	public void setLeftPosition(String xpos) {
+		setLeftPosition(xpos, DEFAULT_UNIT);
+	}
+
+	public void setLeftPosition(String xpos, String unit) {
+		setStyleAttribute(LEFT, xpos+unit);
+		if (!isPositionSet()) {
+			setStyleAttribute(POSITION, ABSOLUTE);
 		}
-		return "";
 	}
-	public void print(IWContext iwc) throws Exception
-	{
-		if (doPrint(iwc))
-		{
-			if (getLanguage().equals("HTML"))
-			{
-				boolean alignSet = isMarkupAttributeSet(HORIZONTAL_ALIGNMENT);
-				print("<" + layerType + " ");
-				if (_nowrap)
-				{
-					print(_ATTRIB_NOWRAP + " ");
-				}
-				if (alignSet)
-				{
-					print("align=\"" + getHorizontalAlignment() + "\" ");
-					removeMarkupAttribute(HORIZONTAL_ALIGNMENT); //does this slow
-														   // things down?
-				}
-				if (noStyle)
-				{
-					print(getMarkupAttributesString() + " ");
-				}
-				else
-				{
-					print("id=\"" + getID() + "\" ");
-					print("style=\"" + getMarkupAttributesString() + "\" ");
-				}
-				println(getOnMouseOut() + ">");
-				super.print(iwc);
-				println("\n</" + layerType + ">");
-			} //end if (getLanguage(...
+
+	public void setNoWrap() {
+		setNoWrap(true);
+	}
+
+	public void setNoWrap(boolean noWrap) {
+		if (noWrap) {
+			setStyleAttribute(WHITESPACE, NOWRAP);
 		}
+		else {
+			TextStyler styler = new TextStyler(this.getStyleAttribute());
+			styler.removeStyleValue(WHITESPACE);
+		}
+	}
+
+	public void setOnMouseOut(String action) {
+		setMarkupAttributeMultivalued(MOUSE_OUT, action);
+	}
+	
+	public void setOnMouseOver(String action) {
+		setMarkupAttributeMultivalued(MOUSE_OVER, action);
+	}
+
+	public void setOverflow(String overflowType) {
+		setStyleAttribute(OVERFLOW, overflowType);
+	}
+
+	public void setPositionType(String absoluteOrRelative) {
+		setStyleAttribute(POSITION, absoluteOrRelative);
+	}
+
+	public void setTopPosition(int ypos) {
+		setTopPosition(ypos, DEFAULT_UNIT);
+	}
+
+	public void setTopPosition(int ypos, String unit) {
+		setTopPosition(String.valueOf(ypos), unit);
+	}
+
+	public void setTopPosition(String ypos) {
+		setTopPosition(ypos, DEFAULT_UNIT);
+	}
+
+	public void setTopPosition(String ypos, String unit) {
+		setStyleAttribute(TOP, ypos+unit);
+		if (!isPositionSet()) {
+			setStyleAttribute(POSITION, ABSOLUTE);
+		}
+	}
+
+	public void setVisibility(String visibilityType) {
+		setStyleAttribute(VISIBILITY, visibilityType);
+	}
+
+	public void setWidth(int width) {
+		setWidthStyle(Integer.toString(width));
+	}
+
+	public void setZIndex(int index) {
+		setZIndex(String.valueOf(index));
+	}
+
+	public void setZIndex(String index) {
+		setStyleAttribute(ZINDEX, index);
 	}
 }
