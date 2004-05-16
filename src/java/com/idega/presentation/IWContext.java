cd8a7b4a74b2d955511ac88c011090f08b6e50f3
Thorhallur Helgason <laddi@idega.com>
2004-05-16 14:29:03 +0000

    Removed unused imports.

--- src/java/com/idega/presentation/IWContext.java
+++ src/java/com/idega/presentation/IWContext.java
@@ -19,18 +19,14 @@ import java.util.WeakHashMap;
 
 import javax.faces.application.Application;
 import javax.faces.application.FacesMessage;
-import javax.faces.application.ViewHandler;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseStream;
 import javax.faces.context.ResponseWriter;
-import javax.faces.event.FacesEvent;
 import javax.faces.render.RenderKit;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
