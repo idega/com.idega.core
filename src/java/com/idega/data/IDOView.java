b63570bc8b50dfb4c1a22ea66a90fa3946a49579
Aron Birkir Gudmundsson <aron@idega.com>
2004-06-05 07:59:58 +0000

    General view interface to be used for database views

---
+++ src/java/com/idega/data/IDOView.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Jan 20, 2004
+ *
+ */
+package com.idega.data;
+
+/**
+ * IDOView
+ * @author aron 
+ * @version 1.0
+ */
+public interface IDOView {
+	public String getCreationSQL();
+}
