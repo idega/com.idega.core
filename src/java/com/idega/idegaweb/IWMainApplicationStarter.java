794c569bda4e5651864ec020e98195955edb6b91
Thomas Hilbig <thomas@idega.com>
2004-06-23 18:07:53 +0000

    moved away the registration to the bundles that are responsible for the registration

--- src/java/com/idega/idegaweb/IWMainApplicationStarter.java
+++ src/java/com/idega/idegaweb/IWMainApplicationStarter.java
@@ -14,6 +14,8 @@ import javax.ejb.FinderException;
 
 import com.idega.business.IBOLookup;
 import com.idega.core.accesscontrol.business.LoginBusinessBean;
+import com.idega.core.user.data.OldUserBMPBean;
+import com.idega.core.user.data.User;
 import com.idega.data.EntityControl;
 import com.idega.data.IDOContainer;
 import com.idega.data.IDOLookupException;
@@ -237,15 +239,7 @@ public class IWMainApplicationStarter {
 					if(userSystem.equalsIgnoreCase("OLD")){
 						sendStartMessage("Using Old idegaWeb User System");
 						LoginBusinessBean.USING_OLD_USER_SYSTEM=true;
-						try
-						{
-							IBOLookup.registerImplementationForBean("com.idega.core.user.data.User","com.idega.core.user.data.OldUserBMPBean");
-						}
-						catch (ClassNotFoundException e)
-						{
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}	
+						IBOLookup.registerImplementationForBean(User.class,OldUserBMPBean.class);	
 					}
 				}
 				String accControlType = application.getSettings().getProperty(IWMainApplication.IW_ACCESSCONTROL_TYPE_PROPERTY);
@@ -263,67 +257,50 @@ public class IWMainApplicationStarter {
 	 */
 	private void registerSystemBeans()
 	{
-		try
-		{
-			sendStartMessage("Registering System Beans");
-			IBOLookup.registerImplementationForBean("com.idega.core.builder.data.ICDomain","com.idega.builder.data.IBDomainBMPBean");
-			IBOLookup.registerImplementationForBean("com.idega.core.builder.data.ICPage","com.idega.builder.data.IBPageBMPBean");
-			
-			IBOLookup.registerImplementationForBean("com.idega.core.builder.business.BuilderService","com.idega.builder.business.IBMainServiceBean");
-			IBOLookup.registerImplementationForBean("com.idega.core.file.business.ICFileSystem","com.idega.block.media.business.MediaFileSystemBean");
-			
-			RefactorClassRegistry rfregistry = RefactorClassRegistry.getInstance();
-			rfregistry.registerRefactoredClass("com.idega.builder.data.IBDomain","com.idega.core.builder.data.ICDomain");
-			rfregistry.registerRefactoredClass("com.idega.builder.data.IBPage","com.idega.core.builder.data.ICPage");
+		RefactorClassRegistry rfregistry = RefactorClassRegistry.getInstance();
+		rfregistry.registerRefactoredClass("com.idega.builder.data.IBDomain","com.idega.core.builder.data.ICDomain");
+		rfregistry.registerRefactoredClass("com.idega.builder.data.IBPage","com.idega.core.builder.data.ICPage");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICFile","com.idega.core.file.data.ICFile");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICFileType","com.idega.core.file.data.ICFileType");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICFileTypeHandler","com.idega.core.file.data.ICFileTypeHandler");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICFileCategory","com.idega.core.file.data.ICFileCategory");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICMimeType","com.idega.core.file.data.ICMimeType");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICFile","com.idega.core.file.data.ICFile");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICFileType","com.idega.core.file.data.ICFileType");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICFileTypeHandler","com.idega.core.file.data.ICFileTypeHandler");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICFileCategory","com.idega.core.file.data.ICFileCategory");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICMimeType","com.idega.core.file.data.ICMimeType");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICCategory","com.idega.core.category.data.ICCategory");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICCategoryBMPBean","com.idega.core.category.data.ICCategoryBMPBean");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICCategoryICObjectInstance","com.idega.core.category.data.ICCategoryICObjectInstance");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICCategoryTranslation","com.idega.core.category.data.ICCategoryTranslation");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationCategory","com.idega.core.category.data.ICInformationCategory");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationCategoryBMPBean","com.idega.core.category.data.ICInformationCategoryBMPBean");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationCategoryTranslation","com.idega.core.category.data.ICInformationCategoryTranslation");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationFolder","com.idega.core.category.data.ICInformationFolder");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationFolderBMPBean","com.idega.core.category.data.ICInformationFolderBMPBean");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICCategory","com.idega.core.category.data.ICCategory");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICCategoryBMPBean","com.idega.core.category.data.ICCategoryBMPBean");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICCategoryICObjectInstance","com.idega.core.category.data.ICCategoryICObjectInstance");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICCategoryTranslation","com.idega.core.category.data.ICCategoryTranslation");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationCategory","com.idega.core.category.data.ICInformationCategory");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationCategoryBMPBean","com.idega.core.category.data.ICInformationCategoryBMPBean");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationCategoryTranslation","com.idega.core.category.data.ICInformationCategoryTranslation");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationFolder","com.idega.core.category.data.ICInformationFolder");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICInformationFolderBMPBean","com.idega.core.category.data.ICInformationFolderBMPBean");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICObject","com.idega.core.component.data.ICObject");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICObjectField","com.idega.core.component.data.ICObjectField");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICObjectInstance","com.idega.core.component.data.ICObjectInstance");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICObjectType","com.idega.core.component.data.ICObjectType");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICObject","com.idega.core.component.data.ICObject");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICObjectField","com.idega.core.component.data.ICObjectField");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICObjectInstance","com.idega.core.component.data.ICObjectInstance");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICObjectType","com.idega.core.component.data.ICObjectType");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.AreaCode","com.idega.core.contact.data.AreaCode");
-			rfregistry.registerRefactoredClass("com.idega.core.data.Email","com.idega.core.contact.data.Email");
-			rfregistry.registerRefactoredClass("com.idega.core.data.EmailType","com.idega.core.contact.data.EmailType");
-			rfregistry.registerRefactoredClass("com.idega.core.data.Phone","com.idega.core.contact.data.Phone");
-			rfregistry.registerRefactoredClass("com.idega.core.data.PhoneType","com.idega.core.contact.data.PhoneType");
-			rfregistry.registerRefactoredClass("com.idega.core.data.CountryCode","com.idega.core.contact.data.CountryCode");
+		rfregistry.registerRefactoredClass("com.idega.core.data.AreaCode","com.idega.core.contact.data.AreaCode");
+		rfregistry.registerRefactoredClass("com.idega.core.data.Email","com.idega.core.contact.data.Email");
+		rfregistry.registerRefactoredClass("com.idega.core.data.EmailType","com.idega.core.contact.data.EmailType");
+		rfregistry.registerRefactoredClass("com.idega.core.data.Phone","com.idega.core.contact.data.Phone");
+		rfregistry.registerRefactoredClass("com.idega.core.data.PhoneType","com.idega.core.contact.data.PhoneType");
+		rfregistry.registerRefactoredClass("com.idega.core.data.CountryCode","com.idega.core.contact.data.CountryCode");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICLanaguage","com.idega.core.localisation.data.ICLanaguage");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICLocale","com.idega.core.localisation.data.ICLocale");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICLanaguage","com.idega.core.localisation.data.ICLanaguage");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICLocale","com.idega.core.localisation.data.ICLocale");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.Address","com.idega.core.location.data.Address");
-			rfregistry.registerRefactoredClass("com.idega.core.data.AddressType","com.idega.core.location.data.AddressType");
-			rfregistry.registerRefactoredClass("com.idega.core.data.Commune","com.idega.core.location.data.Commune");
-			rfregistry.registerRefactoredClass("com.idega.core.data.Country","com.idega.core.location.data.Country");
-			rfregistry.registerRefactoredClass("com.idega.core.data.PostalCode","com.idega.core.location.data.PostalCode");
-			rfregistry.registerRefactoredClass("com.idega.core.data.Province","com.idega.core.location.data.Province");
+		rfregistry.registerRefactoredClass("com.idega.core.data.Address","com.idega.core.location.data.Address");
+		rfregistry.registerRefactoredClass("com.idega.core.data.AddressType","com.idega.core.location.data.AddressType");
+		rfregistry.registerRefactoredClass("com.idega.core.data.Commune","com.idega.core.location.data.Commune");
+		rfregistry.registerRefactoredClass("com.idega.core.data.Country","com.idega.core.location.data.Country");
+		rfregistry.registerRefactoredClass("com.idega.core.data.PostalCode","com.idega.core.location.data.PostalCode");
+		rfregistry.registerRefactoredClass("com.idega.core.data.Province","com.idega.core.location.data.Province");
 
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICNetwork","com.idega.core.net.data.ICNetwork");
-			rfregistry.registerRefactoredClass("com.idega.core.data.ICProtocol","com.idega.core.net.data.ICProtocol");
-
-
-		}
-		catch (ClassNotFoundException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICNetwork","com.idega.core.net.data.ICNetwork");
+		rfregistry.registerRefactoredClass("com.idega.core.data.ICProtocol","com.idega.core.net.data.ICProtocol");
 	}
 
 	protected void insertStartData() {
