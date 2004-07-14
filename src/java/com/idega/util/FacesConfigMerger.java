1b782cd2f4fef2e1ec60007340508db547f30986
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2004-07-14 15:12:02 +0000

    improved header formatting

--- src/java/com/idega/util/FacesConfigMerger.java
+++ src/java/com/idega/util/FacesConfigMerger.java
@@ -14,8 +14,8 @@ public class FacesConfigMerger extends BundleFileMerger {
 	public FacesConfigMerger(){
 		String xmlHeader = 
 			"<?xml version=\"1.0\"?>\n"+
-			"<!DOCTYPE faces-config PUBLIC\n"+
-			"\t\"-//Sun Microsystems, Inc.//DTD JavaServer Faces Config 1.0//EN\"\n"+
+			"<!DOCTYPE faces-config \n"+
+			"\tPUBLIC \"-//Sun Microsystems, Inc.//DTD JavaServer Faces Config 1.0//EN\"\n"+
 			"\t\"http://java.sun.com/dtd/web-facesconfig_1_0.dtd\">\n";
 			//+"<!-- Generated file by idegaWeb please don't modify the module markers -->";
 		setFileHeader(xmlHeader);
