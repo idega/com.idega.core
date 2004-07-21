d2f8dc93f106a8e656044f19bea495559eb8fa42
Thorhallur Helgason <laddi@idega.com>
2004-07-21 15:09:38 +0000

    A utility to parse a String of attributes to a Map.

---
+++ src/java/com/idega/util/text/AttributeParser.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 21.7.2004
+ */
+package com.idega.util.text;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+/**
+ * @author laddi
+ */
+public class AttributeParser {
+
+	public Map parse(String stringToParse) {
+		Map map = new HashMap();
+		
+		StringTokenizer tokens = new StringTokenizer(stringToParse, " ");
+		while (tokens.hasMoreTokens()) {
+			String attributes = tokens.nextToken();
+			if (attributes.indexOf("=") != -1) {
+				String[] values = attributes.split("=");
+				String attribute = values[0].trim();
+				String value = values[1];
+				
+				map.put(attribute, value);
+			}
+		}
+		
+		return map;
+	}
+}
