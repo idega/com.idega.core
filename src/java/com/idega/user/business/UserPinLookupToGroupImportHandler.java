8f73cd92b94ee7e505c5c9812eb965d49f2044d6
Thomas Hilbig <thomas@idega.com>
2004-07-28 18:34:45 +0000

    removed references from the "com.idega.user" bundle to the "is" tree

---
+++ src/java/com/idega/user/business/UserPinLookupToGroupImportHandler.java
@@ -0,0 +1,16 @@
+package com.idega.user.business;
+
+import com.idega.repository.data.ImplementorPlaceholder;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Jul 28, 2004
+ */
+public interface UserPinLookupToGroupImportHandler	extends ImplementorPlaceholder {
+	// a placeholder
+}
