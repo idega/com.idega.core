8a5a8bb8766954046f7cc73d1f96d826f28ca16c
Thomas Hilbig <thomas@idega.com>
2004-07-29 14:29:46 +0000

    reference to a user constant moved from core bundle to com.idega.user bundle

---
+++ src/java/com/idega/user/business/UserConstants.java
@@ -0,0 +1,19 @@
+package com.idega.user.business;
+
+import com.idega.repository.data.ConstantsPlaceholder;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Jul 29, 2004
+ */
+public class UserConstants implements ConstantsPlaceholder {
+	// do not set this variable to final
+	// the help bundle identfier might be changed by bundle starters
+	public static String HELP_BUNDLE_IDENTFIER = "com.idega.user";
+	
+}
--- src/java/com/idega/user/util/ICUserConstants.java
+++ src/java/com/idega/user/util/ICUserConstants.java
@@ -13,8 +13,4 @@ public class ICUserConstants implements ConstantsPlaceholder {
 	public static final String GROUP_TYPE_ALIAS = "alias";
 	public static final String GROUP_TYPE_GENERAL = "general";
 	public static final String REQUEST_PARAMETER_SELECTED_GROUP_ID = "r_sel_gr_id";
-	// do not set this variable to final
-	// the help bundle identfier might be changed by bundle starters
-	public static String HELP_BUNDLE_IDENTFIER = "com.idega.user";
-	
 }
