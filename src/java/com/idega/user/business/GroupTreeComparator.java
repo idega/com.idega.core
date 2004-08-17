203c1bc36cc6d7c2d6b2fb40b4d0174127d86ca2
Sigtryggur Simonarson <sigtryggur@idega.com>
2004-08-17 10:52:51 +0000

    Extended the functionality of the sorter. Now he sorts aftur localised groupTypeName

--- src/java/com/idega/user/business/GroupTreeComparator.java
+++ src/java/com/idega/user/business/GroupTreeComparator.java
@@ -5,6 +5,7 @@ import java.text.Collator;
 import java.util.Comparator;
 import java.util.Locale;
 
+import com.idega.presentation.IWContext;
 import com.idega.user.data.Group;
 
 
@@ -17,6 +18,7 @@ import com.idega.user.data.Group;
 public class GroupTreeComparator implements Comparator {
 
 	  private Locale _locale;
+	  private final static String IW_BUNDLE_IDENTIFIER = "com.idega.user";
 		
 	  public GroupTreeComparator(Locale locale) {
 	  	_locale = locale;
@@ -29,7 +31,11 @@ public class GroupTreeComparator implements Comparator {
 		Group g1 = (Group) o1;
 		Group g2 = (Group) o2;
 	
-		returner = collator.compare(g1.getGroupType(), g2.getGroupType());
+		IWContext iwc = IWContext.getInstance();
+		String groupType1 = iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_locale).getLocalizedString(g1.getGroupType());
+		String groupType2 = iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_locale).getLocalizedString(g2.getGroupType());
+
+		returner = collator.compare(groupType1, groupType2);
 		if (returner == 0) {
 			Integer groupIntValue1 = getIntegerFromBeginnigOfString(g1.getName());
 			Integer groupIntValue2 = getIntegerFromBeginnigOfString(g2.getName());
