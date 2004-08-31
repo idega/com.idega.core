63e8a09ba8b3147a26ebc6e2a53c8fd2a1877d36
Eirikur Hrafnsson <eiki@idega.com>
2004-08-31 02:38:56 +0000

    added reset function

--- src/java/com/idega/util/Timer.java
+++ src/java/com/idega/util/Timer.java
@@ -16,6 +16,10 @@ public class Timer {
   public void start()   {  startTime = System.currentTimeMillis();       }
   public void stop()    {  endTime   = System.currentTimeMillis();       }
   public long getTime() {  return endTime - startTime;                   }
+  public void reset(){
+  	startTime = 0;
+  	endTime = 0;
+  }
   public String getTimeString() {
   	return new IWTimestamp(getTime()).getDateString("m:ss.SSS");
   }
