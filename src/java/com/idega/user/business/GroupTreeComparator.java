608d8f1e282daa0b6b8cc0fbb36f31b2433fe86d
Sigtryggur Simonarson <sigtryggur@idega.com>
2004-09-07 13:19:23 +0000

    Only handles now GroupTreeNodes, not PageTreeNodes

--- src/java/com/idega/user/business/GroupTreeComparator.java
+++ src/java/com/idega/user/business/GroupTreeComparator.java
@@ -27,42 +27,44 @@ public class GroupTreeComparator implements Comparator {
 		int returner = 0;
 	  	Collator collator = Collator.getInstance(_locale);
 		
-		GroupTreeNode g1 = (GroupTreeNode) o1;
-		GroupTreeNode g2 = (GroupTreeNode) o2;
-	
-		IWContext iwc = IWContext.getInstance();
-		String groupType1 = iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_locale).getLocalizedString(g1.getGroupType());
-		String groupType2 = iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_locale).getLocalizedString(g2.getGroupType());
+	  	if (o1 instanceof GroupTreeNode &&  o2 instanceof GroupTreeNode) {
+			GroupTreeNode g1 = (GroupTreeNode) o1;
+			GroupTreeNode g2 = (GroupTreeNode) o2;
 		
-		if (groupType1 != null && groupType2 == null) {
-		    returner = -1;
-		} else if (groupType1 == null && groupType2 != null) {
-		    returner = 1;
-		} else if (groupType1 != null && groupType2 != null) { 
-			returner = collator.compare(groupType1, groupType2);
-			if (returner == 0) {
-				Integer groupIntValue1 = getIntegerFromBeginnigOfString(g1.getNodeName());
-				Integer groupIntValue2 = getIntegerFromBeginnigOfString(g2.getNodeName());
-	
-				if (groupIntValue1 != null && groupIntValue2 != null) {
-					if (groupIntValue1.intValue() == groupIntValue2.intValue()) {
-						returner = 0;
+			IWContext iwc = IWContext.getInstance();
+			String groupType1 = iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_locale).getLocalizedString(g1.getGroupType());
+			String groupType2 = iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_locale).getLocalizedString(g2.getGroupType());
+			
+			if (groupType1 != null && groupType2 == null) {
+			    returner = -1;
+			} else if (groupType1 == null && groupType2 != null) {
+			    returner = 1;
+			} else if (groupType1 != null && groupType2 != null) { 
+				returner = collator.compare(groupType1, groupType2);
+				if (returner == 0) {
+					Integer groupIntValue1 = getIntegerFromBeginnigOfString(g1.getNodeName());
+					Integer groupIntValue2 = getIntegerFromBeginnigOfString(g2.getNodeName());
+		
+					if (groupIntValue1 != null && groupIntValue2 != null) {
+						if (groupIntValue1.intValue() == groupIntValue2.intValue()) {
+							returner = 0;
+						}
+						else if (groupIntValue1.intValue() < groupIntValue2.intValue()) {
+							returner = -1;
+						}
+						else
+							returner = 1;
+						
+						if (returner == 0)
+							returner = collator.compare(g1.getNodeName(), g2.getNodeName());
 					}
-					else if (groupIntValue1.intValue() < groupIntValue2.intValue()) {
-						returner = -1;
-					}
-					else
-						returner = 1;
-					
-					if (returner == 0)
+					else {
 						returner = collator.compare(g1.getNodeName(), g2.getNodeName());
+					}
 				}
-				else {
-					returner = collator.compare(g1.getNodeName(), g2.getNodeName());
-				}
+			} else {
+				returner = collator.compare(g1.getNodeName(), g2.getNodeName());
 			}
-		} else {
-			returner = collator.compare(g1.getNodeName(), g2.getNodeName());
 		}
 		return returner;
 	}
