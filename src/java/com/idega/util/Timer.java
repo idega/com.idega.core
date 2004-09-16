7ea2341a22d91e99f2a6b99ec6626e1706ab263e
Eirikur Hrafnsson <eiki@idega.com>
2004-09-16 17:42:46 +0000

    now you can call getTime without stopping the Timer first

--- src/java/com/idega/util/Timer.java
+++ src/java/com/idega/util/Timer.java
@@ -1,26 +1,53 @@
 package com.idega.util;
 
 /**
- * <p>Title: Timer</p>
- * <p>Description: A simple "stopwatch" class with millisecond accuracy</p>
- * <p>Copyright (c) 2002</p>
- * <p>Company: Idega Software</p>
- * @author <a href="mailto:eiki@idega.is"> Eirikur Sveinn Hrafnsson</a>
+ * <p>
+ * Title: Timer
+ * </p>
+ * <p>
+ * Description: A simple "stopwatch" class with millisecond accuracy
+ * </p>
+ * <p>
+ * Copyright (c) 2002
+ * </p>
+ * <p>
+ * Company: Idega Software
+ * </p>
+ * 
+ * @author <a href="mailto:eiki@idega.is"> Eirikur Sveinn Hrafnsson </a>
  * @version 1.0
  */
-
 public class Timer {
 
-  private long startTime, endTime;
+	private long startTime, endTime;
 
-  public void start()   {  startTime = System.currentTimeMillis();       }
-  public void stop()    {  endTime   = System.currentTimeMillis();       }
-  public long getTime() {  return endTime - startTime;                   }
-  public void reset(){
-  	startTime = 0;
-  	endTime = 0;
-  }
-  public String getTimeString() {
-  	return new IWTimestamp(getTime()).getDateString("m:ss.SSS");
-  }
+	private boolean hasStopped = false;
+
+	public void start() {
+		startTime = System.currentTimeMillis();
+	}
+
+	public void stop() {
+		hasStopped = true;
+		endTime = System.currentTimeMillis();
+	}
+
+	public long getTime() {
+		if(hasStopped){
+			return endTime - startTime;
+		}
+		else{
+			return System.currentTimeMillis()-startTime;
+		}
+	}
+
+	public void reset() {
+		startTime = 0;
+		endTime = 0;
+		hasStopped = false;
+	}
+
+	public String getTimeString() {
+		return new IWTimestamp(getTime()).getDateString("m:ss.SSS");
+	}
 }
