567d1b177b864bec8c513b32384611d2b9cce1bf
Aron Birkir Gudmundsson <aron@idega.com>
2004-09-27 13:51:39 +0000

    Refactoring, EntityFinder usage removed from IBPageFinder

--- src/java/com/idega/core/builder/data/ICPageHome.java
+++ src/java/com/idega/core/builder/data/ICPageHome.java
@@ -8,5 +8,10 @@ public interface ICPageHome extends com.idega.data.IDOHome
  public ICPage findByPrimaryKey(int id) throws javax.ejb.FinderException;
  public ICPage findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
  public ICPage findByPrimaryKeyLegacy(int id) throws java.sql.SQLException;
+/**
+ * @param integer
+ * @return
+ */
+public java.util.Collection findByTemplate(Integer integer)throws javax.ejb.FinderException;
 
 }
