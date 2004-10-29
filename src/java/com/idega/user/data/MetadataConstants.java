63621f8b213db4a02811db47af086b244f3253a1
Thomas Hilbig <thomas@idega.com>
2004-10-29 10:22:01 +0000

    new Constants class added

---
+++ src/java/com/idega/user/data/MetadataConstants.java
@@ -0,0 +1,26 @@
+/*
+ * $Id: MetadataConstants.java,v 1.1 2004/10/29 10:22:01 thomas Exp $
+ * Created on Oct 29, 2004
+ *
+ * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.user.data;
+
+
+/**
+ * 
+ *  Last modified: $Date: 2004/10/29 10:22:01 $ by $Author: thomas $
+ * 
+ * @author <a href="mailto:thomas@idega.com">thomas</a>
+ * @version $Revision: 1.1 $
+ */
+public class MetadataConstants {
+	
+	public static final String GOLF_META_PREFIX = "golf_";
+	public static final String SUB_CLUBS_GOLF_META_DATA_KEY = GOLF_META_PREFIX +"sub_clubs";
+	public static final String MAIN_CLUB_GOLF_META_DATA_KEY = GOLF_META_PREFIX +"main_club";
+	public static final String HANDICAP_GOLF_META_DATA_KEY  = GOLF_META_PREFIX +"handicap";
+}
