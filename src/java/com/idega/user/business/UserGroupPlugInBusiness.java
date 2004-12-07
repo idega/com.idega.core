fe65a727f65ed110cb5239cd749ec00d1db1612e
Eirikur Hrafnsson <eiki@idega.com>
2004-12-07 15:55:16 +0000

    Added javadoc and the method canCreateSubGroup(group) and remove some unused methods: getListViewerFieldsfindGroupsByFieldsgetPresentationObject

--- src/java/com/idega/user/business/UserGroupPlugInBusiness.java
+++ src/java/com/idega/user/business/UserGroupPlugInBusiness.java
@@ -1,23 +1,21 @@
 package com.idega.user.business;
 
 import java.rmi.RemoteException;
-import java.util.Collection;
 import java.util.List;
-
 import javax.ejb.CreateException;
 import javax.ejb.RemoveException;
-
 import com.idega.business.IBOService;
 import com.idega.presentation.PresentationObject;
 import com.idega.user.data.Group;
 import com.idega.user.data.User;
 
 /**
- * Title:        idegaWeb User Subsystem
+ * Title:        This inteface defines methods you can implement to customize the behavior of the user system. <br>
+ * Methods returning a boolean should return true by default other methods can return null objects or empty lists where applicable.
  * Description:  idegaWeb User Subsystem
  * Copyright:    Copyright (c) 2002
  * Company:      idega
- * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>,<a href="mailto:eiki@idega.is">Eirikur Hrafnsson</a>
  * @version 1.0
  */
 
@@ -28,30 +26,44 @@ public interface UserGroupPlugInBusiness extends IBOService {
 
     public void beforeGroupRemove(Group group)throws RemoveException,RemoteException;
     public void afterGroupCreateOrUpdate(Group group)throws CreateException,RemoteException;
-
-    public Class getPresentationObjectClass()throws RemoteException;
-    /*
-    public UserGroupPlugInPresentable instanciateEditor(Group group)throws RemoteException;
-    public UserGroupPlugInPresentable instanciateViewer(Group group)throws RemoteException;
-	*/
+ 
 	public PresentationObject instanciateEditor(Group group)throws RemoteException;
     public PresentationObject instanciateViewer(Group group)throws RemoteException;
     
+    
+    /**
+     * Return a list of Collectable presentation objects that extend UserTab.
+     * These objects will be used in the User Property Window as tabs.
+     * @param user The currently selected user
+     * @return a list of Collectable presentation object, preferably something that extends UserTab OR null
+     * @throws RemoteException
+     */
     public List getUserPropertiesTabs(User user) throws RemoteException;
+    
+    /**
+     * Return a list of Collectable presentation objects
+     * These objects will be used as Tabs in the Group Property Window
+     * @param group The currently selected group
+     * @return a list of Collectable presentation object or null
+     * @throws RemoteException
+     */
     public List getGroupPropertiesTabs(Group group) throws RemoteException;
+    
+    /**
+     * Return a list of com.idega.user.app.ToolbarElement's that you want to be added to the main toolbar
+     * @return A list of com.idega.user.app.ToolbarElement's that you want to be added to the group view toolbar
+     * @throws RemoteException
+     */
     public List getMainToolbarElements() throws RemoteException;
+    
+    /**
+     * Return a list of com.idega.user.app.ToolbarElement's that you want to be added to the group view toolbar
+     * @param group The currently selected group
+     * @return A list of com.idega.user.app.ToolbarElement's that you want to be added to the group view toolbar
+     * @throws RemoteException
+     */
     public List getGroupToolbarElements(Group group) throws RemoteException;
 
-    /**
-     * Returns a Collection of ListViewerField Objects
-     */
-    public Collection getListViewerFields()throws RemoteException;
-
-    /**
-     * Returns a Collection of Group Objects
-     */
-    public Collection findGroupsByFields(Collection listViewerFields,Collection finderOperators,Collection listViewerFieldValues)throws RemoteException;
-   
    /** Checks if the user is assignable from the specified source to the specified target.
    * 
    * @param user the user that should be moved.
@@ -68,4 +80,14 @@ public interface UserGroupPlugInBusiness extends IBOService {
    * @return a message that says what is wrong else null.
    */
     public String isUserSuitedForGroup(User user, Group targetGroup) throws RemoteException;
+
+    /**
+     * Checks if it is allowed to create a child (sub) group under the supplied group.
+     * @param group
+     * @return Should return null by default but a string (explanation) if the plugin doesn't allow subgroups
+     * @throws RemoteException
+     */
+    public String canCreateSubGroup(Group group) throws RemoteException;
+    
+    
 }
