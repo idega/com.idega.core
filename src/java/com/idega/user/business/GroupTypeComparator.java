fd64c99670eefab3a9ed8b7c5451a8f87aac1b0a
Sigtryggur Simonarson <sigtryggur@idega.com>
2004-12-16 09:36:55 +0000

    New class to sort group_types by localized name

---
+++ src/java/com/idega/user/business/GroupTypeComparator.java
@@ -0,0 +1,35 @@
+/*
+ * Created on Dec 15, 2004
+  */
+package com.idega.user.business;
+
+import java.text.Collator;
+import java.util.Comparator;
+
+import com.idega.presentation.IWContext;
+import com.idega.user.data.GroupType;
+
+/**
+ * @author Sigtryggur
+ * 
+ */
+public class GroupTypeComparator implements Comparator {
+
+    private IWContext _iwc;
+	private final static String IW_BUNDLE_IDENTIFIER = "com.idega.user";	
+
+    public GroupTypeComparator(IWContext iwc) {
+	  	_iwc = iwc;
+	}
+
+    public int compare(Object o1, Object o2) {
+        Collator collator = Collator.getInstance(_iwc.getCurrentLocale());
+        
+        GroupType groupType1 = (GroupType)o1;
+        GroupType groupType2 = (GroupType)o2;
+        
+        String localizedGroupType1 = _iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_iwc.getCurrentLocale()).getLocalizedString(groupType1.getType());
+        String localizedGroupType2 = _iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(_iwc.getCurrentLocale()).getLocalizedString(groupType2.getType());
+        return  collator.compare(localizedGroupType1, localizedGroupType2);
+    }
+}
