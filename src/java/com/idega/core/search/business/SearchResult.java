f9912052cb355fe50f1ccc7cabb256bf54f79617
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-01-18 12:27:22 +0000

    Renaming URI to URL

--- src/java/com/idega/core/search/business/SearchResult.java
+++ src/java/com/idega/core/search/business/SearchResult.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SearchResult.java,v 1.1 2005/01/18 11:53:25 tryggvil Exp $
+ * $Id: SearchResult.java,v 1.2 2005/01/18 12:27:22 tryggvil Exp $
  * Created on 18.1.2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -15,10 +15,10 @@ package com.idega.core.search.business;
  * An instance of this class corresponds to a "search result" i.e. each row of a search result.
  * </p>
  * 
- *  Last modified: $Date: 2005/01/18 11:53:25 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2005/01/18 12:27:22 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface SearchResult {
 	
@@ -26,6 +26,6 @@ public interface SearchResult {
 	public String getSearchResultHeadline();
 	public String getSearchResultDescription();
 	public String getSearchResultAbstract();
-	public String getSearchResultURI();
+	public String getSearchResultURL();
 	
 }
