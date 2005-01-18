10eda0e1113ee89ff4e70dec992004f64c91348a
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-01-18 12:33:54 +0000

    Added attributes method

--- src/java/com/idega/core/search/business/SearchResult.java
+++ src/java/com/idega/core/search/business/SearchResult.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SearchResult.java,v 1.2 2005/01/18 12:27:22 tryggvil Exp $
+ * $Id: SearchResult.java,v 1.3 2005/01/18 12:33:54 tryggvil Exp $
  * Created on 18.1.2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -9,16 +9,18 @@
  */
 package com.idega.core.search.business;
 
+import java.util.Map;
+
 
 /**
  * <p>
  * An instance of this class corresponds to a "search result" i.e. each row of a search result.
  * </p>
  * 
- *  Last modified: $Date: 2005/01/18 12:27:22 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2005/01/18 12:33:54 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public interface SearchResult {
 	
@@ -27,5 +29,9 @@ public interface SearchResult {
 	public String getSearchResultDescription();
 	public String getSearchResultAbstract();
 	public String getSearchResultURL();
-	
+	/**
+	 * Can return any arbitrary non-standard attributes with a Collection of attributeKey=attributeValue.
+	 * @return
+	 */
+	public Map getSearchResultAttributes();
 }
