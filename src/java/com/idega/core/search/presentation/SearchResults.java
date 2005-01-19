58d5fffec7d9b186976255e6855d8080a73bbd7f
Eirikur Hrafnsson <eiki@idega.com>
2005-01-19 18:55:49 +0000

    *** empty log message ***

--- src/java/com/idega/core/search/presentation/SearchResults.java
+++ src/java/com/idega/core/search/presentation/SearchResults.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SearchResults.java,v 1.2 2005/01/19 01:48:30 eiki Exp $ Created on Jan
+ * $Id: SearchResults.java,v 1.3 2005/01/19 18:57:55 eiki Exp $ Created on Jan
  * 17, 2005
  * 
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -23,32 +23,33 @@ import com.idega.core.search.data.SimpleSearchQuery;
 import com.idega.presentation.Block;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Layer;
+import com.idega.presentation.PresentationObject;
 import com.idega.presentation.text.Link;
 import com.idega.presentation.text.Text;
 
 /**
- * Last modified: $Date: 2005/01/19 01:48:30 $ by $Author: eiki $
+ * Last modified: $Date: 2005/01/19 18:57:55 $ by $Author: eiki $
  * 
  * This block can use all SearchPlugin objects registered in bundles and sets up
  * the search results (simple by default or advanced) <br>
  * Use setInputParameterName if you want to have different searches on the same
- * page, remember to set the same parameter <br>
- * for the Searcher block.
+ * page, remember to set the same parameter for the Searcher block.<br>
+ * The look of the results are set via stylesheet classes defined in iw_core.css.<br>
+ * Do not change core_iw.css, rather add your own custom stylesheet after the iw_core.css is added and override the styles.
  * 
  * @author <a href="mailto:eiki@idega.com">Eirikur S. Hrafnsson </a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class SearchResults extends Block {
 
-	public static final String DEFAULT_STYLE_CLASS = "iw_search_results";
-
-	public static final String DEFAULT_ROW_EVEN_STYLE_CLASS = "iw_search_results_row_even";
-
-	public static final String DEFAULT_ROW_ODD_STYLE_CLASS = "iw_search_results_row_odd";
 
 	public static final String DEFAULT_SEARCH_PARAMETER_NAME = Searcher.DEFAULT_SEARCH_PARAMETER_NAME;
 	
+	public static final String DEFAULT_STYLE_CLASS = "iw_search_results";
+	public static final String DEFAULT_ROW_EVEN_STYLE_CLASS = "iw_search_results_row_even";
+	public static final String DEFAULT_ROW_ODD_STYLE_CLASS = "iw_search_results_row_odd";
 	public static final String DEFAULT_LINK_STYLE_CLASS = "iw_search_result_link";
+	public static final String DEFAULT_ICON_STYLE_CLASS = "iw_search_result_icon";
 	public static final String DEFAULT_ABSTRACT_TEXT_STYLE_CLASS = "iw_search_result_abstract";
 	public static final String DEFAULT_EXTRA_INFO_TEXT_STYLE_CLASS = "iw_search_result_extra_info";
 	private static final String DEFAULT_SEARCH_NAME_STYLE_CLASS = "iw_search_name";
@@ -62,6 +63,7 @@ public class SearchResults extends Block {
 	
 	private String styleClass = DEFAULT_STYLE_CLASS;
 
+	private String iconStyleClass = DEFAULT_ICON_STYLE_CLASS;
 	private String rowEvenStyleClass = DEFAULT_ROW_EVEN_STYLE_CLASS;
 
 	private String rowOddStyleClass = DEFAULT_ROW_ODD_STYLE_CLASS;
@@ -154,6 +156,9 @@ public class SearchResults extends Block {
 			Layer oddRowProtoType = new Layer();
 			oddRowProtoType.setStyleClass(getRowOddStyleClass());
 			
+			Layer iconPrototype = new Layer();
+			iconPrototype.setStyleClass(getIconStyleClass());
+			
 			Link linkProtoType = new Link();
 			linkProtoType.setStyleClass(getLinkStyleClass());
 			
@@ -206,6 +211,13 @@ public class SearchResults extends Block {
 							int row = 1;
 							Iterator iterator = results.iterator();
 							while (iterator.hasNext()) {
+								SearchResult result = (SearchResult) iterator.next();
+								String textOnLink = result.getSearchResultName();
+								String uri = result.getSearchResultURI();
+								String abstractText = result.getSearchResultAbstract();
+								String extraInfo = result.getSearchResultExtraInformation();
+								//todo group by type
+								String type = result.getSearchResultType();
 								
 								if(row%2==0){
 									rowContainer = (Layer)evenRowProtoType.clone();
@@ -213,18 +225,19 @@ public class SearchResults extends Block {
 								else{
 									rowContainer = (Layer)oddRowProtoType.clone();
 								}
+								//add an extra optional style with the search type suffix
+								addSearchResultTypeStyleClass(rowContainer, type);
 								
-								SearchResult result = (SearchResult) iterator.next();
-								String textOnLink = result.getSearchResultName();
-								String uri = result.getSearchResultURI();
-								String abstractText = result.getSearchResultAbstract();
-								String extraInfo = result.getSearchResultExtraInformation();
-								//todo group by type
-								//String type = result.getSearchResultType();
+								//add an optional icon, use a background image in the style
+								Layer icon = (Layer) iconPrototype.clone();
+								addSearchResultTypeStyleClass(icon,type);
+								rowContainer.add(icon);
 								
 								if(textOnLink!=null){
 									Link link = (Link) linkProtoType.clone();
 									link.setText(textOnLink);
+//									add an extra optional style with the search type suffix
+									addSearchResultTypeStyleClass(link,type);
 								
 									if(uri!=null){
 										link.setURL(uri);
@@ -232,17 +245,23 @@ public class SearchResults extends Block {
 									rowContainer.add(link);
 								}
 								
-								if(abstractText!=null){
-									Text abstractT = (Text)abstractTextProtoType.clone();
-									abstractT.setText(abstractText);
-									rowContainer.add(abstractT);
-								}
-								
 								if(extraInfo!=null){
 									Text extraInfoText = (Text)extraInfoTextProtoType.clone();
 									extraInfoText.setText(extraInfo);
+//									add an extra optional style with the search type suffix
+									addSearchResultTypeStyleClass(extraInfoText,type);
 									rowContainer.add(extraInfoText);
 								}
+								
+								if(abstractText!=null){
+									Text abstractT = (Text)abstractTextProtoType.clone();
+									abstractT.setText(abstractText);
+									//add an extra optional style with the search type suffix
+									addSearchResultTypeStyleClass(abstractT,type);
+									rowContainer.add(abstractT);
+								}
+								
+								
 								
 								container.add(rowContainer);
 								row++;
@@ -255,6 +274,16 @@ public class SearchResults extends Block {
 		}
 	}
 	
+	/**
+	 * Adds an extra optional style with the search type suffix
+	 * @param obj
+	 * @param type
+	 */
+	private void addSearchResultTypeStyleClass(PresentationObject obj, String type) {
+		String style = obj.getStyleClass();
+		obj.setStyleClass(style+" "+style+"_"+type);
+	}
+
 	/**
 	 * @return Returns the linkStyleClass.
 	 */
@@ -302,5 +331,17 @@ public class SearchResults extends Block {
 	 */
 	public void setSearchNameStyleClass(String searchNameStyleClass) {
 		this.searchNameStyleClass = searchNameStyleClass;
+	}
+	/**
+	 * @return Returns the iconStyleClass.
+	 */
+	public String getIconStyleClass() {
+		return iconStyleClass;
+	}
+	/**
+	 * @param iconStyleClass The iconStyleClass to set.
+	 */
+	public void setIconStyleClass(String iconStyleClass) {
+		this.iconStyleClass = iconStyleClass;
 	}
 }
