99197387617ec09524462c7a4de715d588ac7447
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-03-06 12:23:07 +0000

    changing roles from author to content_author and edit to content_edit

--- src/java/com/idega/core/accesscontrol/business/StandardRoles.java
+++ src/java/com/idega/core/accesscontrol/business/StandardRoles.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StandardRoles.java,v 1.1 2005/03/03 06:24:48 tryggvil Exp $
+ * $Id: StandardRoles.java,v 1.2 2005/03/06 12:23:07 tryggvil Exp $
  * Created on 2.3.2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -14,10 +14,10 @@ package com.idega.core.accesscontrol.business;
  * <p>
  * This class holds the keys for the standard idegaWeb system level roles
  * </p>
- * Last modified: $Date: 2005/03/03 06:24:48 $ by $Author: tryggvil $
+ * Last modified: $Date: 2005/03/06 12:23:07 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class StandardRoles {
 	
@@ -25,7 +25,7 @@ public class StandardRoles {
 	public static final String ROLE_KEY_BUILDER="builder";
 	public static final String ROLE_KEY_DEVELOPER="developer";
 	public static final String ROLE_KEY_USERADMIN="userapplication";
-	public static final String ROLE_KEY_EDITOR="editor";
-	public static final String ROLE_KEY_AUTHOR="author";
+	public static final String ROLE_KEY_EDITOR="content_editor";
+	public static final String ROLE_KEY_AUTHOR="content_author";
 	
 }
