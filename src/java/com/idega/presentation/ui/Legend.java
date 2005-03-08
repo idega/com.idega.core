6eb3d6eea58418d8d5f43949af8e44945df82e98
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-03-08 11:39:13 +0000

    Made non-transient and JSF state aware.

--- src/java/com/idega/presentation/ui/Legend.java
+++ src/java/com/idega/presentation/ui/Legend.java
@@ -6,6 +6,7 @@
  */
 package com.idega.presentation.ui;
 
+import javax.faces.context.FacesContext;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
 
@@ -16,13 +17,25 @@ import com.idega.presentation.PresentationObject;
 public class Legend extends PresentationObject {
 	
 	private String _legend;
-	
+
+	public Object saveState(FacesContext ctx) {
+		Object values[] = new Object[2];
+		values[0] = super.saveState(ctx);
+		values[1] = _legend;
+		return values;
+	}
+	public void restoreState(FacesContext ctx, Object state) {
+		Object values[] = (Object[]) state;
+		super.restoreState(ctx, values[0]);
+		_legend = ((String) values[1]);
+	}
 	public Legend() {
 		this("untitled");
 	}
 	
 	public Legend(String legend) {
 		_legend = legend;
+		setTransient(false);
 	}
 
 	public void print(IWContext iwc) throws Exception {
