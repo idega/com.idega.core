615cb6a8c6438e4b0aad3f366c9741d1b25cca73
Thorhallur Helgason <laddi@idega.com>
2005-03-08 14:47:47 +0000

    Removed unused imports.

--- src/java/com/idega/faces/IWBundleStarter.java
+++ src/java/com/idega/faces/IWBundleStarter.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IWBundleStarter.java,v 1.4 2005/03/06 12:26:17 tryggvil Exp $
+ * $Id: IWBundleStarter.java,v 1.5 2005/03/08 14:47:47 laddi Exp $
  * Created on 2.11.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -9,7 +9,6 @@
  */
 package com.idega.faces;
 
-import com.idega.builder.view.BuilderRootViewNode;
 import com.idega.core.view.DefaultViewNode;
 import com.idega.core.view.ViewManager;
 import com.idega.faces.viewnode.WindowViewNode;
@@ -19,10 +18,10 @@ import com.idega.idegaweb.IWBundleStartable;
 
 /**
  * 
- *  Last modified: $Date: 2005/03/06 12:26:17 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2005/03/08 14:47:47 $ by $Author: laddi $
  * 
  * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class IWBundleStarter implements IWBundleStartable {
 
