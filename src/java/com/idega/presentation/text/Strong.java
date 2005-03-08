6e9e5559631a70ec839242976b43b0de5f3c486f
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-03-08 19:25:19 +0000

    Documentation update

--- src/java/com/idega/presentation/text/Strong.java
+++ src/java/com/idega/presentation/text/Strong.java
@@ -1,5 +1,11 @@
 /*
+ * $Id: Strong.java,v 1.3 2005/03/08 19:25:19 tryggvil Exp $
  * Created on 10.7.2004
+ *
+ * Copyright (C) 2004-2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
  */
 package com.idega.presentation.text;
 
@@ -7,12 +13,13 @@ import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObjectContainer;
 
 /**
- * Title: Strong
- * Description:
- * Copyright: Copyright (c) 2004
- * Company: idega Software
- * @author 2004 - idega team - <br><a href="mailto:gummi@idega.is">Gudmundur Agust Saemundsson</a><br>
- * @version 1.0
+ * <p>
+ * This component renders out a &lt;STRONG&gt; element around its children
+ * </p>
+ *  Last modified: $Date: 2005/03/08 19:25:19 $ by $Author: tryggvil $
+ * 
+ * @author <a href="mailto:gummi@idega.com">Gudmundur Agust Saemundsson</a>,<a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
+ * @version $Revision: 1.3 $
  */
 public class Strong  extends PresentationObjectContainer{
 
@@ -22,7 +29,6 @@ public class Strong  extends PresentationObjectContainer{
 	public Strong() {
 		super();
 		setTransient(false);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void print(IWContext iwc)throws Exception{
