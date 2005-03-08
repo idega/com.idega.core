87705ee3d022034ecebacfee51fb48713a2f7925
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-03-08 19:43:45 +0000

    Made non-transient and JSF state aware.Updated javadoc documentation.

--- src/java/com/idega/presentation/text/PreformattedText.java
+++ src/java/com/idega/presentation/text/PreformattedText.java
@@ -1,17 +1,44 @@
 /*
+ * $Id: PreformattedText.java,v 1.4 2005/03/08 19:43:45 tryggvil Exp $
  * Created on 2.5.2003
+ *
+ * Copyright (C) 2000-2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
  */
 package com.idega.presentation.text;
 
+import javax.faces.context.FacesContext;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
 
 /**
- * @author laddi
+ * <p>
+ * This component renders out a 'preformatted' or &lt;PRE&gt; element around its children.<br>
+ * This can be used to escape out html or xml tags to be presented to a user without rendering.
+ * </p>
+ *  Last modified: $Date: 2005/03/08 19:43:45 $ by $Author: tryggvil $
+ * 
+ * @author <a href="mailto:laddi@idega.com">laddi</a>,<a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
+ * @version $Revision: 1.4 $
  */
 public class PreformattedText extends PresentationObject {
 
-	String _text;
+	private String _text;
+	
+	public Object saveState(FacesContext ctx) {
+		Object values[] = new Object[2];
+		values[0] = super.saveState(ctx);
+		values[1] = _text;
+		return values;
+	}
+	
+	public void restoreState(FacesContext ctx, Object state) {
+		Object values[] = (Object[]) state;
+		super.restoreState(ctx, values[0]);
+		_text = (String)values[1];
+	}
 	
 	public PreformattedText(){
 		this("");
@@ -20,6 +47,7 @@ public class PreformattedText extends PresentationObject {
 	public PreformattedText(String text){
 		super();
 		setText(text);
+		setTransient(false);
 	}
 
 	public void print(IWContext iwc)throws Exception{
