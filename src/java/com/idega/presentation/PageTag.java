2afe93c9e392c9bd6fff5ce3d880af1b203aeacb
Grimur Jonsson <gimmi@idega.com>
2005-03-08 10:22:32 +0000

    javascripturls and onload added to PageTag

--- src/java/com/idega/presentation/PageTag.java
+++ src/java/com/idega/presentation/PageTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PageTag.java,v 1.1 2005/01/18 20:58:29 tryggvil Exp $
+ * $Id: PageTag.java,v 1.2 2005/03/08 10:22:32 gimmi Exp $
  * Created on 17.01.2005 by Tryggvi Larusson
  * 
  * Copyright (C) 2004 Idega. All Rights Reserved.
@@ -10,19 +10,22 @@
  */
 package com.idega.presentation;
 
+import javax.faces.component.UIComponent;
 import javax.faces.webapp.UIComponentTag;
 
 /**
  * <p>
  * This is a JSP tag for the Page component.
  * </p>
- * Last modified: $Date: 2005/01/18 20:58:29 $ by $Author: tryggvil $
+ * Last modified: $Date: 2005/03/08 10:22:32 $ by $Author: gimmi $
  *
  * @author tryggvil
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class PageTag extends UIComponentTag {
 	
+	String urls;
+	String onload;
 	/**
 	 * @see javax.faces.webapp.UIComponentTag#getRendererType()
 	 */
@@ -36,4 +39,27 @@ public class PageTag extends UIComponentTag {
 	public String getComponentType() {
 		return "Page";
 	}
+	
+	public void setJavascripturls(String urls) {
+		this.urls = urls;
+	}
+	
+	public void setOnload(String onload) {
+		this.onload = onload;
+	}
+	
+	public void release() {
+		urls = null;
+		onload = null;
+	}
+
+	protected void setProperties(UIComponent component) {      
+		super.setProperties(component);
+		if (component != null) {
+			Page page = (Page) component;
+			page.setJavascriptURLs(urls);
+			page.setOnLoad(onload);
+		}
+	}	
+	
 }
