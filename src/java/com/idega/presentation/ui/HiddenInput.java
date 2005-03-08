f8c13b9da0958f9e7f5fe2e5d67a9192863aa9e9
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-03-08 20:42:17 +0000

    Documentation update

--- src/java/com/idega/presentation/ui/HiddenInput.java
+++ src/java/com/idega/presentation/ui/HiddenInput.java
@@ -1,14 +1,26 @@
-//idega 2000 - Tryggvi Larusson
 /*
-*Copyright 2000 idega.is All Rights Reserved.
-*/
+ * $Id: HiddenInput.java,v 1.5 2005/03/08 20:42:17 tryggvil Exp $
+ * Created in 2000 by Tryggvi Larusson
+ *
+ * Copyright (C) 2000-2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
 package com.idega.presentation.ui;
 
-
 /**
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-*@version 1.2
-*/
+ * <p>
+ * This class is used to render out an (invisible hidden input) to add a parameter to a Form.<br>
+ * This class is mainly preserved for backwards compatability and the newer recommended objects are Parameter 
+ * or in JSF javax.faces.component.UIParameter.
+ * </p>
+ *  Last modified: $Date: 2005/03/08 20:42:17 $ by $Author: tryggvil $
+ * 
+ * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
+ * @version $Revision: 1.5 $
+ * @see Parameter
+ */
 public class HiddenInput extends Parameter {
 
 	/**
