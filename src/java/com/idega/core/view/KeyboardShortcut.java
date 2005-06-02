0ac0fa142e05653295a456ae3f93181cdd7383af
Eirikur Hrafnsson <eiki@idega.com>
2005-06-02 17:09:46 +0000

    first impl

--- src/java/com/idega/core/view/KeyboardShortcut.java
+++ src/java/com/idega/core/view/KeyboardShortcut.java
@@ -1,5 +1,5 @@
 /*
- * $Id: KeyboardShortcut.java,v 1.1 2004/11/14 23:24:47 tryggvil Exp $
+ * $Id: KeyboardShortcut.java,v 1.2 2005/06/02 17:09:46 eiki Exp $
  * Created on 13.9.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -12,10 +12,36 @@ package com.idega.core.view;
 
 /**
  * 
- *  Last modified: $Date: 2004/11/14 23:24:47 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2005/06/02 17:09:46 $ by $Author: eiki $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class KeyboardShortcut {
+	private String actionKey;
+
+	
+	public KeyboardShortcut(){}
+	
+	/**
+	 * @param actionKey is a single key modifier
+	 */
+	public KeyboardShortcut(String actionKey){
+		this.actionKey = actionKey;
+	}
+	/**
+	 * @return Returns the actionKey.
+	 */
+	public String getActionKey() {
+		return actionKey;
+	}
+
+	
+	/**
+	 * @param actionKey The actionKey to set.
+	 */
+	public void setActionKey(String actionKey) {
+		this.actionKey = actionKey;
+	}
+	
 }
