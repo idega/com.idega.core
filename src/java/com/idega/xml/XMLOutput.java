8326b538b2d41de55f107717fd4caff5acb0fcf0
Pall Helgason <palli@idega.com>
2005-06-06 23:49:50 +0000

    Fixed nasty bug in code, which resulted in none of the format commands doing anything to the xml document.

--- src/java/com/idega/xml/XMLOutput.java
+++ src/java/com/idega/xml/XMLOutput.java
@@ -1,11 +1,11 @@
 /*
- * $Id: XMLOutput.java,v 1.8 2005/02/15 17:14:43 joakim Exp $
- *
+ * $Id: XMLOutput.java,v 1.9 2005/06/06 23:49:50 palli Exp $
+ * 
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
- *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
- *
+ * 
+ * This software is the proprietary information of Idega hf. Use is subject to
+ * license terms.
+ * 
  */
 package com.idega.xml;
 
@@ -22,84 +22,96 @@ import org.jdom.output.XMLOutputter;
  * @version 1.0
  */
 public class XMLOutput {
-  XMLOutputter _output = null;
 
-  /**
-   *
-   */
-  public XMLOutput() {
-    _output = new XMLOutputter();
-  }
+	XMLOutputter _output = null;
 
-  /**
-   *
-   */
-  public XMLOutput(String indent, boolean newlines) {
-  	Format f = (newlines)?Format.getPrettyFormat():Format.getCompactFormat();
-  	f.setIndent(indent);
-    _output = new XMLOutputter(f);
-  }
+	/**
+	 * 
+	 */
+	public XMLOutput() {
+		_output = new XMLOutputter();
+	}
 
-  /**
-   *
-   */
-  public void setLineSeparator(String seperator) {
-    if (_output != null){
-    	  Format f = _output.getFormat();
-    	  if(f!=null){
-    	  	f.setLineSeparator(seperator);
-    	  	_output.setFormat(f);
-    	  }
-    }
-  }
+	/**
+	 * 
+	 */
+	public XMLOutput(String indent, boolean newlines) {
+		Format f = (newlines) ? Format.getPrettyFormat() : Format.getCompactFormat();
+		f.setIndent(indent);
+		_output = new XMLOutputter(f);
+	}
 
-  /**
-   *
-   */
-  public void setTextNormalize(boolean normalize) {
-    if (_output != null && normalize){
-      Format f = _output.getFormat();
-      if(f!=null){
-      	f.setTextMode(Format.TextMode.NORMALIZE);
-      }
-    }
-  }
+	/**
+	 * 
+	 */
+	public void setLineSeparator(String seperator) {
+		if (_output != null) {
+			Format f = _output.getFormat();
+			if (f != null) {
+				f.setLineSeparator(seperator);
+				_output.setFormat(f);
+			}
+		}
+	}
 
-  /**
-   *
-   */
-  public void output(XMLDocument document, OutputStream stream) throws IOException {
-    if (_output != null)
-      _output.output((Document)document.getDocument(),stream);
-  }
+	/**
+	 * 
+	 */
+	public void setTextNormalize(boolean normalize) {
+		if (_output != null && normalize) {
+			Format f = _output.getFormat();
+			if (f != null) {
+				f.setTextMode(Format.TextMode.NORMALIZE);
+				_output.setFormat(f);
+			}
+		}
+	}
 
-  public String outputString(XMLElement element) {
-    if (_output != null) {
-      Element el = (Element)element.getElement();
-      if (el != null)
-        return(_output.outputString(el));
-      else
-        return(null);
-    }
-    else
-      return(null);
-  }
+	/**
+	 * 
+	 */
+	public void output(XMLDocument document, OutputStream stream) throws IOException {
+		if (_output != null)
+			_output.output((Document) document.getDocument(), stream);
+	}
+
+	public String outputString(XMLElement element) {
+		if (_output != null) {
+			Element el = (Element) element.getElement();
+			if (el != null)
+				return (_output.outputString(el));
+			else
+				return (null);
+		}
+		else
+			return (null);
+	}
 
 	public String outputString(XMLDocument document) throws IOException {
 		StringWriter writer = new StringWriter();
-		Document doc = (Document)document.getDocument();
+		Document doc = (Document) document.getDocument();
 		XMLOutputter outputter = new XMLOutputter();
 		outputter.output(doc, writer);
 		return writer.toString();
 	}
- 
-  public void setEncoding(String encoding) {
-  	if (_output != null)
-  		_output.getFormat().setEncoding(encoding);
-  }
-  
-  public void setSkipEncoding(boolean skip) {
-  	if (_output != null)
-  		_output.getFormat().setOmitEncoding(skip);
-  }
+
+	public void setEncoding(String encoding) {
+		if (_output != null) {
+			Format f = _output.getFormat();
+			if (f != null) {
+				f.setEncoding(encoding);
+				_output.setFormat(f);
+			}
+		}
+	}
+
+	public void setSkipEncoding(boolean skip) {
+		if (_output != null) {
+			Format f = _output.getFormat();
+			if (f != null) {
+				f.setOmitEncoding(skip);
+				_output.setFormat(f);
+			}
+		}
+	}
 }
