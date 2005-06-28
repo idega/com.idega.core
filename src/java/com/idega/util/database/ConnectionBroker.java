71e97b57330e02178327258ecd107eda14a49d77
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-06-28 11:23:17 +0000

    Formatting code and finalising JNDI support

--- src/java/com/idega/util/database/ConnectionBroker.java
+++ src/java/com/idega/util/database/ConnectionBroker.java
@@ -1,12 +1,16 @@
-//idega 2000 - Tryggvi Larusson
 /*
-
-*Copyright 2000 idega.is All Rights Reserved.
-
-*/
+ * $Id: ConnectionBroker.java,v 1.10 2005/06/28 11:23:17 tryggvil Exp $
+ *
+ * Copyright (C) 2000-2005 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
 package com.idega.util.database;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import javax.naming.InitialContext;
 import javax.naming.Context;
@@ -16,11 +20,18 @@ import javax.sql.DataSource;
 import com.idega.transaction.IdegaTransaction;
 import com.idega.transaction.IdegaTransactionManager;
 /**
- *
+ *<p>
+ * This class is an abstraction of the underlying Database Pool.<br>
+ * It can deliver connections from the old style idegaWeb PoolManager or a J2EE style JDBC/JNDI DataSource.<br>
+ * Works in conjunction with com.idega.transaction.IdegaTransactionManager,
+ * com.idega.util.database.PoolManager and javax.sql.DataSource.<br>
+ * 
+ * Whenever a connection object is gotten with getConnection(x) there should always follow a call to freeConnection(x) 
+ * when the Connection is not used anymore, otherwise the Pool could get empty and unused connections left out in limbo.
+ * <br>
+ * </p>
  *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
- *@version 1.3
- * Class to deliver datastore connections through the PoolManager
- * Works in conjunction with com.idega.transaction.IdegaTransactionManager and com.idega.util.database.PoolManager
+ * @version $Revision: 1.10 $
 */
 public class ConnectionBroker
 {
@@ -31,8 +42,10 @@ public class ConnectionBroker
 	public final static int POOL_MANAGER_TYPE_JDBC_DATASOURCE = 3;
 	//temprorary - change
 	public static int POOL_MANAGER_TYPE = POOL_MANAGER_TYPE_IDEGA; //POOL_MANAGER_TYPE_JDBC_DATASOURCE;
-	private static String DEFAULT_JDBC_JNDI_URL="java:comp/env/DefaultDS";
-	private static DataSource ds;
+	private static String DEFAULT_JDBC_JNDI_URL = "jdbc/DefaultDS";
+	
+	private static DataSource defaultDs;
+	private static Logger log = Logger.getLogger(ConnectionBroker.class.getName());
 	/**	
 	 * Returns a Datastore connection from the default datasource
 	 */
@@ -83,9 +96,7 @@ public class ConnectionBroker
 				//try{
 				//conn = com.codestudio.util.SQLManager.getInstance().requestConnection();
 				/**
-				
 				 * @todo: Commit in support for com.codestudio.util PoolMan
-				
 				 */
 				//}
 				//catch(SQLException e){
@@ -130,7 +141,7 @@ public class ConnectionBroker
 					//System.out.println("Getting connection from pool for datasource: "+dataSourceName);
 					conn = PoolManager.getInstance().getConnection(dataSourceName);
 				}
-				else if (isUsingJDBCDatasource())
+				else if (isUsingJNDIDatasource())
 				{
 					try
 					{
@@ -178,7 +189,7 @@ public class ConnectionBroker
 		}
 		else
 		{
-			PoolManager.getInstance().freeConnection(connection);
+			freePooledConnection(null,connection);
 		}
 	}
 	/**
@@ -190,36 +201,43 @@ public class ConnectionBroker
 	{
 		if (doTransactionCheck && !((IdegaTransactionManager) IdegaTransactionManager.getInstance()).hasCurrentThreadBoundTransaction())
 		{
-			//PoolManager.getInstance().freeConnection(connection);
-			if (isUsingIdegaPool())
-			{
-				PoolManager.getInstance().freeConnection(connection);
-			}
-			else if (isUsingPoolManPool())
-			{
-				/**
-				
-				 * @todo: Commit in support for com.codestudio.util PoolMan
-				
-				 */
-				//com.codestudio.util.SQLManager.getInstance().returnConnection(connection);
-			}
+			freePooledConnection(null,connection);
 		}
 		else if (!doTransactionCheck)
 		{
-			if (isUsingIdegaPool())
-			{
+			freePooledConnection(null,connection);
+		}
+	}
+	
+	
+	private static void freePooledConnection(String dataSourceName,Connection connection){
+		if (isUsingIdegaPool())
+		{
+			if(dataSourceName==null){
 				PoolManager.getInstance().freeConnection(connection);
 			}
-			else if (isUsingPoolManPool())
-			{
-				/**
-				
-				 * @todo: Commit in support for com.codestudio.util PoolMan
-				
-				 */
-				//com.codestudio.util.SQLManager.getInstance().returnConnection(connection);
+			else{
+				PoolManager.getInstance().freeConnection(dataSourceName,connection);
 			}
+		}
+		else if (isUsingJNDIDatasource())
+		{
+			try {
+				connection.close();
+			}
+			catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if (isUsingPoolManPool())
+		{
+			/**
+			
+			 * @todo: Commit in support for com.codestudio.util PoolMan
+			
+			 */
+			//com.codestudio.util.SQLManager.getInstance().returnConnection(connection);
 		}
 	}
 	/**
@@ -237,57 +255,11 @@ public class ConnectionBroker
 		{
 			if (doTransactionCheck && !((IdegaTransactionManager) IdegaTransactionManager.getInstance()).hasCurrentThreadBoundTransaction())
 			{
-				if (isUsingIdegaPool())
-				{
-					PoolManager.getInstance().freeConnection(dataSourceName, connection);
-				}
-				else if (isUsingJDBCDatasource())
-				{
-					try
-					{
-						connection.close();
-					}
-					catch (SQLException e)
-					{
-						e.printStackTrace();
-					}
-				}
-				else if (isUsingPoolManPool())
-				{
-					/**
-					
-					 * @todo: Commit in support for com.codestudio.util PoolMan
-					
-					 */
-					//com.codestudio.util.SQLManager.getInstance().returnConnection(connection);
-				}
+				freePooledConnection(dataSourceName,connection);
 			}
 			else if (!doTransactionCheck)
 			{
-				if (isUsingIdegaPool())
-				{
-					PoolManager.getInstance().freeConnection(dataSourceName, connection);
-				}
-				else if (isUsingJDBCDatasource())
-				{
-					try
-					{
-						connection.close();
-					}
-					catch (SQLException e)
-					{
-						e.printStackTrace();
-					}
-				}
-				else if (isUsingPoolManPool())
-				{
-					/**
-					
-					 * @todo: Commit in support for com.codestudio.util PoolMan
-					
-					 */
-					//com.codestudio.util.SQLManager.getInstance().returnConnection(connection);
-				}
+				freePooledConnection(dataSourceName,connection);
 			}
 		}
 	}
@@ -298,33 +270,6 @@ public class ConnectionBroker
 	 */
 	public static void freeConnection(String dataSourceName, Connection connection)
 	{
-		/*if(dataSourceName==null){
-		
-		  freeConnection(connection);
-		
-		}
-		
-		else{
-		
-		      if (!((IdegaTransactionManager)IdegaTransactionManager.getInstance()).hasCurrentThreadBoundTransaction()){
-		
-		        //PoolManager.getInstance().freeConnection(dataSourceName,connection);
-		
-		        if(isUsingIdegaPool()){
-		
-		          PoolManager.getInstance().freeConnection(dataSourceName,connection);
-		
-		        }
-		
-		        else if(isUsingPoolManPool()){
-		
-		            //((com.codestudio.util.JDBCPool)com.codestudio.util.SQLManager.getInstance().getPool(dataSourceName)).returnObject(connection);
-		
-		        }
-		
-		      }
-		
-		}*/
 		freeConnection(dataSourceName, connection, true);
 	}
 	public static String[] getDatasources()
@@ -367,38 +312,63 @@ public class ConnectionBroker
 	{
 		return PoolManager.getInstance().recycleConnection(conn);
 	}
-	static boolean isUsingIdegaPool()
+	public static boolean isUsingIdegaPool()
 	{
 		return (POOL_MANAGER_TYPE == POOL_MANAGER_TYPE_IDEGA);
 	}
-	static boolean isUsingPoolManPool()
+	public static boolean isUsingPoolManPool()
 	{
 		return (POOL_MANAGER_TYPE == POOL_MANAGER_TYPE_POOLMAN);
 	}
-	static boolean isUsingJDBCDatasource()
+	public static boolean isUsingJNDIDatasource()
 	{
 		return (POOL_MANAGER_TYPE == POOL_MANAGER_TYPE_JDBC_DATASOURCE);
 	}
+	
+	
+	public static boolean tryDefaultJNDIDataSource(){
+		/**
+		 * @todo change:
+		* only supports the default datasource now implement support for others
+		*/
+		DataSource ds;
+		try{
+			log.info("Trying DataSource with url: '"+DEFAULT_JDBC_JNDI_URL+"'");
+			ds = getDataSource(DEFAULT_POOL);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+
+		if(ds!=null){
+			POOL_MANAGER_TYPE=POOL_MANAGER_TYPE_JDBC_DATASOURCE;
+			log.info("Successfully enabled Database from DataSource: "+DEFAULT_JDBC_JNDI_URL);
+			return true;
+		}
+		return false;
+	}
+	
 	static DataSource getDataSource(String datasourceName)
 	{
 		/**
 		 * @todo change:
 		* only supports the default datasource now implement support for others
 		*/
-		if (ds == null)
+		if (defaultDs == null)
 		{
 			try
 			{
 				//ds = (DataSource) new InitialContext().lookup("java:comp/env/datasource");
 				
-				ds = (DataSource) getInitialContext().lookup(DEFAULT_JDBC_JNDI_URL);
+				defaultDs = (DataSource) getEnvContext().lookup(DEFAULT_JDBC_JNDI_URL);
 			}
 			catch (NamingException e)
 			{
 				throw new RuntimeException("Error initializing datasource: " + datasourceName + ". Error was: " + e.getMessage());
 			}
 		}
-		return ds;
+		return defaultDs;
 	}
 	/**
 	 * Sets the url to the default datasource if the JDBC datasource is the current pool type
@@ -408,10 +378,14 @@ public class ConnectionBroker
 		DEFAULT_JDBC_JNDI_URL=url;
 	}
 	
-	private static Context getInitialContext()throws NamingException{
+	private static Context getEnvContext()throws NamingException{
 		if(initialContext==null){
 			initialContext = new InitialContext();
 		}
-		return initialContext;
+		return (Context) initialContext.lookup("java:comp/env");
+	}
+	
+	public static String getDefaultJNDIUrl(){
+		return DEFAULT_JDBC_JNDI_URL;
 	}
 }
