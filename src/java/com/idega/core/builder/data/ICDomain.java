cc5b3da68c4cfe18d4d42dcd96c562ae846729d7
Thomas Hilbig <thomas@idega.com>
2005-08-15 14:17:13 +0000

    added methods: setDomainName setUrl

--- src/java/com/idega/core/builder/data/ICDomain.java
+++ src/java/com/idega/core/builder/data/ICDomain.java
@@ -6,9 +6,11 @@ public interface ICDomain extends com.idega.data.IDOLegacyEntity
 {
  public int getStartTemplateID();
  public void setName(java.lang.String p0);
+ public void setDomainName(java.lang.String p0);
  public int getStartPageID();
  public java.util.Collection getTopLevelGroupsUnderDomain()throws com.idega.data.IDORelationshipException, java.rmi.RemoteException, FinderException;
  public java.lang.String getURL();
+ public void setURL(java.lang.String p0);
  public java.lang.String getName();
  public void setStartTemplate(com.idega.core.builder.data.ICPage p0);
  public java.lang.String getDomainName();
