e1cc8b673d9db6550b784469d16919eb956eb7c1
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-09-14 00:45:25 +0000

    adding setstyleclass property

--- src/java/com/idega/presentation/PageTag.java
+++ src/java/com/idega/presentation/PageTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PageTag.java,v 1.2 2005/03/08 10:22:32 gimmi Exp $
+ * $Id: PageTag.java,v 1.3 2005/09/14 00:45:25 tryggvil Exp $
  * Created on 17.01.2005 by Tryggvi Larusson
  * 
  * Copyright (C) 2004 Idega. All Rights Reserved.
@@ -17,15 +17,17 @@ import javax.faces.webapp.UIComponentTag;
  * <p>
  * This is a JSP tag for the Page component.
  * </p>
- * Last modified: $Date: 2005/03/08 10:22:32 $ by $Author: gimmi $
+ * Last modified: $Date: 2005/09/14 00:45:25 $ by $Author: tryggvil $
  *
  * @author tryggvil
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class PageTag extends UIComponentTag {
 	
 	String urls;
 	String onload;
+	String styleClass;
+	
 	/**
 	 * @see javax.faces.webapp.UIComponentTag#getRendererType()
 	 */
@@ -53,12 +55,19 @@ public class PageTag extends UIComponentTag {
 		onload = null;
 	}
 
+	public void setStyleClass(String styleClass){
+		this.styleClass=styleClass;
+	}
+	
 	protected void setProperties(UIComponent component) {      
 		super.setProperties(component);
 		if (component != null) {
 			Page page = (Page) component;
 			page.setJavascriptURLs(urls);
 			page.setOnLoad(onload);
+			if(styleClass!=null){
+				page.setStyleClass(styleClass);
+			}
 		}
 	}	
 	
