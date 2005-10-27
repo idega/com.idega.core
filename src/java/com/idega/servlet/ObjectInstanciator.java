f3491d4090dc869621ed42d9b8865c73a1c991dc
Grimur Jonsson <gimmi@idega.com>
2005-10-27 16:55:17 +0000

    referer url sent to log when class is not found.

--- src/java/com/idega/servlet/ObjectInstanciator.java
+++ src/java/com/idega/servlet/ObjectInstanciator.java
@@ -5,21 +5,25 @@ import com.idega.presentation.Page;
 import com.idega.presentation.PresentationObject;
 import com.idega.repository.data.RefactorClassRegistry;
 /**
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-*@version 1.0
-*/
+ *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ *@version 1.0
+ */
 public class ObjectInstanciator extends DynamicTemplateServlet {
 	//TEMPORARY IMPLEMENTATION - See DynamicTemplateServlet
 	public void main(IWContext iwc) throws Exception {
-		String className =
-			IWMainApplication.decryptClassName(iwc.getParameter(IWMainApplication.classToInstanciateParameter));
+		String className =IWMainApplication.decryptClassName(iwc.getParameter(IWMainApplication.classToInstanciateParameter));
 		if ( className != null) {
-			PresentationObject obj = (PresentationObject) RefactorClassRegistry.forName(className).newInstance();
-			if (obj instanceof Page) {
-				this.setPage((Page) obj);
-			}
-			else {
-				add(obj);
+			try {
+				PresentationObject obj = (PresentationObject) RefactorClassRegistry.forName(className).newInstance();
+				if (obj instanceof Page) {
+					this.setPage((Page) obj);
+				}
+				else {
+					add(obj);
+				}
+			} catch (ClassNotFoundException e) {
+				System.err.println("[ObjectInstanciator] ClassNotFound : "+className+", referer = "+iwc.getReferer());
+				throw e;
 			}
 		} else {
 			log("no class found to instanciate");
