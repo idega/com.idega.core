6b5d827d17979827577f7df49d2e85889425165b
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-11-18 14:42:16 +0000

    Documentation update

--- src/java/com/idega/idegaweb/IWException.java
+++ src/java/com/idega/idegaweb/IWException.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IWException.java,v 1.4 2005/11/18 14:40:28 tryggvil Exp $
+ * $Id: IWException.java,v 1.5 2005/11/18 14:42:16 tryggvil Exp $
  * 
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  * This software is the proprietary information of Idega hf.
@@ -8,12 +8,22 @@
 package com.idega.idegaweb;
 
 /**
- * 
- * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
- * @version 1.0
- * 
+ * <p>
+ * This is a custom exception used in some places in idegaWeb to 
+ * singal special exceptions.
+ * </p>
+ * Copyright: Copyright (c) 2001-2005 idega software<br/>
+ * Last modified: $Date: 2005/11/18 14:42:16 $ by $Author: tryggvil $
+ *  
+ * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
+ * @version $Revision: 1.5 $
  */
 public class IWException extends Exception {
+
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 8977895189197995682L;
 
 	public IWException() {
 		super("IWException");
