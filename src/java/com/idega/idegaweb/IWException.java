33e3da9c37a25d53eab63e8d19739bbe8535344a
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-11-18 14:40:28 +0000

    Formatted source

--- src/java/com/idega/idegaweb/IWException.java
+++ src/java/com/idega/idegaweb/IWException.java
@@ -1,57 +1,25 @@
 /*
-
- * $Id: IWException.java,v 1.3 2002/04/06 19:07:44 tryggvil Exp $
-
- *
-
+ * $Id: IWException.java,v 1.4 2005/11/18 14:40:28 tryggvil Exp $
+ * 
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
-
- *
-
  * This software is the proprietary information of Idega hf.
-
  * Use is subject to license terms.
-
- *
-
  */
-
- package com.idega.idegaweb;
-
-
+package com.idega.idegaweb;
 
 /**
+ * 
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
+ * 
+ */
+public class IWException extends Exception {
 
- *
+	public IWException() {
+		super("IWException");
+	}
 
-*@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
-
-*@version 1.0
-
-*
-
-*/
-
-public class IWException extends Exception{
-
-
-
-
-
-  public IWException(){
-
-      super("IWException");
-
-  }
-
-
-
-  public IWException(String message){
-
-      super("IWException: "+message);
-
-  }
-
-
-
+	public IWException(String message) {
+		super("IWException: " + message);
+	}
 }
