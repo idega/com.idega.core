230ac5080219f03b25eaf9d3247bf1b8764a0b17
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-11-25 14:23:48 +0000

    Documentation update

--- src/java/com/idega/idegaweb/IWApplicationContext.java
+++ src/java/com/idega/idegaweb/IWApplicationContext.java
@@ -1,16 +1,28 @@
+/*
+ * $Id: IWApplicationContext.java,v 1.13 2005/11/25 14:23:48 tryggvil Exp $
+ * Created in 2002 by Tryggvi Larusson
+ *
+ * Copyright (C) 2002-2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
 package com.idega.idegaweb;
 
 import com.idega.core.builder.data.ICDomain;
 
 /**
- * Title:        idegaclasses
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:      idega
- * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
- * @version 1.0
+ * <p>
+ * This Context object lives on in "application" (or servletcontext) scope
+ * and gives access to some idegaWeb specific application bound instances such
+ * as IWMainApplication.
+ * </p>
+ * 
+ * Last modified: $Date: 2005/11/25 14:23:48 $ by $Author: tryggvil $
+ * 
+ * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
+ * @version $Revision: 1.13 $
  */
-
 public interface IWApplicationContext extends java.io.Serializable{
 
   public IWMainApplication getIWMainApplication();
