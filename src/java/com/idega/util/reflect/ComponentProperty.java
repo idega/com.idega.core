e6667cfde0b6601342bd45b8b36ede4a66358c78
Thomas Hilbig <thomas@idega.com>
2005-12-05 19:33:04 +0000

    first version

---
+++ src/java/com/idega/util/reflect/ComponentProperty.java
@@ -0,0 +1,99 @@
+/*
+ * $Id: ComponentProperty.java,v 1.1 2005/12/05 19:33:04 thomas Exp $
+ * Created on Dec 5, 2005
+ *
+ * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.util.reflect;
+
+import java.util.StringTokenizer;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import org.apache.myfaces.renderkit.JSFAttr;
+import org.apache.myfaces.taglib.UIComponentTagUtils;
+
+/**
+ * This class holds an instance of a property with its value(s).
+ * A property is in this case an attribute value pair for a JSF component<br>
+ * This is used in the Builder where properties are set via this class on JSF components.
+ * 
+ */ 
+
+public class ComponentProperty extends Property {
+	
+	private String name = null;
+	
+	private String clazz = null;
+	
+
+/**
+ * Constructs a property
+ * @param componentProperty componentIdentifier a string in the format ':componentProperty:[componentProperty]:[parameterClass]:' , 
+ * example: ':componentProperty:value:java.lang.String:'
+
+ */
+	public ComponentProperty(String componentProperty) {
+		initialize(componentProperty);
+	}
+
+	public void initialize(String componentProperty) {
+		StringTokenizer tokenizer = new StringTokenizer(componentProperty ,":");
+		if (tokenizer.countTokens() == 3) {
+			tokenizer.nextToken();
+			name =tokenizer.nextToken();
+			clazz = tokenizer.nextToken();
+		}
+	}
+	
+	/**
+	 * @param propertyValues
+	 *            The propertyValues to set.
+	 */
+	public void setPropertyValues(String[] stringPropertyValues) {
+		// tricky: super class has such a method
+		setPropertyValues((Object[]) stringPropertyValues);
+	}	
+	
+	/**
+	 * Sets the property on the object instance
+	 * 
+	 * @param instance
+	 */
+	public void setPropertyOnInstance(Object  instance) {
+		Object[] values = getPropertyValues();
+		String value = (String) values[0];
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		if (JSFAttr.ACTION_ATTR.equals(name)) {
+			UIComponentTagUtils.setActionProperty(facesContext, (UIComponent) instance, value);
+		}
+		else if (JSFAttr.CONVERTER_ATTR.equals(name)) {
+			UIComponentTagUtils.setConverterProperty(facesContext, (UIComponent) instance, value);
+		}
+		else if (JSFAttr.VALIDATOR_ATTR.equals(name)) {
+			UIComponentTagUtils.setValidatorProperty(facesContext, (UIComponent) instance, value);
+		}
+		else if (JSFAttr.VALUE_ATTR.equals(name)) {
+			UIComponentTagUtils.setValueProperty(facesContext, (UIComponent) instance, value);
+		}
+		// where is actionListener attribute defined?
+		else if ("actionListener".equals(name)) {
+			UIComponentTagUtils.setActionListenerProperty(facesContext, (UIComponent) instance, value);
+		}
+		// where is valueChangedListener attribute defined?
+		else if ("valueChangedListener".equals(name)) {
+			UIComponentTagUtils.setValueChangedListenerProperty(facesContext, (UIComponent) instance, value);
+		}
+		else if (Integer.class.getName().equals(clazz)) {
+			UIComponentTagUtils.setIntegerProperty(facesContext, (UIComponent) instance, name, value);
+		}
+		else if (Boolean.class.getName().equals(clazz)) {
+			UIComponentTagUtils.setBooleanProperty(facesContext, (UIComponent) instance, name, value);
+		}
+		else if (String.class.getName().equals(clazz)) {
+			UIComponentTagUtils.setBooleanProperty(facesContext, (UIComponent) instance, name, value);
+		}
+	}
+}
