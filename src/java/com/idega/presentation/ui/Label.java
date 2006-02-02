08d74d79fadc5b63d2a3c0faf04e2baebc056ab9
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2006-02-02 13:31:28 +0000

    Added methods for better supporting JSF UIInput components

--- src/java/com/idega/presentation/ui/Label.java
+++ src/java/com/idega/presentation/ui/Label.java
@@ -1,7 +1,7 @@
 /*
  * $Id: Label.java,v 1.3 2005/03/02 09:18:49 laddi Exp $
  *
- * Copyright (C) 2002-2005 Idega Software hf. All Rights Reserved.
+ * Copyright (C) 2002-2006 Idega Software hf. All Rights Reserved.
  *
  * This software is the proprietary information of Idega hf.
  * Use is subject to license terms.
@@ -10,10 +10,13 @@ package com.idega.presentation.ui;
 
 import java.util.Iterator;
 import java.util.List;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+
 import com.idega.idegaweb.IWConstants;
 import com.idega.presentation.IWContext;
-import com.idega.presentation.PresentationObject;
 import com.idega.presentation.text.Text;
 
 /**
@@ -22,7 +25,7 @@ import com.idega.presentation.text.Text;
  * </p>
  *  Last modified: $Date: 2005/03/02 09:18:49 $ by $Author: laddi $
  * 
- * @author <a href="mailto:laddi@idega.com">Laddi</a>
+ * @author <a href="mailto:laddi@idega.com">Laddi</a>,<a href="mailto:tryggvi@idega.com">Tryggvi Larusson</a>
  * @version $Revision: 1.3 $
  */
 public class Label extends InterfaceObject {
@@ -45,8 +48,17 @@ public class Label extends InterfaceObject {
 		this(null, object);
 	}
 	
+	public Label(UIInput inputObject){
+		this(null,inputObject);
+	}
+	
+	public Label(String label, UIInput object) {
+		setFor(object.getClientId(FacesContext.getCurrentInstance()));
+		initialize(label);
+	}
+	
 	public Label(String label, InterfaceObject object) {
-		setMarkupAttribute("for", object.getID());
+		setFor(object.getID());
 		initialize(label);
 	}
 	
@@ -57,15 +69,27 @@ public class Label extends InterfaceObject {
 		setTransient(false);
 	}
 	
+	public void setFor(UIInput input,FacesContext context){
+		setFor(input.getClientId(context));
+	}
+	
+	public void setFor(UIInput input){
+		setFor(input,FacesContext.getCurrentInstance());
+	}
+	
+	public void setFor(String forId){
+		setMarkupAttribute("for", forId);
+	}
+	
 	public void print(IWContext iwc) throws Exception {
 		if (getMarkupLanguage().equals("HTML")) {
 			print("<label "+getMarkupAttributesString()+" >");
 
-			List theObjects = this.getChildren();
-			if (theObjects != null) {
-				Iterator iter = theObjects.iterator();
+			List children = this.getChildren();
+			if (children != null) {
+				Iterator iter = children.iterator();
 				while (iter.hasNext()) {
-					PresentationObject item = (PresentationObject) iter.next();
+					UIComponent item = (UIComponent) iter.next();
 					renderChild(iwc,item);
 				}
 			}
