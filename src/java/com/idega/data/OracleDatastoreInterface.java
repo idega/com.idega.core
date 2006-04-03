1ae55c8655658b88ed932486f92725f962a9048a
Thorhallur Helgason <laddi@idega.com>
2006-04-03 17:04:08 +0000

    Changed to use Locale setting from database instead of from idegaweb.pxml.

--- src/java/com/idega/data/OracleDatastoreInterface.java
+++ src/java/com/idega/data/OracleDatastoreInterface.java
@@ -32,8 +32,6 @@ import com.idega.util.database.ConnectionBroker;
  */
 public class OracleDatastoreInterface extends DatastoreInterface {
 
-	public static Locale oracleLocale = null;
-	
 	protected OracleDatastoreInterface() {
 		super();
 		EntityControl.limitTableNameToThirtyCharacters = true;
@@ -331,10 +329,7 @@ public class OracleDatastoreInterface extends DatastoreInterface {
 	}
 	
 	private Locale getDefaultLocale() {
-		if (oracleLocale == null) {
-			oracleLocale = IWMainApplication.getDefaultIWApplicationContext().getApplicationSettings().getDefaultLocaleFromIWPropertyList();
-		}
-		return oracleLocale;
+		return IWMainApplication.getDefaultIWApplicationContext().getApplicationSettings().getDefaultLocale();
 	}
 
 	/**
