553376865580b94a1af00a87044f1dc92e64803f
Grimur Jonsson <gimmi@idega.com>
2006-04-23 17:43:02 +0000

    remove requisite of not being logged in when callingLogOffMethods (cause of loggedinasanotherPerson)

--- src/java/com/idega/servlet/filter/IWAuthenticator.java
+++ src/java/com/idega/servlet/filter/IWAuthenticator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IWAuthenticator.java,v 1.23 2006/03/20 11:48:24 laddi Exp $ Created on 31.7.2004
+ * $Id: IWAuthenticator.java,v 1.24 2006/04/23 17:43:02 gimmi Exp $ Created on 31.7.2004
  * in project com.idega.core
  * 
  * Copyright (C) 2004-2005 Idega Software hf. All Rights Reserved.
@@ -46,10 +46,10 @@ import com.idega.util.RequestUtil;
  * When the user has a "remember me" cookie set then this filter reads that and
  * logs the user into the system.
  * </p>
- * Last modified: $Date: 2006/03/20 11:48:24 $ by $Author: laddi $
+ * Last modified: $Date: 2006/04/23 17:43:02 $ by $Author: gimmi $
  * 
  * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.23 $
+ * @version $Revision: 1.24 $
  */
 public class IWAuthenticator extends BaseFilter {
 
@@ -192,7 +192,8 @@ public class IWAuthenticator extends BaseFilter {
 				return;
 			}
 		}
-		else if(loginBusiness.isLogOffAction(request) && !isLoggedOn && lastLoggedOnAsUser!=null){
+//		else if(loginBusiness.isLogOffAction(request) && !isLoggedOn && lastLoggedOnAsUser!=null){
+		else if(loginBusiness.isLogOffAction(request) && lastLoggedOnAsUser!=null){
 			try {
 				AuthenticationBusiness authenticationBusiness = getAuthenticationBusiness(request);
 				//TODO: Remove IWContext
