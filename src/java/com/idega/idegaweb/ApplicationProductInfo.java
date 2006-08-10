d52b21913e4fb9fedcd654b4c1ab4b2576710f60
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2006-08-10 19:12:00 +0000

    updated version to 3.5.0-SNAPSHOT

--- src/java/com/idega/idegaweb/ApplicationProductInfo.java
+++ src/java/com/idega/idegaweb/ApplicationProductInfo.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ApplicationProductInfo.java,v 1.10 2006/06/21 18:08:49 tryggvil Exp $
+ * $Id: ApplicationProductInfo.java,v 1.11 2006/08/10 19:12:00 tryggvil Exp $
  * Created on 4.1.2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -21,17 +21,17 @@ import com.idega.util.IWTimestamp;
 /**
  *  This class holds information about the application product installed.<br>
  * 
- *  Last modified: $Date: 2006/06/21 18:08:49 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2006/08/10 19:12:00 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.10 $
+ * @version $Revision: 1.11 $
  */
 public class ApplicationProductInfo {
 	
 	//This will be swapped out by reading from /WEB-INF/idegaweb/properties/product.properties
 	private String inceptionYear="2000";
-	private String version="3.2.0";
-	private String platformVersion="3.2.0";
+	private String version="3.5.0-SNAPSHOT";
+	private String platformVersion="3.5.0-SNAPSHOT";
 	private String buildId="20060701.000000";
 	private String vendor="idega software";
 	private String name="ePlatform";
