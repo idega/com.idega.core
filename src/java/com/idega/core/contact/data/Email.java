99378b16afba08c81e75da46544098390648a16d
Sigtryggur Simonarson <sigtryggur@idega.com>
2006-10-14 22:38:26 +0000

    Added two getters that return the email address as a mailto link, the second one is able to set subject for the email

--- src/java/com/idega/core/contact/data/Email.java
+++ src/java/com/idega/core/contact/data/Email.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Email.java,v 1.3 2006/05/18 11:43:43 laddi Exp $
+ * $Id: Email.java,v 1.4 2006/10/14 22:38:26 sigtryggur Exp $
  * Created on May 16, 2006
  *
  * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
@@ -13,10 +13,10 @@ package com.idega.core.contact.data;
 
 /**
  * 
- *  Last modified: $Date: 2006/05/18 11:43:43 $ by $Author: laddi $
+ *  Last modified: $Date: 2006/10/14 22:38:26 $ by $Author: sigtryggur $
  * 
  * @author <a href="mailto:thomas@idega.com">thomas</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public interface Email extends com.idega.data.IDOLegacyEntity, EmailDataView {
 
@@ -29,7 +29,11 @@ public interface Email extends com.idega.data.IDOLegacyEntity, EmailDataView {
 	 * @see com.idega.core.contact.data.EmailBMPBean#getEmailAddress
 	 */
 	public String getEmailAddress();
-	
+
+	public java.lang.String getEmailAddressMailtoFormatted();
+
+ 	public java.lang.String getEmailAddressMailtoFormattedWithSubject(String subject);	
+
 	public void setEmailType(EmailType emailType);
 	
 	public EmailType getEmailType();
