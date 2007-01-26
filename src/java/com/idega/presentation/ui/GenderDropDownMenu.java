41afc9fe679b35ec752795305a0a610b0a345982
idega <programmers@idega.com>
2007-01-26 19:43:56 +0000

    Some inputhandlers refactored to a new location

---
+++ src/java/com/idega/presentation/ui/GenderDropDownMenu.java
@@ -0,0 +1,94 @@
+/*
+ * $Id: GenderDropDownMenu.java,v 1.2.2.2 2007/01/26 19:43:56 idegaweb Exp $
+ * Created on 26.1.2007
+ *
+ * Copyright (C) 2007 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.presentation.ui;
+
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+/**
+ * A presentation object for dynamic reports to genders. Both,male or female. both is default.
+ * 
+ * @author <a href="mailto:eiki@idega.is">Eirikur S. Hrafnsson</a>
+ */
+public class GenderDropDownMenu extends DropDownMenuInputHandler {
+
+	private static final String MALE = "m"; //same as in workreportmember
+	private static final String FEMALE = "f"; //same as in workreportmember
+	private static final String BOTH = "b";
+
+	protected static String IW_BUNDLE_IDENTIFIER = "com.idega.user";
+
+	public GenderDropDownMenu() {
+		super();
+	}
+
+	public void main(IWContext iwc) throws Exception {
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		this.addMenuElement(MALE, iwrb.getLocalizedString("GenderDropdownmenu.male", "Male"));
+		this.addMenuElement(FEMALE, iwrb.getLocalizedString("GenderDropdownmenu.female", "Female"));
+		this.addMenuElement(BOTH, iwrb.getLocalizedString("GenderDropdownmenu.both", "Both"));
+		String selectedElement = getSelectedElementValue();
+		if (selectedElement == null || selectedElement.length() == 0) {
+			this.setSelectedElement(BOTH);
+		}
+	}
+	
+	/**
+	 * @return the year, Integer
+	 *  
+	 */
+	public Object getResultingObject(String[] values, IWContext iwc) throws Exception {
+		if (values != null && values.length > 0) {
+			String gender = values[0];
+			if (BOTH.equals(gender)) {
+				return null;
+			}
+			else {
+				return gender;
+			}
+		}
+		else {
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.idega.business.InputHandler#getDisplayNameOfValue(java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public String getDisplayForResultingObject(Object value, IWContext iwc) {
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		if (value != null) {
+			
+			String displayName = "";
+			if (BOTH.equals(value)) {
+				displayName = iwrb.getLocalizedString("GenderDropdownmenu.both", "Both");
+			}
+			else if (MALE.equals(value)) {
+					displayName = iwrb.getLocalizedString("GenderDropdownmenu.male", "Male");
+			}
+			else {
+				displayName = iwrb.getLocalizedString("GenderDropdownmenu.female", "Female");
+			}
+
+			return displayName;
+		}
+		else {
+			return iwrb.getLocalizedString("GenderDropdownmenu.both", "Both");
+		}
+	}
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#getBundleIdentifier()
+	 */
+	public String getBundleIdentifier() {
+		return IW_BUNDLE_IDENTIFIER;
+	}
+
+}
---
+++ src/java/com/idega/presentation/ui/PostalCodeBox.java
@@ -0,0 +1,126 @@
+/*
+ * $Id: PostalCodeBox.java,v 1.2.2.2 2007/01/26 19:43:56 idegaweb Exp $
+ * Created on 26.1.2007
+ *
+ * Copyright (C) 2007 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.presentation.ui;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import javax.ejb.FinderException;
+import com.idega.business.IBOLookup;
+import com.idega.business.IBORuntimeException;
+import com.idega.business.InputHandler;
+import com.idega.core.location.business.AddressBusiness;
+import com.idega.core.location.data.Country;
+import com.idega.core.location.data.PostalCode;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+
+/**
+ * @author laddi
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class PostalCodeBox extends SelectionBox implements InputHandler {
+
+	public void main(IWContext iwc) {
+		try {
+			Country country = getAddressBusiness(iwc).getCountryHome().findByCountryName("Iceland");			
+			
+			if( country!=null ){
+				Collection postals = getAddressBusiness(iwc).getPostalCodeHome().findAllByCountryIdOrderedByPostalCode(((Integer)country.getPrimaryKey()).intValue());
+				Iterator iter = postals.iterator();
+				while (iter.hasNext()) {
+					PostalCode element = (PostalCode) iter.next();
+					String id = element.getPostalCode();
+					String code = element.getPostalAddress();
+					if( code!=null ) {
+						addMenuElement(id,code);
+					}						
+				}
+			}
+			else {
+				addMenuElement("-1", "No country selected");
+			}
+		}
+		catch (RemoteException re) {
+			re.printStackTrace();
+		}
+		catch (FinderException fe) {
+			fe.printStackTrace();
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.idega.business.InputHandler#getDisplayNameOfValue(java.lang.Object, com.idega.presentation.IWContext)
+	 */
+	public String getDisplayForResultingObject(Object value, IWContext iwc) {
+		if (value != null) {
+			Iterator iter = ((Collection) value).iterator();
+			StringBuffer names = new StringBuffer();
+
+			while (iter.hasNext()) {
+				names.append((String) iter.next());
+				if (iter.hasNext()) {
+					names.append(", ");
+				}
+			}
+
+			return names.toString();
+		}
+		return "";
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.idega.business.InputHandler#getHandlerObject(java.lang.String, java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		setName(name);
+		return this;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.idega.business.InputHandler#getResultingObject(java.lang.String[], com.idega.presentation.IWContext)
+	 */
+	public Object getResultingObject(String[] values, IWContext iwc) throws Exception {
+		Collection codes = new ArrayList();
+		if (values != null) {
+			for (int i = 0; i < values.length; i++) {
+				codes.add(values[i]);
+			}
+		}
+		return codes;
+	}
+
+	private AddressBusiness getAddressBusiness(IWApplicationContext iwc) {
+		try {
+			return (AddressBusiness) IBOLookup.getServiceInstance(iwc,AddressBusiness.class);
+		}
+		catch (RemoteException e) {
+			throw new IBORuntimeException(e);
+		}
+	}
+
+	public PresentationObject getHandlerObject(String name, Collection values, IWContext iwc) {
+		String value = (String) Collections.min(values);
+		return getHandlerObject(name, value, iwc);
+	}
+
+
+	/* (non-Javadoc)
+	 * @see com.idega.business.InputHandler#convertResultingObjectToType(java.lang.Object, java.lang.String)
+	 */
+	public Object convertSingleResultingObjectToType(Object value, String className) {
+		return value;
+	}
+}
