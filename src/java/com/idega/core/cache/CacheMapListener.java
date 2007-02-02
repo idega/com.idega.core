3316bccea03e00b513dc0c832f145dc63fcfcc0a
Thomas Hilbig <thomas@idega.com>
2007-02-02 01:01:23 +0000

    removed String and changed to Object - keys are sometimes at least Integers

--- src/java/com/idega/core/cache/CacheMapListener.java
+++ src/java/com/idega/core/cache/CacheMapListener.java
@@ -9,18 +9,18 @@ package com.idega.core.cache;
  * Listener that can be attached to a CacheMap for listening to when
  * objects are put, get, removed and cleared.
  * </p>
- *  Last modified: $Date: 2006/02/28 14:47:17 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2007/02/02 01:01:23 $ by $Author: thomas $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.1.2.1 $
  */
 public interface CacheMapListener {
 	
-	public void removedObject(String key);
+	public void removedObject(Object key);
 	
-	public void putObject(String key,Object object);
+	public void putObject(Object key,Object object);
 	
-	public void gotObject(String key,Object object);
+	public void gotObject(Object key,Object object);
 	
 	/**
 	 * <p>
