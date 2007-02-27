923e7c594e0d63ac0b009ca56f1f488ba1f9dd4b
Valdas Zemaitis <valdas@idega.com>
2007-02-27 10:31:16 +0000

    *** empty log message ***

--- resources/iw_core.js
+++ resources/iw_core.js
@@ -961,7 +961,10 @@ function getLinkToComments() {
 }
 
 function setActiveReverseAjax() {
-	if (!isSafariBrowser()) {
+	if (isSafariBrowser()) {
+		setTimeout("enableReverseAjax()", 5000);
+	}
+	else {
 		enableReverseAjax();
 	}
 }
