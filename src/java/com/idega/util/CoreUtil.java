c7d00370871cb26233fe9da2b299139620ba489d
Valdas Zemaitis <valdas@idega.com>
2007-05-11 13:41:01 +0000

    New method to get IWContext and new constants

--- src/java/com/idega/util/CoreUtil.java
+++ src/java/com/idega/util/CoreUtil.java
@@ -1,12 +1,28 @@
 package com.idega.util;
 
+import javax.faces.context.FacesContext;
+
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWMainApplication;
+import com.idega.presentation.IWContext;
 
 public class CoreUtil {
 	
 	public static IWBundle getCoreBundle() {
 		return IWMainApplication.getDefaultIWMainApplication().getBundle(CoreConstants.CORE_IW_BUNDLE_IDENTIFIER);
+	}
+	
+	public static IWContext getIWContext() {
+		try {
+			FacesContext context = FacesContext.getCurrentInstance();
+			if (context == null) {
+				return null;
+			}
+			return IWContext.getIWContext(context);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
