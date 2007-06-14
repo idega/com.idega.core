aac61c209173d70171c0b7a9c862ee9da5153bd7
Vitautas Civilis <civilis@idega.com>
2007-06-14 19:00:07 +0000

    utils for String

---
+++ src/java/com/idega/util/StringUtil.java
@@ -0,0 +1,23 @@
+package com.idega.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StringUtil {
+
+	public static List<String> getValuesFromString(String value, String separator) {
+		
+		if (value == null || separator == null)
+			return null;
+
+		String[] values = value.split(separator);
+		
+		if (values == null)
+			return null;
+
+		List<String> extractedValues = new ArrayList<String>();
+		Collections.addAll(extractedValues, values);
+		return extractedValues;
+	}
+}
