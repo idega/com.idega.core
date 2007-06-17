95d417d44baaae0410381fe5a17a6bd218582fa0
Valdas Zemaitis <valdas@idega.com>
2007-06-17 20:52:47 +0000

    This is needed for DWR as converter

---
+++ src/java/com/idega/bean/AdvancedProperty.java
@@ -0,0 +1,21 @@
+package com.idega.bean;
+
+public class AdvancedProperty {
+
+	private String id = null;
+	private String value = null;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
