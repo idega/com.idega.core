3483a6b170f54cbb24ad524f5714e537115c2aa9
Valdas Zemaitis <valdas@idega.com>
2007-06-17 18:17:19 +0000

    Added localization for user's status

--- src/java/com/idega/bean/UserPropertiesBean.java
+++ src/java/com/idega/bean/UserPropertiesBean.java
@@ -1,5 +1,7 @@
 package com.idega.bean;
 
+import java.util.List;
+
 import com.idega.util.CoreUtil;
 
 public class UserPropertiesBean extends PropertiesBean {
@@ -26,6 +28,8 @@ public class UserPropertiesBean extends PropertiesBean {
 	private String imageHeight = "90";
 	
 	private String defaultPhoto = CoreUtil.getCoreBundle().getVirtualPathWithFileNameString("default_photo.jpg");
+	
+	private List statusLocalization = null;
 	
 	public UserPropertiesBean() {}
 	
@@ -152,5 +156,11 @@ public class UserPropertiesBean extends PropertiesBean {
 	}
 	public void setAddReflection(boolean addReflection) {
 		this.addReflection = addReflection;
+	}
+	public List getStatusLocalization() {
+		return statusLocalization;
+	}
+	public void setStatusLocalization(List statusLocalization) {
+		this.statusLocalization = statusLocalization;
 	}
 }
