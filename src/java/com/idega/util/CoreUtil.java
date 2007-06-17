91166a97a78b3b2ee87cb16ad3b27d4e6e3f0829
Valdas Zemaitis <valdas@idega.com>
2007-06-17 13:31:48 +0000

    added method to get core's bundle

--- src/java/com/idega/util/CoreUtil.java
+++ src/java/com/idega/util/CoreUtil.java
@@ -2,9 +2,15 @@ package com.idega.util;
 
 import javax.faces.context.FacesContext;
 
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWMainApplication;
 import com.idega.presentation.IWContext;
 
 public class CoreUtil {
+	
+	public static IWBundle getCoreBundle() {
+		return IWMainApplication.getDefaultIWMainApplication().getBundle(CoreConstants.CORE_IW_BUNDLE_IDENTIFIER);
+	}
 	
 	public static IWContext getIWContext() {
 		try {
