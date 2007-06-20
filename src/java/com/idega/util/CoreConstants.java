541a95966001590193f531d88739ba216271588c
Justinas Rakita <justinas@idega.com>
2007-06-20 16:25:58 +0000

    *** empty log message ***

--- src/java/com/idega/util/CoreConstants.java
+++ src/java/com/idega/util/CoreConstants.java
@@ -29,5 +29,7 @@ public class CoreConstants {
 	public static final String IW_USER_BUNDLE_IDENTIFIER = "com.idega.user";
 
 	public static final String GROUP_SERVICE_DWR_INTERFACE_SCRIPT = "/dwr/interface/GroupService.js";
+	
+	public static final String SCHEDULE_SESSION_DWR_INTERFACE_SCRIPT = "/dwr/interface/ScheduleSession.js";
 
 }
