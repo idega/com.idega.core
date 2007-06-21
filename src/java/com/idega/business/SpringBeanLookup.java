fc7364101068cc016db7094e5eeafea68d2ae013
Vitautas Civilis <civilis@idega.com>
2007-06-21 18:54:30 +0000

    additional more convenient lookup methods added

--- src/java/com/idega/business/SpringBeanLookup.java
+++ src/java/com/idega/business/SpringBeanLookup.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import org.springframework.context.ApplicationContext;
 
+import com.idega.idegaweb.IWApplicationContext;
+
 /**
  * <p>Glue code for legacy non-spring beans. Lookup spring bean by interface. Strive as much not to use this class.</p>
  * <p>This class should be mainly used for legacy code, which used to lookup bean by using IBOLookup.getSessionInstance</p>
@@ -35,7 +37,7 @@ public class SpringBeanLookup {
 	
 	/**
 	 * 
-	 * 
+	 * Additionaly retrieves ServletContext from HttpSession instance
 	 * 
 	 * @param session - current http session
 	 * @param interface_class - interface class, annotated with com.idega.business.SpringBeanName 
@@ -43,30 +45,36 @@ public class SpringBeanLookup {
 	 */
 	public Object getSpringBean(HttpSession session, Class interface_class) {
 		
+		return getSpringBean(session.getServletContext(), interface_class);
+	}
+	
+	/**
+	 * 
+	 * @param ctx - current ServletContext
+	 * @param interface_class - interface class, annotated with com.idega.business.SpringBeanName 
+	 * @return Spring managed bean. Null if bean not found or interface not annotated.
+	 */
+	public Object getSpringBean(ServletContext ctx, Class interface_class) {
+		
 		if(!interface_class.isAnnotationPresent(SpringBeanName.class))
 			return null;
 		
 		SpringBeanName bname = (SpringBeanName)interface_class.getAnnotation(SpringBeanName.class);
 		
-		ServletContext servlet_ctx = session != null ? session.getServletContext() :
-			(ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext();
-		
-		 ApplicationContext ac = org.springframework.web.context.support.WebApplicationContextUtils.getRequiredWebApplicationContext(
-				 servlet_ctx);
-		 return ac.getBean(bname.value());
+		ApplicationContext ac = org.springframework.web.context.support.WebApplicationContextUtils.getRequiredWebApplicationContext(
+				ctx);
+		return ac.getBean(bname.value());
 	}
 	
 	/**
-	 * Retrieves ServletContext from FacesContext current instance
+	 * Retrieves ServletContext from IWMainApplication instance
 	 * 
-	 * 
-	 * @param session - current http session
+	 * @param iwac - current IWApplicationContext
 	 * @param interface_class - interface class, annotated with com.idega.business.SpringBeanName 
 	 * @return Spring managed bean. Null if bean not found or interface not annotated.
 	 */
-	public Object getSpringBean(Class interface_class) {
+	public Object getSpringBean(IWApplicationContext iwac, Class interface_class) {
 		
-		return getSpringBean(null, interface_class);
+		return getSpringBean(iwac.getIWMainApplication().getServletContext(), interface_class);
 	}
-	
 }
