dd8375c68ebcb9622471669f4e1a768bdb67b229
Valdas Zemaitis <valdas@idega.com>
2007-06-22 12:44:38 +0000

    Moved status order to Status class

--- src/java/com/idega/user/data/Status.java
+++ src/java/com/idega/user/data/Status.java
@@ -6,4 +6,14 @@ public interface Status extends com.idega.data.IDOEntity
  public java.lang.String getStatusKey();
  public void initializeAttributes();
  public void setStatusKey(java.lang.String p0);
+ 
+	/**
+	 * @see com.idega.user.data.StatusBMPBean#setOrder
+	 */
+	public void setOrder(Integer order);
+	
+	/**
+	 * @see com.idega.user.data.StatusBMPBean#getOrder
+	 */
+	public Integer getOrder();
 }
--- src/java/com/idega/user/data/UserStatusBMPBean.java
+++ src/java/com/idega/user/data/UserStatusBMPBean.java
@@ -30,8 +30,6 @@ public class UserStatusBMPBean extends GenericEntity implements UserStatus {
 	protected static final String DATE_FROM = "date_from";
 	protected static final String DATE_TO = "date_to";
 	protected static final String CREATED_BY = "created_by";
-	protected static final String ORDER = "order";
-
 	public UserStatusBMPBean() {
 		super();
 	}
@@ -58,7 +56,6 @@ public class UserStatusBMPBean extends GenericEntity implements UserStatus {
 		addManyToOneRelationship(CREATED_BY, User.class);
 		addAttribute(DATE_FROM, "Date from", true, true, java.sql.Timestamp.class);
 		addAttribute(DATE_TO, "Date to", true, true, java.sql.Timestamp.class);
-		addAttribute(ORDER, "Order", true, true, Integer.class);
 
 		addIndex("IDX_IC_USERGROUP_STATUS_1", new String[]{IC_USER, DATE_FROM});
 	}
@@ -145,14 +142,6 @@ public class UserStatusBMPBean extends GenericEntity implements UserStatus {
 
 	public Timestamp getDateTo() {
 		return (Timestamp) getColumnValue(DATE_TO);
-	}
-	
-	public void setOrder(Integer order) {
-		setColumn(ORDER, order);
-	}
-	
-	public Integer getOrder() {
-		return (Integer) getColumnValue(ORDER);
 	}
 
 	public Collection ejbFindAll() throws FinderException {
