707f5454575d7aeaed3f4ce39eb9fa3369fe895a
Justinas Rakita <justinas@idega.com>
2007-07-11 18:26:43 +0000

    *** empty log message ***

--- src/java/com/idega/core/component/business/ComponentRegistry.java
+++ src/java/com/idega/core/component/business/ComponentRegistry.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ComponentRegistry.java,v 1.9 2007/07/11 15:09:10 justinas Exp $ Created on 8.9.2005
+ * $Id: ComponentRegistry.java,v 1.10 2007/07/11 18:26:43 justinas Exp $ Created on 8.9.2005
  * in project com.idega.core
  * 
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -33,10 +33,10 @@ import com.idega.idegaweb.IWModuleLoader;
  * This means user interface components (such as Elements,Blocks, JSF UIComponents and JSP tags) but also
  * non UI components such as business beans, JSF Managed beans etc.
  * </p>
- * Last modified: $Date: 2007/07/11 15:09:10 $ by $Author: justinas $
+ * Last modified: $Date: 2007/07/11 18:26:43 $ by $Author: justinas $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
 public class ComponentRegistry {
 
@@ -112,6 +112,9 @@ public class ComponentRegistry {
 	}
 
 	public void registerComponent(ComponentInfo info) {
+//GroupAndCalendarChooserSystem.out.println("registerComponent "+info.getComponentClass().getName());
+		if(info.getComponentClass().getName().equals("com.idega.block.cal.presentation.CalendarViewer"))
+			System.out.println("calendarViewer");
 		internalGetComponentList().add(info);
 	}
 
@@ -177,6 +180,10 @@ public class ComponentRegistry {
 			//int icObjectId = ((Integer)ico.getPrimaryKey()).intValue();
 			ICObjectComponentInfo info = new ICObjectComponentInfo(ico);
 			registerComponent(info);
+			
+			if(info.getComponentClass().getName().equals("com.idega.block.cal.presentation.CalendarViewer"))
+				System.out.println("calendarViewer");
+			
 			return info;
 		} 
 		catch (ClassNotFoundException e) {
@@ -216,16 +223,16 @@ public class ComponentRegistry {
 	 * @return
 	 */
 	public ComponentInfo getComponentByClassName(String componentClassName) {
-System.out.println("componentClassName "+componentClassName);
+//System.out.println("componentClassName "+componentClassName);
 		List componentList = getAllComponents();
 		for (Iterator iter = componentList.iterator(); iter.hasNext();) {
 			ComponentInfo info = (ComponentInfo) iter.next();
-System.out.println("info.getComponentClass().getName() "+info.getComponentClass().getName());
+//System.out.println("info.getComponentClass().getName() "+info.getComponentClass().getName());
 			if(info.getComponentClass().getName().equals(componentClassName)){
 				return info;
 			}
 		}
-System.out.println("null");		
+//System.out.println("null");		
 		return null;
 	}
 
