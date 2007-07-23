585d0eecb6337b81642cdbb9eaa800c7fed4e0fa
Vitautas Civilis <civilis@idega.com>
2007-07-23 08:19:00 +0000

    head and tail methods copied from the 3_1 branch

--- src/java/com/idega/util/FileUtil.java
+++ src/java/com/idega/util/FileUtil.java
@@ -13,6 +13,7 @@ package com.idega.util;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -27,6 +28,7 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -881,5 +883,94 @@ public class FileUtil {
           id = "b";
       }
       return nf.format((bytes < 0 ? -1 : 1) * relSize) + " "+id;
+  }
+  
+  /**
+   * <p>
+   * Gets the "head" of a text file, as a List of Strings with the first lines with length numberOfLinesToRead
+   * </p>
+   * @param file
+   * @param encoding
+   * @param numberOfLinesToRead
+   * @return
+   * @throws IOException
+   */
+  public static List<String> head(File file, int numberOfLinesToRead) throws IOException
+  {
+      return head(file, "UTF-8" , numberOfLinesToRead);
+  }
+  
+  /**
+   * <p>
+   * Gets the "head" of a text file, as a List of Strings with the first lines with length numberOfLinesToRead
+   * </p>
+   * @param file
+   * @param encoding
+   * @param numberOfLinesToRead
+   * @return
+   * @throws IOException
+   */
+  public static List<String> head(File file, String encoding, int numberOfLinesToRead) throws IOException
+  {
+      //assert (file != null) && file.exists() && file.isFile() && file.canRead();
+      //assert numberOfLinesToRead > 0;
+      //assert encoding != null;
+      
+      LinkedList<String> lines = new LinkedList<String>();
+      BufferedReader reader= new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+      for (String line = null; (numberOfLinesToRead-- > 0) && (line = reader.readLine()) != null;)
+      {
+          lines.addLast(line);
+      }
+      reader.close();
+      return lines;
+  }
+  /**
+   * <p>
+   * Gets the "tail" of a text file, as a List of Strings of the last lines with length numberOfLinesToRead
+   * </p>
+   * @param file
+   * @param encoding
+   * @param numberOfLinesToRead
+   * @return
+   * @throws IOException
+   */
+  public static List<String> tail(File file, int numberOfLinesToRead) throws IOException
+  {
+      return tail(file, "UTF-8" , numberOfLinesToRead);
+  }
+  
+  /**
+   * <p>
+   * Gets the "tail" of a text file, as a List of Strings of the last lines with length numberOfLinesToRead
+   * </p>
+   * @param file
+   * @param encoding
+   * @param numberOfLinesToRead
+   * @return
+   * @throws IOException
+   */
+  public static List<String> tail(File file, String encoding, int numberOfLinesToRead) throws IOException
+  {
+      //assert (file != null) && file.exists() && file.isFile() && file.canRead();
+      //assert numberOfLinesToRead > 0;
+      //assert (encoding != null) && encoding.matches("(?i)(iso-8859|ascii|us-ascii).*");
+      
+      LinkedList<String> lines = new LinkedList<String>();
+      BufferedReader reader= new BufferedReader(new InputStreamReader(new BackwardsFileInputStream(file), encoding));
+      for (String line = null; (numberOfLinesToRead-- > 0) && (line = reader.readLine()) != null;)
+      {
+          // Reverse the order of the characters in the string
+          char[] chars = line.toCharArray();
+          for (int j = 0, k = chars.length - 1; j < k ; j++, k--)
+          {
+              char temp = chars[j];
+              chars[j] = chars[k];
+              chars[k]= temp;
+          }
+          lines.addFirst(new String(chars));
+      }
+      reader.close();
+      return lines;
   }
 }
