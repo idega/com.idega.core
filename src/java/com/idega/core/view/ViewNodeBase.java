b90d0e036d57440ab836fd1eeeaa578417df5cf0
Valdas Zemaitis <valdas@idega.com>
2007-08-23 13:22:20 +0000

    New node type for ICEfaces

--- src/java/com/idega/core/view/ViewNodeBase.java
+++ src/java/com/idega/core/view/ViewNodeBase.java
@@ -2,16 +2,17 @@ package com.idega.core.view;
 
 /**
  * 
- *  Last modified: $Date: 2007/07/27 15:44:02 $ by $Author: civilis $
+ *  Last modified: $Date: 2007/08/23 13:22:20 $ by $Author: valdas $
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public enum ViewNodeBase {
 	
 	UNSPECIFIED {public String extension() {throw new UnsupportedOperationException("Unspecified view node don't have an extension");}},
 	JSP {public String extension() {return ".jsp";}},
 	FACELET {public String extension() {return ".xhtml";}},
+	ICEFACE {public String extension() {return ".iface";}},
 	COMPONENT {public String extension() {throw new UnsupportedOperationException("Component based view node don't have an extension");}};
 	
 	public abstract String extension();
