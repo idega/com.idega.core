76bef1c6e6e89f2a8c2b75ca84d3749f3591ea50
Valdas Zemaitis <valdas@idega.com>
2007-09-17 14:47:20 +0000

    Now using faster function to set value for innerHTML

--- resources/iw_core.js
+++ resources/iw_core.js
@@ -1507,6 +1507,26 @@ function prepareDwr(interfaceClass, path) {
 	interfaceClass._path = path;
 }
 
+/**
+ * This is much faster than using (el.innerHTML = str) when there are many
+ * existing descendants, because in some browsers, innerHTML spends much longer
+ * removing existing elements than it does creating new ones.
+ * http://blog.stevenlevithan.com/archives/faster-than-innerhtml
+ */
+function replaceHtml(el, html) {
+	var oldEl = (typeof el === "string" ? document.getElementById(el) : el);
+	/*@cc_on // Pure innerHTML is slightly faster in IE
+		oldEl.innerHTML = html;
+		return oldEl;
+	@*/
+	var newEl = oldEl.cloneNode(false);
+	newEl.innerHTML = html;
+	oldEl.parentNode.replaceChild(newEl, oldEl);
+	/* Since we just removed the old element from the DOM, return a reference
+	to the new element, which can be used to restore variable references. */
+	return newEl;
+}
+
 String.prototype.cropEnd = function(symbols_count, string_to_append) {
 
 	if(this.length <= symbols_count)
