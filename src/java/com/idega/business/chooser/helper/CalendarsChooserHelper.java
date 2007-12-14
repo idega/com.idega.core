b781ed094cb801a5e208c061e4563cb42bec7461
Valdas Zemaitis <valdas@idega.com>
2007-12-14 12:46:07 +0000

    Changes for refactored Calendar

--- src/java/com/idega/business/chooser/helper/CalendarsChooserHelper.java
+++ src/java/com/idega/business/chooser/helper/CalendarsChooserHelper.java
@@ -1,7 +1,12 @@
 package com.idega.business.chooser.helper;
 
+import java.util.List;
+
+import com.idega.builder.bean.AdvancedProperty;
 import com.idega.cal.bean.CalendarPropertiesBean;
+import com.idega.core.builder.business.ICBuilderConstants;
 import com.idega.user.bean.PropertiesBean;
+import com.idega.util.CoreConstants;
 
 public class CalendarsChooserHelper extends GroupsChooserHelper {
 
@@ -13,8 +18,64 @@ public class CalendarsChooserHelper extends GroupsChooserHelper {
 		
 		CalendarPropertiesBean calendarProperties = new CalendarPropertiesBean(propBean);
 		
-		//	TODO
+		if (value == null) {
+			return calendarProperties;
+		}
+		String[] values = value.split(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR);
+		if (values == null) {
+			return calendarProperties;
+		}
+		
+		List<String> ledgers = getValuesFromString(values[values.length - 1], CoreConstants.COMMA);
+		List<String> events = getValuesFromString(values[values.length - 2], CoreConstants.COMMA);
+		
+		if (isValueListValid(events)) {
+			calendarProperties.setEvents(events);
+		}
+		if (isValueListValid(ledgers)) {
+			calendarProperties.setLedgers(ledgers);
+		}
 		
 		return calendarProperties;
+	}
+	
+	public String[] getPropertyValue(List<AdvancedProperty> properties) {
+		String[] basicValues = super.getPropertyValue(properties);
+		if (basicValues == null) {
+			return null;
+		}
+		
+		if (basicValues.length > 1) {
+			return basicValues;
+		}
+		
+		StringBuffer value = new StringBuffer(basicValues[0]).append(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR);
+		
+		String events = findPropertyValue(properties, ICBuilderConstants.CALENDAR_EVENTS_ADVANCED_PROPERTY_KEY);
+		if (events == null) {
+			events = CoreConstants.MINUS;
+		}
+		String ledgers = findPropertyValue(properties, ICBuilderConstants.CALENDAR_LEDGERS_ADVANCED_PROPERTY_KEY);
+		if (ledgers == null) {
+			ledgers = CoreConstants.MINUS;
+		}
+		
+		value.append(events).append(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR).append(ledgers);
+		
+		return new String[] {value.toString()};
+	}
+	
+	private boolean isValueListValid(List<String> values) {
+		if (values == null) {
+			return false;
+		}
+		
+		if (values.size() == 1) {
+			if (values.get(0).equals(CoreConstants.MINUS)) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
--- src/java/com/idega/business/chooser/helper/GroupsChooserHelper.java
+++ src/java/com/idega/business/chooser/helper/GroupsChooserHelper.java
@@ -3,8 +3,10 @@ package com.idega.business.chooser.helper;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.idega.builder.bean.AdvancedProperty;
 import com.idega.core.builder.business.ICBuilderConstants;
 import com.idega.user.bean.PropertiesBean;
+import com.idega.util.CoreConstants;
 import com.idega.util.CoreUtil;
 
 public class GroupsChooserHelper {
@@ -20,17 +22,72 @@ public class GroupsChooserHelper {
 		}
 		
 		PropertiesBean bean = null;
-		if (values.length == 5)	{	//	server, user, login, ids, connection
+		if (values.length >= 5)	{	//	server, user, login, ids, connection
 			bean = new PropertiesBean();
 			
 			bean.setServer(values[0]);
 			bean.setLogin(values[1]);
 			bean.setPassword(CoreUtil.getDecodedValue(values[2]));
-			bean.setUniqueIds(getValuesFromString(values[3], ","));
+			bean.setUniqueIds(getValuesFromString(values[3], CoreConstants.COMMA));
 			bean.setRemoteMode(getBooleanValue(values[4]));
 		}
 		
 		return bean;
+	}
+	
+	public String[] getPropertyValue(List<AdvancedProperty> properties) {
+		String server = null;
+		String login = null;
+		String password = null;
+		String uniqueIds = null;
+		
+		String connection = findPropertyValue(properties, "connection");
+		if (connection == null) {
+			return null;
+		}
+		
+		if (connection.equals(ICBuilderConstants.GROUPS_CHOOSER_REMOTE_CONNECTION)) {
+			//	Settings for remote connection
+			server = findPropertyValue(properties, "server");
+			login = findPropertyValue(properties, "login");
+			password = CoreUtil.getEncodedValue(findPropertyValue(properties, "password"));
+		}
+		else {
+			//	Settings for local connection
+			server = connection;
+			login = connection;
+			password = connection;
+		}
+		uniqueIds = findPropertyValue(properties, "uniqueids");
+		
+		if (server == null || login == null || password == null || uniqueIds == null) {
+			return null;
+		}
+		StringBuffer value = new StringBuffer(server).append(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR);
+		value.append(login).append(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR).append(password);
+		value.append(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR).append(uniqueIds);
+		value.append(ICBuilderConstants.BUILDER_MODULE_PROPERTY_VALUES_SEPARATOR).append(connection);
+		return new String[] {value.toString()};
+	}
+	
+	/**
+	 * Finds value in list
+	 * @param properties
+	 * @param id
+	 * @return
+	 */
+	protected String findPropertyValue(List<AdvancedProperty> properties, String id) {
+		String value = null;
+		boolean found = false;
+		AdvancedProperty property = null;
+		for (int i = 0; (i < properties.size() && !found); i++) {
+			property = properties.get(i);
+			if (id.equals(property.getId())) {
+				value = property.getValue();
+				found = true;
+			}
+		}
+		return value;
 	}
 	
 	private boolean getBooleanValue(String value) {
@@ -40,7 +97,7 @@ public class GroupsChooserHelper {
 		return value.equals(ICBuilderConstants.GROUPS_CHOOSER_REMOTE_CONNECTION);
 	}
 	
-	private List<String> getValuesFromString(String value, String separator) {
+	protected List<String> getValuesFromString(String value, String separator) {
 		if (value == null) {
 			return null;
 		}
