1bbfc505bd8c1caf7ff63ff83d03d2a5f82b346f
Anton Makarov <anton@idega.com>
2008-04-01 17:47:29 +0000

    *** empty log message ***

--- src/java/com/idega/idegaweb/IWMainSlideStartedEvent.java
+++ src/java/com/idega/idegaweb/IWMainSlideStartedEvent.java
@@ -1,29 +1,27 @@
 package com.idega.idegaweb;
 
-import javax.faces.context.FacesContext;
-
 import org.springframework.context.ApplicationEvent;
 
 /**
  * @author <a href="mailto:anton@idega.com">Anton Makarov</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/03/31 16:06:56 $ by $Author: anton $
+ * Last modified: $Date: 2008/04/01 17:47:29 $ by $Author: anton $
  *
  */
 public class IWMainSlideStartedEvent extends ApplicationEvent  {
 
 	private static final long serialVersionUID = -493360564216547575L;
 	
-	//IWMainApplication iwma;
+	IWMainApplication iwma;
 
 	public IWMainSlideStartedEvent(Object source) {
 		super(source);
 		
-		//iwma = IWMainApplication.getIWMainApplication();
+		iwma = (IWMainApplication)source;
 	}
 	
-//	public IWMainApplication getIWMA() {
-//		return iwma;
-//	}
+	public IWMainApplication getIWMA() {
+		return iwma;
+	}
 }
