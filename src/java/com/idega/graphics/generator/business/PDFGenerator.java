aa2395581b016c770601ebba5507918d1e634bd4
Valdas Zemaitis <valdas@idega.com>
2008-04-18 23:46:24 +0000

    Improvements

--- src/java/com/idega/graphics/generator/business/PDFGenerator.java
+++ src/java/com/idega/graphics/generator/business/PDFGenerator.java
@@ -5,7 +5,6 @@ import javax.faces.component.UIComponent;
 import org.w3c.dom.Document;
 
 import com.idega.business.SpringBeanName;
-import com.idega.idegaweb.IWApplicationContext;
 import com.idega.presentation.IWContext;
 import com.idega.util.CoreConstants;
 
@@ -14,7 +13,7 @@ public interface PDFGenerator {
 	
 	public boolean generatePDF(IWContext iwc, UIComponent component, String fileName, String uploadPath, boolean cleanHtml);
 	
-	public boolean generatePDF(IWApplicationContext iwac, Document doc, String fileName, String uploadPath);
+	public boolean generatePDF(IWContext iwc, Document doc, String fileName, String uploadPath);
 	
 	public boolean generatePDFFromComponent(String componentId, String fileName, String uploadPath, boolean cleanHtml);
 	
