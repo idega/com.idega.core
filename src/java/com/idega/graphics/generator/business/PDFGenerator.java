f9c8ec3e901332aa2cb4651e3207fc48aaead99a
Valdas Zemaitis <valdas@idega.com>
2008-04-25 21:09:52 +0000

    Made converts for input and select tags

--- src/java/com/idega/graphics/generator/business/PDFGenerator.java
+++ src/java/com/idega/graphics/generator/business/PDFGenerator.java
@@ -3,12 +3,16 @@ package com.idega.graphics.generator.business;
 import javax.faces.component.UIComponent;
 
 import com.idega.presentation.IWContext;
+import com.idega.util.CoreConstants;
 
+/**
+ * Spring bean that generates PDF from UIComponent. Bean id: {@link CoreConstants.SPRING_BEAN_NAME_PDF_GENERATOR}
+ * @author valdas
+ *
+ */
 public interface PDFGenerator {
 	
 	public boolean generatePDF(IWContext iwc, UIComponent component, String fileName, String uploadPath, boolean replaceInputs);
-	
-//	public boolean generatePDF(IWContext iwc, Document doc, String fileName, String uploadPath);
 	
 	public boolean generatePDFFromComponent(String componentUUID, String fileName, String uploadPath, boolean replaceInputs);
 	
