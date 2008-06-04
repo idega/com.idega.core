70152680c1befc2a3197662bd7b49985443ab30e
Grimur Jonsson <gimmi@idega.com>
2008-06-04 20:33:09 +0000

    fixed size

--- src/java/com/idega/user/data/UserBMPBean.java
+++ src/java/com/idega/user/data/UserBMPBean.java
@@ -88,9 +88,9 @@ public class UserBMPBean extends AbstractGroupBMPBean implements User, Group, co
 	public void initializeAttributes() {
 		// addAttribute(getIDColumnName());
 		super.addGeneralGroupRelation();
-		addAttribute(getColumnNameFirstName(), "First name", true, true, java.lang.String.class, 45);
+		addAttribute(getColumnNameFirstName(), "First name", true, true, java.lang.String.class, 90);
 		addAttribute(getColumnNameMiddleName(), "Middle name", true, true, java.lang.String.class, 90);
-		addAttribute(getColumnNameLastName(), "Last name", true, true, java.lang.String.class, 45);
+		addAttribute(getColumnNameLastName(), "Last name", true, true, java.lang.String.class, 90);
 		addAttribute(getColumnNameDisplayName(), "Display name", true, true, java.lang.String.class, 180);
 		addAttribute(getColumnNameDescription(), "Description", true, true, java.lang.String.class);
 		addAttribute(getColumnNameDateOfBirth(), "Birth date", true, true, java.sql.Date.class);
