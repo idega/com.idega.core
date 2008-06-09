c7bcca0c25e48cc463ac26eec50caf702f271593
Valdas Zemaitis <valdas@idega.com>
2008-06-09 10:32:16 +0000

    Using Locale in AdvancedPropertyComparator

--- src/java/com/idega/builder/business/AdvancedPropertyComparator.java
+++ src/java/com/idega/builder/business/AdvancedPropertyComparator.java
@@ -1,17 +1,22 @@
 package com.idega.builder.business;
 
+import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 
 import com.idega.builder.bean.AdvancedProperty;
 
 public class AdvancedPropertyComparator implements Comparator<AdvancedProperty> {
 	
 	private boolean sortById = false;
+	private Locale locale = null;
 	
-	public AdvancedPropertyComparator() {}
+	public AdvancedPropertyComparator(Locale locale) {
+		this.locale = locale;
+	}
 	
-	public AdvancedPropertyComparator(boolean sortById) {
-		this();
+	public AdvancedPropertyComparator(boolean sortById, Locale locale) {
+		this(locale);
 		this.sortById = sortById;
 	}
 
@@ -24,22 +29,25 @@ public class AdvancedPropertyComparator implements Comparator<AdvancedProperty> 
 		if (sortById) {
 			value1 = prop1.getId();
 			value2 = prop2.getId();
+			
+			if (value1 == null && value2 == null) {
+				result = 0;
+			}
+			else if (value2 == null) {
+				result = 1;
+			}
+			else if (value1 == null) {
+				result = -1;
+			}
+			else {
+				result = value1.compareTo(value2);
+			}
+			
+			return result;
 		}
 		
-		if (value1 == null && value2 == null) {
-			result = 0;
-		}
-		else if (value2 == null) {
-			result = 1;
-		}
-		else if (value1 == null) {
-			result = -1;
-		}
-		else {
-			result = value1.compareTo(value2);
-		}
-		
-		return result;
+		Collator collator = Collator.getInstance(locale);
+		return collator.compare(value1, value2);
 	}
 
 }
