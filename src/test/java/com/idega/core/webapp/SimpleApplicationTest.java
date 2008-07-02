2170178b73a9502f528a37f0946b4980a87b1c18
Vitautas Civilis <civilis@idega.com>
2008-07-02 19:27:58 +0000

    moved and springified

--- src/test/java/com/idega/core/webapp/SimpleApplicationTest.java
+++ src/test/java/com/idega/core/webapp/SimpleApplicationTest.java
@@ -1,4 +1,12 @@
+package com.idega.core.webapp;
 import junit.textui.TestRunner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.test.IWApplicationTestCase;
 
@@ -6,13 +14,21 @@ import com.idega.test.IWApplicationTestCase;
  * <p>
  * Simple test that starts up an idegaWeb application.
  * </p>
- *  Last modified: $Date: 2006/12/08 10:32:11 $ by $Author: gediminas $
+ *  Last modified: $Date: 2008/07/02 19:27:58 $ by $Author: civilis $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.1 $
  */
-public class SimpleApplicationTest extends IWApplicationTestCase{
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SimpleApplicationTest extends IWApplicationTestCase {
+	
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
 
+	@Test
 	public void testSimpleApp() throws Throwable {
 		IWMainApplication iwma = IWMainApplication.getDefaultIWMainApplication();
 		
@@ -21,6 +37,12 @@ public class SimpleApplicationTest extends IWApplicationTestCase{
 		Object gotObj = iwma.getAttribute("testattr");
 		
 		assertEquals(testObj,gotObj);		
+	}
+	
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
 	}
 	
 	public static void main(String[] args){
