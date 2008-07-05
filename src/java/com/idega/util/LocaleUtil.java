6f9dcec5edb83c033c6c4d15ed9785e8662f1115
Vitautas Civilis <civilis@idega.com>
2008-07-05 15:03:40 +0000

    cleanup

--- src/java/com/idega/util/LocaleUtil.java
+++ src/java/com/idega/util/LocaleUtil.java
@@ -1,63 +1,59 @@
 package com.idega.util;
 
 import java.util.Locale;
+
 import com.idega.core.localisation.business.ICLocaleBusiness;
 
 /**
- * Title:        idega Framework
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:      idega
+ * Title: idega Framework Description: Copyright: Copyright (c) 2001 Company:
+ * idega
+ * 
  * @author <a href=mailto:"tryggvi@idega.is">Tryggvi Larusson</a>
  * @version 1.0
  */
-
 public class LocaleUtil {
 
-  	private static Locale icelandicLocale;
+	private static Locale icelandicLocale;
 	private static Locale swedishLocale;
 
-  private static final String ICELANDIC_IDENTIFIER="is_IS";
-  private static final String ENGLISH_IDENTIFIER="en";
-  private static final String US_IDENTIFIER="en_US";
-  private static final String UK_IDENTIFIER="en_UK";
-  private LocaleUtil() {
-  }
+	private static final String ICELANDIC_IDENTIFIER = "is_IS";
+	private static final String ENGLISH_IDENTIFIER = "en";
+	private static final String US_IDENTIFIER = "en_US";
+	private static final String UK_IDENTIFIER = "en_UK";
 
-  public static Locale getIcelandicLocale(){
-  	if(icelandicLocale==null){
-  		icelandicLocale=new Locale("is","IS");
-  	}
-    return icelandicLocale;
-  }
-  
-  public static Locale getSwedishLocale(){
-  	if(swedishLocale==null){
-  		swedishLocale=new Locale("sv","SE");
-  	}
-    return swedishLocale;
-  }
+	private LocaleUtil() {
+	}
 
-  public static Locale getLocale(String localeIdentifier){
-    if(localeIdentifier.equals(ICELANDIC_IDENTIFIER)){
-      return getIcelandicLocale();
-    }
-    else if(localeIdentifier.equals(ENGLISH_IDENTIFIER)){
-      return Locale.ENGLISH;
-    }
-    else if(localeIdentifier.equals(US_IDENTIFIER)){
-      return Locale.US;
-    }
-    else if(localeIdentifier.equals(UK_IDENTIFIER)){
-      return Locale.UK;
-    }
-    else {
-      if ( localeIdentifier.length() > 0 ) {
-        return ICLocaleBusiness.getLocaleFromLocaleString(localeIdentifier);
-      }
-    }
+	public static Locale getIcelandicLocale() {
+		if (icelandicLocale == null) {
+			icelandicLocale = new Locale("is", "IS");
+		}
+		return icelandicLocale;
+	}
 
-    return null;
-  }
+	public static Locale getSwedishLocale() {
+		if (swedishLocale == null) {
+			swedishLocale = new Locale("sv", "SE");
+		}
+		return swedishLocale;
+	}
 
+	public static Locale getLocale(String localeIdentifier) {
+		if (localeIdentifier.equals(ICELANDIC_IDENTIFIER)) {
+			return getIcelandicLocale();
+		} else if (localeIdentifier.equals(ENGLISH_IDENTIFIER)) {
+			return Locale.ENGLISH;
+		} else if (localeIdentifier.equals(US_IDENTIFIER)) {
+			return Locale.US;
+		} else if (localeIdentifier.equals(UK_IDENTIFIER)) {
+			return Locale.UK;
+		} else {
+			if (localeIdentifier.length() > 0) {
+				return ICLocaleBusiness
+						.getLocaleFromLocaleString(localeIdentifier);
+			}
+		}
+
+		return null;
+	}
 }
