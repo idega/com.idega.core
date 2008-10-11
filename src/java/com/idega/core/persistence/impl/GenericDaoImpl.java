e07d7396f1e28b921681cfb72a3d9a4f57a6edb9
Valdas Zemaitis <valdas@idega.com>
2008-10-11 14:27:28 +0000

    Added converter from Integer to Long

--- src/java/com/idega/core/persistence/impl/GenericDaoImpl.java
+++ src/java/com/idega/core/persistence/impl/GenericDaoImpl.java
@@ -19,9 +19,9 @@ import com.idega.core.persistence.Param;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.15 $
+ * @version $Revision: 1.16 $
  *
- * Last modified: $Date: 2008/10/11 12:12:05 $ by $Author: valdas $
+ * Last modified: $Date: 2008/10/11 14:27:28 $ by $Author: valdas $
  */
 @Repository("genericDAO")
 @Transactional(readOnly=true)
@@ -128,6 +128,9 @@ public class GenericDaoImpl implements GenericDao {
 						
 						logger.log(Level.INFO, "Converting BigDecimal: " + item + " to Long");
 						longsResult.add(Long.valueOf(((BigDecimal) item).longValue()));
+					} else if (item instanceof Integer) {
+						logger.log(Level.INFO, "Converting Integer: " + item + " to Long");
+						longsResult.add(Long.valueOf(((Integer) item)));
 					} else {
 						logger.log(Level.WARNING, "Unsupported -expected long- type="+item.getClass().getName()+", item="+item);
 					}
