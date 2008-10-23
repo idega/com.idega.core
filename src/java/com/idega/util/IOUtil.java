b0ea1aa81982d19e7f50278bee1a24e4bda61c1b
Valdas Zemaitis <valdas@idega.com>
2008-10-23 12:27:01 +0000

    Initial version of I/O actions utility

---
+++ src/java/com/idega/util/IOUtil.java
@@ -0,0 +1,46 @@
+package com.idega.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipInputStream;
+
+import com.idega.io.ZipInstaller;
+
+public class IOUtil {
+	
+	public static final void closeInputStream(InputStream stream) {
+		if (stream == null) {
+			return;
+		}
+		
+		try {
+			stream.close();
+		} catch(IOException e) {}
+	}
+	
+	public static final void closeOutputStream(OutputStream stream) {
+		if (stream == null) {
+			return;
+		}
+		
+		try {
+			stream.close();
+		} catch(IOException e) {}
+	}
+	
+	public static final InputStream getStreamFromCurrentZipEntry(ZipInputStream zipStream) throws IOException {
+		if (zipStream == null) {
+			throw new IOException("Unable to read from provided zip stream");
+		}
+		
+		ZipInstaller zipInstaller = new ZipInstaller();
+		
+		ByteArrayOutputStream memory = new ByteArrayOutputStream();
+		zipInstaller.writeFromStreamToStream(zipStream, memory);
+		return new ByteArrayInputStream(memory.toByteArray());
+	}
+
+}
