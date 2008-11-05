24e28aadd3c579ba2566d82291fd9d04dae8c54e
Valdas Zemaitis <valdas@idega.com>
2008-11-05 13:31:51 +0000

    New plugin: jQuery progress bar

--- src/java/com/idega/block/web2/business/JQueryPlugin.java
+++ src/java/com/idega/block/web2/business/JQueryPlugin.java
@@ -2,10 +2,36 @@ package com.idega.block.web2.business;
 
 public enum JQueryPlugin {
 	
-	COOKIE { public String getFileName() { return ".cookie.js"; } },
-	LISTEN { public String getFileName() { return ".listen.js"; } },
-	METADATA { public String getFileName() { return ".metadata.js"; } },
-	XSLT { public String getFileName() { return ".xslt.js"; } };
+	COOKIE {
+		@Override
+		public String getFileName() {
+			return ".cookie.js";
+		}
+	},
+	LISTEN {
+		@Override
+		public String getFileName() {
+			return ".listen.js";
+		}
+	},
+	METADATA {
+		@Override
+		public String getFileName() {
+			return ".metadata.js";
+		}
+	},
+	XSLT {
+		@Override
+		public String getFileName() {
+			return ".xslt.js";
+		}
+	},
+	PROGRESS_BAR {
+		@Override
+		public String getFileName() {
+			return ".progressbar.js";
+		}
+	};
 
 	public abstract String getFileName();
 }
