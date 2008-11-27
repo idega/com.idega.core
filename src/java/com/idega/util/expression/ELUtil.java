bcc297feb9a4e5ed64841ff5902d787454bb1e8e
Juozas Zabuka <juozas@idega.com>
2008-11-27 12:42:11 +0000

    Added evalutate expression method

--- src/java/com/idega/util/expression/ELUtil.java
+++ src/java/com/idega/util/expression/ELUtil.java
@@ -1,5 +1,9 @@
 package com.idega.util.expression;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,9 +23,9 @@ import com.idega.util.CoreConstants;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  *
- * Last modified: $Date: 2008/11/17 08:40:07 $ by $Author: laddi $
+ * Last modified: $Date: 2008/11/27 12:42:11 $ by $Author: juozas $
  *
  */
 @Scope("singleton")
@@ -125,5 +129,77 @@ public class ELUtil implements ApplicationContextAware {
 		@SuppressWarnings("unchecked")
 		T bean = (T) getBean(bname.value());
 		return bean;
+	}
+	
+	/**
+	 * Evaluates expression
+	 * 
+	 * @param exp
+	 * @return
+	 * @throws Exception
+	 */
+	public Object evaluateExpression(String exp) throws Exception{
+		//TODO: use unified EL resolvers???
+		
+		String beanName = getBeanName(exp);
+		String methodName = getMethodName(exp);
+		List<String> argsList = getArgs(exp);
+		
+		@SuppressWarnings("unchecked")
+		Class[] classParams = new Class[argsList.size()];
+		
+		Object[] strParams = new String[argsList.size()];
+		for(int i = 0;i<argsList.size();i++){
+			classParams[i] = String.class;
+			strParams[i] = argsList.get(i);
+			
+		}
+	
+		
+		Object obj = ELUtil.getInstance().getBean(beanName);
+		Object returnedObj = null;
+		try {
+			Method method = obj.getClass().getMethod(methodName, classParams);
+			returnedObj = method.invoke(obj, strParams);
+		} catch (Exception e) {
+			throw new Exception("Exeption accured while trying to invoke method " + methodName, e);
+		} 
+		
+		return returnedObj;
+		
+		
+	}
+	
+	private String getBeanName(String exp){
+		String beanName = cleanupExp(exp);
+		while(true){
+			beanName = beanName.substring(0, beanName.lastIndexOf("."));
+			if (beanName.matches("[a-zA-Z0-9.]+")){
+				break;
+			}
+		}
+		return beanName;
+	}
+	
+	private String getMethodName(String exp){
+		String beanName = getBeanName(exp);
+		String methodName = cleanupExp(exp);
+		
+		methodName = methodName.substring(beanName.length()+1, methodName.indexOf("("));
+		return methodName;
+	}
+	
+	private List<String> getArgs(String exp){
+		String argsList = exp.substring(exp.indexOf("(") +1, exp.lastIndexOf(")"));
+		String removedApo = argsList.replaceAll("'", "");
+		List<String> returnArray = new ArrayList<String>();
+		
+		StringTokenizer tokenizer = new  StringTokenizer(removedApo,",");
+		while(tokenizer.hasMoreTokens()){
+			String token = tokenizer.nextToken();
+			returnArray.add(token.trim());
+		}
+		
+		return returnArray;
 	}
 }
