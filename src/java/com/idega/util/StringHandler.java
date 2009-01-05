b3d8102825a0c76cd2b05abf5ba5e9d9e5dcb72d
Valdas Zemaitis <valdas@idega.com>
2009-01-05 05:24:40 +0000

    *** empty log message ***

--- src/java/com/idega/util/StringHandler.java
+++ src/java/com/idega/util/StringHandler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StringHandler.java,v 1.49 2008/01/28 06:20:30 valdas Exp $ Created on
+ * $Id: StringHandler.java,v 1.50 2009/01/05 05:24:40 valdas Exp $ Created on
  * 14.9.2004
  * 
  * Copyright (C) 2001-2004 Idega Software hf. All Rights Reserved.
@@ -34,11 +34,11 @@ import com.idega.presentation.IWContext;
 
 /**
  * This class has utility methods to work with strings. <br>
- * Last modified: $Date: 2008/01/28 06:20:30 $ by $Author: valdas $
+ * Last modified: $Date: 2009/01/05 05:24:40 $ by $Author: valdas $
  * 
  * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson </a>, <a
  *         href="mailto:gummi@idega.is">Gudmundur Saemundsson </a>
- * @version $Revision: 1.49 $
+ * @version $Revision: 1.50 $
  */
 public class StringHandler {
 
@@ -1309,7 +1309,7 @@ public class StringHandler {
 
 		try {
 			while ((line = br.readLine()) != null) {
-				sb.append(line);
+				sb.append(line).append("\n");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
