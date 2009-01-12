c1e28633528be2678a97564b58762c96d24b9791
Valdas Zemaitis <valdas@idega.com>
2009-01-12 05:52:57 +0000

    *** empty log message ***

--- src/java/com/idega/util/RenderUtils.java
+++ src/java/com/idega/util/RenderUtils.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RenderUtils.java,v 1.8 2008/01/23 09:35:25 alexis Exp $
+ * $Id: RenderUtils.java,v 1.9 2009/01/12 05:52:57 valdas Exp $
  * Created on 25.8.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -20,10 +20,10 @@ import javax.faces.context.ResponseWriter;
 /**
  * Utility class for rendering logic in JSF.
  * 
- *  Last modified: $Date: 2008/01/23 09:35:25 $ by $Author: alexis $
+ *  Last modified: $Date: 2009/01/12 05:52:57 $ by $Author: valdas $
  * 
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  */
 public class RenderUtils {
 	
@@ -41,37 +41,32 @@ public class RenderUtils {
 	 * @param child which child to render
 	 */
 	public static void renderChild(FacesContext context, UIComponent child) throws IOException {
-		//child.encodeBegin(context);
-		//child.encodeChildren(context);
-		//child.encodeEnd(context);
-		if(child!=null){
-			if(child.isRendered()){
-				child.encodeBegin(context);
-				if(child.getRendersChildren()){
-					child.encodeChildren(context);
-				}
-				else
-				//if(child instanceof UIForm)
-				{
-					//Special case for forms:
-					Collection fChildren = child.getChildren();
-					for (Iterator iter = fChildren.iterator(); iter.hasNext();) {
-						UIComponent fChild = (UIComponent) iter.next();
-						renderChild(context,fChild);
-					}
-				}
-				child.encodeEnd(context);
+		if (child == null) {
+			return;
+		}
+		if (!child.isRendered()) {
+			return;
+		}
+		
+		child.encodeBegin(context);
+		if (child.getRendersChildren()) {
+			child.encodeChildren(context);
+		}
+		else {
+			//	Special case for forms:
+			Collection<UIComponent> fChildren = child.getChildren();
+			for (Iterator<UIComponent> iter = fChildren.iterator(); iter.hasNext();) {
+				renderChild(context, iter.next());
 			}
 		}
-	
-	
+		child.encodeEnd(context);
 	}
 
 	/**
 	 * Render the specified facet.
 	 */
 	public static void renderFacet(FacesContext context, UIComponent component, String facetName) throws IOException {
-		UIComponent facet = (UIComponent) (component.getFacets().get(facetName));
+		UIComponent facet = (component.getFacets().get(facetName));
 		if (facet != null) {
 			facet.encodeBegin(context);
 			if(facet.getRendersChildren()){
