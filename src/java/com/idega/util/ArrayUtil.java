a0d21ccf1fce1810905db903af43fb5b56c1c1f5
Valdas Zemaitis <valdas@idega.com>
2009-01-12 04:24:24 +0000

    *** empty log message ***

--- src/java/com/idega/util/ArrayUtil.java
+++ src/java/com/idega/util/ArrayUtil.java
@@ -35,7 +35,7 @@ public class ArrayUtil  {
 	
 	@SuppressWarnings("unchecked")
 	public static final <T>T[] convertListToArray(Collection<T> collection) {
-		if (collection == null || collection.isEmpty()) {
+		if (ListUtil.isEmpty(collection)) {
 			return null;
 		}
 		
