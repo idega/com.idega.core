d59dc8f1d3cb68cc7c95c7d12687afb53fb9b03d
Vitautas Civilis <civilis@idega.com>
2009-02-20 14:26:59 +0000

    couple zip extract methods added

--- src/java/com/idega/io/ZipInstaller.java
+++ src/java/com/idega/io/ZipInstaller.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ZipInstaller.java,v 1.2 2006/09/19 14:13:22 valdas Exp $
+ * $Id: ZipInstaller.java,v 1.3 2009/02/20 14:26:59 civilis Exp $
  * Created on Dec 6, 2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -19,125 +19,149 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map.Entry;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
 import com.idega.util.FileUtil;
 
-
 /**
- * Extracts zip files and keeps the original timestamps. 
- * E.g. used fo extracting idegaweb archive bundles.
+ * Extracts zip files and keeps the original timestamps. E.g. used fo extracting
+ * idegaweb archive bundles.
  * 
- *  Last modified: $Date: 2006/09/19 14:13:22 $ by $Author: valdas $
+ * Last modified: $Date: 2009/02/20 14:26:59 $ by $Author: civilis $
  * 
  * @author <a href="mailto:thomas@idega.com">thomas</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
+@Service
+@Scope("singleton")
 public class ZipInstaller {
+
+	public boolean extract(File zipFile, File destinationFolder)
+			throws IOException {
+
+		try {
+			ZipInputStream zipInputStream = new ZipInputStream(
+					new BufferedInputStream(new FileInputStream(zipFile)));
+			return extract(zipInputStream, destinationFolder);
+
+		} catch (FileNotFoundException e) {
+			throw new IOException(
+					"[ZipInstaller] Zip file could not be found: "
+							+ zipFile.getPath());
+		}
+	}
 	
-	public boolean extract(File zipFile, File destinationFolder) throws IOException {
-		ZipInputStream zipInputStream = null;
-		HashMap fileNamesTimes = new HashMap();
+	public boolean extract(ZipInputStream zipInputStream, File destinationFolder)
+	throws IOException {
+		
+		extractZIP(zipInputStream, destinationFolder);
+		return true;
+	}
+
+	public Set<String> extractZIP(ZipInputStream zipInputStream, File destinationFolder)
+			throws IOException {
+
+		HashMap<File, Long> fileNamesTimes = new HashMap<File, Long>();
+		HashSet<String> entries = new HashSet<String>();
 		try {
 			ZipEntry entry = null;
-			zipInputStream = new ZipInputStream(new BufferedInputStream(new FileInputStream(zipFile)));
+
 			while ((entry = zipInputStream.getNextEntry()) != null) {
 				try {
 					OutputStream outputStream = null;
 					String fileName = entry.getName();
+					entries.add(fileName);
 					long timestamp = entry.getTime();
-					File file = FileUtil.createFileRelativeToFile(destinationFolder, fileName);
+					File file = FileUtil.createFileRelativeToFile(
+							destinationFolder, fileName);
 					if (file.isFile()) {
 						try {
 							outputStream = getOutputStream(file);
-							writeFromStreamToStream(zipInputStream, outputStream);
-						}
-						finally {
+							writeFromStreamToStream(zipInputStream,
+									outputStream);
+						} finally {
 							close(outputStream);
 							setTimestamp(file, timestamp);
 						}
-					}
-					else {
+					} else {
 						fileNamesTimes.put(file, new Long(timestamp));
 					}
-				}
-				finally {
+				} finally {
 					closeEntry(zipInputStream);
 				}
 			}
-		}
-		catch (FileNotFoundException e) {
-			throw new IOException("[ZipInstaller] Zip file could not be found: " + zipFile.getPath());
-		}
-		finally {
+		} finally {
 			close(zipInputStream);
 		}
-		// set the timestamps of the folders (they might be not correct because the files have been extracted and therefore modified the folder)
-		Iterator iterator = fileNamesTimes.keySet().iterator();
-		while (iterator.hasNext()) {
-			File file = (File) iterator.next();
-			Long timestamp = (Long) fileNamesTimes.get(file);
-			setTimestamp(file, timestamp);
+		// set the timestamps of the folders (they might be not correct because
+		// the files have been extracted and therefore modified the folder)
+		for (Entry<File, Long> entry : fileNamesTimes.entrySet()) {
+
+			setTimestamp(entry.getKey(), entry.getValue());
 		}
-		return true;
+		return entries;
 	}
-	
-	public OutputStream getOutputStream(File file) throws FileNotFoundException { 			
+
+	public OutputStream getOutputStream(File file) throws FileNotFoundException {
 		return new BufferedOutputStream(new FileOutputStream(file));
-	} 
-	
-	public void writeFromStreamToStream(InputStream source, OutputStream destination) throws IOException { 
-		// parts of this method  were copied from "Java in a nutshell" by David Flanagan
-		byte[] buffer = new byte[4096];  // A buffer to hold file contents
-		int bytesRead;                       
-		while((bytesRead = source.read(buffer)) != -1)  {  // Read bytes until EOF
-			destination.write(buffer, 0, bytesRead);    
+	}
+
+	public void writeFromStreamToStream(InputStream source,
+			OutputStream destination) throws IOException {
+		// parts of this method were copied from "Java in a nutshell" by David
+		// Flanagan
+		byte[] buffer = new byte[4096]; // A buffer to hold file contents
+		int bytesRead;
+		while ((bytesRead = source.read(buffer)) != -1) { // Read bytes until
+			// EOF
+			destination.write(buffer, 0, bytesRead);
 		}
 	}
-	
-	
+
 	public void setTimestamp(File file, long time) {
 		if (file != null) {
 			file.setLastModified(time);
 		}
 	}
-	
+
 	public void setTimestamp(File file, Long time) {
 		setTimestamp(file, time.longValue());
 	}
-	
+
 	public void closeEntry(ZipInputStream input) {
-	  	try {
-	  		if (input != null) {
-	  			input.closeEntry();
-	  		}
-	  	}
-	  	catch (IOException io) {
-	  	  	// do not hide an existing exception
-	  	}
+		try {
+			if (input != null) {
+				input.closeEntry();
+			}
+		} catch (IOException io) {
+			// do not hide an existing exception
+		}
 	}
-	
-	
+
 	public void close(InputStream input) {
 		try {
 			if (input != null) {
 				input.close();
 			}
-	  	}
-	  	catch (IOException io) {
-	  	  	// do not hide an existing exception
-	  	}
-	}	
-	
+		} catch (IOException io) {
+			// do not hide an existing exception
+		}
+	}
+
 	public void close(OutputStream output) {
 		try {
-	  		if (output != null) {
-	  			output.close();
-	  		}
-	  	}
-	  	catch (IOException io) {
-	  	  	// do not hide an existing exception
-	  	}
+			if (output != null) {
+				output.close();
+			}
+		} catch (IOException io) {
+			// do not hide an existing exception
+		}
 	}
 }
