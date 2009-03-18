a31737300de268470af89bbffad6d46140e70863
Thorhallur Helgason <laddi@idega.com>
2009-03-18 10:27:56 +0000

    Removed calling 'this.href=#' when submitting form with link click.

--- src/java/com/idega/presentation/text/Link.java
+++ src/java/com/idega/presentation/text/Link.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Link.java,v 1.178 2009/02/10 16:38:29 valdas Exp $
+ * $Id: Link.java,v 1.179 2009/03/18 10:27:56 laddi Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -1898,9 +1898,8 @@ public class Link extends Text {
 			action = ("document.forms['" + formID + "']." + IWMainApplication.IWEventSessionAddressParameter + ".value='" + this.getID() + "';document.forms['" + formID + "'].submit();");
 		}
 		else {
-			action = ("document.forms['" + formID + "'].submit()");
+			action = ("document.forms['" + formID + "'].submit();");
 		}
-		action += ";this.href='#';";
 		//setOnClick(action);
 		setFinalUrl(JAVASCRIPT + action);
 	}
@@ -1930,7 +1929,7 @@ public class Link extends Text {
 	}
 	
 	public void setToFormReset(Form form) {
-		String action = ("document.forms['" + form.getID() + "'].reset()");
+		String action = ("document.forms['" + form.getID() + "'].reset();");
 		setFinalUrl(JAVASCRIPT + action);
 	}
 	
