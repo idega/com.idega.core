ec2962e4228392648f8db804bf9cccd64de139c6
Vitautas Civilis <civilis@idega.com>
2009-03-23 13:59:34 +0000

    removed generics from api as a quick fix

--- src/java/com/idega/transaction/TransactionContextImpl.java
+++ src/java/com/idega/transaction/TransactionContextImpl.java
@@ -6,20 +6,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $ Last modified: $Date: 2009/03/23 10:48:18 $ by $Author: civilis $
+ * @version $Revision: 1.2 $ Last modified: $Date: 2009/03/23 13:59:34 $ by $Author: civilis $
  */
 @Service
 @Scope("singleton")
 public class TransactionContextImpl implements TransactionContext {
 	
 	@Transactional
-	public <T> T executeInTransaction(TransactionalCallback callback) {
+	public Object executeInTransaction(TransactionalCallback callback) {
 		
 		return callback.execute();
 	}
 	
 	@Transactional(readOnly = true)
-	public <T> T executeInReadonlyTransaction(TransactionalCallback callback) {
+	public Object executeInReadonlyTransaction(TransactionalCallback callback) {
 		
 		return callback.execute();
 	}
