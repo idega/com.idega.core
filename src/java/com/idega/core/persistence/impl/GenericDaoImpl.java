623338fb75b360654078b61313d5dd68c55f0ba9
Valdas Zemaitis <valdas@idega.com>
2009-04-02 14:43:14 +0000

    Made more generic Number converter

--- src/java/com/idega/core/persistence/impl/GenericDaoImpl.java
+++ src/java/com/idega/core/persistence/impl/GenericDaoImpl.java
@@ -1,6 +1,8 @@
 package com.idega.core.persistence.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,12 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.idega.core.persistence.GenericDao;
 import com.idega.core.persistence.Param;
+import com.idega.util.ListUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.29 $
+ * @version $Revision: 1.30 $
  * 
- *          Last modified: $Date: 2009/03/24 16:52:04 $ by $Author: juozas $
+ *          Last modified: $Date: 2009/04/02 14:43:14 $ by $Author: valdas $
  */
 @Repository("genericDAO")
 public class GenericDaoImpl implements GenericDao {
@@ -28,6 +31,15 @@ public class GenericDaoImpl implements GenericDao {
 	private EntityManager entityManager;
 	private static final Logger logger = Logger.getLogger(GenericDaoImpl.class
 			.getName());
+
+	private static final List<Class<?>> IMPLENENTED_CONVERTERS = Collections.unmodifiableList(Arrays.asList(new Class<?>[] {
+			Long.class,
+			Integer.class,
+			Float.class,
+			Byte.class,
+			Double.class,
+			Short.class
+	}));
 
 	protected EntityManager getEntityManager() {
 
@@ -111,43 +123,56 @@ public class GenericDaoImpl implements GenericDao {
 
 				q.setParameter(param.getParamName(), param.getParamValue());
 			}
+		
 		final List<Expected> fresult;
 		
-		if (Long.class.equals(expectedReturnType)) {
-
-			List<Object> result = q.getResultList();
-
-			if (result != null) {
-
-				List<Long> longsResult = new ArrayList<Long>(result.size());
-
-				for (Object item : result) {
-
-					if (item instanceof Long) {
-						longsResult.add((Long) item);
-
-					} else if (item instanceof Number) {
-						longsResult.add(((Number) item).longValue());
-
-					} else {
-
-						logger.log(Level.SEVERE,
-								"Not a Number returned, when expected Long data type, returned = "
-										+ item.getClass().getName() + ", item="
-										+ item);
-					}
-				}
-
-				fresult = (List<Expected>) longsResult;
-			} else {
-				fresult = null;
-			}
-
+		if (IMPLENENTED_CONVERTERS.contains(expectedReturnType)) {
+			fresult = getRealResults(q.getResultList(), expectedReturnType);
 		} else {
 			fresult = q.getResultList();
 		}
 
 		return fresult;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private <Exptected> List<Exptected> getRealResults(List<Object> results, Class<Exptected> expectedReturnType) {
+		if (ListUtil.isEmpty(results)) {
+			return null;
+		}
+		
+		List<Exptected> realResults = new ArrayList<Exptected>();
+		for (Object result: results) {
+			if (expectedReturnType.isInstance(result)) {
+				realResults.add((Exptected) result);
+			} else if (result instanceof Number) {
+				Number number = (Number) result;
+				if (expectedReturnType.equals(Long.class)) {
+					//	Long
+					realResults.add((Exptected) Long.valueOf(number.longValue()));
+				} else if (expectedReturnType.equals(Integer.class)) {
+					//	Integer
+					realResults.add((Exptected) Integer.valueOf(number.intValue()));
+				} else if (expectedReturnType.equals(Float.class)) {
+					//	Float
+					realResults.add((Exptected) Float.valueOf(number.floatValue()));
+				} else if (expectedReturnType.equals(Byte.class)) {
+					//	Byte
+					realResults.add((Exptected) Byte.valueOf(number.byteValue()));
+				} else if (expectedReturnType.equals(Double.class)) {
+					//	Double
+					realResults.add((Exptected) Double.valueOf(number.doubleValue()));
+				} else if (expectedReturnType.equals(Short.class)) {
+					//	Short
+					realResults.add((Exptected) Short.valueOf(number.shortValue()));
+				}
+			} else {
+				logger.log(Level.SEVERE, "Can not convert " + result + " ("+result.getClass()+") to: " + expectedReturnType +
+																												": such converter is not implemented yet!");
+			}
+		}
+		
+		return ListUtil.isEmpty(realResults) ? null : realResults;
 	}
 
 	@Transactional(readOnly = true)
