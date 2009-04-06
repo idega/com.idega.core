14f2ed32df43dc1cc06c17e24aa6a9f991e32fb9
Vitautas Civilis <civilis@idega.com>
2009-04-06 14:47:40 +0000

    new

---
+++ src/java/com/idega/block/web2/business/JQuery.java
@@ -0,0 +1,23 @@
+package com.idega.block.web2.business;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
+ * @version $Revision: 1.1 $ Last modified: $Date: 2009/04/06 14:47:40 $ by $Author: civilis $
+ */
+public interface JQuery {
+	
+	public abstract String getBundleURIToJQueryLib();
+	
+	public abstract String getBundleURIToJQueryLib(String jqueryLibraryVersion);
+	
+	public abstract String getBundleURIToJQueryUILib(JQueryUIType type);
+	
+	public abstract String getBundleURIToJQueryUILib(
+	        String jqueryUILibraryVersion, String fileName);
+	
+	public abstract String getBundleURIToJQueryPlugin(JQueryPlugin plugin);
+	
+	public abstract List<String> getBundleURISToValidation();
+}
