af97c4069c30583a4f464cb1bfe30136b52cc631
Valdas Žemaitis <valdas@idega.com>
2010-06-06 20:27:05 +0200

    Using JSF 1.2 features

--- src/java/com/idega/util/reflect/ComponentProperty.java
+++ src/java/com/idega/util/reflect/ComponentProperty.java
@@ -13,9 +13,13 @@ import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import org.apache.myfaces.shared_tomahawk.renderkit.JSFAttr;
+import org.apache.myfaces.shared_tomahawk.taglib.UIComponentELTagUtils;
 import org.apache.myfaces.shared_tomahawk.taglib.UIComponentTagUtils;
 
 import com.idega.util.CoreUtil;
@@ -28,29 +32,24 @@ import com.idega.util.CoreUtil;
  */ 
 public class ComponentProperty extends Property {
 	
-	/**
-	 * Comment for <code>serialVersionUID</code>
-	 */
 	private static final long serialVersionUID = 1065802679114981731L;
+
 	public final static String ACTION_LISTENER_ATTR = "actionListener";
 	public final static String VALUE_CHANGED_LISTENER_ATTR = "valueChangedListener";
 	
 	private String name = null;
-	private Class componentClass;
-	private Class propertyType;
+	private Class<?> componentClass;
+	private Class<?> propertyType;
 	
-	//private String clazz = null;
-	
-
 	/**
 	 * Constructs a property with the propertyName (Java beans convention) and the componentClass 
 	 * declaring this property
 	 */
-	public ComponentProperty(String propertyName,Class componentClass) {
+	public ComponentProperty(String propertyName, Class<?> componentClass) {
 		initialize(propertyName,componentClass);
 	}
 
-	public void initialize(String componentProperty,Class componentClass) {
+	public void initialize(String componentProperty, Class<?> componentClass) {
 		this.name=componentProperty;
 		this.componentClass=componentClass;
 	}
@@ -59,6 +58,7 @@ public class ComponentProperty extends Property {
 	 * @param propertyValues
 	 *            The propertyValues to set.
 	 */
+	@Override
 	public void setPropertyValues(String[] stringPropertyValues) {
 		// tricky: super class has such a method
 		setPropertyValues((Object[]) stringPropertyValues);
@@ -69,49 +69,57 @@ public class ComponentProperty extends Property {
 	 * 
 	 * @param instance
 	 */
-	public void setPropertyOnInstance(Object  instance) {
+	@Override
+	public void setPropertyOnInstance(Object instance) {
 		Object[] values = getPropertyValues();
 		String value = (String) values[0];
 
+		UIComponent component = (UIComponent) instance;
 		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExpressionFactory exprFactory = facesContext.getApplication().getExpressionFactory();
+		
+		Class<?> propertyType = getPropertyType();
 		if (JSFAttr.ACTION_ATTR.equals(this.name)) {
-			UIComponentTagUtils.setActionProperty(facesContext, (UIComponent) instance, value);
+//			UIComponentELTagUtils.setActionProperty(facesContext, component, exprFactory.createMethodExpression(facesContext.getELContext(), value, propertyType,
+//			null));
+			UIComponentTagUtils.setActionProperty(facesContext, component, value);
 		}
 		else if (JSFAttr.CONVERTER_ATTR.equals(this.name)) {
-			UIComponentTagUtils.setConverterProperty(facesContext, (UIComponent) instance, value);
+			UIComponentTagUtils.setConverterProperty(facesContext, component, value);
 		}
 		else if (JSFAttr.VALIDATOR_ATTR.equals(this.name)) {
-			UIComponentTagUtils.setValidatorProperty(facesContext, (UIComponent) instance, value);
+			UIComponentTagUtils.setValidatorProperty(facesContext, component, value);
 		}
 		else if (JSFAttr.VALUE_ATTR.equals(this.name)) {
-			UIComponentTagUtils.setValueProperty(facesContext, (UIComponent) instance, value);
+			UIComponentTagUtils.setValueProperty(facesContext, component, value);
 		}
 		// where is actionListener attribute defined?
 		else if (ACTION_LISTENER_ATTR.equals(this.name)) {
-			UIComponentTagUtils.setActionListenerProperty(facesContext, (UIComponent) instance, value);
+			UIComponentTagUtils.setActionListenerProperty(facesContext, component, value);
 		}
 		// where is valueChangedListener attribute defined?
 		else if (VALUE_CHANGED_LISTENER_ATTR.equals(this.name)) {
-			UIComponentTagUtils.setValueChangedListenerProperty(facesContext, (UIComponent) instance, value);
-		}
-		else{
-			Class propertyType=getPropertyType();
+			UIComponentTagUtils.setValueChangedListenerProperty(facesContext, component, value);
+		} else {
+			boolean booleanType = Boolean.class.equals(propertyType) || Boolean.TYPE.equals(propertyType);
+			value = booleanType ? String.valueOf(CoreUtil.getBooleanValueFromString(value)) : value;
+			ValueExpression ve = exprFactory.createValueExpression(facesContext.getELContext(), value, propertyType);
 			
 			if (Integer.class.equals(propertyType) || Integer.TYPE.equals(propertyType)) {
-				UIComponentTagUtils.setIntegerProperty(facesContext, (UIComponent) instance, this.name, value);
-			}
-			else if (Boolean.class.equals(propertyType) || Boolean.TYPE.equals(propertyType)) {
-				UIComponentTagUtils.setBooleanProperty(facesContext, (UIComponent) instance, this.name, String.valueOf(CoreUtil.getBooleanValueFromString(value)));
-			}
-			//Fallback on handling the property as String:
-			else{// if (String.class.equals(propertyType)) {
-				UIComponentTagUtils.setStringProperty(facesContext, (UIComponent) instance, this.name, value);
+				UIComponentELTagUtils.setIntegerProperty(component, this.name, ve);
+			} else if (booleanType) {
+				UIComponentELTagUtils.setBooleanProperty(component, this.name, ve);
+			} else if (Long.class.equals(propertyType) || Long.TYPE.equals(propertyType)) {
+				UIComponentELTagUtils.setLongProperty(component, this.name, ve);
+			} else if (String.class.equals(propertyType)) {
+				UIComponentELTagUtils.setStringProperty(component, this.name, ve);
+			} else {
+				UIComponentELTagUtils.setValueBinding(facesContext, component, name, ve);
 			}
 		}
 	}
 	
-	
-	public Class getPropertyType(){
+	public Class<?> getPropertyType(){
 		if(this.propertyType==null){
 			BeanInfo beanInfo;
 			try {
