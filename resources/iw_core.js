ea529e0fc31815d63d8cfb0c8540200e9fbeca58
Valdas Žemaitis <valdas@idega.com>
2010-08-10 15:26:02 +0300

    New functions: to select text; to set cursor position

--- resources/iw_core.js
+++ resources/iw_core.js
@@ -2071,3 +2071,20 @@ IWCORE.sendExceptionNotification = function(msg, ex, reloadPageMessage) {
 String.prototype.trim = function () {
 	return this.replace(/^\s*/, '').replace(/\s*$/, '');
 }
+
+IWCORE.setSelectionRange = function(input, selectionStart, selectionEnd) {
+	if (input.setSelectionRange) {
+		input.focus();
+		input.setSelectionRange(selectionStart, selectionEnd);
+	} else if (input.createTextRange) {
+		var range = input.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', selectionEnd);
+		range.moveStart('character', selectionStart);
+		range.select();
+	}
+}
+
+IWCORE.setCaretToPos = function(input, pos) {
+	IWCORE.setSelectionRange(input, pos, pos);
+}
