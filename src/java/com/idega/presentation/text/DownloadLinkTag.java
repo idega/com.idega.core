6620e0dad229eb2a5c5d060d0610d77d337dd32d
Valdas Žemaitis <valdas@idega.com>
2010-08-17 15:28:31 +0300

    Enabling JSP 2.1 for JSF 1.2

--- src/java/com/idega/presentation/text/DownloadLinkTag.java
+++ src/java/com/idega/presentation/text/DownloadLinkTag.java
@@ -1,20 +1,21 @@
 package com.idega.presentation.text;
 
+import javax.el.ValueExpression;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
-import org.apache.myfaces.shared_tomahawk.taglib.UIComponentELTagBase;
-
 import com.idega.io.MediaWritable;
+import com.idega.presentation.ComponentTag;
 import com.idega.util.reflect.Property;
 
-public class DownloadLinkTag extends UIComponentELTagBase {
-	
-	private String downloadWriter;
+public class DownloadLinkTag extends ComponentTag {
+
 	private String id;
 	private String styleClass;
-	private String value;
-	
+
+	private Object downloadWriter;
+	private Object value;
+
 	@Override
 	public String getComponentType() {
 		return DownloadLink.COMPONENT_TYPE;
@@ -25,10 +26,11 @@ public class DownloadLinkTag extends UIComponentELTagBase {
 		return null;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void release() {
 		super.release();
-		
+
 		this.downloadWriter = null;
 		this.id = null;
 		this.styleClass = null;
@@ -36,31 +38,84 @@ public class DownloadLinkTag extends UIComponentELTagBase {
 	}
 
 	@SuppressWarnings("unchecked")
+	private Class<? extends MediaWritable> getClass(String className) {
+		try {
+			return (Class<? extends MediaWritable>) Class.forName(className);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
 	@Override
 	protected void setProperties(UIComponent component) {
 		if (component instanceof DownloadLink) {
 			super.setProperties(component);
-		
+
 			DownloadLink link = (DownloadLink) component;
-			try {
-				link.setMediaWriterClass((Class<? extends MediaWritable>) Class.forName(Property.getValueFromExpression(downloadWriter, String.class)));
-			} catch (Exception e) {
-				e.printStackTrace();
+			Class<? extends MediaWritable> mediaWriter = null;
+			if (downloadWriter instanceof ValueExpression) {
+				try {
+					Object writer = ((ValueExpression) downloadWriter).getValue(getELContext());
+					if (writer instanceof Class) {
+						mediaWriter = (Class<? extends MediaWritable>) writer;
+					} else if (writer instanceof String) {
+						mediaWriter = getClass((String) writer);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (downloadWriter instanceof String) {
+				try {
+					String writer = Property.getValueFromExpression((String) downloadWriter, String.class);
+					mediaWriter = getClass(writer);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (downloadWriter instanceof Class) {
+				try {
+					mediaWriter = (Class<? extends MediaWritable>) downloadWriter;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-			
+			if (mediaWriter != null) {
+				link.setMediaWriterClass(mediaWriter);
+			}
+
+
 			link.setId(id);
 			link.setStyleClass(styleClass);
-			
-			FacesContext fc = FacesContext.getCurrentInstance();
-			link.setValueExpression("value", fc.getApplication().getExpressionFactory().createValueExpression(fc.getELContext(), value, String.class));
+
+			ValueExpression valueExpression = null;
+			if (value instanceof String) {
+				try {
+					valueExpression = FacesContext.getCurrentInstance().getApplication().getExpressionFactory()
+						.createValueExpression(getELContext(), (String) value, String.class);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (value instanceof ValueExpression) {
+				valueExpression = (ValueExpression) value;
+			}
+			if (valueExpression != null) {
+				link.setValueExpression("value", valueExpression);
+			}
 		}
 	}
 
 	public String getDownloadWriter() {
-		return downloadWriter;
+		return getValue(downloadWriter);
 	}
 
 	public void setDownloadWriter(String downloadWriter) {
+		this.downloadWriter = downloadWriter;
+	}
+	public void setDownloadWriter(Object downloadWriter) {
+		this.downloadWriter = downloadWriter;
+	}
+	public void setDownloadWriter(ValueExpression downloadWriter) {
 		this.downloadWriter = downloadWriter;
 	}
 
@@ -73,10 +128,16 @@ public class DownloadLinkTag extends UIComponentELTagBase {
 	}
 
 	public String getValue() {
-		return value;
+		return getValue(value);
 	}
 
 	public void setValue(String value) {
+		this.value = value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	public void setValue(ValueExpression value) {
 		this.value = value;
 	}
 
@@ -89,5 +150,5 @@ public class DownloadLinkTag extends UIComponentELTagBase {
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 }
