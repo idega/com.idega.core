b9985fcb67716d5480e9400862313965ae697ccb
Valdas Žemaitis <valdas@idega.com>
2010-11-01 14:09:05 +0100

    Using RepositoryService

--- src/java/com/idega/core/content/IdegaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.idega.core.content;
-
-import javax.jcr.Repository;
-
-public interface IdegaRepository extends Repository {
-	
-	public static final String SPRING_BEAN_IDENTIFIER="idegaRepository";
-	
-}
