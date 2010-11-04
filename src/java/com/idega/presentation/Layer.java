59c80074aa0161db1e7a91e73e47d3dde45d913b
Þórhallur Helgason <laddi@idega.com>
2010-11-04 14:52:21 +0000

    Removing style setters, should be done with CSS directly.

--- src/java/com/idega/presentation/Layer.java
+++ src/java/com/idega/presentation/Layer.java
@@ -6,10 +6,11 @@ package com.idega.presentation;
 
 import java.io.IOException;
 import java.util.Iterator;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+
 import com.idega.idegaweb.IWConstants;
-import com.idega.util.text.TextStyler;
 
 /**
  * This component renders a layer or a 'div' tag in HTML around its children.
@@ -19,27 +20,8 @@ import com.idega.util.text.TextStyler;
  */
 public class Layer extends PresentationObjectContainer {
 	
-	public static final String ABSOLUTE = "absolute";
-	public static final String FIXED = "fixed";
-	public static final String DEFAULT_UNIT = "px";
-	public static final String NOWRAP = "nowrap";
-	public static final String PADDING = "padding";
-	public static final String RELATIVE = "relative";
-
 	public static final String DIV = "div";
 	public static final String SPAN = "span";
-
-	public static final String BACKGROUND_COLOR = "background-color";
-	public static final String BACKGROUND_IMAGE = "background-image";
-	public static final String LEFT = "left";
-	public static final String MOUSE_OUT = "onmouseout";
-	public static final String MOUSE_OVER = "onmouseover";
-	public static final String OVERFLOW = "overflow";
-	public static final String POSITION = "position";
-	public static final String TOP = "top";
-	public static final String VISIBILITY = "visibility";
-	public static final String WHITESPACE = "white-space";
-	public static final String ZINDEX = "z-index";
 
 	String layerType = DIV;
 
@@ -53,32 +35,17 @@ public class Layer extends PresentationObjectContainer {
 		setTransient(false);
 	}
 
-	private boolean isPositionSet() {
-		TextStyler styler = new TextStyler(getStyleAttribute());
-		return styler.isStyleSet(POSITION);
-	}
-
 	public void print(IWContext iwc) throws Exception {
-		/*if (doPrint(iwc)) {
-			if (getMarkupLanguage().equals("HTML")) {
-				print("<" + this.layerType + " ");
-				print(getMarkupAttributesString() + ">");
-				super.print(iwc);
-				println("</" + this.layerType + ">");
-			}
-		}*/
 		encodeBegin(iwc);
 		encodeChildren(iwc);
 		encodeEnd(iwc);
 	}
 	
 	public void encodeBegin(FacesContext context)throws IOException{
-		//if (doPrint(iwc)) {
-			if (getMarkupLanguage().equals(IWConstants.MARKUP_LANGUAGE_HTML)) {
-				print("<" + this.layerType + " ");
-				print(getMarkupAttributesString() + ">");
-			}
-		//}
+		if (getMarkupLanguage().equals(IWConstants.MARKUP_LANGUAGE_HTML)) {
+			print("<" + this.layerType + " ");
+			print(getMarkupAttributesString() + ">");
+		}
 	}
 	
 	public void encodeChildren(FacesContext context) throws IOException{
@@ -92,140 +59,29 @@ public class Layer extends PresentationObjectContainer {
 	}
 	
 	public void encodeEnd(FacesContext context)throws IOException{
-		//if (doPrint(iwc)) {
-			if (getMarkupLanguage().equals(IWConstants.MARKUP_LANGUAGE_HTML)) {
-				println("</" + this.layerType + ">");
-			}
-		//}
-	}
-
-	public void setName(String name) {
-		setMarkupAttribute("name", name);
-		super.setName(name);
-	}
-	
-	public void setBackgroundColor(String backgroundColor) {
-		setStyleAttribute(BACKGROUND_COLOR, backgroundColor);
-	}
-
-	public void setBackgroundImage(Image image) {
-		setBackgroundImage(image.getURL());
-	}
-
-	public void setBackgroundImage(String url) {
-		setStyleAttribute(BACKGROUND_IMAGE, "url(" + url + ")");
-	}
-
-	public void setHeight(int height) {
-		setHeightStyle(Integer.toString(height));
-	}
-
-	public void setHeight(String height) {
-		setHeightStyle(height);
+		if (getMarkupLanguage().equals(IWConstants.MARKUP_LANGUAGE_HTML)) {
+			println("</" + this.layerType + ">");
+		}
 	}
 
 	public void setLayerType(String layerType) {
 		this.layerType = layerType;
 	}
 
-	public void setLeftPosition(int xpos) {
-		setLeftPosition(xpos, DEFAULT_UNIT);
+	public void setHeight(int height) {
+		setHeightStyle(Integer.toString(height));
 	}
 	
-	public void setLeftPosition(int xpos, String unit) {
-		setLeftPosition(String.valueOf(xpos), unit);
+	public void setHeight(String height) {
+		setHeightStyle(height);
 	}
 	
-	public void setLeftPosition(String xpos) {
-		setLeftPosition(xpos, DEFAULT_UNIT);
-	}
-
-	public void setLeftPosition(String xpos, String unit) {
-		setStyleAttribute(LEFT, xpos+unit);
-		if (!isPositionSet()) {
-			setStyleAttribute(POSITION, ABSOLUTE);
-		}
-	}
-
-	public void setNoWrap() {
-		setNoWrap(true);
-	}
-
-	public void setNoWrap(boolean noWrap) {
-		if (noWrap) {
-			setStyleAttribute(WHITESPACE, NOWRAP);
-		}
-		else {
-			TextStyler styler = new TextStyler(this.getStyleAttribute());
-			styler.removeStyleValue(WHITESPACE);
-		}
-	}
-
-	public void setOnMouseOut(String action) {
-		setMarkupAttributeMultivalued(MOUSE_OUT, action);
-	}
-	
-	public void setOnMouseOver(String action) {
-		setMarkupAttributeMultivalued(MOUSE_OVER, action);
-	}
-	
-	public void setOnClick(String action) {
-		setMarkupAttributeMultivalued(CLICK, action);
-	}
-
-	public void setOverflow(String overflowType) {
-		setStyleAttribute(OVERFLOW, overflowType);
-	}
-	
-	public void setPadding(int padding, String unit) {
-		setStyleAttribute(PADDING, padding+unit);
-	}
-	
-	public void setPadding(int padding) {
-		setPadding(padding, DEFAULT_UNIT);
-	}
-
-	public void setPositionType(String absoluteOrRelative) {
-		setStyleAttribute(POSITION, absoluteOrRelative);
-	}
-
-	public void setTopPosition(int ypos) {
-		setTopPosition(ypos, DEFAULT_UNIT);
-	}
-
-	public void setTopPosition(int ypos, String unit) {
-		setTopPosition(String.valueOf(ypos), unit);
-	}
-
-	public void setTopPosition(String ypos) {
-		setTopPosition(ypos, DEFAULT_UNIT);
-	}
-
-	public void setTopPosition(String ypos, String unit) {
-		setStyleAttribute(TOP, ypos+unit);
-		if (!isPositionSet()) {
-			setStyleAttribute(POSITION, ABSOLUTE);
-		}
-	}
-
-	public void setVisibility(String visibilityType) {
-		setStyleAttribute(VISIBILITY, visibilityType);
-	}
-
 	public void setWidth(int width) {
 		setWidthStyle(Integer.toString(width));
 	}
-
+	
 	public void setWidth(String width) {
 		setWidthStyle(width);
-	}
-
-	public void setZIndex(int index) {
-		setZIndex(String.valueOf(index));
-	}
-
-	public void setZIndex(String index) {
-		setStyleAttribute(ZINDEX, index);
 	}
 	
 	public void restoreState(FacesContext context, Object state) {
