df2be53bac9f9b63bb2623ecf37b08f587c9a354
civilis <civilis@vmacbookpro-3.local>
2011-02-05 10:10:11 +0200

    Item made Serializable

--- src/java/com/idega/util/text/Item.java
+++ src/java/com/idega/util/text/Item.java
@@ -1,25 +1,27 @@
 package com.idega.util.text;
 
+import java.io.Serializable;
+
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- *
- * Last modified: $Date: 2008/11/26 08:48:11 $ by $Author: arunas $
+ * @version $Revision: 1.1 $ Last modified: $Date: 2008/11/26 08:48:11 $ by $Author: arunas $
  */
-public class Item {
-	 
+public class Item implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
 	private String itemLabel;
 	private String itemValue;
 	
-	public Item() {	}
+	public Item() {
+	}
+	
 	/**
-	 * Single item  with label and value of list selection
-	 * @param label 
-	 * @param value 
-	 *  <select>
-  	 *	  <option value = value>label</option>
-  	 *	</select>
+	 * Single item with label and value of list selection
 	 * 
+	 * @param label
+	 * @param value
+	 *            <select> <option value = value>label</option> </select>
 	 */
 	public Item(String value, String label) {
 		setItemValue(value);
@@ -29,12 +31,15 @@ public class Item {
 	public String getItemLabel() {
 		return itemLabel;
 	}
+	
 	public void setItemLabel(String itemLabel) {
 		this.itemLabel = itemLabel;
 	}
+	
 	public String getItemValue() {
 		return itemValue;
 	}
+	
 	public void setItemValue(String itemValue) {
 		this.itemValue = itemValue;
 	}
