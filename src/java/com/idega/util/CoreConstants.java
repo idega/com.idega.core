c6cb2c7d1061980177dc01083d981e7257c3b56c
Aleksandras Sivkovas <aleksandras@idega.com>
2011-07-26 08:29:05 +0300

    added new constant JS_PARAM_END "');"

--- src/java/com/idega/util/CoreConstants.java
+++ src/java/com/idega/util/CoreConstants.java
@@ -12,15 +12,15 @@ import com.idega.core.messaging.MessagingSettings;
 
 @SpringBeanName("coreConstants")
 public class CoreConstants {
-	
+
 	private static Object ARTICLE_CONSTANTS = null;
 	private static Class<?> ARTICLE_ITEM_VIEWER_NAME = null;
-	
+
 	public static final String CORE_IW_BUNDLE_IDENTIFIER = "com.idega.core";
-	
+
 	private static final String[] _HEXIDECIMAL_LETTERS = new String[] {"a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F"};
 	public static final List <String> HEXIDECIMAL_LETTERS = Collections.unmodifiableList(Arrays.asList(_HEXIDECIMAL_LETTERS));
-	
+
 	public static final String EMPTY = "";
 	public static final String SLASH = "/";
 	public static final String BACK_SLASH = "\\";
@@ -46,9 +46,10 @@ public class CoreConstants {
 	public static final String BRACKET_RIGHT = ")";
 	public static final String QOUTE_MARK = "\"";
 	public static final String QOUTE_SINGLE_MARK = "'";
-	
+
 	public static final String JS_STR_PARAM_SEPARATOR = "','";
-	
+	public static final String JS_STR_PARAM_END = "');";
+
 	public static final String PROP_SYSTEM_SMTP_MAILSERVER = MessagingSettings.PROP_SYSTEM_SMTP_MAILSERVER;
 	public static final String PROP_SYSTEM_MAIL_FROM_ADDRESS = MessagingSettings.PROP_MESSAGEBOX_FROM_ADDRESS;
 	public static final String PROP_SYSTEM_SMTP_USER = MessagingSettings.PROP_SYSTEM_SMTP_USER_NAME;
@@ -56,7 +57,7 @@ public class CoreConstants {
 	public static final String PROP_SYSTEM_SMTP_USE_AUTHENTICATION = MessagingSettings.PROP_SYSTEM_SMTP_USE_AUTHENTICATION;
 	public static final String PROP_SYSTEM_ACCOUNT = "mail_user_account";
 	public static final String PROP_SHOW_ADMIN_TOOLBAR = "show.admin.toolbar";
-	
+
 	public static final String HANDLER_PARAMETER = "handler_parameter";
 	public static final String BUILDER_PORPERTY_SETTER_STYLE_CLASS = "modulePropertySetter";
 
@@ -73,32 +74,32 @@ public class CoreConstants {
 	public static final String IW_USER_BUNDLE_IDENTIFIER = "com.idega.user";
 
 	public static final String GROUP_SERVICE_DWR_INTERFACE_SCRIPT = "/dwr/interface/GroupService.js";
-	
+
 	public static final String SCHEDULE_SESSION_DWR_INTERFACE_SCRIPT = "/dwr/interface/ScheduleSession.js";
-	
+
 	public static final String APPLICATION_PROPERTY_TO_USE_OLD_THEME_PREVIEW_GENERATOR = "useOldThemeGenerator";
 
 	public static final String WEBDAV_SERVLET_URI = "/content";
-	
+
 	public static final String ENCODING_UTF8 = "UTF-8";
-	
+
 	public static final String DWR_ENGINE_SCRIPT = "/dwr/engine.js";
 	public static final String DWR_UTIL_SCRIPT = "/dwr/util.js";
 	public static final String HIDDEN_PAGE_IN_MENU_STYLE_CLASS = "hiddenPageInNavigationMenu";
-	
+
 	public void setArticleConstantsInstance(Object o) {
 		CoreConstants.ARTICLE_CONSTANTS = o;
 	}
-	
+
 	public static Class<?> getArticleItemViewerClass() {
 		if (ARTICLE_ITEM_VIEWER_NAME != null) {
 			return ARTICLE_ITEM_VIEWER_NAME;
 		}
-		
+
 		if (ARTICLE_CONSTANTS == null) {
 			return null;
 		}
-		
+
 		try {
 			Class<?> clazz = Class.forName(ARTICLE_CONSTANTS.getClass().getName());
 			Method[] methods = clazz.getDeclaredMethods();
@@ -119,30 +120,30 @@ public class CoreConstants {
 			e.printStackTrace();
 			return null;
 		}
-		
+
 		return ARTICLE_ITEM_VIEWER_NAME;
 	}
-	
+
 	public static final String SINGLE_UICOMPONENT_RENDERING_PROCESS = "singleUIComponentRenderingProcess";
-	
+
 	public static final String WORKSPACE_VIEW_MANAGER_ID = "workspace";
 	public static final String CONTENT_VIEW_MANAGER_ID = "content";
 	public static final String PAGES_VIEW_MANAGER_ID = "pages";
-	
+
 	public static final String WORKSPACE_BUNDLE_IDENTIFIER = "com.idega.workspace";
-	
+
 	public static final String BUILDER = "builder";
 	public static final String BUILDER_APPLICATION = "builderApplication";
 	public static final String BUILDER_MODULE_PROPERTY_YES_VALUE = "Y";
 	public static final String BUILDER_MODULE_PROPERTY_NO_VALUE = "N";
-	
+
 	public static final String PAGE_ERROR_403_HANDLER_PORPERTY = HttpServletResponse.SC_FORBIDDEN + "_PAGE_URI";
 	public static final String PAGE_ERROR_404_HANDLER_PORPERTY = HttpServletResponse.SC_NOT_FOUND + "_PAGE_URI";
-	
+
 	public static final String SYSTEM_RESTART_IN_PROPERTY = "system_restart_in";
-	
+
 	public static final String PARAMETER_SESSION_ID = "JSESSIONID";
-	
+
 	public static final String EMAIL_DEFAULT_FROM = "staff@idega.is",
 								EMAIL_DEFAULT_HOST = "smtp.emailsrvr.com";
 
