5226f9aef4c9111dbd4b275cd21843d94ecb58e6
Valdas Žemaitis <valdas@idega.com>
2011-10-08 23:05:41 +0300

    Extending generic DAO

--- src/java/com/idega/user/dao/UserDAO.java
+++ src/java/com/idega/user/dao/UserDAO.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.idega.user.dao;
 
@@ -10,32 +10,33 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.idega.business.SpringBeanName;
 import com.idega.core.contact.data.bean.Email;
+import com.idega.core.persistence.GenericDao;
 import com.idega.user.data.bean.Gender;
 import com.idega.user.data.bean.Group;
 import com.idega.user.data.bean.User;
 
 @SpringBeanName("userDAO")
-public interface UserDAO {
+public interface UserDAO extends GenericDao {
 
 	@Transactional(readOnly = false)
 	public User createUser(String firstName, String middleName, String lastName, String displayName, String personalID, String description, Gender gender, Date dateOfBirth, Group primaryGroup);
 
 	public User getUser(Integer userID);
-	
+
 	public User getUser(String personalID);
 
 	public User getUserByUUID(String uniqueID);
-	
+
 	public List<User> getUsersByNames(String firstName, String middleName, String lastName);
 
 	public Email getUsersMainEmail(User user);
-	
+
 	public Email updateUserMainEmail(User user, String address);
-	
+
 	public Gender getGender(String name);
-	
+
 	public Gender getMaleGender();
-	
+
 	public Gender getFemaleGender();
-	
+
 }
--- src/java/com/idega/user/dao/impl/UserDAOImpl.java
+++ src/java/com/idega/user/dao/impl/UserDAOImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.idega.user.dao.impl;
 
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,19 +22,20 @@ import com.idega.user.data.bean.Group;
 import com.idega.user.data.bean.User;
 import com.idega.user.data.bean.UserGroupRepresentative;
 
-@Scope("singleton")
 @Repository("userDAO")
 @Transactional(readOnly = true)
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class UserDAOImpl extends GenericDaoImpl implements UserDAO {
 
 	@Autowired
 	ContactDAO contactDAO;
-	
+
+	@Override
 	@Transactional(readOnly = false)
 	public User createUser(String firstName, String middleName, String lastName, String displayName, String personalID, String description, Gender gender, Date dateOfBirth, Group primaryGroup) {
 		UserGroupRepresentative group = new UserGroupRepresentative();
 		persist(group);
-		
+
 		User user = new User();
 		user.setFirstName(firstName);
 		user.setMiddleName(middleName);
@@ -46,35 +48,41 @@ public class UserDAOImpl extends GenericDaoImpl implements UserDAO {
 		user.setPrimaryGroup(primaryGroup);
 		user.setGroup(group);
 		persist(user);
-		
+
 		return user;
 	}
-	
+
+	@Override
 	public User getUser(Integer userID) {
 		return find(User.class, userID);
 	}
 
+	@Override
 	public User getUser(String personalID) {
 		Param param = new Param("personalID", personalID);
 		return getSingleResult("user.findByPersonalID", User.class, param);
 	}
 
+	@Override
 	public User getUserByUUID(String uniqueID) {
 		Param param = new Param("uniqueId", uniqueID);
 		return getSingleResult("user.findByUniqueID", User.class, param);
 	}
 
+	@Override
 	public List<User> getUsersByNames(String firstName, String middleName, String lastName) {
 		Param param1 = new Param("firstName", firstName);
 		Param param2 = new Param("middleName", middleName);
 		Param param3 = new Param("lastName", lastName);
 		return getResultList("user.findByNames", User.class, param1, param2, param3);
 	}
-	
+
+	@Override
 	public Email getUsersMainEmail(User user) {
 		return contactDAO.findEmailForUserByType(user, contactDAO.getMainEmailType());
 	}
-	
+
+	@Override
 	public Email updateUserMainEmail(User user, String address) {
 		Email email = getUsersMainEmail(user);
 		if (email == null) {
@@ -84,23 +92,26 @@ public class UserDAOImpl extends GenericDaoImpl implements UserDAO {
 			email.setAddress(address);
 		}
 		persist(email);
-		
+
 		user.getEmails().add(email);
 		persist(user);
-		
+
 		return email;
 	}
-	
+
+	@Override
 	public Gender getGender(String name) {
 		Param param = new Param("name", name);
 
 		return getSingleResult("gender.findByName", Gender.class, param);
 	}
-	
+
+	@Override
 	public Gender getMaleGender() {
 		return getGender(Gender.NAME_MALE);
 	}
-	
+
+	@Override
 	public Gender getFemaleGender() {
 		return getGender(Gender.NAME_FEMALE);
 	}
