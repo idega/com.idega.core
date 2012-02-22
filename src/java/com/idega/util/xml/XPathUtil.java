d55df98f1dc9778b970cf2e975d0199cdd2e39d7
Valdas Žemaitis <valdas@idega.com>
2012-02-22 11:22:37 +0200

    Using one more prefix

--- src/java/com/idega/util/xml/XPathUtil.java
+++ src/java/com/idega/util/xml/XPathUtil.java
@@ -13,133 +13,125 @@ import org.w3c.dom.NodeList;
 
 /**
  * when using xpath variables, access to XPathUtil instance needs to be synchronized
- * 
+ *
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
  * @version $Revision: 1.3 $
  *
  * Last modified: $Date: 2008/10/07 13:34:11 $ by $Author: civilis $
  */
 public class XPathUtil {
-	
-	private static final String xformsNS = "http://www.w3.org/2002/xforms";
-	private static final String xhtmlNS = "http://www.w3.org/1999/xhtml";
+
+	private static final String xformsNS = "http://www.w3.org/2002/xforms",
+								xhtmlNS = "http://www.w3.org/1999/xhtml",
+								idegaNS = "http://idega.com/xforms";
 
 	private XPathExpression xpathExpression;
 	private XPathVariableResolverImpl variableResolver;
-	
+
 	public XPathUtil(String xpathExpressionStr) {
-		
 		NamespaceContextImpl nmspcContext = new NamespaceContextImpl();
+		nmspcContext.addPrefix("idega", idegaNS);
 		nmspcContext.addPrefix("xf", xformsNS);
 		nmspcContext.addPrefix("h", xhtmlNS);
-		
+
 		compileXPathExpression(xpathExpressionStr, nmspcContext);
 	}
-	
+
 	public XPathUtil(String xpathExpressionStr, Prefix... prefixes) {
-		
 		NamespaceContextImpl nmspcContext = new NamespaceContextImpl();
-		
-		if(prefixes != null) {
-			
+
+		if (prefixes != null) {
 			for (Prefix prefix : prefixes) {
 				nmspcContext.addPrefix(prefix.getPrefix(), prefix.getNamespace());
 			}
 		}
-		
-		if(nmspcContext.getPrefix(xformsNS) == null)
+
+		if (nmspcContext.getPrefix(xformsNS) == null)
 			nmspcContext.addPrefix("xf", xformsNS);
-		
-		if(nmspcContext.getPrefix(xhtmlNS) == null)
+		if (nmspcContext.getPrefix(xhtmlNS) == null)
 			nmspcContext.addPrefix("h", xhtmlNS);
-		
+		if (nmspcContext.getPrefix(idegaNS) == null)
+			nmspcContext.addPrefix("idega", idegaNS);
+
 		compileXPathExpression(xpathExpressionStr, nmspcContext);
 	}
-	
+
 	public XPathUtil(String xpathExpressionStr, NamespaceContext nmspcContext) {
-		
 		compileXPathExpression(xpathExpressionStr, nmspcContext);
 	}
-	
+
 	private void compileXPathExpression(String xpathExpressionStr, NamespaceContext nmspcContext) {
-		
 		try {
 			XPathFactory factory = XPathFactory.newInstance();
 			XPath xpath = factory.newXPath();
-			
+
 			variableResolver = new XPathVariableResolverImpl();
 			xpath.setXPathVariableResolver(variableResolver);
-			
-			if(nmspcContext != null)
+
+			if (nmspcContext != null)
 				xpath.setNamespaceContext(nmspcContext);
-			
+
 			xpathExpression = xpath.compile(xpathExpressionStr);
-			
 		} catch (XPathException e) {
 			throw new RuntimeException("Could not compile XPath expression: " + e.getMessage(), e);
 		}
 	}
-	
+
 	public <T extends Node>T getNode(Node context) {
-		
 		try {
 			synchronized (xpathExpression) {
 				@SuppressWarnings("unchecked")
 				T node = (T)xpathExpression.evaluate(context, XPathConstants.NODE);
 				return node;
 			}
-				
+
 		} catch (XPathException e) {
 			throw new RuntimeException("Could not evaluate XPath expression: " + e.getMessage(), e);
 		}
 	}
-	
+
 	public NodeList getNodeset(Node context) {
-		
 		try {
 			synchronized (xpathExpression) {
 				return (NodeList)xpathExpression.evaluate(context, XPathConstants.NODESET);
 			}
-				
+
 		} catch (XPathException e) {
 			throw new RuntimeException("Could not evaluate XPath expression: " + e.getMessage(), e);
 		}
 	}
-	
+
 	public String getString(Node context) {
-		
 		try {
 			synchronized (xpathExpression) {
 				return (String)xpathExpression.evaluate(context, XPathConstants.STRING);
 			}
-				
+
 		} catch (XPathException e) {
 			throw new RuntimeException("Could not evaluate XPath expression: " + e.getMessage(), e);
 		}
 	}
-	
-	public Object getObject(Node context, QName objType) {
 
+	public Object getObject(Node context, QName objType) {
 		try {
 			synchronized (xpathExpression) {
 				return xpathExpression.evaluate(context, objType);
 			}
-				
+
 		} catch (XPathException e) {
 			throw new RuntimeException("Could not evaluate XPath expression: " + e.getMessage(), e);
 		}
 	}
-	
+
 	public void setVariable(QName variable, Object value) {
 		variableResolver.addVariable(variable, value);
 	}
-	
+
 	public void setVariable(String variable, String value) {
-		
 		QName qvariable = new QName(variable);
 		setVariable(qvariable, value);
 	}
-	
+
 	public void clearVariables() {
 		variableResolver.clearVariables();
 	}
