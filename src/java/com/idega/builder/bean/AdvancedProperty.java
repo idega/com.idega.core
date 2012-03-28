e101d66004b75cde0d747f6b685c13fa10947018
Valdas Žemaitis <valdas@idega.com>
2012-03-28 10:38:28 +0300

    Enabled serialization

--- src/java/com/idega/builder/bean/AdvancedProperty.java
+++ src/java/com/idega/builder/bean/AdvancedProperty.java
@@ -1,35 +1,44 @@
 package com.idega.builder.bean;
 
+import java.io.Serializable;
+
 import com.idega.util.CoreConstants;
 
-public class AdvancedProperty {
-	
-	private boolean selected = false;
-	private String id;
-	private String value;
-	
-	public AdvancedProperty() {}
-	
+public class AdvancedProperty implements Serializable {
+
+	private static final long serialVersionUID = 8520116287821698275L;
+
+	private boolean selected;
+	private String id, value;
+
+	public AdvancedProperty() {
+		super();
+	}
+
 	public AdvancedProperty(String id) {
 		this();
+
 		this.id = id;
 	}
-	
+
 	public AdvancedProperty(String id, String value) {
-		this();
-		this.id = id;
+		this(id);
+
 		this.value = value;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getValue() {
 		return value;
 	}
+
 	public void setValue(String value) {
 		this.value = value;
 	}
@@ -41,7 +50,7 @@ public class AdvancedProperty {
 	public void setSelected(boolean selected) {
 		this.selected = selected;
 	}
-	
+
 	@Override
 	public String toString() {
 		return new StringBuilder(id).append(CoreConstants.COLON).append(CoreConstants.SPACE).append(value).toString();
