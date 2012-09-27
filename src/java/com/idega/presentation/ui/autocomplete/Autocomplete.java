7dc6bd28525a3f9163c843dafd453fd1f8bab8f7
aleksandras <aleksandras@idega.com>
2012-09-27 08:41:42 +0300

    some uncommited code missing

--- src/java/com/idega/presentation/ui/autocomplete/Autocomplete.java
+++ src/java/com/idega/presentation/ui/autocomplete/Autocomplete.java
@@ -1,6 +1,5 @@
 package com.idega.presentation.ui.autocomplete;
 
-import com.idega.core.bean.AutocompleteBean;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.IWUIBase;
 import com.idega.util.PresentationUtil;
@@ -12,38 +11,37 @@ import com.idega.util.PresentationUtil;
  */
 public abstract class Autocomplete extends IWUIBase{
 
-	private String selectionFunction = null;
-	private AutocompleteBean autocompleteBean = null;
-	
-	/**
-	 * Set an instance of {@link AutocompleteBean} to manage queries of autocomplete.
-	 * @param autocompleteBean
-	 */
-	public void setAutocompleteBean(AutocompleteBean autocompleteBean) {
-		this.autocompleteBean = autocompleteBean;
-	}
-
-	public AutocompleteBean AutocompleteBeangetAutocompleteBean() {
-		return autocompleteBean;
-	}
-
-	/**
-	 * Set javascript function that will take care of 
-	 * selected item of autocomplete
-	 * @return
-	 */
-	public String getSelectionFunction() {
-		return selectionFunction;
-	}
-
-	public void setSelectionFunction(String selectionFunction) {
-		this.selectionFunction = selectionFunction;
-	}
-
-	@Override
-	protected void addFiles(IWContext iwc){
-		PresentationUtil.addJavaScriptSourcesLinesToHeader(iwc, getScripts());
-		PresentationUtil.addStyleSheetsToHeader(iwc, getStyleSheets());
-	}
-	
+//	private String selectionFunction = null;
+//	private AutocompleteBean autocompleteBean = null;
+//	
+//	/**
+//	 * Set an instance of {@link AutocompleteBean} to manage queries of autocomplete.
+//	 * @param autocompleteBean
+//	 */
+//	public void setAutocompleteBean(AutocompleteBean autocompleteBean) {
+//		this.autocompleteBean = autocompleteBean;
+//	}
+//
+//	public AutocompleteBean AutocompleteBeangetAutocompleteBean() {
+//		return autocompleteBean;
+//	}
+//
+//	/**
+//	 * Set javascript function that will take care of 
+//	 * selected item of autocomplete
+//	 * @return
+//	 */
+//	public String getSelectionFunction() {
+//		return selectionFunction;
+//	}
+//
+//	public void setSelectionFunction(String selectionFunction) {
+//		this.selectionFunction = selectionFunction;
+//	}
+//
+//	@Override
+//	protected void addFiles(IWContext iwc){
+//		PresentationUtil.addJavaScriptSourcesLinesToHeader(iwc, getScripts());
+//		PresentationUtil.addStyleSheetsToHeader(iwc, getStyleSheets());
+//	}
 }
