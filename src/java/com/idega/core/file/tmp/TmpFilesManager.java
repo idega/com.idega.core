2c06ab9c44f7c91edbdc67aead5c5d091d9b0fd3
Valdas Žemaitis <valdas@idega.com>
2012-10-10 12:40:49 +0300

    Various improvements

--- src/java/com/idega/core/file/tmp/TmpFilesManager.java
+++ src/java/com/idega/core/file/tmp/TmpFilesManager.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URI;
 import java.util.Collection;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -15,29 +16,25 @@ import com.idega.util.FileUtil;
 /**
  * @author <a href="mailto:arunas@idega.com">Arūnas Vasmanas</a>
  * @version $Revision: 1.3 $
- * 
+ *
  *          Last modified: $Date: 2009/02/20 14:26:23 $ by $Author: civilis $
  */
-@Scope("singleton")
 @Service
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class TmpFilesManager {
 
 	static String UPLOADS_PATH;
 	private Collection<TmpFileResolver> resResolvers;
 
 	public void init(Collection<TmpFileResolver> resResolvers) {
-
-		IWMainApplication iwma = IWMainApplication
-				.getDefaultIWMainApplication();
+		IWMainApplication iwma = IWMainApplication.getDefaultIWMainApplication();
 		String realPath = iwma.getApplicationRealPath();
 
 		UPLOADS_PATH = realPath + "tmpFiles/";
 		FileUtil.createFolder(UPLOADS_PATH);
 
 		if (resResolvers != null) {
-
 			for (TmpFileResolver resResolver : resResolvers) {
-
 				String ctxFolder = resResolver.getContextPath();
 				if (ctxFolder.startsWith(CoreConstants.SLASH))
 					ctxFolder = ctxFolder.substring(1);
@@ -51,16 +48,10 @@ public class TmpFilesManager {
 		this.resResolvers = resResolvers;
 	}
 
-	public void cleanup(String identifier, Object resource,
-			TmpFileResolver resolver) {
-
-		Collection<File> filesToClean = resolver.getFilesToCleanUp(identifier,
-				resource);
-
+	public void cleanup(String identifier, Object resource, TmpFileResolver resolver) {
+		Collection<File> filesToClean = resolver.getFilesToCleanUp(identifier, resource);
 		for (File f : filesToClean) {
-
 			if (f.exists()) {
-
 				if (f.isDirectory()) {
 					FileUtil.deleteNotEmptyDirectory(f);
 				} else {
@@ -71,19 +62,15 @@ public class TmpFilesManager {
 	}
 
 	public void doPeriodicalCleanup() {
-
 		if (resResolvers != null) {
-
 			for (TmpFileResolver resResolver : resResolvers) {
-
-				FileUtil.deleteAllFilesAndFolderInFolderOlderThan(resResolver
-						.getRealBasePath(), EventTimer.THREAD_SLEEP_24_HOURS);
+				FileUtil.deleteAllFilesAndFolderInFolderOlderThan(resResolver.getRealBasePath(), EventTimer.THREAD_SLEEP_24_HOURS);
 			}
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @param identifier
 	 *            - might be some specific identifier, that you want to get
 	 *            files for, e.g., variable in xforms submission data. Highly
@@ -93,9 +80,7 @@ public class TmpFilesManager {
 	 * @param resolver
 	 * @return files uris. Might be local files, or stored in slide etc.
 	 */
-	public Collection<URI> getFilesUris(String identifier, Object resource,
-			TmpFileResolver resolver) {
-
+	public Collection<URI> getFilesUris(String identifier, Object resource, TmpFileResolver resolver) {
 		return resolver.resolveFilesUris(identifier, resource);
 	}
 }
