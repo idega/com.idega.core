c0100e136917ffcbb97abfef93b50578990a0de9
Valdas Žemaitis <valdas@idega.com>
2012-10-31 19:24:24 +0200

    New method to get ICObject instances by UI class name

--- src/java/com/idega/core/component/data/ICObjectInstanceHomeImpl.java
+++ src/java/com/idega/core/component/data/ICObjectInstanceHomeImpl.java
@@ -1,29 +1,36 @@
 package com.idega.core.component.data;
 
 
-import com.idega.data.IDOException;
-import java.util.Collection;
-import javax.ejb.CreateException;
 import java.sql.SQLException;
+import java.util.Collection;
+
+import javax.ejb.CreateException;
 import javax.ejb.FinderException;
-import com.idega.data.IDORemoveRelationshipException;
+import javax.faces.component.UIComponent;
+
 import com.idega.data.IDOEntity;
+import com.idega.data.IDOException;
 import com.idega.data.IDOFactory;
+import com.idega.data.IDORemoveRelationshipException;
 
 public class ICObjectInstanceHomeImpl extends IDOFactory implements ICObjectInstanceHome {
 
-	public Class getEntityInterfaceClass() {
+	@Override
+	public Class<ICObjectInstance> getEntityInterfaceClass() {
 		return ICObjectInstance.class;
 	}
 
+	@Override
 	public ICObjectInstance create() throws CreateException {
 		return (ICObjectInstance) super.createIDO();
 	}
 
+	@Override
 	public ICObjectInstance findByPrimaryKey(Object pk) throws FinderException {
 		return (ICObjectInstance) super.findByPrimaryKeyIDO(pk);
 	}
 
+	@Override
 	public ICObjectInstance createLegacy() {
 		try {
 			return create();
@@ -33,10 +40,12 @@ public class ICObjectInstanceHomeImpl extends IDOFactory implements ICObjectInst
 		}
 	}
 
+	@Override
 	public ICObjectInstance findByPrimaryKey(int id) throws FinderException {
 		return (ICObjectInstance) super.findByPrimaryKeyIDO(id);
 	}
 
+	@Override
 	public ICObjectInstance findByPrimaryKeyLegacy(int id) throws SQLException {
 		try {
 			return findByPrimaryKey(id);
@@ -46,12 +55,14 @@ public class ICObjectInstanceHomeImpl extends IDOFactory implements ICObjectInst
 		}
 	}
 
+	@Override
 	public void removeRelation(ICObjectInstance instance, Class relatedEntity) throws IDORemoveRelationshipException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		((ICObjectInstanceBMPBean) entity).ejbHomeRemoveRelation(instance, relatedEntity);
 		this.idoCheckInPooledEntity(entity);
 	}
 
+	@Override
 	public ICObjectInstance findByUniqueId(String uuid) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Object pk = ((ICObjectInstanceBMPBean) entity).ejbFindByUniqueId(uuid);
@@ -59,6 +70,7 @@ public class ICObjectInstanceHomeImpl extends IDOFactory implements ICObjectInst
 		return this.findByPrimaryKey(pk);
 	}
 
+	@Override
 	public Collection findByPageKey(String pageKey) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Collection ids = ((ICObjectInstanceBMPBean) entity).ejbFindByPageKey(pageKey);
@@ -66,10 +78,19 @@ public class ICObjectInstanceHomeImpl extends IDOFactory implements ICObjectInst
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
+	@Override
 	public int getCountByICObject(ICObject ico) throws IDOException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		int theReturn = ((ICObjectInstanceBMPBean) entity).ejbHomeGetCountByICObject(ico);
 		this.idoCheckInPooledEntity(entity);
 		return theReturn;
+	}
+
+	@Override
+	public Collection<ICObjectInstance> getByClassName(Class<? extends UIComponent> className) throws FinderException {
+		IDOEntity entity = this.idoCheckOutPooledEntity();
+		Collection<?> ids = ((ICObjectInstanceBMPBean) entity).ejbFindByClassName(className);
+		this.idoCheckInPooledEntity(entity);
+		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 }
