762663e910131ee5f17816a84d78859e96f34a01
Valdas Žemaitis <valdas@idega.com>
2013-04-04 15:00:28 +0300

    Removed sessions daemon

--- src/java/com/idega/event/IWHttpSessionEventListener.java
+++ src/java/com/idega/event/IWHttpSessionEventListener.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.idega.core.accesscontrol.business.LoginSession;
 import com.idega.idegaweb.IWMainApplication;
+import com.idega.util.CoreConstants;
 import com.idega.util.expression.ELUtil;
 
 public class IWHttpSessionEventListener implements HttpSessionListener {
@@ -38,7 +39,7 @@ public class IWHttpSessionEventListener implements HttpSessionListener {
 					//	Redirect
 					ScriptCallerInterface scriptCaller = getScriptCaller();
 					if (scriptCaller != null) {
-						scriptCaller.setScript("window.location.pathname = '/pages/';");
+						scriptCaller.setScript("window.location.pathname = '" + CoreConstants.PAGES_URI_PREFIX + "';");
 						scriptCaller.setSessionId(destroyedSession.getId());
 						scriptCaller.run();
 					}
--- src/java/com/idega/event/IWHttpSessionsManager.java
+++ src/java/com/idega/event/IWHttpSessionsManager.java
@@ -1,10 +1,7 @@
 package com.idega.event;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpSession;
@@ -17,31 +14,29 @@ import org.springframework.stereotype.Service;
 
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.servlet.filter.RequestResponseProvider;
-import com.idega.util.CoreConstants;
-import com.idega.util.ListUtil;
 import com.idega.util.expression.ELUtil;
 
 @Service
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 public class IWHttpSessionsManager {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(IWHttpSessionsManager.class.getName());
-	
+
 	@Autowired
 	private ApplicationContext context;
-	
+
 	private Map<String, HttpSession> sessions;
-	
+
 	private IWHttpSessionsManager() {
 		sessions = new HashMap<String, HttpSession>();
 	}
-	
+
 	void addSession(HttpSession session) {
 		String id = session.getId();
 		synchronized (session) {
 			sessions.put(id, session);
 		}
-		
+
 		if (IWMainApplication.getDefaultIWMainApplication().getSettings().getBoolean("log_session_creation", Boolean.FALSE)) {
 			String uri = "unknown";
 			try {
@@ -51,66 +46,22 @@ public class IWHttpSessionsManager {
 			LOGGER.info("********************************* HttpSession '" + id + "' created for request: " + uri);
 		}
 	}
-	
+
 	void removeSession(String id) {
 		HttpSession session = null;
 		synchronized (sessions) {
 			session = sessions.remove(id);
 		}
-		
+
 		long lastAccessedTime = session == null ? 0 : session.getLastAccessedTime();
 		int maxInactiveInterval = session == null ? 0 : session.getMaxInactiveInterval();
 		getContext().publishEvent(new HttpSessionDestroyed(this, id, lastAccessedTime, maxInactiveInterval));
 	}
-	
+
 	public boolean isSessionValid(String id) {
 		synchronized (sessions) {
 			return sessions.containsKey(id);
 		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	String removeUselessSessions() {
-		if (sessions.isEmpty() || sessions.size() <= 0) {
-			return CoreConstants.EMPTY;
-		}
-		
-		Set<String> keysSet = sessions.keySet();
-		if (ListUtil.isEmpty(keysSet)) {
-			return CoreConstants.EMPTY;
-		}
-		List<String> keys = new ArrayList<String>(keysSet);
-		
-		List<String> sessionsToRemove = new ArrayList<String>();
-		long currentTime = System.currentTimeMillis();
-		for (String key: keys) {
-			HttpSession session = sessions.get(key);
-			if (session == null) {
-				continue;
-			}
-			
-			long idleTime = currentTime - session.getLastAccessedTime();
-			if (idleTime >= 600000) {
-				//	Session "was" idle for 10 minutes or more
-				
-				Object chibaManager = session.getAttribute("chiba.session.manager");
-				if (chibaManager != null) {
-					continue;
-				}
-				
-				Object principal = session.getValue("org.apache.slide.webdav.method.principal");
-				//	Checking if session was created by Slide's root user
-				if (principal instanceof String && "root".equals(principal)) {
-					sessionsToRemove.add(session.getId());
-				}
-			}
-		}
-		
-		for (String sessionId: sessionsToRemove) {
-			removeSession(sessionId);
-		}
-		
-		return ListUtil.isEmpty(sessionsToRemove) ? CoreConstants.EMPTY : sessionsToRemove.toString();
 	}
 
 	public ApplicationContext getContext() {
