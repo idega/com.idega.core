d1241ac280a9abe0d633f495f9964b5a5964da93
Valdas Žemaitis <valdas@idega.com>
2013-04-15 16:41:47 +0300

    Modified method to get client's IP

--- src/java/com/idega/presentation/IWContext.java
+++ src/java/com/idega/presentation/IWContext.java
@@ -1101,7 +1101,24 @@ public class IWContext extends FacesContext implements IWUserContext, IWApplicat
 	}
 
 	public String getRemoteIpAddress() {
-		return getRequest().getRemoteAddr();
+		HttpServletRequest request = getRequest();
+		String ip = request.getHeader("X-Forwarded-For");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 	}
 
 	public String getRemoteHostName() {
