2d1106d3cdb995489dc9158495c8794b6ffbafe4
lapiukshtiss <martynas.stake@gmail.com>
2013-09-19 12:27:59 +0300

    Visibility change

--- src/java/com/idega/idegaweb/DefaultIWBundle.java
+++ src/java/com/idega/idegaweb/DefaultIWBundle.java
@@ -1103,7 +1103,7 @@ public class DefaultIWBundle implements IWBundle, Serializable {
 		}
 	}
 
-	private IWPropertyList getPropertyList() {
+	protected IWPropertyList getPropertyList() {
 		return this.propertyList;
 	}
 
