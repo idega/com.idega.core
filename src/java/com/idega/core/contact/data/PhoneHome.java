accfbb58ce6629cc062b096ea63e2f256d5b9aea
lapiukshtiss <martynas.stake@gmail.com>
2013-11-25 12:05:25 +0200

    Find and update methods for phone, bug fixes

--- src/java/com/idega/core/contact/data/PhoneHome.java
+++ src/java/com/idega/core/contact/data/PhoneHome.java
@@ -1,5 +1,8 @@
 package com.idega.core.contact.data;
 
+import java.util.Collection;
+import java.util.Collections;
+
 
 public interface PhoneHome extends com.idega.data.IDOHome
 {
@@ -14,4 +17,24 @@ public Phone findUsersWorkPhone(com.idega.user.data.User user)throws javax.ejb.F
 public Phone findUsersMobilePhone(com.idega.user.data.User user)throws javax.ejb.FinderException,java.rmi.RemoteException;
 public Phone findUsersFaxPhone(com.idega.user.data.User user)throws javax.ejb.FinderException,java.rmi.RemoteException;
 
+	/**
+	 * 
+	 * <p>Creates/updates {@link Phone} in data source.</p>
+	 * @param primaryKey is {@link Phone#getPrimaryKey()}, if not <code>null</code>
+	 * then existing entity will be updated;
+	 * @param phoneNumber is {@link Phone#getNumber()}, not <code>null</code>;
+	 * @param phoneTypeId TODO
+	 * @return updated/created {@link Phone} or <code>null</code> on failure;
+	 * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
+	 */
+	public Phone update(String primaryKey, String phoneNumber, String phoneTypeId);
+
+	/**
+	 * 
+	 * @param phoneNumber is {@link Phone#getNumber()}, not <code>null</code>;
+	 * @return {@link Phone}s by given number or {@link Collections#emptyList()}
+	 * on failure;
+	 * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
+	 */
+	public Collection<Phone> findByPhoneNumber(String phoneNumber);
 }
--- src/java/com/idega/core/contact/data/PhoneHomeImpl.java
+++ src/java/com/idega/core/contact/data/PhoneHomeImpl.java
@@ -1,14 +1,28 @@
 package com.idega.core.contact.data;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.logging.Level;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
+import com.idega.data.IDOStoreException;
+import com.idega.util.ListUtil;
+import com.idega.util.StringUtil;
+
+
 
 public class PhoneHomeImpl extends com.idega.data.IDOFactory implements PhoneHome
 {
- protected Class getEntityInterfaceClass(){
+	private static final long serialVersionUID = 6038610517398600545L;
+
+protected Class getEntityInterfaceClass(){
   return Phone.class;
  }
 
  public Phone create() throws javax.ejb.CreateException{
-  return (Phone) super.idoCreate();
+  return (Phone) super.createEntity();
  }
 
  public Phone createLegacy(){
@@ -26,7 +40,7 @@ public class PhoneHomeImpl extends com.idega.data.IDOFactory implements PhoneHom
  }
 
  public Phone findByPrimaryKey(Object pk) throws javax.ejb.FinderException{
-  return (Phone) super.idoFindByPrimaryKey(pk);
+  return (Phone) super.findByPrimaryKeyIDO(pk);
  }
 
  public Phone findByPrimaryKeyLegacy(int id) throws java.sql.SQLException{
@@ -43,7 +57,6 @@ public Phone findUsersHomePhone(com.idega.user.data.User user)throws javax.ejb.F
 {
 	com.idega.data.IDOEntity entity = this.idoCheckOutPooledEntity();
 	Object pk = ((PhoneBMPBean)entity).ejbFindUsersHomePhone(user);
-	this.idoCheckInPooledEntity(entity);
 	return this.findByPrimaryKey(pk);
 }
 
@@ -51,7 +64,6 @@ public Phone findUsersWorkPhone(com.idega.user.data.User user)throws javax.ejb.F
 {
 	com.idega.data.IDOEntity entity = this.idoCheckOutPooledEntity();
 	Object pk = ((PhoneBMPBean)entity).ejbFindUsersWorkPhone(user);
-	this.idoCheckInPooledEntity(entity);
 	return this.findByPrimaryKey(pk);
 }
 
@@ -59,7 +71,6 @@ public Phone findUsersMobilePhone(com.idega.user.data.User user)throws javax.ejb
 {
 	com.idega.data.IDOEntity entity = this.idoCheckOutPooledEntity();
 	Object pk = ((PhoneBMPBean)entity).ejbFindUsersMobilePhone(user);
-	this.idoCheckInPooledEntity(entity);
 	return this.findByPrimaryKey(pk);
 }
 
@@ -67,10 +78,92 @@ public Phone findUsersFaxPhone(com.idega.user.data.User user)throws javax.ejb.Fi
 {
 	com.idega.data.IDOEntity entity = this.idoCheckOutPooledEntity();
 	Object pk = ((PhoneBMPBean)entity).ejbFindUsersFaxPhone(user);
-	this.idoCheckInPooledEntity(entity);
 	return this.findByPrimaryKey(pk);
 }
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.core.contact.data.PhoneHome#findByPhoneNumber(java.lang.String)
+	 */
+	@Override
+	public Collection<Phone> findByPhoneNumber(String phoneNumber) {
+		PhoneBMPBean entity = (PhoneBMPBean) idoCheckOutPooledEntity();
+		Collection<Object> ids = ((PhoneBMPBean)entity).ejbFindByPhoneNumber(phoneNumber);
+		try {
+			return getEntityCollectionForPrimaryKeys(ids);
+		} catch (FinderException e) {
+			java.util.logging.Logger.getLogger(getClass().getName()).log(
+					Level.WARNING, 
+					"Failed to get " + getEntityInterfaceClass().getName() + 
+					"'s by primary keys: " + ids);
+		}
 
+		return Collections.emptyList();
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.core.contact.data.PhoneHome#update(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Phone update(String primaryKey, String phoneNumber, String phoneTypeId) {
+		Phone phone = null;
+		if (!StringUtil.isEmpty(primaryKey)) {
+			try {
+				phone = findByPrimaryKey(primaryKey);
+			} catch (FinderException e) {
+				java.util.logging.Logger.getLogger(getClass().getName()).log(
+						Level.WARNING, "Phone by primary key: '" + primaryKey + 
+						"' not found! Will check by phone number!");
+			}
+		}
+
+		if (!StringUtil.isEmpty(phoneNumber)) {
+			if (phone == null) {
+				Collection<Phone> phones = findByPhoneNumber(phoneNumber);
+				if (!ListUtil.isEmpty(phones)) {
+					phone = phones.iterator().next();
+				} else {
+					try {
+						phone = createEntity();
+					} catch (CreateException e) {
+						java.util.logging.Logger.getLogger(getClass().getName()).log(
+								Level.WARNING, 
+								"Failed to create " + this.getClass().getName() + 
+								" cause of: ", e);
+						return null;
+					}
+				}
+			}
+			
+			phone.setNumber(phoneNumber);
+		}
+
+		if (phone == null) {
+			return null;
+		}
+
+		if (!StringUtil.isEmpty(phoneTypeId)) {
+			try {
+				phone.setPhoneTypeId(Integer.valueOf(phoneTypeId));
+			} catch (NumberFormatException e) {
+				java.util.logging.Logger.getLogger(getClass().getName()).log(
+						Level.WARNING, 
+						"Failed to convert " + phoneTypeId + 
+						" to: " + Integer.class.getName());
+			}
+		}
+		
+		try {
+			phone.store();
+			return phone;
+		} catch (IDOStoreException e) {
+			java.util.logging.Logger.getLogger(getClass().getName()).log(
+					Level.WARNING, 
+					"Failed to store " + this.getClass().getName() + 
+					" cause of: ", e);
+		}
+
+		return null;
+	}
 }
