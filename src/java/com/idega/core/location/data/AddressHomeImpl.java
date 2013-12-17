ce80ca4b8aafcb9f966b3daa6f4b1b592506c8c8
lapiukshtiss <martynas.stake@gmail.com>
2013-12-17 13:33:00 +0200

    Update method

--- src/java/com/idega/core/location/data/AddressHomeImpl.java
+++ src/java/com/idega/core/location/data/AddressHomeImpl.java
@@ -3,6 +3,8 @@ package com.idega.core.location.data;
 
 import java.rmi.RemoteException;
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
@@ -10,6 +12,8 @@ import javax.ejb.FinderException;
 import com.idega.data.IDOEntity;
 import com.idega.data.IDOFactory;
 import com.idega.data.IDOQuery;
+import com.idega.data.IDOStoreException;
+import com.idega.util.StringUtil;
 
 public class AddressHomeImpl extends IDOFactory implements AddressHome {
 	
@@ -34,25 +38,30 @@ public class AddressHomeImpl extends IDOFactory implements AddressHome {
 
 		 }
 
-	public Address findByPrimaryKey(Object pk) throws FinderException {
-		return (Address) super.findByPrimaryKeyIDO(pk);
+	public Address findByPrimaryKey(Object pk) {
+		if (pk != null) {
+			try {
+				return (Address) super.findByPrimaryKeyIDO(pk);
+			} catch (FinderException e) {
+				java.util.logging.Logger.getLogger(getClass().getName()).log(
+						Level.WARNING, 
+						"Failed to get " + getEntityBeanClass().getSimpleName() + 
+						" by primary key: " + pk);
+			}
+
+		}
+
+		return null;
 	}
 	
 	 public Address findByPrimaryKeyLegacy(int id) throws java.sql.SQLException{
-			try{
-				return findByPrimaryKey(id);
-			}
-			catch(javax.ejb.FinderException fe){
-				throw new java.sql.SQLException(fe.getMessage());
-			}
-
-		 }
+		return findByPrimaryKey(id);
+	 }
 
 	public AddressType getAddressType1() throws RemoteException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		AddressType theReturn = ((AddressBMPBean) entity)
 				.ejbHomeGetAddressType1();
-		this.idoCheckInPooledEntity(entity);
 		return theReturn;
 	}
 
@@ -60,14 +69,12 @@ public class AddressHomeImpl extends IDOFactory implements AddressHome {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		AddressType theReturn = ((AddressBMPBean) entity)
 				.ejbHomeGetAddressType2();
-		this.idoCheckInPooledEntity(entity);
 		return theReturn;
 	}
 
 	public Address findPrimaryUserAddress(int userID) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Object pk = ((AddressBMPBean) entity).ejbFindPrimaryUserAddress(userID);
-		this.idoCheckInPooledEntity(entity);
 		return this.findByPrimaryKey(pk);
 	}
 
@@ -76,7 +83,6 @@ public class AddressHomeImpl extends IDOFactory implements AddressHome {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Object pk = ((AddressBMPBean) entity).ejbFindUserAddressByAddressType(
 				userID, type);
-		this.idoCheckInPooledEntity(entity);
 		return this.findByPrimaryKey(pk);
 	}
 
@@ -85,7 +91,6 @@ public class AddressHomeImpl extends IDOFactory implements AddressHome {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Collection ids = ((AddressBMPBean) entity)
 				.ejbFindPrimaryUserAddresses(userIDs);
-		this.idoCheckInPooledEntity(entity);
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
@@ -94,32 +99,72 @@ public class AddressHomeImpl extends IDOFactory implements AddressHome {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Collection ids = ((AddressBMPBean) entity)
 				.ejbFindPrimaryUserAddresses(query);
-		this.idoCheckInPooledEntity(entity);
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
 	public Collection findUserAddressesByAddressType(int userID,
 			AddressType type) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
-		Collection ids = ((AddressBMPBean) entity)
+		Collection<?> ids = ((AddressBMPBean) entity)
 				.ejbFindUserAddressesByAddressType(userID, type);
-		this.idoCheckInPooledEntity(entity);
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
-	public Collection findByPostalCode(Integer postalCodeID)
+	public Collection<Address> findByPostalCode(Integer postalCodeID)
 			throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
-		Collection ids = ((AddressBMPBean) entity)
+		Collection<?> ids = ((AddressBMPBean) entity)
 				.ejbFindByPostalCode(postalCodeID);
-		this.idoCheckInPooledEntity(entity);
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
 	public Address findByStreetAddress(String address) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Object pk = ((AddressBMPBean) entity).ejbFindByStreetAddress(address);
-		this.idoCheckInPooledEntity(entity);
 		return this.findByPrimaryKey(pk);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.core.location.data.AddressHome#update(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Address update(String primaryKey, String streetName) {
+		Address address = null;
+		if (!StringUtil.isEmpty(primaryKey)) {
+			address = findByPrimaryKey(primaryKey);
+		}
+
+		if (address == null) {
+			try {
+				address = createEntity();
+				Logger.getLogger(getClass().getName()).info(
+						"New " + Address.class.getSimpleName() + 
+						" sucessfully created!");
+			} catch (CreateException e) {
+				Logger.getLogger(getClass().getName()).log(
+						Level.WARNING, 
+						"Failed to create " + Address.class.getSimpleName() + 
+						" cause of: ", e);
+				return null;
+			}
+		}
+
+		if (!StringUtil.isEmpty(streetName)) {
+			address.setStreetName(streetName);
+		}
+
+		try {
+			address.store();
+			Logger.getLogger(getClass().getName()).info(
+					Address.class.getSimpleName() + " sucessfully updated!");
+		} catch (IDOStoreException e) {
+			Logger.getLogger(getClass().getName()).log(
+					Level.WARNING, 
+					"Failed to update " + Address.class.getSimpleName() + 
+					" cause of: ", e);
+		}
+
+		return address;
 	}
 }
