72288c9b55119807b680bc624648b1317af16dfc
lapiukshtiss <martynas.stake@gmail.com>
2014-05-01 17:48:14 +0300

    Datepicker version attribute

--- src/java/com/idega/presentation/ui/IWDatePicker.java
+++ src/java/com/idega/presentation/ui/IWDatePicker.java
@@ -56,6 +56,8 @@ public class IWDatePicker extends TextInput {
 
 	private boolean showTime;
 
+	private String alternateFieldId = null;
+
 	private static final String INITIAL_DATE_PROPERTY = "initialDate";
 	private static final String INITIAL_DATE_TO_PROPERTY = "initialDateTo";
 	private static final String MAX_DATE_PROPERTY = "maxDate";
@@ -70,6 +72,8 @@ public class IWDatePicker extends TextInput {
 	private static final String SHOW_TIME_PROPERTY = "showTime";
 	private static final String USE_CURRENT_PROPERTY = "useCurrent";
 	private static final String YEAR_RANGE_PROPERTY = "yearRange";
+	private static final String DATEPICKER_VERSION_PROPERTY = "version";
+	private static final String ALTERNATE_FIELD_PROPERTY = "alternateFieldId";
 
 	private String version;
 
@@ -174,6 +178,22 @@ public class IWDatePicker extends TextInput {
 	    	setUseCurrentDateIfNotSet(useCurrent);
     	}
 
+    	ve = getValueExpression(DATEPICKER_VERSION_PROPERTY);
+    	if (ve != null) {
+	    	String version = (String) ve.getValue(context.getELContext());
+	    	if (!StringUtil.isEmpty(version)) {
+	    		setVersion(version);
+	    	}
+    	}
+
+    	ve = getValueExpression(ALTERNATE_FIELD_PROPERTY);
+    	if (ve != null) {
+	    	String version = (String) ve.getValue(context.getELContext());
+	    	if (!StringUtil.isEmpty(version)) {
+	    		setAlternateFieldId(version);
+	    	}
+    	}    	
+
     	super.encodeBegin(context);
     }
 
@@ -264,6 +284,10 @@ public class IWDatePicker extends TextInput {
 
 		if (isShowTime()) {
 			initAction.append(", showTime: true");
+		}
+
+		if (!StringUtil.isEmpty(getAlternateFieldId())) {
+			initAction.append(", altField: '" + getAlternateFieldId() + "'");
 		}
 
 		// onSelect action
@@ -452,4 +476,11 @@ public class IWDatePicker extends TextInput {
 		this.version = version;
 	}
 
+	public String getAlternateFieldId() {
+		return alternateFieldId;
+	}
+
+	public void setAlternateFieldId(String alternateFieldId) {
+		this.alternateFieldId = alternateFieldId;
+	}
 }
