ca37d1e3d98974beac6fcec96338078edeb5e9ef
Valdas Žemaitis <valdas@idega.com>
2014-06-16 10:04:41 +0300

    No synchronization needed

--- src/java/com/idega/notifier/business/InstantMessageNotifier.java
+++ src/java/com/idega/notifier/business/InstantMessageNotifier.java
@@ -33,30 +33,30 @@ public class InstantMessageNotifier extends BasicNotifier implements Notifier {
 	private static final long serialVersionUID = 9198750757903839409L;
 
 	private static final Logger LOGGER = Logger.getLogger(InstantMessageNotifier.class.getName());
-	
-	private Map<String, InstantMessage> messages; 
-	
+
+	private Map<String, InstantMessage> messages;
+
 	@Override
 	public void setActive(boolean active) {
 		super.setActive(active);
-		
+
 		this.messages = null;
 	}
 
 	@Override
 	public UIComponent getManagementPanel() {
 		Layer layer = new Layer();
-		
+
 		TextInput titleInput = new TextInput("setTitle" + getClassNameIdentifier());
 		Label titleLabel = new Label("Title", titleInput);
 		layer.add(titleLabel);
 		layer.add(titleInput);
-		
+
 		TextInput messageInput = new TextInput("setMessage" + getClassNameIdentifier());
 		Label messageLabel = new Label("Message", messageInput);
 		layer.add(messageLabel);
 		layer.add(messageInput);
-		
+
 		return layer;
 	}
 
@@ -69,76 +69,74 @@ public class InstantMessageNotifier extends BasicNotifier implements Notifier {
 	public NotificationType getType() {
 		return NotificationType.SHOW_ONCE;
 	}
-	
+
 	private Map<String, InstantMessage> getMessages() {
 		if (messages == null) {
 			messages = new HashMap<String, InstantMessage>();
 		}
-		
+
 		return messages;
 	}
-	
-	private void addNotification(String id, InstantMessage message) {		
-		synchronized (getMessages()) {
-			if (message == null) {
-				LOGGER.warning("Message is not specified!");
-				return;
-			}
-			
-			if (getMessages().get(id) == null) {
-				getMessages().put(id, message);
-			}
+
+	private void addNotification(String id, InstantMessage message) {
+		if (message == null) {
+			LOGGER.warning("Message is not specified!");
+			return;
+		}
+
+		if (getMessages().get(id) == null) {
+			getMessages().put(id, message);
 		}
 	}
-	
+
 	private InstantMessage getMessage(String id) {
 		if (StringUtil.isEmpty(id)) {
 			LOGGER.warning("ID must be specified!");
 		}
-		
+
 		InstantMessage message = getMessages().get(id);
 		if (message == null) {
 			message = new InstantMessage();
 		}
-		
+
 		return message;
 	}
-	
+
 	public void setTitle(String title) {
 		if (!isActive()) {
 			return;
 		}
-		
+
 		String id = getRequestId();
-		
+
 		InstantMessage message = getMessage(id);
 		if (message == null) {
 			LOGGER.warning("Title can not be set for IM!");
 			return;
 		}
-		
+
 		message.setTitle(title);
 		addNotification(id, message);
 	}
-	
+
 	public void setMessage(String text) {
 		if (!isActive()) {
 			return;
 		}
-		
+
 		if (StringUtil.isEmpty(text)) {
 			LOGGER.warning("Text must be specified for IM!");
 			return;
 		}
-		
+
 		String id = getRequestId();
-		
+
 		InstantMessage message = getMessage(id);
 		if (message == null) {
 			LOGGER.warning("Text can not be set for IM!");
 			return;
 		}
-		
+
 		message.setMessage(text);
 		addNotification(id, message);
 	}
@@ -148,29 +146,27 @@ public class InstantMessageNotifier extends BasicNotifier implements Notifier {
 		if (messages == null || messages.isEmpty()) {
 			return null;
 		}
-		
+
 		String id = session.getId();
-		
+
 		List<BasicNotification> notifications = new ArrayList<BasicNotification>();
-		
-		synchronized (getMessages()) {
-			for (InstantMessage message: getMessages().values()) {
-				String messageText = message.getMessage();
-				
-				if (StringUtil.isEmpty(messageText)) {
-					continue;
-				}
-				
-				if (message.canSendToSession(id)) {
-					notifications.add(new InstantMessageNotification(message.getTitle(), messageText));
-					message.addSentToSession(id);
-				}
+
+		for (InstantMessage message: getMessages().values()) {
+			String messageText = message.getMessage();
+
+			if (StringUtil.isEmpty(messageText)) {
+				continue;
+			}
+
+			if (message.canSendToSession(id)) {
+				notifications.add(new InstantMessageNotification(message.getTitle(), messageText));
+				message.addSentToSession(id);
 			}
 		}
-		
+
 		return notifications;
 	}
-	
+
 	@Override
 	public void markNotificationsAsSent(String httpSessionCallerId, Collection<String> httpSessionsIds) {
 		if (StringUtil.isEmpty(httpSessionCallerId) || ListUtil.isEmpty(httpSessionsIds)) {
@@ -179,13 +175,9 @@ public class InstantMessageNotifier extends BasicNotifier implements Notifier {
 		if (messages == null || messages.isEmpty()) {
 			return;
 		}
-		
-		synchronized (getMessages()) {
-			for (InstantMessage message: getMessages().values()) {
-				if (message.getSentToSessions().contains(httpSessionCallerId)) {
-					message.addSentToSession(httpSessionsIds);
-				}
-			}
+
+		for (InstantMessage message: getMessages().values()) {
+			message.addSentToSession(httpSessionsIds);
 		}
 	}
 
@@ -194,7 +186,7 @@ public class InstantMessageNotifier extends BasicNotifier implements Notifier {
 		if (requestProvider == null) {
 			return null;
 		}
-		
+
 		return requestProvider.getRequest().toString();
 	}
 
