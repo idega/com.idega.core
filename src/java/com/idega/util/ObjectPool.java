3ea0ee5d2a5ea2aec2dbe882ca060565209db9d5
Valdas Žemaitis <valdas@idega.com>
2014-06-16 10:14:56 +0300

    No synchronization needed

--- src/java/com/idega/util/ObjectPool.java
+++ src/java/com/idega/util/ObjectPool.java
@@ -9,8 +9,9 @@ package com.idega.util;
  * @version 1.0
  */
 
-import java.util.Hashtable;
-import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class ObjectPool
 {
@@ -18,28 +19,25 @@ public abstract class ObjectPool
   public ObjectPool()
   {
    this.expirationTime = 30000; // 30 seconds
-   this.locked = new Hashtable();
-   this.unlocked = new Hashtable();
+   this.locked = new ConcurrentHashMap<Object, Long>();
+   this.unlocked = new ConcurrentHashMap<Object, Long>();
   }
 
    private long expirationTime;
-   private Hashtable locked, unlocked;
+   private Map<Object, Long> locked, unlocked;
 
    abstract Object create();
    abstract boolean validate( Object o );
    abstract void expire( Object o );
 
-    synchronized Object checkOut()
+   Object checkOut()
     {
        long now = System.currentTimeMillis();
-       Object o;
        if( this.unlocked.size() > 0 )
        {
-          Enumeration e = this.unlocked.keys();
-          while( e.hasMoreElements() )
-          {
-             o = e.nextElement();
-             if( ( now - ( ( Long ) this.unlocked.get( o ) ).longValue() ) > this.expirationTime )
+    	  Set<Object> e = this.unlocked.keySet();
+         for (Object o : e) {
+             if( ( now - this.unlocked.get( o ).longValue() ) > this.expirationTime )
              {
                 // object has expired
                 this.unlocked.remove( o );
@@ -65,13 +63,13 @@ public abstract class ObjectPool
           }
        }
        // no objects available, create a new one
-       o = create();
+       Object o = create();
        this.locked.put( o, new Long( now ) );
        return( o );
     }
 
 
-    synchronized void checkIn( Object o )
+   void checkIn( Object o )
     {
        this.locked.remove( o );
        this.unlocked.put( o, new Long( System.currentTimeMillis() ) );
