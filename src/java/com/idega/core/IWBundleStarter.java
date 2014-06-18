2b04f09b1aad0653d27c9612a689382600a782f5
Valdas Žemaitis <valdas@idega.com>
2014-06-18 16:08:07 +0300

    Making sure column is long enough

--- src/java/com/idega/core/IWBundleStarter.java
+++ src/java/com/idega/core/IWBundleStarter.java
@@ -2,19 +2,26 @@ package com.idega.core;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.idega.core.accesscontrol.data.bean.ICPermission;
+import com.idega.core.accesscontrol.data.bean.ICRole;
 import com.idega.core.builder.dao.ICPageDAO;
 import com.idega.core.builder.data.ICPageBMPBean;
 import com.idega.data.SimpleQuerier;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
+import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.util.ArrayUtil;
 import com.idega.util.ListUtil;
 import com.idega.util.expression.ELUtil;
 
 public class IWBundleStarter implements IWBundleStartable {
+
+	private static final Logger LOGGER = Logger.getLogger(IWBundleStarter.class.getName());
 
 	@Autowired
 	private ICPageDAO icPageDAO;
@@ -28,6 +35,26 @@ public class IWBundleStarter implements IWBundleStartable {
 
 	@Override
 	public void start(IWBundle starterBundle) {
+		doChangePermissionLength(starterBundle.getApplication().getSettings());
+		doCachePages();
+	}
+
+	private void doChangePermissionLength(IWMainApplicationSettings settings) {
+		String key = "ic_permission_context_increased";
+		if (!settings.getBoolean(key, false)) {
+			String sql = null;
+			try {
+				sql = "alter table " + ICPermission.ENTITY_NAME + " modify (" + ICPermission.COLUMN_CONTEXT_VALUE + " varchar(" + ICRole.ROLE_KEY_MAX_LENGTH + "))";
+				if (SimpleQuerier.execute(sql)) {
+					settings.setProperty(key, Boolean.TRUE.toString());
+				}
+			} catch (Exception e) {
+				LOGGER.log(Level.WARNING, "Error executing: '" + sql + "'", e);
+			}
+		}
+	}
+
+	private void doCachePages() {
 		//	Temporary solution to cache
 		try {
 			String query = "select p." + ICPageBMPBean.ENTITY_NAME + "_ID from " + ICPageBMPBean.ENTITY_NAME + " p";
@@ -48,7 +75,7 @@ public class IWBundleStarter implements IWBundleStartable {
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.log(Level.WARNING, "Error caching pages", e);
 		}
 	}
 
