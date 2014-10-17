7b8ec3f2164158173154f196f055b98458923abc
amzinas <zygimantas@idega.com>
2014-10-17 13:17:19 +0300

    Spring bean Jarfalla services to reach Jarfalla services.

---
+++ src/java/com/idega/core/accesscontrol/business/JarfallaServices.java
@@ -0,0 +1,23 @@
+package com.idega.core.accesscontrol.business;
+
+import com.idega.business.SpringBeanName;
+import com.idega.user.data.User;
+
+@SpringBeanName("jarfallaServices")
+public interface JarfallaServices {
+
+	/**
+	 * Getting User from Infotrog system according given peronal id
+	 * @param personalId User personal id
+	 * @return User from Infotrog
+	 */
+	public User getUserFromInfotrog(String personalId);
+
+	/**
+	 * Send SMS message to the given phone number with the given message
+	 * @param toPhone Phone number send SMS message to
+	 * @param message SMS message
+	 * @return true/false if SMS message was successfully/unsuccessfully sent
+	 */
+	public boolean sendSMSMessage(String toPhone, String message);
+}
