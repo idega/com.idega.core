8e455c738f14a5a55ae02c9695dd2ca19a144c68
Valdas Žemaitis <valdas@idega.com>
2014-12-22 08:55:34 +0200

    Checking if explicit sync is enabled

--- src/java/com/idega/business/ExplicitSynchronizationBusiness.java
+++ src/java/com/idega/business/ExplicitSynchronizationBusiness.java
@@ -6,9 +6,15 @@ import java.util.Map;
 import com.idega.data.ExplicitlySynchronizedEntity;
 
 public interface ExplicitSynchronizationBusiness {
+
 	public static final String BEAN_NAME = "explicitSynchronizationBusiness";
+
 	public void synchronize(ExplicitlySynchronizedEntity entity);
+
 	public void activateSynchronization(Collection<ExplicitlySynchronizedEntity> entities);
+
 	public void activateSynchronization(Collection<ExplicitlySynchronizedEntity> entities,Map<String, Object> properties);
+
 	public void addSynchronizer(String key,ExplicitSynchronizer synchronizer);
+
 }
--- src/java/com/idega/data/ExplicitlySynchronizedEntity.java
+++ src/java/com/idega/data/ExplicitlySynchronizedEntity.java
@@ -3,8 +3,17 @@ package com.idega.data;
 import java.util.Collection;
 
 public interface ExplicitlySynchronizedEntity {
+
 	public String getSynchronizerKey();
+
 	public ExplicitlySynchronizedEntity getSimpleExplicitlySynchronizedEntityEntity();
+
 	public String getPK();
+
 	public Collection<String> getChanges();
+
+	public void setSynchronizationEnabled(boolean enabled);
+
+	public boolean isSynchronizationEnabled();
+
 }
