5a426169661a4a57f1dfbf1f6ae4226fa200b9d0
Valdas Žemaitis <valdas@idega.com>
2015-05-23 07:23:07 +0200

    Improvements

--- src/java/com/idega/user/business/GroupTypeComparator.java
+++ src/java/com/idega/user/business/GroupTypeComparator.java
@@ -5,31 +5,47 @@ package com.idega.user.business;
 
 import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.user.data.GroupType;
+import com.idega.util.CoreConstants;
+import com.idega.util.StringUtil;
 
 /**
  * @author Sigtryggur
- * 
+ *
  */
 public class GroupTypeComparator implements Comparator {
 
-    private IWContext _iwc;
-	private final static String IW_BUNDLE_IDENTIFIER = "com.idega.user";	
+	private final static String IW_BUNDLE_IDENTIFIER = "com.idega.user";
+
+	private Collator collator = null;
+	private IWResourceBundle iwrb = null;
 
     public GroupTypeComparator(IWContext iwc) {
-	  	this._iwc = iwc;
+	  	Locale locale = iwc == null ? Locale.ENGLISH : iwc.getCurrentLocale();
+	  	locale = locale == null ? Locale.ENGLISH : locale;
+	  	collator = Collator.getInstance(locale);
+	  	iwrb = IWMainApplication.getDefaultIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(locale);
 	}
 
-    public int compare(Object o1, Object o2) {
-        Collator collator = Collator.getInstance(this._iwc.getCurrentLocale());
-        
-        GroupType groupType1 = (GroupType)o1;
-        GroupType groupType2 = (GroupType)o2;
-        
-        String localizedGroupType1 = this._iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(this._iwc.getCurrentLocale()).getLocalizedString(groupType1.getType());
-        String localizedGroupType2 = this._iwc.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER).getResourceBundle(this._iwc.getCurrentLocale()).getLocalizedString(groupType2.getType());
-        return  collator.compare(localizedGroupType1, localizedGroupType2);
+	public int compare(Object o1, Object o2) {
+		GroupType groupType1 = (GroupType) o1;
+		GroupType groupType2 = (GroupType) o2;
+		
+        String localizedGroupType1 = iwrb.getLocalizedString(groupType1.getType());
+        String localizedGroupType2 = iwrb.getLocalizedString(groupType2.getType());
+
+        localizedGroupType1 = StringUtil.isEmpty(localizedGroupType1) ? groupType1.getType() : localizedGroupType1;
+        localizedGroupType1 = StringUtil.isEmpty(localizedGroupType1) ? CoreConstants.EMPTY : localizedGroupType1;
+
+        localizedGroupType2 = StringUtil.isEmpty(localizedGroupType2) ? groupType2.getType() : localizedGroupType2;
+        localizedGroupType2 = StringUtil.isEmpty(localizedGroupType2) ? CoreConstants.EMPTY : localizedGroupType2;
+
+        return collator.compare(localizedGroupType1, localizedGroupType2);
     }
+	
 }
