517be8f641495fa6db2012457e790dda67f6831e
Arūnas Proscevičius <arunas.proscevicius@gmail.com>
2015-12-10 11:52:06 +0200

    member details update

--- src/java/com/idega/user/data/bean/GroupRelation.java
+++ src/java/com/idega/user/data/bean/GroupRelation.java
@@ -39,6 +39,7 @@ import com.idega.util.IWTimestamp;
 @NamedQueries({
 	@NamedQuery(name = GroupRelation.QUERY_FIND_BY_RELATED_GROUP, query = "select distinct r.group from GroupRelation r where r.relatedGroup = :relatedGroup and r.status = '" + GroupRelation.STATUS_ACTIVE + "' and r.groupRelationType = '" + GroupRelation.RELATION_TYPE_GROUP_PARENT + "'"),
 	@NamedQuery(name = GroupRelation.QUERY_FIND_BY_RELATED_GROUP_AND_TYPE, query = "select distinct r.group from GroupRelation r join r.group g where r.relatedGroup = :relatedGroup and g.groupType in (:groupTypes) and r.status = '" + GroupRelation.STATUS_ACTIVE + "' and r.groupRelationType = '" + GroupRelation.RELATION_TYPE_GROUP_PARENT + "'"),
+	@NamedQuery(name = GroupRelation.QUERY_GET_HISTORY, query = "select r from GroupRelation r join r.group g where r.relatedGroup.groupID = :"+GroupRelation.PARAM_RELATED_GROUP_ID+" and r.groupRelationType = '" + GroupRelation.RELATION_TYPE_GROUP_PARENT + "'"),
 	@NamedQuery(name = "groupRelation.findBiDirectionalRelation", query = "select r from GroupRelation r where (r.group = :group and r.relatedGroup = :relatedGroup) or (r.relatedGroup = :group and r.group = :relatedGroup) and r.status = '" + GroupRelation.STATUS_ACTIVE + "'")
 })
 @Cacheable
@@ -47,8 +48,11 @@ public class GroupRelation implements Serializable, MetaDataCapable {
 	private static final long serialVersionUID = 5850270896539731950L;
 
 	public static final String	QUERY_FIND_BY_RELATED_GROUP = "groupRelation.findByRelatedGroup",
-								QUERY_FIND_BY_RELATED_GROUP_AND_TYPE = "groupRelation.findByRelatedGroupAndType";
+								QUERY_FIND_BY_RELATED_GROUP_AND_TYPE = "groupRelation.findByRelatedGroupAndType",
+								QUERY_GET_HISTORY = "groupRelation.getHistory";
 
+	public static final String PARAM_RELATED_GROUP_ID = "relatedGroupId";
+	
 	public final static String STATUS_ACTIVE = "ST_ACTIVE";
 	public final static String STATUS_PASSIVE = "ST_PASSIVE";
 	public final static String STATUS_PASSIVE_PENDING = "PASS_PEND";
