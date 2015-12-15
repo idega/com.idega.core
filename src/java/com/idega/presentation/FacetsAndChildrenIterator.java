b80d560abcb90570cc47b9ce4f1c0f381928786c
Valdas Žemaitis <valdas@idega.com>
2015-12-15 16:17:24 +0200

    Checking if there are more elements

--- src/java/com/idega/presentation/FacetsAndChildrenIterator.java
+++ src/java/com/idega/presentation/FacetsAndChildrenIterator.java
@@ -19,9 +19,9 @@ import javax.faces.component.UIComponent;
 
 /**
  * Class to override the standard iterator for the method getFacetsAndChildren() in UIComponent
- * 
+ *
  * Last modified: $Date: 2007/12/28 13:23:05 $ by $Author: valdas $
- * 
+ *
  * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
  * @version $Revision: 1.5 $
  */
@@ -29,32 +29,34 @@ class FacetsAndChildrenIterator implements Iterator<UIComponent>
 {
     private Iterator<UIComponent> _facetsIterator;
     private Iterator<UIComponent> _childrenIterator;
-    
+
     FacetsAndChildrenIterator(Map<String, UIComponent> facetMap, List<UIComponent> childrenList)
     {
         this._facetsIterator   = facetMap != null ? facetMap.values().iterator() : null;
         this._childrenIterator = childrenList != null ? childrenList.iterator() : null;
     }
 
-    public boolean hasNext()
+    @Override
+	public boolean hasNext()
     {
     		boolean facetsHasNext = false;
     		boolean childrenHasNext=false;
-    		
+
     		facetsHasNext = (this._facetsIterator != null && this._facetsIterator.hasNext());
     		childrenHasNext = (this._childrenIterator != null && this._childrenIterator.hasNext());
-    		
+
     		return ( facetsHasNext || childrenHasNext );
     }
 
-    public UIComponent next()
+    @Override
+	public UIComponent next()
     {
 		boolean facetsHasNext = false;
 		boolean childrenHasNext=false;
-		
+
 		facetsHasNext = (this._facetsIterator != null && this._facetsIterator.hasNext());
 		childrenHasNext = (this._childrenIterator != null && this._childrenIterator.hasNext());
-    	
+
         if (facetsHasNext)
         {
             return this._facetsIterator.next();
@@ -62,8 +64,8 @@ class FacetsAndChildrenIterator implements Iterator<UIComponent>
         else if (childrenHasNext)
         {
         		try{
-        			return this._childrenIterator.next();
-        
+        			return this._childrenIterator.hasNext() ? this._childrenIterator.next() : null;
+
         		}
         		catch(NoSuchElementException nse){
         			nse.printStackTrace();
@@ -80,7 +82,8 @@ class FacetsAndChildrenIterator implements Iterator<UIComponent>
         }
     }
 
-    public void remove()
+    @Override
+	public void remove()
     {
         throw new UnsupportedOperationException(this.getClass().getName() + " UnsupportedOperationException");
     }
