09689ae17145d6951d0dfb256dd01c57f59627bd
Valdas Žemaitis <valdas@idega.com>
2015-12-16 09:00:53 +0200

    Made code re-usable

--- src/java/com/idega/presentation/ui/DynamicLayoutDropDownMenu.java
+++ src/java/com/idega/presentation/ui/DynamicLayoutDropDownMenu.java
@@ -10,9 +10,15 @@
 package com.idega.presentation.ui;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
+
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
+
+import com.idega.builder.bean.AdvancedProperty;
 import com.idega.core.file.data.ICFile;
 import com.idega.core.file.data.ICFileHome;
 import com.idega.data.IDOLookup;
@@ -20,13 +26,14 @@ import com.idega.data.IDOLookupException;
 import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
+import com.idega.util.CoreConstants;
 
 /**
  * <p>
  * DropDownMenu so it users can have a range of JasperReportLayouts to select from.
  * </p>
  *  Last modified: $Date: 2007/01/26 06:36:36 $ by $Author: idegaweb $
- * 
+ *
  * @author <a href="mailto:sigtryggur@idega.com">sigtryggur</a>
  * @version $Revision: 1.2 $
  */
@@ -36,37 +43,49 @@ public class DynamicLayoutDropDownMenu extends DropDownMenuInputHandler {
 	protected static String IW_BUNDLE_IDENTIFIER = "com.idega.user";
 	protected static final String LAYOUT_FOLDER_NAME = "layout";
 	private ICFileHome fileHome;
-	
+
 	public DynamicLayoutDropDownMenu() {
 		super();
 	}
 
-    public PresentationObject getHandlerObject(String name, String value, IWContext iwc) {
+    @Override
+	public PresentationObject getHandlerObject(String name, String value, IWContext iwc) {
     	IWResourceBundle resourceBundle = getResourceBundle(iwc);
     	this.setName(name);
     	String defaultLayout = resourceBundle.getLocalizedString("DynamicLayoutDropDownMenu.default_layout","Default layout");
 		this.addMenuElement("-1", defaultLayout);
-		ICFile layoutFolder = lookUpLayoutFolder(value);
-		if(layoutFolder!=null){
-			Iterator iterator = layoutFolder.getChildrenIterator();
-			if (iterator != null) {
-				while (iterator.hasNext())	{
-					ICFile file = (ICFile) iterator.next();
-					if (!file.isFolder()) {
-						String display = file.getName();
-						if (display != null && display.lastIndexOf('.') != -1 ) {
-							display = display.substring(0, display.lastIndexOf('.'));
-						}
-						int fileID = file.getNodeID();
-						String fileIDValue = Integer.toString(fileID);
-						this.addMenuElement(fileIDValue, display);
-					}
-				}
-  			}
+		ICFile layoutFolder = getLayoutFolder(value);
+		for (AdvancedProperty layout: getLayouts(layoutFolder)) {
+			this.addMenuElement(layout.getId(), layout.getValue());
 		}
 		return this;
     }
 
+    public List<AdvancedProperty> getLayouts(ICFile layoutFolder) {
+    	if (layoutFolder == null) {
+    		return Collections.emptyList();
+    	}
+
+    	List<AdvancedProperty> layouts = new ArrayList<AdvancedProperty>();
+    	Iterator<ICFile> iterator = layoutFolder.getChildrenIterator();
+		if (iterator != null) {
+			while (iterator.hasNext())	{
+				ICFile file = iterator.next();
+				if (!file.isFolder()) {
+					String display = file.getName();
+					if (display != null && display.lastIndexOf(CoreConstants.DOT) != -1 ) {
+						display = display.substring(0, display.lastIndexOf(CoreConstants.DOT));
+					}
+					int fileID = Integer.valueOf(file.getId());
+					String fileIDValue = Integer.toString(fileID);
+					layouts.add(new AdvancedProperty(fileIDValue, display));
+				}
+			}
+		}
+		return layouts;
+    }
+
+	@Override
 	public String getDisplayForResultingObject(Object value, IWContext iwc) {
 		if (value != null) {
 			if (value.equals("-1")) {
@@ -75,23 +94,23 @@ public class DynamicLayoutDropDownMenu extends DropDownMenuInputHandler {
 			}
 			try {
 	    		ICFile file = getFileHome().findByPrimaryKey(value);
-	    		return file.getName();	
+	    		return file.getName();
 	    	}
 	    	catch (FinderException e) {
 	    		return value.toString();
 	    	}
 		} else {
-			return "";
+			return CoreConstants.EMPTY;
 		}
 	}
 
-	private ICFile lookUpLayoutFolder(String userDefinedLayoutFolderName)  {
+	public ICFile getLayoutFolder(String userDefinedLayoutFolderName)  {
 		ICFile layoutFolder = null;
 		try{
 			try {
 	    		layoutFolder = getFileHome().findByFileName(LAYOUT_FOLDER_NAME);
 			}
-			catch (FinderException findEx) {	
+			catch (FinderException findEx) {
 	    		layoutFolder = createAndReturnLayoutFolder(LAYOUT_FOLDER_NAME);
 	    		try {
 	    			ICFile rootFolder = getFileHome().findRootFolder();
@@ -101,12 +120,12 @@ public class DynamicLayoutDropDownMenu extends DropDownMenuInputHandler {
 	    			logError("[DynamicLayoutDropDownMenu] Could not find Root-folder");
 	    		}
 	    	}
-			if (userDefinedLayoutFolderName != null && !userDefinedLayoutFolderName.equals("")) {
+			if (userDefinedLayoutFolderName != null && !userDefinedLayoutFolderName.equals(CoreConstants.EMPTY)) {
 				ICFile userDefinedLayoutFolder = null;
 				try {
 					userDefinedLayoutFolder = getFileHome().findByFileName(userDefinedLayoutFolderName);
 				}
-				catch (FinderException findEx) {	
+				catch (FinderException findEx) {
 					userDefinedLayoutFolder = createAndReturnLayoutFolder(userDefinedLayoutFolderName);
 					layoutFolder.addChild(userDefinedLayoutFolder);
 		    	}
@@ -146,6 +165,7 @@ public class DynamicLayoutDropDownMenu extends DropDownMenuInputHandler {
 	    return fileHome;
 	  }
 
+	@Override
 	public String getBundleIdentifier() {
 		return IW_BUNDLE_IDENTIFIER;
 	}
