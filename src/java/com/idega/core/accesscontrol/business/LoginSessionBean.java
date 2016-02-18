fc2a349c7514f214d33d7811adeb2fbe7cb75649
Valdas Žemaitis <valdas@idega.com>
2016-02-18 12:34:04 +0200

    Checking if permission group is available

--- src/java/com/idega/core/accesscontrol/business/LoginSessionBean.java
+++ src/java/com/idega/core/accesscontrol/business/LoginSessionBean.java
@@ -18,6 +18,7 @@ import java.util.Stack;
 import java.util.logging.Logger;
 
 import com.idega.business.IBOLookup;
+import com.idega.core.accesscontrol.data.bean.PermissionGroup;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.presentation.IWContext;
@@ -278,8 +279,13 @@ public class LoginSessionBean implements LoginSession, Serializable {
 					return true;
 				}
 
+				PermissionGroup permGroup = getAccessController().getPermissionGroupAdministrator();
+				if (permGroup == null) {
+					return false;
+				}
+
 				UserBusiness userBusiness = IBOLookup.getServiceInstance(iwac, UserBusiness.class);
-				return userBusiness.isMemberOfGroup(getAccessController().getPermissionGroupAdministrator().getID(), userBusiness.getUser(user.getId()));
+				return userBusiness.isMemberOfGroup(permGroup.getID(), userBusiness.getUser(user.getId()));
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
