f7df8dee0c1605d7c2311f7ff6060241312d1d2e
lapiukshtiss <martynas.stake@gmail.com>
2016-04-20 12:26:50 +0300

    Methods for searching user

--- src/java/com/idega/user/dao/UserDAO.java
+++ src/java/com/idega/user/dao/UserDAO.java
@@ -3,6 +3,8 @@
  */
 package com.idega.user.dao;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -18,6 +20,14 @@ import com.idega.user.data.bean.User;
 @SpringBeanName("userDAO")
 public interface UserDAO extends GenericDao {
 
+	/**
+	 * 
+	 * <p>Updates/creates entity</p>
+	 * @param user to update/persist, not <code>null</code>;
+	 * @return updated created entity or <code>null</code> on failure;
+	 */
+	public User update(User user);
+	
 	@Transactional(readOnly = false)
 	public User createUser(String firstName, String middleName, String lastName, String displayName, String personalID, String description, Gender gender, Date dateOfBirth, Group primaryGroup);
 
@@ -44,5 +54,24 @@ public interface UserDAO extends GenericDao {
 	public Gender getFemaleGender();
 
 	public User getByEmailAddress(String emailAddress);
+
+	/**
+	 * 
+	 * @param primaryKeys is {@link Collection} of {@link User#getId()}, 
+	 * not <code>null</code>;
+	 * @return entities or {@link Collections#emptyList()} on failure;
+	 */
+	List<User> findByPrimaryKeys(Collection<Integer> primaryKeys);
+
+	/**
+	 * 
+	 * @param personalId {@link User#getPersonalID()} to filter by, skipped if <code>null</code>;
+	 * @param firstName {@link User#getFirstName()} to filter by, skipped if <code>null</code>;
+	 * @param middleName {@link User#getMiddleName()} to filter by, skipped if <code>null</code>;
+	 * @param lastName {@link User#getLastName()} to filter by, skipped if <code>null</code>;
+	 * @return filtered entities or {@link Collections#emptyList()} on failure;
+	 */
+	List<User> findFilteredBy(String personalId, String firstName,
+			String middleName, String lastName);
 
 }
