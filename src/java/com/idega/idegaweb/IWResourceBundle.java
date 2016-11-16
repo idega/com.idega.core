7d4d38751f9184806603aed035826ed2ec06bef0
Valdas Žemaitis <valdas@idega.com>
2016-11-16 09:50:21 +0200

    Changed app property

--- src/java/com/idega/idegaweb/IWResourceBundle.java
+++ src/java/com/idega/idegaweb/IWResourceBundle.java
@@ -658,7 +658,7 @@ public class IWResourceBundle extends ResourceBundle implements MessageResource,
 	}
 
 	private void doMakeSureAllFilesLoaded(Map<String, String> data) {
-		if (IWMainApplication.getDefaultIWMainApplication().getSettings().getBoolean("check_loaded_localized_keys", false)) {
+		if (IWMainApplication.getDefaultIWMainApplication().getSettings().getBoolean("skip_check_for_localized_keys", true)) {
 			return;
 		}
 
