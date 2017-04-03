0b44cdae3f86cd0cfeb392f48132f44a18f32c02
Valdas Žemaitis <valdas@idega.com>
2017-04-03 10:11:40 +0300

    Removed needless code

--- src/java/com/idega/user/dao/impl/UserDAOImpl.java
+++ src/java/com/idega/user/dao/impl/UserDAOImpl.java
@@ -3,7 +3,6 @@
  */
 package com.idega.user.dao.impl;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -11,10 +10,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 import java.util.logging.Level;
-
-import javax.ejb.CreateException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -23,7 +19,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.idega.business.IBOLookup;
-import com.idega.business.IBOLookupException;
 import com.idega.core.contact.dao.ContactDAO;
 import com.idega.core.contact.dao.EMailDAO;
 import com.idega.core.contact.data.bean.Email;
@@ -33,19 +28,10 @@ import com.idega.core.location.data.bean.Address;
 import com.idega.core.location.data.bean.AddressType;
 import com.idega.core.persistence.Param;
 import com.idega.core.persistence.impl.GenericDaoImpl;
-import com.idega.data.IDOLookup;
-import com.idega.data.IDOLookupException;
-import com.idega.data.IDOStoreException;
 import com.idega.data.bean.Metadata;
 import com.idega.idegaweb.IWMainApplication;
-import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.user.business.UserBusiness;
-import com.idega.user.dao.GroupDAO;
 import com.idega.user.dao.UserDAO;
-import com.idega.user.data.GroupHome;
-import com.idega.user.data.GroupType;
-import com.idega.user.data.GroupTypeHome;
-import com.idega.user.data.UserHome;
 import com.idega.user.data.bean.Gender;
 import com.idega.user.data.bean.Group;
 import com.idega.user.data.bean.User;
@@ -72,120 +58,12 @@ public class UserDAOImpl extends GenericDaoImpl implements UserDAO {
 	@Autowired
 	private EMailDAO eMailDAO;
 
-	@Autowired
-	private GroupDAO groupDAO;
-
-	private GroupHome getGroupHome() {
-		try {
-			return (GroupHome) IDOLookup.getHome(com.idega.user.data.Group.class);
-		} catch (IDOLookupException e) {
-			getLogger().log(Level.WARNING, "Failed to get " + GroupHome.class + " cuase of: ", e);
-		}
-
-		return null;
-	}
-
-	private GroupTypeHome getGroupTypeHome() {
-		try {
-			return (GroupTypeHome) IDOLookup.getHome(GroupType.class);
-		} catch (IDOLookupException e) {
-			getLogger().log(Level.WARNING, "Failed to get " + GroupTypeHome.class + " cuase of: ", e);
-		}
-
-		return null;
-	}
-
-	private GroupDAO getGroupDAO() {
-		if (this.groupDAO == null) {
-			ELUtil.getInstance().autowire(this);
-		}
-
-		return this.groupDAO;
-	}
-
 	private EMailDAO getEMailDAO() {
 		if (this.eMailDAO == null) {
 			ELUtil.getInstance().autowire(this);
 		}
 
 		return this.eMailDAO;
-	}
-
-	private UserBusiness getUserBusiness() {
-		try {
-			return IBOLookup.getServiceInstance(
-					IWMainApplication.getDefaultIWApplicationContext(),
-					UserBusiness.class);
-		} catch (IBOLookupException e) {
-			getLogger().log(Level.WARNING, "Failed to get " + UserBusiness.class + " cause of: ", e);
-		}
-
-		return null;
-	}
-
-	private IWMainApplicationSettings getSettings() {
-		return IWMainApplication.getDefaultIWMainApplication().getSettings();
-	}
-
-	private Integer getUnassignedMembersGroupId() {
-		IWMainApplicationSettings settings = getSettings();
-		if (settings != null) {
-			String property = settings.getProperty(PROPERTY_UNASSIGNED_MEMBERS_GROUP_ID, "-1");
-			if (property != null) {
-				return Integer.valueOf(property);
-			}
-		}
-
-		return null;
-	}
-
-	private void setUnassignedMembersGroupId(Integer id) {
-		IWMainApplicationSettings settings = getSettings();
-		if (settings != null && id != null) {
-			settings.setProperty(PROPERTY_UNASSIGNED_MEMBERS_GROUP_ID, id.toString());
-		}
-	}
-
-	private Group getUnassignedMembersGroup() {
-		Group group = getGroupDAO().findGroup(getUnassignedMembersGroupId());
-		if (group == null) {
-			com.idega.user.data.Group newGroup = null;
-			try {
-				newGroup = getGroupHome().create();
-			} catch (CreateException e) {
-				getLogger().log(Level.WARNING, "Failed to create group, cause of:", e);
-			}
-
-			if (newGroup != null) {
-				newGroup.setCreated(new Timestamp(System.currentTimeMillis()));
-				newGroup.setDescription("Group for containing members without any access rights");
-				newGroup.setName("Unassigned Members Group");
-				newGroup.setGroupType(getGroupTypeHome().getPermissionGroupTypeString());
-				newGroup.setIsPermissionControllingGroup(Boolean.FALSE);
-				newGroup.setUniqueId(UUID.randomUUID().toString());
-
-				try {
-					newGroup.store();
-				} catch (IDOStoreException e) {
-					getLogger().log(Level.WARNING, "Failed to save group, cause of:", e);
-				}
-
-				setUnassignedMembersGroupId((Integer) newGroup.getPrimaryKey());
-				group = getUnassignedMembersGroup();
-			}
-		}
-
-		return group;
-	}
-
-	private UserHome getUserHome() {
-		try {
-			return (UserHome) IDOLookup.getHome(com.idega.user.data.User.class);
-		} catch (IDOLookupException e) {
-			getLogger().log(Level.WARNING, "Failed to get " + UserHome.class + " cause of: ", e);
-		}
-
-		return null;
 	}
 
 	@Override
@@ -209,8 +87,6 @@ public class UserDAOImpl extends GenericDaoImpl implements UserDAO {
 
 		return user;
 	}
-
-
 
 	@Override
 	public User getUser(Integer userID) {
@@ -579,12 +455,13 @@ public class UserDAOImpl extends GenericDaoImpl implements UserDAO {
 
 			try {
 				String[] splittedName = name.split(CoreConstants.SPACE);
+				UserBusiness userBusiness = IBOLookup.getServiceInstance(IWMainApplication.getDefaultIWApplicationContext(), UserBusiness.class);
 				if (splittedName.length > 2) {
-					ejbUser = getUserBusiness().createUser(splittedName[0], splittedName[1], splittedName[2], personalId);
+					ejbUser = userBusiness.createUser(splittedName[0], splittedName[1], splittedName[2], personalId);
 				} else if (splittedName.length > 1) {
-					ejbUser = getUserBusiness().createUser(splittedName[0], null, splittedName[1], personalId);
+					ejbUser = userBusiness.createUser(splittedName[0], null, splittedName[1], personalId);
 				} else if (splittedName.length > 0) {
-					ejbUser = getUserBusiness().createUser(splittedName[0], null, null, personalId);
+					ejbUser = userBusiness.createUser(splittedName[0], null, null, personalId);
 				}
 			} catch (Exception e) {
 				getLogger().log(Level.WARNING, "Failed to create user, cause of:", e);
