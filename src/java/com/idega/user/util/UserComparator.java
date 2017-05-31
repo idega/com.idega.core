47a15e8d2a5fcf2b0ba474e6d1388308f6e025f2
Valdas Žemaitis <valdas@idega.com>
2017-05-31 12:46:09 +0300

    Instance of collator now is created in constructor

--- src/java/com/idega/user/util/UserComparator.java
+++ src/java/com/idega/user/util/UserComparator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.idega.user.util;
 
@@ -12,31 +12,27 @@ import com.idega.util.text.Name;
 
 
 /**
- * <p>
- * TODO laddi Describe Type UserComparator
- * </p>
  *  Last modified: $Date: 2008/05/24 14:12:39 $ by $Author: valdas $
- * 
+ *
  * @author <a href="mailto:laddi@idega.com">laddi</a>
  * @version $Revision: 1.2 $
  */
 public class UserComparator implements Comparator<User> {
 
 	private Locale iLocale;
-	
+	private Collator collator;
+
 	public UserComparator(Locale locale) {
 		this.iLocale = locale;
+		collator = Collator.getInstance(iLocale);
 	}
-	
-	/* (non-Javadoc)
-	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-	 */
+
+	@Override
 	public int compare(User user1, User user2) {
-		Collator collator = Collator.getInstance(iLocale);
-		
 		Name name1 = new Name(user1.getFirstName(), user1.getMiddleName(), user1.getLastName());
 		Name name2 = new Name(user2.getFirstName(), user2.getMiddleName(), user2.getLastName());
-		
+
 		return collator.compare(name1.getName(iLocale), name2.getName(iLocale));
 	}
+
 }
