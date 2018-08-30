6661f85536352b5f97a08a9f2eb79099917db98b
L4p1n45 <martynas.stake@gmail.com>
2018-08-30 16:55:57 +0300

    Cleaning whitespaces from role names

--- src/java/com/idega/core/accesscontrol/business/AccessControl.java
+++ src/java/com/idega/core/accesscontrol/business/AccessControl.java
@@ -2247,7 +2247,7 @@ public class AccessControl extends IWServiceImpl implements AccessController {
 
 		if (!ListUtil.isEmpty(userRolesFromGroup)) {
 			for (String key: userRolesFromGroup) {
-				s.add(key);
+				s.add(key.replaceAll("\\s+",""));
 			}
 		}
 
