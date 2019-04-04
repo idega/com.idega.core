f83052e31717994a6d1db8a97a35ad5b0eceb830
Valdas Žemaitis <valdas@idega.com>
2019-04-04 16:28:05 +0300

    Improvement getting client's IP address

--- src/java/com/idega/presentation/IWContext.java
+++ src/java/com/idega/presentation/IWContext.java
@@ -1183,22 +1183,30 @@ public class IWContext extends FacesContext implements IWUserContext, IWApplicat
 	public String getRemoteIpAddress() {
 		HttpServletRequest request = getRequest();
 		String ip = request.getHeader("X-Forwarded-For");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (!isValidIP(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (!isValidIP(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (!isValidIP(ip)) {
 			ip = request.getHeader("HTTP_CLIENT_IP");
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (!isValidIP(ip)) {
 			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (!isValidIP(ip)) {
 			ip = request.getRemoteAddr();
 		}
 		return ip;
+	}
+
+	private boolean isValidIP(String ip) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip) || "127.0.0.1".equals(ip)) {
+			return false;
+		}
+
+		return true;
 	}
 
 	public String getRemoteHostName() {
