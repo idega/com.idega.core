76bc520faf6fb91922c75b195471960ff95352bf
Valdas Žemaitis <valdas@idega.com>
2019-08-15 14:10:09 +0300

    Using generics

--- src/java/com/idega/user/data/GroupDomainRelationHome.java
+++ src/java/com/idega/user/data/GroupDomainRelationHome.java
@@ -5,7 +5,7 @@ public interface GroupDomainRelationHome extends com.idega.data.IDOHome
 {
  public GroupDomainRelation create() throws javax.ejb.CreateException;
  public GroupDomainRelation findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
- public java.util.Collection findDomainsRelationshipsContaining(com.idega.user.data.Group p0)throws javax.ejb.FinderException;
+ public java.util.Collection<GroupDomainRelation> findDomainsRelationshipsContaining(com.idega.user.data.Group p0)throws javax.ejb.FinderException;
  public java.util.Collection findDomainsRelationshipsContaining(com.idega.core.builder.data.ICDomain p0,com.idega.user.data.Group p1)throws javax.ejb.FinderException;
  public java.util.Collection findDomainsRelationshipsContaining(int p0)throws javax.ejb.FinderException;
  public java.util.Collection findGroupsRelationshipsContaining(int p0,int p1)throws javax.ejb.FinderException;
