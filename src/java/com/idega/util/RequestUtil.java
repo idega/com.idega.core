7abb9ef2a62aedfd4eae477a390f0f511e265a1c
Valdas Žemaitis <valdas@idega.com>
2022-09-22 10:51:40 +0300

    New header

--- src/java/com/idega/util/RequestUtil.java
+++ src/java/com/idega/util/RequestUtil.java
@@ -44,7 +44,8 @@ public class RequestUtil {
 	public static final String	HEADER_ACCEPT_LANGUAGE = "Accept-Language",
 								HEADER_USER_AGENT = "User-agent",
 								HEADER_AUTHORIZATION = "Authorization",
-								HEADER_CONTENT_TYPE = "Content-Type";
+								HEADER_CONTENT_TYPE = "Content-Type",
+								HEADER_ACCEPT = "Accept";
 
 	/**
 	 * Calls the method HttpServletRequest.getRequestURI() and cuts front of it
